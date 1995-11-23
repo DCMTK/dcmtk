a4ebd582fb99e0ea295ddd2acca1c6e6b1a01e3a
Andrew Hewett <dicom@offis.de>
1995-11-23 16:52:25 +0000

    Added support for loadable data dictionary and MacOS.

--- config/acconfig.h
+++ config/acconfig.h
@@ -9,4 +9,14 @@
 /* Define path seperator */
 #define PATH_SEPERATOR '/'
 
+/* Define the environment variable path separator */
+#define ENVIRONMENT_PATH_SEPARATOR ':'
+
+/* Define if your system cannot pass command line arguments into main() (e.g. Macintosh) */
+/* #define HAVE_EMPTY_ARGC_ARGV 1 */
+
+/* Compile in the debug code */
+/* #define DEBUG 1 */
+
+
 #endif
