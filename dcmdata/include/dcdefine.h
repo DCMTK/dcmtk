d78d6ca0ec93504125b4c0ed51760238a67304fe
Marco Eichelberg <dicom@offis.de>
1996-03-22 13:33:09 +0000

    improved readability, minor corrections.

--- dcmdata/include/dcdefine.h
+++ dcmdata/include/dcdefine.h
@@ -9,10 +9,10 @@
 ** Implementation of common defines for configuration
 **
 **
-** Last Update:		$Author: andreas $
-** Update Date:		$Date: 1996-01-05 13:22:53 $
+** Last Update:		$Author: meichel $
+** Update Date:		$Date: 1996-03-22 13:33:09 $
 ** Source File:		$Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmdata/include/Attic/dcdefine.h,v $
-** CVS/RCS Revision:	$Revision: 1.1 $
+** CVS/RCS Revision:	$Revision: 1.2 $
 ** Status:		$State: Exp $
 **
 */
@@ -21,26 +21,26 @@
 #define DCDEFINE_H
 
 #if STDC_HEADERS
-#include <string.h>
-#undef memzero
-#define memzero(d, n) memset((d), 0, (n))
+#  include <string.h>
+#  undef memzero
+#  define memzero(d, n) memset((d), 0, (n))
 #else /*!STDC_HEADERS*/
-#ifndef HAVE_STRCHR
-#undef strchr
-#define strchr index
-#undef strrchr
-#define strrchr rindex
-#endif /*HAVE_STRCHR*/
-#ifndef HAVE_MEMCPY
-#undef memcpy
-#define memcpy(d, s, n) bcopy((s), (d), (n))
-#undef memmove
-#define memmove(d, s, n) bcopy ((s), (d), (n));
-#undef memzero
-#define memzero(d, n) bzero((d), (n));
-#undef memcmp
-#define memcmp(d, s, n) bcmp((s), (d), (n))
-#endif /*HAVE_MEMCPY*/
+#  ifndef HAVE_STRCHR
+#    undef strchr
+#    define strchr index
+#    undef strrchr
+#    define strrchr rindex
+#  endif /*HAVE_STRCHR*/
+#  ifndef HAVE_MEMCPY
+#    undef memcpy
+#    define memcpy(d, s, n) bcopy((s), (d), (n))
+#    undef memmove
+#    define memmove(d, s, n) bcopy ((s), (d), (n))
+#    undef memzero
+#    define memzero(d, n) bzero((d), (n))
+#    undef memcmp
+#    define memcmp(d, s, n) bcmp((s), (d), (n))
+#  endif /*HAVE_MEMCPY*/
 #endif /*!STDC_HEADERS*/
 
 #endif
