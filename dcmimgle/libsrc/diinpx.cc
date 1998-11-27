68928f66bc82cd32e94cdd548a2d28ae54251206
Joerg Riesmeier <dicom@offis.de>
1998-11-27 16:02:13 +0000

    Added copyright message.

---
+++ dcmimgle/libsrc/diinpx.cc
@@ -0,0 +1,70 @@
+/*
+ *
+ *  Copyright (C) 1996-99, OFFIS
+ *
+ *  This software and supporting documentation were developed by
+ *
+ *    Kuratorium OFFIS e.V.
+ *    Healthcare Information and Communication Systems
+ *    Escherweg 2
+ *    D-26121 Oldenburg, Germany
+ *
+ *  THIS SOFTWARE IS MADE AVAILABLE,  AS IS,  AND OFFIS MAKES NO  WARRANTY
+ *  REGARDING  THE  SOFTWARE,  ITS  PERFORMANCE,  ITS  MERCHANTABILITY  OR
+ *  FITNESS FOR ANY PARTICULAR USE, FREEDOM FROM ANY COMPUTER DISEASES  OR
+ *  ITS CONFORMITY TO ANY SPECIFICATION. THE ENTIRE RISK AS TO QUALITY AND
+ *  PERFORMANCE OF THE SOFTWARE IS WITH THE USER.
+ *
+ *  Module:  dcmimgle
+ *
+ *  Author:  Joerg Riesmeier
+ *
+ *  Purpose: DicomInputPixel (Source)
+ *
+ *  Last Update:      $Author: joergr $
+ *  Update Date:      $Date: 1998-11-27 16:02:13 $
+ *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimgle/libsrc/diinpx.cc,v $
+ *  CVS/RCS Revision: $Revision: 1.1 $
+ *  Status:           $State: Exp $
+ *
+ *  CVS/RCS Log at end of file
+ *
+ */
+
+
+#include "osconfig.h"
+
+#include "diinpx.h"
+
+
+/*----------------*
+ *  constructors  *
+ *----------------*/
+
+DiInputPixel::DiInputPixel()
+  : Count(0)
+{
+}
+
+
+/*--------------*
+ *  destructor  *
+ *--------------*/
+
+DiInputPixel::~DiInputPixel()
+{
+}
+
+
+/*
+**
+** CVS/RCS Log:
+** $Log: diinpx.cc,v $
+** Revision 1.1  1998-11-27 16:02:13  joergr
+** Added copyright message.
+**
+** Revision 1.2  1998/05/11 14:52:29  joergr
+** Added CVS/RCS header to each file.
+**
+**
+*/
