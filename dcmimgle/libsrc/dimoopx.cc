b43343d0520663fd39936a1bd940951e19de11b2
Joerg Riesmeier <dicom@offis.de>
1998-11-27 16:15:02 +0000

    Added copyright message.

---
+++ dcmimgle/libsrc/dimoopx.cc
@@ -0,0 +1,73 @@
+/*
+ *
+ *  Copyright (C) 1996-99, OFFIS
+ *
+ *  This software and supporting documentation were developed by
+ *
+ *    Kuratorium OFFIS e.V.
+ *    Healthcare Information and Communication Systems
+ *    Escherweg 2
+ *    D-26121 Oldenburg, Germany
+ *
+ *  THIS SOFTWARE IS MADE AVAILABLE,  AS IS,  AND OFFIS MAKES NO  WARRANTY
+ *  REGARDING  THE  SOFTWARE,  ITS  PERFORMANCE,  ITS  MERCHANTABILITY  OR
+ *  FITNESS FOR ANY PARTICULAR USE, FREEDOM FROM ANY COMPUTER DISEASES  OR
+ *  ITS CONFORMITY TO ANY SPECIFICATION. THE ENTIRE RISK AS TO QUALITY AND
+ *  PERFORMANCE OF THE SOFTWARE IS WITH THE USER.
+ *
+ *  Module:  dcmimgle
+ *
+ *  Author:  Joerg Riesmeier
+ *
+ *  Purpose: DicomMonoOutputPixel (Source)
+ *
+ *  Last Update:      $Author: joergr $
+ *  Update Date:      $Date: 1998-11-27 16:15:02 $
+ *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimgle/libsrc/dimoopx.cc,v $
+ *  CVS/RCS Revision: $Revision: 1.1 $
+ *  Status:           $State: Exp $
+ *
+ *  CVS/RCS Log at end of file
+ *
+ */
+
+
+#include "osconfig.h"
+
+#include "dimoopx.h"
+#include "dimopx.h"
+
+
+/*----------------*
+ *  constructors  *
+ *----------------*/
+
+DiMonoOutputPixel::DiMonoOutputPixel(const DiMonoPixel *pixel, const unsigned long frames)
+  : Count(0)
+{
+    if (pixel != NULL)
+        Count = pixel->getCount() / frames;
+}
+
+
+/*--------------*
+ *  destructor  *
+ *--------------*/
+
+DiMonoOutputPixel::~DiMonoOutputPixel()
+{
+}
+
+
+/*
+**
+** CVS/RCS Log:
+** $Log: dimoopx.cc,v $
+** Revision 1.1  1998-11-27 16:15:02  joergr
+** Added copyright message.
+**
+** Revision 1.3  1998/05/11 14:52:33  joergr
+** Added CVS/RCS header to each file.
+**
+**
+*/
