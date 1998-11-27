c96550b8bb2a1cf3f7ef51a8896db26231de8d8b
Joerg Riesmeier <dicom@offis.de>
1998-11-27 14:27:58 +0000

    Added copyright message.

--- dcmimage/libsrc/dicoopx.cc
+++ dcmimage/libsrc/dicoopx.cc
@@ -1,21 +1,35 @@
 /*
-**
-** Author:  Joerg Riesmeier
-** Created: 20.12.96
-**
-** Module:  dicoopx.cc
-**
-** Purpose: DicomColorOutputPixel (Source)
-**
-** Last Update:      $Author: joergr $
-** Update Date:      $Date: 1998-05-11 14:52:26 $
-** Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimage/libsrc/dicoopx.cc,v $
-** CVS/RCS Revision: $Revision: 1.3 $
-** Status:           $State: Exp $
-**
-** CVS/RCS Log at end of file
-**
-*/
+ *
+ *  Copyright (C) 1996-99, OFFIS
+ *
+ *  This software and supporting documentation were developed by
+ *
+ *    Kuratorium OFFIS e.V.
+ *    Healthcare Information and Communication Systems
+ *    Escherweg 2
+ *    D-26121 Oldenburg, Germany
+ *
+ *  THIS SOFTWARE IS MADE AVAILABLE,  AS IS,  AND OFFIS MAKES NO  WARRANTY
+ *  REGARDING  THE  SOFTWARE,  ITS  PERFORMANCE,  ITS  MERCHANTABILITY  OR
+ *  FITNESS FOR ANY PARTICULAR USE, FREEDOM FROM ANY COMPUTER DISEASES  OR
+ *  ITS CONFORMITY TO ANY SPECIFICATION. THE ENTIRE RISK AS TO QUALITY AND
+ *  PERFORMANCE OF THE SOFTWARE IS WITH THE USER.
+ *
+ *  Module:  dcmimage
+ *
+ *  Author:  Joerg Riesmeier
+ *
+ *  Purpose: DicomColorOutputPixel (Source)
+ *
+ *  Last Update:      $Author: joergr $
+ *  Update Date:      $Date: 1998-11-27 14:27:58 $
+ *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimage/libsrc/dicoopx.cc,v $
+ *  CVS/RCS Revision: $Revision: 1.4 $
+ *  Status:           $State: Exp $
+ *
+ *  CVS/RCS Log at end of file
+ *
+ */
 
 
 #include "osconfig.h"
@@ -49,7 +63,10 @@ DiColorOutputPixel::~DiColorOutputPixel()
 **
 ** CVS/RCS Log:
 ** $Log: dicoopx.cc,v $
-** Revision 1.3  1998-05-11 14:52:26  joergr
+** Revision 1.4  1998-11-27 14:27:58  joergr
+** Added copyright message.
+**
+** Revision 1.3  1998/05/11 14:52:26  joergr
 ** Added CVS/RCS header to each file.
 **
 **
