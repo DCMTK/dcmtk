f7c9f17622742f5c3a284238caad8ad17458432a
Lutz Vorwerk <dicom@offis.de>
1998-12-22 15:50:06 +0000

    - constructor now with parameter for directory of index.dat - changed const methods in non-const-methods - added methods for attributes of DICOM-documents - added private method for operations on index.dat

--- dcmpstat/include/dviface.h
+++ dcmpstat/include/dviface.h
@@ -22,9 +22,9 @@
  *  Purpose:
  *    classes: DVInterface
  *
- *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 1998-11-30 11:26:59 $
- *  CVS/RCS Revision: $Revision: 1.2 $
+ *  Last Update:      $Author: vorwerk $
+ *  Update Date:      $Date: 1998-12-22 15:50:06 $
+ *  CVS/RCS Revision: $Revision: 1.3 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -32,17 +32,33 @@
  */
 
 
+
+ 
+#ifndef DVIFACE_H
+#define DVIFACE_H
+
+
 #include "dctk.h"
 #include "dvpstat.h"
 
+#include "osconfig.h"
+#include "imagedb.h"
+#include <sys/stat.h>
+#include "dbstore.h"
+#include "didocu.h"
+ #include "dbpriv.h"
 
+   
 class DVInterface
 {
  
  public:
-
-    DVInterface();
+  /** Constructor has @param indexfolder as directory for the indext.dat file.
+    The directory must exist.
+   */
+    DVInterface(const char *indexfolder);
     DVInterface(const DVInterface& obj);
+  /// destructor
     virtual ~DVInterface();
     
     /* load images and presentation states */
@@ -74,60 +90,111 @@ class DVInterface
     
     /* here follow the Browser interface methods */
     
-    /* This enum describes the status of one entry in the database hierarchy.
-       An entry can describe a study, a series or an instance.
-       A study or series is new exactly if all subobjects (series and instances) are new.
-       A study or series contains new subobjecs as long as any subobject
-       (series or instance) has the status objectIsNew.
-       Instances can never have the status DVIF_objectContainsNewSubobjects.
-     */
-    enum DVIFhierarchyStatus
-    {
-      DVIF_objectIsNotNew,
-      DVIF_objectIsNew,
-      DVIF_objectContainsNewSubobjects
-    };
-    
-    /* creates an exclusive lock on the database.
+  
+  /** creates an exclusive lock on the database.
        The methods defined below may only be called during a database lock.
        Other processes (i.e. Store SCPs) will be unable to store new images
        in the database as long as it is locked. */
     E_Condition lockDatabase();
     
-    /* removes the exclusive database lock. */
+  /** removes the exclusive database lock. */
     E_Condition unlockDatabase();
-    
+  /** returns the number of studies in the database. */  
     Uint32 getNumberOfStudies();
+  /** select a study from the range given by the method getNumberOfStudies() */
     E_Condition selectStudy(Uint32 idx);
+  /** returns the study UID of a study which is selected by the method 
+       selectStudy */
     const char *getStudyUID();
+  /** returns the study description of a study which is selected by the method 
+       selectStudy */
     const char *getStudyDescription();
-    DVIFhierarchyStatus getStudyStatus() const;
+  /** returns the DVIFhierarchyStatus of a study which is selected by the 
+       method  selectStudy */
+    DVIFhierarchyStatus getStudyStatus() ;
     
+    /* attributes, which can be examined */
+  /** returns the study date of a study which is selected by the method 
+       selectStudy */
+    const char *getStudyDate();
+  /** returns the study time of a study which is selected by the method 
+       selectStudy */
+    const char *getStudyTime();
+  /** returns the referring physicians name of a study which is selected 
+       by the method  selectStudy */
+    const char *getReferringPhysiciansName();
+  /** returns the accession number of a study which is selected by 
+       the method  selectStudy */
+    const char *getAccessionNumber();
+  /** returns the DVIFhierarchyStatus of a study which is selected by 
+    the method  selectStudy */
+    const char *getNameOfPhysiciansReadingStudy();
+ 
+
     /* number of series within the current study */
     Uint32 getNumberOfSeries();
     E_Condition selectSeries(Uint32 idx);
     const char *getSeriesUID();
-    DVIFhierarchyStatus getSeriesStatus() const;
+    DVIFhierarchyStatus getSeriesStatus();
+
+
     /* returns OFTrue if this series contains presentation stated, OFFalse otherwise.
        A series can never contain both images and presentation states.
      */
     OFBool isPresentationStateSeries();
     
+
+  /** returns the series number of a series which is selected by 
+       the methods  selectStudy and selectSeries */
+    const char *getSeriesNumber();
+  /** returns the series date of a series which is selected by the methods  
+       selectStudy and selectSeries */
+    const char *getSeriesDate();
+  /** returns the series date of a series which is selected by the methods 
+       selectStudy and selectSeries */
+    const char *getSeriesTime();
+  /** returns the series description of a series which is selected by the 
+    methods  selectStudy and selectSeries */
+    const char *getSeriesDescription();
+  /** returns the series performing physicians name of a series which is 
+    selected by the methods  selectStudy and selectSeries */
+    const char *getSeriesPerformingPhysicainsName();
+  /** returns the series protocol name of a series which is selected by the 
+    methods  selectStudy and selectSeries */
+    const char *getSeriesProtocolName();
+  /** returns series operators name of a series which is selected by the 
+    methods selectSeries and selectStudy */
+    const char *getSeriesOperatorsName();
     /* number of instances (images or presentation states) within the current study */
+
+  /** returns the number of studies in the database. */  
     Uint32 getNumberOfInstances();
+
+  /** select a study from the range given by the method getNumberOfStudies() */
     E_Condition selectInstance(Uint32 idx);
-    const char *getInstanceUID();
-    const char *getModality();
-    DVIFhierarchyStatus getInstanceStatus() const;
+  /** returns the instance UID of an instance which is selected by the methods
+        selectStudy, selectSeries and selectInstance */
     
-    /* returns OFTrue if the instance is a presentation state, OFFalse otherwise. */
+    const char *getInstanceUID();
+  /** returns the modality of an instance which is selected by the methods
+        selectStudy, selectSeries and selectInstance */
+  
+    const char *getModality();
+  /** returns the DVIFhierarchyStatus of an instance which is selected by 
+       the methods selectStudy, selectSeries and selectInstance */
+
+    DVIFhierarchyStatus getInstanceStatus() ;
+    
+  /** returns OFTrue if the instance is a presentation state, OFFalse otherwise. */
     OFBool isPresentationState();
     
-    /* instance has been reviewed, change status in DB from isNew to isNotNew if necessary */
+  /** instance has been reviewed, change status in DB from isNew to isNotNew if necessary */
     E_Condition instanceReviewed(const char *studyUID, const char *seriesUID, const char *instanceUID);
-    
+  /** delete instance indentified by studyUID, seriesUID and instanceUID */
     E_Condition deleteInstance(const char *studyUID, const char *seriesUID, const char *instanceUID);
+  /** delete series indentified by studyUID and seriesUID */  
     E_Condition deleteSeries(const char *studyUID, const char *seriesUID);
+  /** delete study indentified by studyUID*/
     E_Condition deleteStudy(const char *studyUID);
     
     /* here follow the Network interface methods */
@@ -155,6 +222,8 @@ class DVInterface
     const char *getTargetDescription(const char *targetID);
     
 
+
+
 protected:
 
     E_Condition loadFileFormat(const char *filename,
@@ -167,12 +236,59 @@ private:
 
     /* member variables */
     DVPresentationState pState;
+
+    /* member variables for database */
+    char selectedStudy[64];
+    char selectedSeries[64];
+    char selectedInstance[64];
+  //IdxRecord idx;
+    struct stat 		buf ;
+    Uint32 SeriesNumber, StudyNumber;  
+    DB_Private_Handle 	*phandle ;  
+    StudyDescRecord 	*pStudyDesc ;
+    char IndexName[255];
+    long MaxStudyCount;
+    long StudySize;
+    DB_Handle 	*handle;
+    IdxRecord idxRec;
+
+   
+    /* private methods for database */
+  /** method getAnInstance:
+       Scans database for an Instance, 
+       counts Series and Instances if count is true,
+       find an indexed Series or Instance if sel is true,
+       compute DVIFhierarchystatus for Study, Series or Instance 
+       if dvistatus flag is set
+       */
+    OFBool getAnInstance(
+			 OFBool dvistatus, 
+			 OFBool count, 
+			 OFBool sel,
+			 IdxRecord *idxRec, 
+			 const char *StudyUID, 
+			 const char *SeriesUID=NULL, 
+			 Uint32 selser=0,
+			 Uint32 *colser=0 ,
+			 OFBool *isNew=NULL, 
+			 Uint32 *idxCounter=NULL,
+			 const char *InstanceUID=NULL 
+		       ) ;
+
+  
 };
+#endif
 
 
 /*
  *  $Log: dviface.h,v $
- *  Revision 1.2  1998-11-30 11:26:59  joergr
+ *  Revision 1.3  1998-12-22 15:50:06  vorwerk
+ *  - constructor now with parameter for directory of index.dat
+ *  - changed const methods in non-const-methods
+ *  - added methods for attributes of DICOM-documents
+ *  - added private method for operations on index.dat
+ *
+ *  Revision 1.2  1998/11/30 11:26:59  joergr
  *  Added CVS/RCS log at end of file.
  *
  *
