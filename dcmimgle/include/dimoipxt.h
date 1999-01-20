e4ecf323a941937874717f424f386dd9c0cbad89
Joerg Riesmeier <dicom@offis.de>
1999-01-20 15:06:24 +0000

    Replaced invocation of getCount() by member variable Count where possible. Added optimization to modality and VOI transformation (using additional LUTs).

--- dcmimgle/include/dimoipxt.h
+++ dcmimgle/include/dimoipxt.h
@@ -22,9 +22,9 @@
  *  Purpose: DicomMonochromeInputPixelTemplate (Header)
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 1998-12-22 14:29:39 $
+ *  Update Date:      $Date: 1999-01-20 15:06:24 $
  *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimgle/include/Attic/dimoipxt.h,v $
- *  CVS/RCS Revision: $Revision: 1.3 $
+ *  CVS/RCS Revision: $Revision: 1.4 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -56,14 +56,14 @@ class DiMonoInputPixelTemplate
                              DiMonoModality *modality)
       : DiMonoPixelTemplate<T3>(pixel, modality)
     {
-        if ((pixel != NULL) && (getCount() > 0))
+        if ((pixel != NULL) && (Count > 0))
         {   
             if ((Modality != NULL) && Modality->hasLookupTable() && (bitsof(T1) <= MAX_TABLE_ENTRY_SIZE))
-                modlut((const T1 *)pixel->getData());
+                modlut(pixel);
             else if ((Modality != NULL) && Modality->hasRescaling())
-                rescale((const T1 *)pixel->getData(), Modality->getRescaleSlope(), Modality->getRescaleIntercept());
+                rescale(pixel, Modality->getRescaleSlope(), Modality->getRescaleIntercept());
             else
-                rescale((const T1 *)pixel->getData(), 1.0, 0.0);
+                rescale(pixel, 1.0, 0.0);
             determineMinMax((T3)pixel->getMinValue(), (T3)pixel->getMaxValue());
         }
     }
@@ -75,14 +75,15 @@ class DiMonoInputPixelTemplate
 
  private:
 
-    inline void modlut(const T1 *pixel)
+    inline void modlut(const DiInputPixel *input)
     {
+        const T1 *pixel = (const T1 *)input->getData();
         if ((pixel != NULL) && (Modality != NULL))
         {
             const DiLookupTable *mlut = Modality->getTableData();
             if (mlut != NULL)
             {
-                Data = new T3[getCount()];
+                Data = new T3[Count];
                 if (Data != NULL)
                 {
                     register T2 value;
@@ -93,54 +94,142 @@ class DiMonoInputPixelTemplate
                     register const T1 *p = pixel;
                     register T3 *q = Data;
                     register unsigned long i;
-                    for (i = 0; i < getCount(); i++)
-                    {
-                        value = (T2)(*(p++));
-                        if (value <= firstentry)
-                            *(q++) = firstvalue;
-                        else if (value >= lastentry)
-                            *(q++) = lastvalue;
-                        else
-                            *(q++) = (T3)mlut->getValue(value);
+                    T3 *lut = NULL;
+                    const unsigned long cnt = (unsigned long)input->getAbsMaxRange(); // number of LUT entries
+                    if ((sizeof(T1) <= 2) && (Count > 3 * cnt))                       // optimization criteria
+                    {                                                                 // use LUT for optimization
+                        lut = new T3[cnt];
+                        if (lut != NULL)
+                        {
+                            if (DicomImageClass::DebugLevel >= DicomImageClass::DL_Informationals)
+                                cerr << "INFO: using optimized routine for 'modlut()'" << endl;
+                            q = lut;
+                            for (i = 0; i < cnt; i++)                                 // calculating LUT entries
+                            {
+                                value = (T2)i;
+                                if (value <= firstentry)
+                                    *(q++) = firstvalue;
+                                else if (value >= lastentry)
+                                    *(q++) = lastvalue;
+                                else
+                                    *(q++) = (T3)mlut->getValue(value);
+                            }
+                            q = Data;
+/*
+                            if (absmin == 0)
+                            {                                                         // unsigned input-representation
+                                for (i = 0; i < Count; i++)
+                                    *(q++) = lut[*(p++)];
+                            } else {                                                  // signed input-representation
+                                for (i = 0; i < Count; i++)
+                                    *(q++) = lut[*(p++) - absmin];
+                            }
+*/
+                            const T3 *lut0 = lut - (T2)input->getAbsMinimum();        // points to 'zero' entry
+                            for (i = 0; i < Count; i++)
+                                *(q++) = *(lut0 + (*(p++)));
+                        }
                     }
+                    if (lut == NULL)                                                  // use "normal" transformation
+                    {
+                        for (i = 0; i < Count; i++)
+                        {
+                            value = (T2)(*(p++));
+                            if (value <= firstentry)
+                                *(q++) = firstvalue;
+                            else if (value >= lastentry)
+                                *(q++) = lastvalue;
+                            else
+                                *(q++) = (T3)mlut->getValue(value);
+                        }
+                    } else
+                        delete[] lut;
                 }
             } 
         }
     }
 
-    inline void rescale(const T1 *pixel,
+    inline void rescale(const DiInputPixel *input,
                         const double slope,
                         const double intercept)
     {
+        const T1 *pixel = (const T1 *)input->getData();
         if (pixel != NULL)
         {
-            Data = new T3[getCount()];
+            Data = new T3[Count];
             if (Data != NULL)
             {
                 register const T1 *p = pixel;
                 register T3 *q = Data;
                 register unsigned long i;
-                if (slope == 1.0) {
-                    if (intercept == 0.0) {
-                        for (i = 0; i < getCount(); i++)            // copy pixel data: can't use copyMem because T1 isn't always equal to T3
-                            *(q++) = (T3)*(p++);
-                    } else {
-                        for (i = 0; i < getCount(); i++)
-                            *(q++) = (T3)((double)*(p++) + intercept);
-                    }
+                if ((slope == 1.0) && (intercept == 0.0))
+                {
+                    for (i = 0; i < Count; i++)            // copy pixel data: can't use copyMem because T1 isn't always equal to T3
+                        *(q++) = (T3)*(p++);
                 } else {
-                    if (intercept == 0.0) {
-                        for (i = 0; i < getCount(); i++)
-                            *(q++) = (T3)((double)*(p++) * slope);
-                    } else {
-                        for (i = 0; i < getCount(); i++)
-                            *(q++) = (T3)((double)*(p++) * slope + intercept);
+                    T3 *lut = NULL;
+                    const unsigned long cnt = (unsigned long)input->getAbsMaxRange(); // number of LUT entries
+                    if ((sizeof(T1) <= 2) && (Count > 3 * cnt))                       // optimization criteria
+                    {                                                                 // use LUT for optimization
+                        lut = new T3[cnt];
+                        if (lut != NULL)
+                        {
+                            if (DicomImageClass::DebugLevel >= DicomImageClass::DL_Informationals)
+                                cerr << "INFO: using optimized routine for 'rescale()'" << endl;
+                            q = lut;
+                            if (slope == 1.0)
+                            {
+                                for (i = 0; i < cnt; i++)                             // calculating LUT entries
+                                    *(q++) = (T3)((double)i + intercept);
+                            } else {
+                                if (intercept == 0.0)
+                                {
+                                    for (i = 0; i < cnt; i++)
+                                        *(q++) = (T3)((double)i * slope);
+                                } else {
+                                    for (i = 0; i < cnt; i++)
+                                        *(q++) = (T3)((double)i * slope + intercept);
+                                }
+                            }
+                            q = Data;
+/*
+                            const T2 absmin = (T2)input->getAbsMinimum();
+                            if (absmin == 0)
+                            {                                                         // unsigned input-representation
+                                for (i = 0; i < Count; i++)
+                                    *(q++) = lut[*(p++)];
+                            } else {                                                  // signed input-representation
+                                for (i = 0; i < Count; i++)
+                                    *(q++) = lut[*(p++) - absmin];
+                            }
+*/
+                            const T3 *lut0 = lut - (T2)input->getAbsMinimum();        // points to 'zero' entry
+                            for (i = 0; i < Count; i++)
+                                *(q++) = *(lut0 + (*(p++)));
+                        }
                     }
+                    if (lut == NULL)                                                  // use "normal" transformation
+                    {
+                        if (slope == 1.0)
+                        {
+                            for (i = 0; i < Count; i++)
+                                *(q++) = (T3)((double)*(p++) + intercept);
+                        } else {
+                            if (intercept == 0.0)
+                            {
+                                for (i = 0; i < Count; i++)
+                                    *(q++) = (T3)((double)*(p++) * slope);
+                            } else {
+                                for (i = 0; i < Count; i++)
+                                    *(q++) = (T3)((double)*(p++) * slope + intercept);
+                            }
+                        }
+                    } else
+                        delete[] lut;
                 }
             }
         }
     }
-
 };
 
 
@@ -151,7 +240,12 @@ class DiMonoInputPixelTemplate
  *
  * CVS/RCS Log:
  * $Log: dimoipxt.h,v $
- * Revision 1.3  1998-12-22 14:29:39  joergr
+ * Revision 1.4  1999-01-20 15:06:24  joergr
+ * Replaced invocation of getCount() by member variable Count where possible.
+ * Added optimization to modality and VOI transformation (using additional
+ * LUTs).
+ *
+ * Revision 1.3  1998/12/22 14:29:39  joergr
  * Replaced method copyMem by for-loop copying each item.
  * Renamed some variables
  *
