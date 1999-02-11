fdb93ce85f47b5c1317916ed4660f0a9116f4d37
Joerg Riesmeier <dicom@offis.de>
1999-02-11 16:53:35 +0000

    Added routine to check whether particular grayscale values are unused in the output data. Removed unused parameter / member variable.

--- dcmimgle/libsrc/dimoopx.cc
+++ dcmimgle/libsrc/dimoopx.cc
@@ -22,9 +22,9 @@
  *  Purpose: DicomMonoOutputPixel (Source)
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 1999-01-20 14:54:30 $
+ *  Update Date:      $Date: 1999-02-11 16:53:35 $
  *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimgle/libsrc/dimoopx.cc,v $
- *  CVS/RCS Revision: $Revision: 1.2 $
+ *  CVS/RCS Revision: $Revision: 1.3 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -43,8 +43,11 @@
  *----------------*/
 
 DiMonoOutputPixel::DiMonoOutputPixel(const DiMonoPixel *pixel,
-                                     const unsigned long frames)
-  : Count(((pixel != NULL) && (frames > 0)) ? pixel->getCount() / frames : 0)
+                                     const unsigned long frames,
+                                     const unsigned long max)
+  : Count(((pixel != NULL) && (frames > 0)) ? pixel->getCount() / frames : 0),
+    UsedValues(NULL),
+    MaxValue(max)
 {
 }
 
@@ -55,6 +58,24 @@ DiMonoOutputPixel::DiMonoOutputPixel(const DiMonoPixel *pixel,
 
 DiMonoOutputPixel::~DiMonoOutputPixel()
 {
+    delete[] UsedValues;
+}
+
+
+/**********************************/
+
+
+int DiMonoOutputPixel::isUnused(const unsigned long value)
+{
+    if (UsedValues == NULL)
+        determineUsedValues();                  // create on demand 
+    if (UsedValues != NULL)
+    {
+        if (value <= MaxValue)
+            return (int)(UsedValues[value] == 0);
+        return 2;                               // out of range
+    }
+    return 0;
 }
 
 
@@ -62,7 +83,12 @@ DiMonoOutputPixel::~DiMonoOutputPixel()
  *
  * CVS/RCS Log:
  * $Log: dimoopx.cc,v $
- * Revision 1.2  1999-01-20 14:54:30  joergr
+ * Revision 1.3  1999-02-11 16:53:35  joergr
+ * Added routine to check whether particular grayscale values are unused in
+ * the output data.
+ * Removed unused parameter / member variable.
+ *
+ * Revision 1.2  1999/01/20 14:54:30  joergr
  * Replaced invocation of getCount() by member variable Count where possible.
  *
  * Revision 1.1  1998/11/27 16:15:02  joergr
