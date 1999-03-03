b592a51abd6b4d1c55a7fc9c9c8059057c9ca702
Joerg Riesmeier <dicom@offis.de>
1999-03-03 12:05:43 +0000

    Added method to invert lookup table data (used for presentation state LUTs).

--- dcmimgle/libsrc/dibaslut.cc
+++ dcmimgle/libsrc/dibaslut.cc
@@ -22,9 +22,9 @@
  *  Purpose: DicomBaseLUT (Source)
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 1999-02-03 17:47:44 $
+ *  Update Date:      $Date: 1999-03-03 12:05:43 $
  *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimgle/libsrc/dibaslut.cc,v $
- *  CVS/RCS Revision: $Revision: 1.1 $
+ *  CVS/RCS Revision: $Revision: 1.2 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -69,16 +69,32 @@ DiBaseLUT::~DiBaseLUT()
 /********************************************************************/
 
 
+int DiBaseLUT::invertTable()
+{
+    if ((Data != NULL) && (Count > 0))
+    {
+        register Uint32 i;
+        register const Uint16 *p = Data;
+        register Uint16 *q = (Uint16 *)Data;                // remove const to modify Data
+        const Uint16 max = DicomImageClass::maxval(Bits);
+        for (i = 0; i < Count; i++)
+            *(q++) = max - *(p++);
+        return 1;
+    }
+    return 0;
+}
 
 
 /*
  *
  * CVS/RCS Log:
  * $Log: dibaslut.cc,v $
- * Revision 1.1  1999-02-03 17:47:44  joergr
+ * Revision 1.2  1999-03-03 12:05:43  joergr
+ * Added method to invert lookup table data (used for presentation state LUTs).
+ *
+ * Revision 1.1  1999/02/03 17:47:44  joergr
  * Added base class for look-up tables (moved main functionality of class
  * DiLookupTable to DiBaseLUT).
- *
  *
  *
  */
