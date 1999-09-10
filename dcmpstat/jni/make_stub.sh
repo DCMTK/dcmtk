6eb1315fe04b68fde4cb3afbf8f4478ecf25a428
Marco Eichelberg <dicom@offis.de>
1999-09-10 12:47:49 +0000

    Updated DICOMscope stub shell script

--- dcmpstat/jni/make_stub.sh
+++ dcmpstat/jni/make_stub.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 JAVA_HOME=/usr/java1.2
-JAVA_OPTS="-Xmx128m"
 JAVA_JNI_LIB_DIR=.
-JAVA_MAINCLASS=DicomTest
-OUTFILE=ecr_browser
+JAVA_MAINCLASS=DICOMscope
+OUTFILE=DICOMscope
 
-/usr/java1.2/bin/javald -H $JAVA_HOME -j $JAVA_OPTS -o $OUTFILE -R $JAVA_JNI_LIB_DIR $JAVA_MAINCLASS
+/usr/java1.2/bin/javald -H $JAVA_HOME -o $OUTFILE -R $JAVA_JNI_LIB_DIR $JAVA_MAINCLASS
 
