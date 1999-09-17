5ab9f75442e19804f77f422aa0db63aa6a515c12
Joerg Riesmeier <dicom@offis.de>
1999-09-17 12:26:25 +0000

    iAdded/changed/completed DOC++ style comments in the header files.

--- dcmimgle/include/dimomod.h
+++ dcmimgle/include/dimomod.h
@@ -22,9 +22,9 @@
  *  Purpose: DicomMonochromeModality (Header)
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 1999-05-31 12:35:15 $
+ *  Update Date:      $Date: 1999-09-17 12:26:25 $
  *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimgle/include/Attic/dimomod.h,v $
- *  CVS/RCS Revision: $Revision: 1.8 $
+ *  CVS/RCS Revision: $Revision: 1.9 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -63,79 +63,151 @@ class DiMonoModality
 
  public:
 
+    /** constructor
+     *
+     ** @param  docu   pointer to dataset (encapsulated)
+     *  @param  pixel  pointer to input pixel data
+     */
     DiMonoModality(const DiDocument *docu,
                    DiInputPixel *pixel);
 
+    /** constructor, rescale
+     *
+     ** @param  docu       pointer to dataset (encapsulated)
+     *  @param  pixel      pointer to input pixel data
+     *  @param  slope      rescale slope (<> 0)
+     *  @param  intercept  rescale intercept
+     */
     DiMonoModality(const DiDocument *docu,
                    DiInputPixel *pixel,
                    const double slope,
                    const double intercept);
 
+    /** constructor, LUT
+     *
+     ** @param  docu         pointer to dataset (encapsulated)
+     *  @param  pixel        pointer to input pixel data
+     *  @param  data         element containing the modality LUT data
+     *  @param  descriptor   element containing the modality LUT descriptor
+     *  @param  explanation  element containing the modality LUT explanation (optional)
+     */
     DiMonoModality(const DiDocument *docu,
                    DiInputPixel *pixel,
                    const DcmUnsignedShort &data,
                    const DcmUnsignedShort &descriptor,
                    const DcmLongString *explanation);
 
+    /** constructor, no modality transform
+     */
     DiMonoModality(const int bits);
 
+    /** destructor
+     */
     virtual ~DiMonoModality();
     
+    /** get integer representation
+     *
+     ** @return integer representation
+     */
     inline EP_Representation getRepresentation() const
     {
         return Representation;
     }
 
+    /** get minimum pixel value after modality transform
+     *
+     ** @return minimum pixel value
+     */
     inline double getMinValue() const
     {
         return MinValue;
     }
     
+    /** get maximum pixel value after modality transform
+     *
+     ** @return maximum pixel value
+     */
     inline double getMaxValue() const
     {
         return MaxValue;
     }
     
+    /** get number of bits describing the width of output data
+     *
+     ** @return number of bits
+     */
     inline unsigned int getBits() const
     {
         return Bits;
     }
     
+    /** get absolute (possible) minimum pixel value after modality transform
+     *
+     ** @return absolute minimum pixel value
+     */
     inline double getAbsMinimum() const
     {
         return AbsMinimum;
     }
     
+    /** get absolute (possible) maximum pixel value after modality transform
+     *
+     ** @return absolute maximum pixel value
+     */
     inline double getAbsMaximum() const
     {
         return AbsMaximum;
     }
     
+    /** get rescale intercept value
+     *
+     ** @return rescale intercept value
+     */
     inline double getRescaleIntercept() const
     {
         return RescaleIntercept;
     }
     
+    /** get rescale slope value
+     *
+     ** @return rescale slope value
+     */
     inline double getRescaleSlope() const
     {
         return RescaleSlope;
     }
     
+    /** get pointer to lookup table data
+     *
+     ** @return pointer to lookup table data or NULL if absent
+     */
     inline const DiLookupTable *getTableData() const
     {
         return TableData;
     }
     
+    /** get modality LUT explanation
+     *
+     ** @return modality LUT explanation or NULL if absent
+     */
     inline const char *getExplanation() const
     {
         return (TableData != NULL) ? TableData->getExplanation() : (const char *)NULL;
     }
 
+    /** check whether lookup table is present
+     *
+     ** @return true if lookup table is present, false otherwise
+     */
     inline int hasLookupTable() const
     {
         return LookupTable;
     }
     
+    /** check whether rescaling is present
+     *
+     ** @return true if rescaling is present, false otherwise
+     */
     inline int hasRescaling() const
     {
         return Rescaling;
@@ -144,31 +216,53 @@ class DiMonoModality
 
  protected:
 
+    /** initialize internal data structures and values
+     *
+     ** @param  docu   pointer to dataset (encapsulated)
+     *  @param  pixel  pointer to input pixel data
+     */
     int Init(const DiDocument *docu,
              DiInputPixel *pixel);
  
+    /** check lookup table for validity (and possibly correct it)
+     */
     void checkTable();
 
+    /** check rescaling for validity (and possibly correct it)
+     *
+     ** @param  pixel  pointer to input pixel data
+     */
     void checkRescaling(const DiInputPixel *pixel);
 
  
  private:
 
+    /// integer representation
     EP_Representation Representation;
     
+    /// minimum pixel value
     double MinValue;
+    /// maximum pixel value
     double MaxValue;
 
+    /// number of bits
     unsigned int Bits;
+    /// absolute minimum pixel value
     double AbsMinimum;
+    /// absolute maximum pixel value
     double AbsMaximum;
     
+    /// rescale intercept
     double RescaleIntercept;
+    /// rescale slope
     double RescaleSlope;
     
+    /// status flag: lookup table present
     int LookupTable;
+    /// status flag: rescaling present
     int Rescaling;
     
+    /// pointer to modality lookup table
     DiLookupTable *TableData;
 
  // --- declarations to avoid compiler warnings
@@ -185,7 +279,10 @@ class DiMonoModality
  *
  * CVS/RCS Log:
  * $Log: dimomod.h,v $
- * Revision 1.8  1999-05-31 12:35:15  joergr
+ * Revision 1.9  1999-09-17 12:26:25  joergr
+ * iAdded/changed/completed DOC++ style comments in the header files.
+ *
+ * Revision 1.8  1999/05/31 12:35:15  joergr
  * Corrected bug concerning the conversion of color images to grayscale.
  *
  * Revision 1.7  1999/04/28 17:03:47  joergr
