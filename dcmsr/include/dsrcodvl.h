0bfce41e579a082a80365715e811725121bb0a0c
Joerg Riesmeier <dicom@offis.de>
2000-10-18 17:00:29 +0000

    Added methods allowing direct access to certain content item values. Added doc++ comments.

--- dcmsr/include/dsrcodvl.h
+++ dcmsr/include/dsrcodvl.h
@@ -23,8 +23,8 @@
  *    classes: DSRCodedEntryValue
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 2000-10-13 07:49:23 $
- *  CVS/RCS Revision: $Revision: 1.1 $
+ *  Update Date:      $Date: 2000-10-18 17:00:29 $
+ *  CVS/RCS Revision: $Revision: 1.2 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -50,82 +50,235 @@
  */
 class DSRCodedEntryValue
 {
+    // allow access to getValuePtr()
+    friend class DSRContentItem;
 
   public:
 
+    /** default contructor
+     */
     DSRCodedEntryValue();
 
+    /** constructor.
+     *  The code triple is only set if it passed the validity check (see setValue()).
+     ** @param  codeValue               computer readable and computer searchable identifier
+     *                                  of the code to be set. (VR=SH, mandatory)
+     *  @param  codingSchemeDesignator  textual identifier of the table where the 'codeValue'
+     *                                  is linked to its 'codeMeaning'. (VR=SH, mandatory)
+     *  @param  codeMeaning             human-readable translation of the 'codeValue'.  Used
+     *                                  for display when code dictionary is not available.
+     *                                  (VR=LO, mandatory)
+     */
     DSRCodedEntryValue(const OFString &codeValue,
                        const OFString &codingSchemeDesignator,
                        const OFString &codeMeaning);
 
+    /** constructor.
+     *  The code 4-tuple is only set if it passed the validity check (see setValue()).
+     ** @param  codeValue               computer readable and computer searchable identifier
+     *                                  of the code to be set. (VR=SH, mandatory)
+     *  @param  codingSchemeDesignator  textual identifier of the table where the 'codeValue'
+     *                                  is linked to its 'codeMeaning'. (VR=SH, mandatory)
+     *  @param  codingSchemeVersion     version of the coding scheme.  Used when a coding
+     *                                  scheme has multiple versions and the 'codingScheme
+     *                                  Designator' does not explicitly (or adequately)
+     *                                  specify the version number. (VR=SH, optional)
+     *  @param  codeMeaning             human-readable translation of the 'codeValue'. Used
+     *                                  for display when (appropriate) code dictionary is not
+     *                                  available. (VR=LO, mandatory)
+     */
     DSRCodedEntryValue(const OFString &codeValue,
                        const OFString &codingSchemeDesignator,
                        const OFString &codingSchemeVersion,
                        const OFString &codeMeaning);
 
+    /** copy constructor.
+     ** @param  codedEntryValue  code to be copied (not checked !)
+     */
     DSRCodedEntryValue(const DSRCodedEntryValue &codedEntryValue);
 
+    /** destructor
+     */
     virtual ~DSRCodedEntryValue();
 
+    /** assignment operator
+     ** @param  codedEntryValue  code to be copied (not checked !)
+     ** @return reference to this code after 'codedEntryValue' has been copied
+     */
     DSRCodedEntryValue &operator=(const DSRCodedEntryValue &codedEntryValue);
 
+    /** comparison operator.
+     *  Two codes are equal if the code value, coding scheme designator and the (optional)
+     *  coding scheme version are equal.  The code meaning is not important for this.
+     ** @param  codedEntryValue  code which should be compared to the current one
+     ** @return OFTrue if both codes are equal, OFFalse otherwise
+     */
     OFBool operator==(const DSRCodedEntryValue &codedEntryValue) const;
 
-    void clear();
+    /** clear all internal variables.
+     *  Since an empty code is invalid the code becomes invalid afterwards.
+     */
+    virtual void clear();
 
-    OFBool isValid() const;
+    /** check whether the current code is valid.
+     *  See checkCode() for details.
+     ** @return OFTrue if code is valid, OFFalse otherwise
+     */
+    virtual OFBool isValid() const;
 
+    /** check whether the current code is empty.
+     *  Checks whether all four components of the code are empty.
+     ** @return OFTrue if code is empty, OFFalse otherwise
+     */
+    virtual OFBool isEmpty() const;
+
+    /** check whether the current code belongs to the private "dcmtk" coding scheme.
+     *  This check is performed by comparing the coding scheme designator and the private
+     *  coding scheme creator UID - a coding scheme version is currently not used.
+     ** @return OFTrue if code belongs to the private "dcmtk" coding scheme, OFFalse otherwise
+     */
     OFBool isPrivateDcmtkCodingScheme() const;
 
+    /** print code.
+     *  The output of a typical code triple looks like this: (1234,99_OFFIS_DCMTK,"Code Meaning")
+     ** @param  stream          output stream to which the code should be printed
+     *  @param  printCodeValue  flag indicating whether the code value and coding scheme
+     *                          designator should be printed (default) or not.  If OFFalse the
+     *                          output looks like this: (,,"Code Meaning")
+     *  @param  printInvalid    flag indicating whether the text "invalid code" should be
+     *                          printed for invalid codes or not (default)
+     */
     void print(ostream &stream,
                const OFBool printCodeValue = OFTrue,
                const OFBool printInvalid = OFFalse) const;
 
+    /** read code sequence from dataset.
+     *  The number of items within the code sequence is checked.  If error/warning output are
+     *  enabled a warning message is printed if the sequence is empty or contains more than
+     *  one item.
+     ** @param  dataset    DICOM dataset from which the code sequence should be read
+     *  @param  tagKey     DICOM tag specifying the attribute (= sequence) which should be read
+     *  @param  type       value type of the sequence (valid value: "1", "2", something else).
+     *                     This parameter is used for checking purpose, any difference is reported.
+     *  @param  logStream  pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition readSequence(DcmItem &dataset,
                              const DcmTagKey &tagKey,
+                             const OFString &type,
                              OFConsole *logStream);
 
+    /** write code sequence to dataset
+     ** @param  dataset    DICOM dataset to which the code sequence should be written
+     *  @param  tagKey     DICOM tag specifying the attribute (= sequence) which should be written
+     *  @param  logStream  pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition writeSequence(DcmItem &dataset,
                               const DcmTagKey &tagKey,
                               OFConsole *logStream) const;
 
+    /** render code in HTML format
+     ** @param  stream     output stream to which the HTML document is written
+     *  @param  logStream  pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition renderHTML(ostream &stream,
                            OFConsole *logStream) const;
 
-    const DSRCodedEntryValue &getValue() const
+    /** get reference to code value
+     ** @return reference to code value
+     */
+    inline const DSRCodedEntryValue &getValue() const
     {
         return *this;
     }
 
+    /** get copy of code value
+     ** @param  codedEntryValue  reference to variable in which the code should be stored
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition getValue(DSRCodedEntryValue &codedEntryValue) const;
 
-    E_Condition setValue(const DSRCodedEntryValue &codedEntryValue);
-
-    const OFString &getCodeValue() const
+    /** get code value.
+     *  This is a computer readable and computer searchable identifier of the code.
+     ** @return current code value (might be invalid or an empty string)
+     */
+    inline const OFString &getCodeValue() const
     {
         return CodeValue;
     }
 
-    const OFString &getCodingSchemeDesignator() const
+    /** get coding scheme designator.
+     *  This is a textual identifier of the table where the code value is linked to its
+     *  code meaning.
+     ** @return current coding scheme designator (might be invalid or an empty string)
+     */
+    inline const OFString &getCodingSchemeDesignator() const
     {
         return CodingSchemeDesignator;
     }
 
-    const OFString &getCodingSchemeVersion() const
+    /** get coding scheme version.
+     *  Optional - Used when a coding scheme has multiple versions and the coding scheme
+     *  designator does not explicitly (or adequately) specify the version number.
+     ** @return current coding scheme version (might be invalid or an empty string)
+     */
+    inline const OFString &getCodingSchemeVersion() const
     {
         return CodingSchemeVersion;
     }
 
-    const OFString &getCodeMeaning() const
+    /** get code meaning.
+     *  Human-readable translation of the code value.  Used for display when code dictionary
+     *  is not available.
+     ** @return current code meaning (might be invalid or an empty string)
+     */
+    inline const OFString &getCodeMeaning() const
     {
         return CodeMeaning;
     }
 
+    /** set code value.
+     *  Before setting the code it is checked (see checkCode()).  If the code is invalid
+     *  the current code is not replaced and remains unchanged.
+     ** @param  codedEntryValue  code to be set
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
+    E_Condition setValue(const DSRCodedEntryValue &codedEntryValue);
+
+    /** set code value.
+     *  Before setting the code it is checked (see checkCode()).  If the code is invalid
+     *  the current code is not replaced and remains unchanged.
+     ** @param  codeValue               computer readable and computer searchable identifier
+     *                                  of the code to be set. (VR=SH, mandatory)
+     *  @param  codingSchemeDesignator  textual identifier of the table where the 'codeValue'
+     *                                  is linked to its 'codeMeaning'. (VR=SH, mandatory)
+     *  @param  codeMeaning             human-readable translation of the 'codeValue'.  Used
+     *                                  for display when code dictionary is not available.
+     *                                  (VR=LO, mandatory)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition setValue(const OFString &codeValue,
                          const OFString &codingSchemeDesignator,
                          const OFString &codeMeaning);
 
+    /** set code value.
+     *  Before setting the specified code it is checked (see checkCode()).  If the code is
+     *  invalid the current code is not replaced and remains unchanged.
+     ** @param  codeValue               computer readable and computer searchable identifier
+     *                                  of the code to be set. (VR=SH, mandatory)
+     *  @param  codingSchemeDesignator  textual identifier of the table where the 'codeValue'
+     *                                  is linked to its 'codeMeaning'. (VR=SH, mandatory)
+     *  @param  codingSchemeVersion     version of the coding scheme.  Used when a coding
+     *                                  scheme has multiple versions and the 'codingScheme
+     *                                  Designator' does not explicitly (or adequately)
+     *                                  specify the version number. (VR=SH, optional)
+     *  @param  codeMeaning             human-readable translation of the 'codeValue'.  Used
+     *                                  for display when code dictionary is not available.
+     *                                  (VR=LO, mandatory)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition setValue(const OFString &codeValue,
                          const OFString &codingSchemeDesignator,
                          const OFString &codingSchemeVersion,
@@ -134,12 +287,39 @@ class DSRCodedEntryValue
 
   protected:
 
+    /** get pointer to code value
+     ** @return pointer to code value (never NULL)
+     */
+    inline DSRCodedEntryValue *getValuePtr()
+    {
+        return this;
+    }
+
+    /** read code from dataset
+     ** @param  dataset    DICOM dataset from which the code should be read
+     *  @param  logStream  pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition readItem(DcmItem &dataset,
                          OFConsole *logStream);
 
+    /** write code to dataset
+     ** @param  dataset    DICOM dataset to which the code should be written
+     *  @param  logStream  pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition writeItem(DcmItem &dataset,
                           OFConsole *logStream) const;
 
+    /** check the specified code for validity.
+     *  Currently the only check that is performed is that the three string values are not empty.
+     *  Later on it might be checked whether the specified code really belongs to the specified
+     *  coding scheme, etc.  This require the presence of the relevant code dictionaries.
+     ** @param  codeValue               code value to be checked
+     *  @param  codingSchemeDesignator  coding scheme designator to be checked
+     *  @param  codeMeaning             code meaning tobe checked
+     ** @return OFTrue if code is valid, OFFalse otherwise
+     */
     OFBool checkCode(const OFString &codeValue,
                      const OFString &codingSchemeDesignator,
                      const OFString &codeMeaning) const;
@@ -147,10 +327,15 @@ class DSRCodedEntryValue
 
   private:
 
+    /// code value (VR=SH, mandatory)
     OFString CodeValue;
+    /// coding scheme designator (VR=SH, mandatory)
     OFString CodingSchemeDesignator;
+    /// coding scheme version (VR=SH, optional)
     OFString CodingSchemeVersion;
+    /// code meaning (VR=LO, mandatory)
     OFString CodeMeaning;
+    /// private coding scheme creator UID.  Used to identify private coding schemes.
     OFString PrivateCodingSchemeCreatorUID;
 };
 
@@ -161,7 +346,11 @@ class DSRCodedEntryValue
 /*
  *  CVS/RCS Log:
  *  $Log: dsrcodvl.h,v $
- *  Revision 1.1  2000-10-13 07:49:23  joergr
+ *  Revision 1.2  2000-10-18 17:00:29  joergr
+ *  Added methods allowing direct access to certain content item values.
+ *  Added doc++ comments.
+ *
+ *  Revision 1.1  2000/10/13 07:49:23  joergr
  *  Added new module 'dcmsr' providing access to DICOM structured reporting
  *  documents (supplement 23).  Doc++ documentation not yet completed.
  *
--- dcmsr/include/dsrimgvl.h
+++ dcmsr/include/dsrimgvl.h
@@ -23,8 +23,8 @@
  *    classes: DSRImageReferenceValue
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 2000-10-16 11:54:12 $
- *  CVS/RCS Revision: $Revision: 1.2 $
+ *  Update Date:      $Date: 2000-10-18 17:04:23 $
+ *  CVS/RCS Revision: $Revision: 1.3 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -51,85 +51,214 @@
 class DSRImageReferenceValue
   : public DSRReferenceValue
 {
+    // allow access to getValuePtr()
+    friend class DSRContentItem;
 
   public:
 
+    /** default contructor
+     */
     DSRImageReferenceValue();
 
+    /** constructor.
+     *  The UID pair is only set if it passed the validity check (see setValue()).
+     ** @param  sopClassUID     referenced SOP class UID of the image object.
+     *                          (VR=UI, mandatory)
+     *  @param  sopInstanceUID  referenced SOP instance UID of the image object.
+     *                          (VR=UI, mandatory)
+     */
     DSRImageReferenceValue(const OFString &sopClassUID,
                            const OFString &sopInstanceUID);
 
+    /** constructor.
+     *  The UID 4-tuple is only set if it passed the validity check (see setValue()).
+     ** @param  imageSOPClassUID      referenced SOP class UID of the image object.
+     *                                (VR=UI, mandatory)
+     *  @param  imageSOPInstanceUID   referenced SOP instance UID of the image object.
+     *                                (VR=UI, mandatory)
+     *  @param  pstateSOPClassUID     referenced SOP class UID of the presentation state
+     *                                object. (VR=UI, optional)
+     *  @param  pstateSOPInstanceUID  referenced SOP instance UID of the presentation state
+     *                                object. (VR=UI, optional)
+     */
     DSRImageReferenceValue(const OFString &imageSOPClassUID,
                            const OFString &imageSOPInstanceUID,
                            const OFString &pstateSOPClassUID,
                            const OFString &pstateSOPInstanceUID);
 
+    /** copy constructor
+     ** @param  referenceValue  image reference value to be copied (not checked !)
+     */
     DSRImageReferenceValue(const DSRImageReferenceValue &referenceValue);
 
+    /** copy constructor
+     ** @param  imageReferenceValue   imagee reference value to be copied (not checked !)
+     *  @param  pstateReferenceValue  presentation state reference value to be copied (not
+     *                                checked !)
+     */
     DSRImageReferenceValue(const DSRReferenceValue &imageReferenceValue,
                            const DSRReferenceValue &pstateReferenceValue);
 
+    /** destructor
+     */
     virtual ~DSRImageReferenceValue();
 
+    /** assignment operator
+     ** @param  referenceValue  image reference value to be copied (not checked !)
+     ** @return reference to this image reference value after 'referenceValue' has been copied
+     */
     DSRImageReferenceValue &operator=(const DSRImageReferenceValue &referenceValue);
 
+    /** clear all internal variables.
+     *  Since an empty image reference is invalid the reference becomes invalid afterwards.
+     */
     virtual void clear();
 
+    /** check whether the current image reference value is valid.
+     *  The reference value is valid if SOP class UID and SOP instance UID are valid (see
+     *  checkSOP...UID() for details) and the optional presentation state is valid (see
+     *  checkPresentationState()).
+     ** @return OFTrue if reference value is valid, OFFalse otherwise
+     */
+    virtual OFBool isValid() const;
+
+    /** check whether the content is short.
+     *  This method is used to check whether the rendered output of this content item can be
+     *  expanded inline or not (used for renderHTML()).
+     ** @param  flags  flag used to customize the output (see DSRTypes::HF_xxx)
+     ** @return OFTrue if the content is short, OFFalse otherwise
+     */
     virtual OFBool isShort(const size_t flags) const;
 
+    /** print image reference.
+     *  The output of a typical image reference value looks like this: (CT image,"1.2.3") or
+     *  (CT image,"1.2.3"),(GSPS,"1.2.3.4") if a presentation state is present.
+     *  If the SOP class UID is unknown the UID is printed instead of the related name.
+     ** @param  stream  output stream to which the image reference value should be printed
+     *  @param  flags   flag used to customize the output (see DSRTypes::PF_xxx)
+     */
     virtual E_Condition print(ostream &stream,
                               const size_t flags) const;
 
+    /** render image reference value in HTML format
+     ** @param  docStream    output stream to which the main HTML document is written
+     *  @param  annexStream  output stream to which the HTML document annex is written
+     *  @param  annexNumber  reference to the variable where the current annex number is stored.
+     *                       Value is increased automatically by 1 after a new entry has been added.
+     *  @param  flags        flag used to customize the output (see DSRTypes::HF_xxx)
+     *  @param  logStream    pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     virtual E_Condition renderHTML(ostream &docStream,
                                    ostream &annexStream,
                                    size_t &annexNumber,
                                    const size_t flags,
                                    OFConsole *logStream) const;
 
-    const DSRImageReferenceValue &getValue() const
+    /** get reference to image reference value
+     ** @return reference to image reference value
+     */
+    inline const DSRImageReferenceValue &getValue() const
     {
         return *this;
     }
 
-    DSRImageReferenceValue *getValuePtr()
-    {
-        return this;
-    }
-
+    /** get copy of image reference value
+     ** @param  referenceValue  reference to variable in which the value should be stored
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition getValue(DSRImageReferenceValue &referenceValue) const;
 
+    /** set image reference value.
+     *  Before setting the reference it is checked (see check...()).  If the value is
+     *  invalid the current value is not replaced and remains unchanged.
+     ** @param  referenceValue  value to be set
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition setValue(const DSRImageReferenceValue &referenceValue);
 
-    const DSRReferenceValue &getPresentationState() const
+    /** get reference to presentation state value
+     ** @return reference to presentation state value (might be empty or invalid)
+     */
+    inline const DSRReferenceValue &getPresentationState() const
     {
         return PresentationState;
     }
 
+    /** set presentation state value.
+     *  Before setting the reference it is checked (see checkPresentationState()).
+     *  If the value is invalid the current value is not replaced and remains unchanged.  
+     ** @param  referenceValue  value to be set
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     E_Condition setPresentationState(const DSRReferenceValue &referenceValue);
 
-    DSRImageFrameList &getFrameList()
+    /** get reference to list of referenced frame numbers
+     ** @return reference to frame list
+     */
+    inline DSRImageFrameList &getFrameList()
     {
         return FrameList;
     }
 
+    /** check whether the image reference applies to a specific frame.
+     *  The image reference applies to a frame (of multiframe images) if the list of
+     *  referenced frame numbers is empty or the frame number is part of the list.
+     ** @param  frameNumber  number of the frame to be checked
+     ** @return OFTrue if reference applies to the specified frame, OFFalse otherwise
+     */
     OFBool appliesToFrame(const Sint32 frameNumber) const;
 
 
   protected:
 
+    /** get pointer to image reference value
+     ** @return pointer to image reference value (never NULL)
+     */
+    inline DSRImageReferenceValue *getValuePtr()
+    {
+        return this;
+    }
+
+    /** read image reference value from dataset
+     ** @param  dataset    DICOM dataset from which the value should be read
+     *  @param  logStream  pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     virtual E_Condition readItem(DcmItem &dataset,
                                  OFConsole *logStream);
 
+    /** write image reference value to dataset
+     ** @param  dataset    DICOM dataset to which the value should be written
+     *  @param  logStream  pointer to error/warning output stream (output disabled if NULL)
+     ** @return status, EC_Normal if successful, an error code otherwise
+     */
     virtual E_Condition writeItem(DcmItem &dataset,
                                   OFConsole *logStream) const;
 
+    /** check the specified SOP class UID for validity.
+     *  The only check that is currently performed is that the UID is not empty.  Later on
+     *  it might be checked whether the specified SOP class is really an image storage SOP
+     *  class.
+     ** @param  sopClassUID  SOP class UID to be checked
+     ** @return OFTrue if SOP class UID is valid, OFFalse otherwise
+     */
     virtual OFBool checkSOPClassUID(const OFString &sopClassUID) const;
+
+    /** check the presentation state object for validity.
+     *  The presentation state object is "valid" if both UIDs are empty or both are not empty
+     *  and SOP class UID equals to "GrayscaleSoftcopyPresentationStateStorage".
+     ** @param  referenceValue  value to be checked
+     ** @return OFTrue if presentation state object is valid, OFFalse otherwise
+     */
+    OFBool checkPresentationState(const DSRReferenceValue &referenceValue) const;
 
 
   private:
 
+    /// reference value (SOP class/instance UID) to presentation state (optional)
     DSRReferenceValue PresentationState;
-
+    /// list of referenced frame numbers (conditional)
     DSRImageFrameList FrameList;
 };
 
@@ -140,7 +269,11 @@ class DSRImageReferenceValue
 /*
  *  CVS/RCS Log:
  *  $Log: dsrimgvl.h,v $
- *  Revision 1.2  2000-10-16 11:54:12  joergr
+ *  Revision 1.3  2000-10-18 17:04:23  joergr
+ *  Added methods allowing direct access to certain content item values.
+ *  Added doc++ comments.
+ *
+ *  Revision 1.2  2000/10/16 11:54:12  joergr
  *  Added new method checking whether an image content item applies to a
  *  certain frame.
  *
