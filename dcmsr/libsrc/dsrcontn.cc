181e0cd833ac3244023b017d98441132fa5923aa
Joerg Riesmeier <dicom@offis.de>
2000-10-23 15:02:49 +0000

    Enhanced implementation of method isValid().

--- dcmsr/libsrc/dsrcontn.cc
+++ dcmsr/libsrc/dsrcontn.cc
@@ -23,8 +23,8 @@
  *    classes: DSRContainerTreeNode
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 2000-10-19 16:03:48 $
- *  CVS/RCS Revision: $Revision: 1.4 $
+ *  Update Date:      $Date: 2000-10-23 15:02:49 $
+ *  CVS/RCS Revision: $Revision: 1.5 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -59,7 +59,8 @@ void DSRContainerTreeNode::clear()
 OFBool DSRContainerTreeNode::isValid() const
 {
     /* ConceptNameCodeSequence required for root node container */
-    return (ContinuityOfContent != COC_invalid) && ((getRelationshipType() != RT_isRoot) || getConceptName().isValid());
+    return DSRDocumentTreeNode::isValid() && (ContinuityOfContent != COC_invalid) &&
+        ((getRelationshipType() != RT_isRoot) || getConceptName().isValid());
 }
 
 
@@ -263,7 +264,10 @@ E_Condition DSRContainerTreeNode::setContinuityOfContent(const E_ContinuityOfCon
 /*
  *  CVS/RCS Log:
  *  $Log: dsrcontn.cc,v $
- *  Revision 1.4  2000-10-19 16:03:48  joergr
+ *  Revision 1.5  2000-10-23 15:02:49  joergr
+ *  Enhanced implementation of method isValid().
+ *
+ *  Revision 1.4  2000/10/19 16:03:48  joergr
  *  Added optional module name to read method to provide more detailed warning
  *  messages.
  *
--- dcmsr/libsrc/dsrstrvl.cc
+++ dcmsr/libsrc/dsrstrvl.cc
@@ -23,8 +23,8 @@
  *    classes: DSRStringValue
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 2000-10-19 16:07:14 $
- *  CVS/RCS Revision: $Revision: 1.3 $
+ *  Update Date:      $Date: 2000-10-23 15:04:13 $
+ *  CVS/RCS Revision: $Revision: 1.4 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -79,7 +79,7 @@ void DSRStringValue::clear()
 
 OFBool DSRStringValue::isValid() const
 {
-   return OFTrue;
+   return checkValue(Value);
 }
 
 
@@ -123,16 +123,19 @@ E_Condition DSRStringValue::setValue(const OFString &stringValue)
 }
 
 
-OFBool DSRStringValue::checkValue(const OFString & /* stringValue */) const
+OFBool DSRStringValue::checkValue(const OFString &stringValue) const
 {
-    return OFTrue;
+    return (stringValue.length() > 0);
 }
 
 
 /*
  *  CVS/RCS Log:
  *  $Log: dsrstrvl.cc,v $
- *  Revision 1.3  2000-10-19 16:07:14  joergr
+ *  Revision 1.4  2000-10-23 15:04:13  joergr
+ *  Enhanced implementation of method isValid().
+ *
+ *  Revision 1.3  2000/10/19 16:07:14  joergr
  *  Added optional module name to read method to provide more detailed warning
  *  messages.
  *
