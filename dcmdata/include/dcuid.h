62fc67ba46994bd6a03ac0b9fea467c7b12eb413
Marco Eichelberg <dicom@offis.de>
2001-12-20 11:16:53 +0000

    Updated Version Number and Implementation Version Name to reflect the   current public release (3.5.1)

--- dcmdata/include/dcuid.h
+++ dcmdata/include/dcuid.h
@@ -24,9 +24,9 @@
  *  routines for finding and creating UIDs.
  *
  *  Last Update:      $Author: meichel $
- *  Update Date:      $Date: 2001-11-08 16:17:29 $
+ *  Update Date:      $Date: 2001-12-20 11:16:53 $
  *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmdata/include/Attic/dcuid.h,v $
- *  CVS/RCS Revision: $Revision: 1.47 $
+ *  CVS/RCS Revision: $Revision: 1.48 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -152,13 +152,13 @@ unsigned long dcmGuessModalityBytes(const char *sopClassUID);
  *   The second name is used to identify files written without dcmdata
  *   (i.e. using the --bit-preserving switch in various tools)
  */
-#define OFFIS_DTK_IMPLEMENTATION_VERSION_NAME   "OFFIS_DCMTK_350"
-#define OFFIS_DTK_IMPLEMENTATION_VERSION_NAME2  "OFFIS_DCMBP_350"
-#define OFFIS_DCMTK_RELEASEDATE                 "2001-06-14"
+#define OFFIS_DTK_IMPLEMENTATION_VERSION_NAME   "OFFIS_DCMTK_351"
+#define OFFIS_DTK_IMPLEMENTATION_VERSION_NAME2  "OFFIS_DCMBP_351"
+#define OFFIS_DCMTK_RELEASEDATE                 "2001-12-21"
 
 #define OFFIS_UID_ROOT                          "1.2.276.0.7230010.3"
-#define OFFIS_DCMTK_VERSION                     "3.5.0"
-#define OFFIS_DCMTK_VERSION_NUMBER              350
+#define OFFIS_DCMTK_VERSION                     "3.5.1"
+#define OFFIS_DCMTK_VERSION_NUMBER              351
 #define OFFIS_IMPLEMENTATION_CLASS_UID          OFFIS_UID_ROOT ".0." OFFIS_DCMTK_VERSION
 #define OFFIS_INSTANCE_CREATOR_UID              OFFIS_IMPLEMENTATION_CLASS_UID
 
@@ -433,7 +433,11 @@ unsigned long dcmGuessModalityBytes(const char *sopClassUID);
 /*
 ** CVS/RCS Log:
 ** $Log: dcuid.h,v $
-** Revision 1.47  2001-11-08 16:17:29  meichel
+** Revision 1.48  2001-12-20 11:16:53  meichel
+** Updated Version Number and Implementation Version Name to reflect the
+**   current public release (3.5.1)
+**
+** Revision 1.47  2001/11/08 16:17:29  meichel
 ** Updated data dictionary, UIDs and transfer syntaxes for DICOM 2001 edition.
 **
 ** Revision 1.46  2001/11/02 15:44:02  meichel
