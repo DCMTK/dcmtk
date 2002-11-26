683e80c61ba4ddca5d411152b7113efa30fbe42b
Joerg Riesmeier <dicom@offis.de>
2002-11-26 15:15:32 +0000

    Removed support for Metrowerks compiler (Apple Macintosh). Renamed config.h to cfunix.h to avoid name clashes with other packages.

--- config/include/osconfig.h
+++ config/include/osconfig.h
@@ -8,7 +8,7 @@
 #define BEGIN_EXTERN_C extern "C" {
 #define END_EXTERN_C }
 #else
-#define BEGIN_EXTERN_C 
+#define BEGIN_EXTERN_C
 #define END_EXTERN_C
 #endif
 
@@ -16,34 +16,27 @@
 /*
 ** This head includes an OS/Compiler specific configuration header.
 ** Add entries for specific non-unix OS/Compiler environments.
-** Under unix the default <config.h> should be used.
+** Under unix the default <cfunix.h> should be used.
 **
 */
 
 #ifdef HAVE_CONFIG_H
 /*
 ** Some sort of Unix platform supported by the GNU Configure utility.
-** Just include the automatically generated <config.h> 
+** Just include the automatically generated <cfunix.h>
 */
-#include <config.h>
-
-#elif defined(__MWERKS__)
-/*
-** Metrowerks C++ compiler for 68k and PowerPC Apple Macintosh
-*/
-#include "cfmwerks.h"
+#include "cfunix.h"
 
 #elif defined(_WIN32)
 /*
-** Visual C++ in a Windows 32 bit environment (WindowsNT/95)
+** Visual C++ in a Windows 32 bit environment (Windows 9x/Me/NT/2000/XP)
 */
 #include "cfwin32.h"
 
 #else
 /*
-** Don't know what sort of machine this is 
+** Don't know what sort of machine this is
 */
 #endif
 
 #endif /* !OSCONFIG_H*/
-
