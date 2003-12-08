bc7aad89cb788619af4dc1e1d222bb224cd9548e
Joerg Riesmeier <dicom@offis.de>
2003-12-08 18:23:09 +0000

    Removed leading underscore characters from preprocessor symbols (reserved symbols). Updated copyright header.

--- dcmimgle/include/diinpx.h
+++ dcmimgle/include/diinpx.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1996-2002, OFFIS
+ *  Copyright (C) 1996-2003, OFFIS
  *
  *  This software and supporting documentation were developed by
  *
@@ -22,9 +22,8 @@
  *  Purpose: DicomInputPixel (Header)
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 2002-06-26 16:02:31 $
- *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmimgle/include/Attic/diinpx.h,v $
- *  CVS/RCS Revision: $Revision: 1.14 $
+ *  Update Date:      $Date: 2003-12-08 18:23:09 $
+ *  CVS/RCS Revision: $Revision: 1.15 $
  *  Status:           $State: Exp $
  *
  *   CVS/RCS Log at end of file
@@ -32,8 +31,8 @@
  */
 
 
-#ifndef __DIINPX_H
-#define __DIINPX_H
+#ifndef DIINPX_H
+#define DIINPX_H
 
 #include "osconfig.h"
 #include "ofconsol.h"
@@ -211,7 +210,11 @@ class DiInputPixel
  *
  * CVS/RCS Log:
  * $Log: diinpx.h,v $
- * Revision 1.14  2002-06-26 16:02:31  joergr
+ * Revision 1.15  2003-12-08 18:23:09  joergr
+ * Removed leading underscore characters from preprocessor symbols (reserved
+ * symbols). Updated copyright header.
+ *
+ * Revision 1.14  2002/06/26 16:02:31  joergr
  * Enhanced handling of corrupted pixel data and/or length.
  *
  * Revision 1.13  2001/09/28 13:04:58  joergr
