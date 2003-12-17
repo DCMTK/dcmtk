f8f70f5192f76bf18ce9695d9eeac417af742cbc
Joerg Riesmeier <dicom@offis.de>
2003-12-17 15:16:20 +0000

    Added note to the comparison operators that the "day overflow" is not yet handled correctly.

--- ofstd/include/ofdatime.h
+++ ofstd/include/ofdatime.h
@@ -22,9 +22,8 @@
  *  Purpose: Combined class for date and time functions
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 2003-09-15 12:12:56 $
- *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/ofstd/include/Attic/ofdatime.h,v $
- *  CVS/RCS Revision: $Revision: 1.3 $
+ *  Update Date:      $Date: 2003-12-17 15:16:20 $
+ *  CVS/RCS Revision: $Revision: 1.4 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -57,23 +56,23 @@ class OFDateTime
      *  Initializes Date and Time to 0.
      */
     OFDateTime();
-    
+
     /** copy constructor
      *  @param dateTime date/time object to be copied
      */
     OFDateTime(const OFDateTime &dateTime);
-    
+
     /** copy constructor
      *  @param dateVal date object to be copied
      *  @param timeVal time object to be copied
      */
     OFDateTime(const OFDate &dateVal,
                const OFTime &timeVal);
-               
+
     /** destructor
      */
     virtual ~OFDateTime();
-    
+
     /** assignment operator
      *  @param dateTime date/time value to be set
      *  @return reference to this object (with new value)
@@ -82,7 +81,8 @@ class OFDateTime
 
     /** comparison operator (equal).
      *  Please note that the time values are first transformed to the Coordinated Universal
-     *  Time (UTC) before they are compared.
+     *  Time (UTC) before they are compared. However, the special case that there is an
+     *  "overflow" from one day to another is currently not handled.
      *  @param dateTime date and time value compared with the current value
      *  @return OFTrue if given date and time is equal, OFFalse otherwise
      */
@@ -90,7 +90,8 @@ class OFDateTime
 
     /** comparison operator (unequal)
      *  Please note that the time values are first transformed to the Coordinated Universal
-     *  Time (UTC) before they are compared.
+     *  Time (UTC) before they are compared. However, the special case that there is an
+     *  "overflow" from one day to another is currently not handled.
      *  @param dateTime date and time value compared with the current value
      *  @return OFTrue if given date or time is unequal, OFFalse otherwise
      */
@@ -100,13 +101,13 @@ class OFDateTime
      *  Sets all date and all time components to '0'. NB: Date becomes invalid.
      */
     virtual void clear();
-    
+
     /** check whether the currently stored date and time value is valid.
      *  See classes OFDate and OFTime for details.
      *  @return OFTrue if the current value is valid, OFFalse otherwise
      */
     virtual OFBool isValid() const;
-    
+
     /** set the date/time value.
      *  Before the new value is set it is checked using the "isValid()" routine.
      *  @param year new year value to be set
@@ -132,31 +133,31 @@ class OFDateTime
      *  @return OFTrue if the new value is valid and has been set, OFFalse otherwise
      */
     OFBool setDate(const OFDate &dateVal);
-    
+
     /** set the time component to the specified time.
      *  Before the new value is set it is checked using the "isValid()" routine.
      *  @param timeVal new time value to be set
      *  @return OFTrue if the new value is valid and has been set, OFFalse otherwise
      */
     OFBool setTime(const OFTime &timeVal);
-    
+
     /** set the date and time value to the current system date and time.
      *  This function uses operating system dependent routines. If the date or time function
      *  (or both) are unavailable for some reason the corresponding value is not modified.
      *  @return OFTrue if the current system date and time has been set, OFFalse otherwise
      */
     OFBool setCurrentDateTime();
-    
+
     /** get the current date component
      *  @return reference to date object
      */
     const OFDate &getDate() const;
-    
+
     /** get the current time component
      *  @return reference to time object
      */
     const OFTime &getTime() const;
-    
+
     /** get the current date/time value in ISO format.
      *  The two ISO time formats supported by this function are
      *    "YYYY-MM-DD HH:MM[:SS[.FFFFFF]][&ZZ:ZZ]" (with delimiters) and
@@ -177,20 +178,20 @@ class OFDateTime
                                    const OFBool showFraction = OFFalse,
                                    const OFBool showTimeZone = OFFalse,
                                    const OFBool showDelimiter = OFTrue) const;
-    
+
     /* --- static helper functions --- */
 
     /** get the current system date and time
      *  @return date/time object with the current system date and time set
      */
     static OFDateTime getCurrentDateTime();
-    
+
 
  private:
 
     /// currently stored date value
     OFDate Date;
-    
+
     /// currently stored time value
     OFTime Time;
 };
@@ -212,7 +213,11 @@ ostream& operator<<(ostream& stream, const OFDateTime &dateTime);
  *
  * CVS/RCS Log:
  * $Log: ofdatime.h,v $
- * Revision 1.3  2003-09-15 12:12:56  joergr
+ * Revision 1.4  2003-12-17 15:16:20  joergr
+ * Added note to the comparison operators that the "day overflow" is not yet
+ * handled correctly.
+ *
+ * Revision 1.3  2003/09/15 12:12:56  joergr
  * Fixed incorrect/improper comments of the comparision operators. Enhanced
  * comment of the default constructor. Made comparison operators const.
  *
