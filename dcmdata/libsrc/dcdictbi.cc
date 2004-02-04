1543f80282c2895333cfb148b8df28dfa7646eeb
Joerg Riesmeier <dicom@offis.de>
2004-02-04 16:52:36 +0000

    Updated copyright header.

--- dcmdata/libsrc/dcdictbi.cc
+++ dcmdata/libsrc/dcdictbi.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2002, OFFIS
+ *  Copyright (C) 1994-2004, OFFIS
  *
  *  This software and supporting documentation were developed by
  *
@@ -21,10 +21,9 @@
  *
  *  Purpose: 
  *
- *  Last Update:      $Author: meichel $
- *  Update Date:      $Date: 2002-11-27 12:06:45 $
- *  Source File:      $Source: /export/gitmirror/dcmtk-git/../dcmtk-cvs/dcmtk/dcmdata/libsrc/dcdictbi.cc,v $
- *  CVS/RCS Revision: $Revision: 1.10 $
+ *  Last Update:      $Author: joergr $
+ *  Update Date:      $Date: 2004-02-04 16:52:36 $
+ *  CVS/RCS Revision: $Revision: 1.11 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -53,7 +52,10 @@ DcmDataDictionary::loadBuiltinDictionary()
 /*
  * CVS/RCS Log:
  * $Log: dcdictbi.cc,v $
- * Revision 1.10  2002-11-27 12:06:45  meichel
+ * Revision 1.11  2004-02-04 16:52:36  joergr
+ * Updated copyright header.
+ *
+ * Revision 1.10  2002/11/27 12:06:45  meichel
  * Adapted module dcmdata to use of new header file ofstdinc.h
  *
  * Revision 1.9  2002/08/27 16:55:45  meichel
@@ -71,6 +73,5 @@ DcmDataDictionary::loadBuiltinDictionary()
  *
  * Revision 1.2  1999/03/31 09:25:24  meichel
  * Updated copyright header in module dcmdata
- *
  *
  */
