59b3c4d93b3ef6557402d592d60c826473ed2366
Joerg Riesmeier <dicom@offis.de>
2008-05-21 16:32:42 +0000

    Reimplemented tests for OFDate, OFTime and OFDateTime classes.

--- ofstd/tests/tofdatim.cc
+++ ofstd/tests/tofdatim.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2002-2006, OFFIS
+ *  Copyright (C) 2002-2008, OFFIS
  *
  *  This software and supporting documentation were developed by
  *
@@ -21,9 +21,9 @@
  *
  *  Purpose: test program for classes OFDate, OFTime and OFDateTime
  *
- *  Last Update:      $Author: meichel $
- *  Update Date:      $Date: 2006-08-14 16:42:48 $
- *  CVS/RCS Revision: $Revision: 1.9 $
+ *  Last Update:      $Author: joergr $
+ *  Update Date:      $Date: 2008-05-21 16:32:42 $
+ *  CVS/RCS Revision: $Revision: 1.10 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -39,6 +39,18 @@
 #include "dcmtk/ofstd/ofconsol.h"
 
 
+void outputTestResult(const int number,
+                      const OFBool result)
+{
+    COUT << "test #" << number << " ";
+    if (result)
+        COUT << "passed";
+    else
+        COUT << "failed!";
+    COUT << OFendl;
+}
+
+
 int main()
 {
     OFDate date1, date2;
@@ -46,100 +58,110 @@ int main()
     OFDateTime dateTime1, dateTime2;
     OFString tmpString, tmpString2;
 
-    COUT << "init date: " << date1 << OFendl;
-    date1.setCurrentDate();
-    COUT << "current date: " << date1 << OFendl;
-    date1.setDate(1972, 5, 17);
-    COUT << "birth date: " << date1 << OFendl;
-    if (!date1.setDate(2000, 13, 1))
-        COUT << "invalid date" << OFendl;
-    else
-        COUT << "valid date:" << date1 << OFendl;
-    date1.setCurrentDate();
-    date1.getISOFormattedDate(tmpString, OFTrue /*delimiter*/);
-    COUT << "current date: " << tmpString << OFendl;
-    date2.setISOFormattedDate(tmpString);
-    COUT << "same date: " << date2 << OFendl;
-
-    COUT << "init time: " << time1 << OFendl;
-    time1.setCurrentTime();
-    COUT << "current time: " << time1 << OFendl;
-    time1.setTime(0, 50, 01);
-    COUT << "birth date: " << time1 << OFendl;
-    if (!time1.setTime(24, 00, 00))
-        COUT << "invalid time" << OFendl;
-    else
-        COUT << "valid time:" << time1 << OFendl;
-    time1.setTime(0, 50, 01.99);
-    time1.getISOFormattedTime(tmpString, OFFalse /*seconds*/);
-    COUT << "current time: " << tmpString << OFendl;
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFFalse /*fraction*/);
-    COUT << "current time: " << tmpString << OFendl;
-    time2.setISOFormattedTime(tmpString);
-    COUT << "same time: " << time2 << OFendl;
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFTrue /*fraction*/, OFFalse /*timeZone*/, OFFalse /*delimiter*/);
-    COUT << "current time: " << tmpString << OFendl;
-
-    time1.setTime(10,  0,  0, -5);
-    time2.setTime(23, 59, 59, -5);
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFFalse /*fraction*/, OFTrue /*timeZone*/);
-    time2.getISOFormattedTime(tmpString2, OFTrue /*seconds*/, OFFalse /*fraction*/, OFTrue /*timeZone*/);
-    COUT << "compare time 1: " << tmpString << " < " << tmpString2 << " : " << ((time1 < time2) ? "true" : "false") << OFendl;
-    COUT << "compare time 2: " << tmpString << " <= " << tmpString2 << " : " << ((time1 <= time2) ? "true" : "false") << OFendl;
-    COUT << "compare time 3: " << tmpString << " >= " << tmpString2 << " : " << ((time1 >= time2) ? "true" : "false") << OFendl;
-    COUT << "compare time 4: " << tmpString << " > " << tmpString2 << " : " << ((time1 > time2) ? "true" : "false") << OFendl;
-    time2.setTime(11, 0, 0, -4);
-    time2.getISOFormattedTime(tmpString2, OFTrue /*seconds*/, OFFalse /*fraction*/, OFTrue /*timeZone*/);
-    COUT << "compare time 5: " << tmpString << " == " << tmpString2 << " : " << ((time1 == time2) ? "true" : "false") << OFendl;
-
+    /* test OFDate */
+    outputTestResult(1, !date1.isValid());
+    outputTestResult(2, !date1.setDate(2000, 13, 1));
+    outputTestResult(3, date1.setDate(2000, 12, 31));
+    outputTestResult(4, date1.getYear() == 2000);
+    outputTestResult(5, date1.getMonth() == 12);
+    outputTestResult(6, date1.getDay() == 31);
+    outputTestResult(7, date1 == OFDate(2000, 12, 31));
     date2 = date1;
-    date2.setDay(date2.getDay() + 1);
-    COUT << "compare date: " << date1 << " > " << date2 << " = " << ((date1 > date2) ? "true" : "false") << OFendl;
+    outputTestResult(8, date1 == date2);
+    outputTestResult(9, !(date1 != date2));
+    date2.clear();
+    outputTestResult(10, !date2.isValid());
+    outputTestResult(11, date1 < OFDate(2001, 1, 1));
+
+    /* test OFTime */
+    outputTestResult(12, time1.isValid());
+    outputTestResult(13, !time1.setTime(24, 0, 0));
+    outputTestResult(14, time1.setTime(12, 15, 30));
+    outputTestResult(15, time1.getHour() == 12);
+    outputTestResult(16, time1.getMinute() == 15);
+    outputTestResult(17, time1.getSecond() == 30);
+    outputTestResult(18, time1 == OFTime(12, 15, 30));
     time2 = time1;
-    time2.setMinute(time2.getMinute() + 1);
-    COUT << "compare time: " << time1 << " < " << time2 << " = " << ((time1 < time2) ? "true" : "false") << OFendl;
+    outputTestResult(19, time1 == time2);
+    outputTestResult(20, !(time1 != time2));
+    time2.clear();
+    outputTestResult(21, time2.isValid());
+    time1.setTime(12, 15, 30, -1);
+    outputTestResult(22, time1 > OFTime(10, 0, 0, -1));
+    time1.setTimeZone(2);
+    outputTestResult(23, time1 < OFTime(10, 0, 0, -1));
+    outputTestResult(24, time1 == OFTime(9, 15, 30, -1));
+    outputTestResult(25, time1.getTimeZone() == 2);
+    time2.setTime(12, 15, 00);
+    outputTestResult(26, time2.getTimeInHours() == 12.25);
+    outputTestResult(27, time1.getCoordinatedUniversalTime() == OFTime(10, 15, 30));
+    time2.setSecond(30.1234);
+    outputTestResult(28, time2.getSecond() == 30.1234);
+    outputTestResult(29, time2.getIntSecond() == 30);
 
-    COUT << "current date/time: " << OFDate::getCurrentDate() << " " << OFTime::getCurrentTime() << OFendl;
+    /* test OFDateTime */
+    outputTestResult(30, !dateTime1.isValid());
+    dateTime1.setDateTime(date1, time1);
+    outputTestResult(31, dateTime1.isValid());
+    outputTestResult(32, !dateTime1.setDateTime(2000, 13, 1, 24, 0, 0));
+    outputTestResult(33, !dateTime1.setDateTime(date2, time2));
+    dateTime1.setDateTime(date1, time1);
+    outputTestResult(34, dateTime1.getDate() == date1);
+    outputTestResult(35, !(dateTime1.getTime() != time1));
+    dateTime2 = dateTime1;
+    outputTestResult(36, dateTime1 == dateTime2);
+    /* "overflow" from one day to another is currently not handled by OFDateTime */
+    outputTestResult(37, dateTime1 != OFDateTime(2001, 1, 1, 0, 15, 30, 12) /* should be equal */);
+    dateTime1.getISOFormattedDateTime(tmpString);
+    outputTestResult(38, tmpString == "2000-12-31 12:15:30");
+    dateTime1.getISOFormattedDateTime(tmpString, OFTrue /*showSeconds*/, OFTrue /*showFraction*/, OFTrue /*showTimeZone*/, OFFalse /*showDelimiter*/, "" /*dateTimeSeparator*/);
+    outputTestResult(39, tmpString == "20001231121530.000000+0200");
+    dateTime2.setISOFormattedDateTime("2000-12-31 10:15:30" /*timeZone: 0*/);
+    outputTestResult(40, dateTime1 == dateTime2);
 
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFTrue /*fraction*/, OFTrue /*timeZone*/, OFFalse /*delimiter*/);
-    COUT << "with time zone: " << tmpString << OFendl;
+    /* output current date */
+    COUT << OFendl;
+    date1.setCurrentDate();
+    COUT << "current date (default): " << date1 << OFendl;
+    date1.getISOFormattedDate(tmpString, OFFalse /*delimiter*/);
+    COUT << "current date (YYYYMMDD): " << tmpString << OFendl;
+    COUT << "current year: " << date1.getYear() << OFendl;
+    COUT << "current month: " << date1.getMonth() << OFendl;
+    COUT << "current day: " << date1.getDay() << OFendl;
+    COUT << OFendl;
+
+    /* output current time */
     time1.setCurrentTime();
+    COUT << "current time (default): " << time1 << OFendl;
+    time1.getISOFormattedTime(tmpString, OFFalse /*seconds*/);
+    COUT << "current time (HH:MM): " << tmpString << OFendl;
+    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFFalse /*fraction*/);
+    COUT << "current time (HH:MM:SS): " << tmpString << OFendl;
     time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFFalse /*fraction*/, OFTrue /*timeZone*/, OFTrue /*delimiter*/);
-    COUT << "current time: " << tmpString << OFendl;
-    time1.setTimeZone(-11, 30);
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFFalse /*fraction*/, OFTrue /*timeZone*/, OFTrue /*delimiter*/);
-    COUT << "new time zone: " << tmpString << OFendl;
-    COUT << "UTC time: " << time1.getCoordinatedUniversalTime() << OFendl;
-    COUT << "local time: " << time1.getLocalTime() << OFendl;
-    time1.setTimeZone(OFTime::getLocalTimeZone());
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFFalse /*fraction*/, OFTrue /*timeZone*/, OFTrue /*delimiter*/);
-    COUT << "new time zone: " << tmpString << OFendl;
-    COUT << "UTC time: " << time1.getCoordinatedUniversalTime() << OFendl;
-    COUT << "local time: " << time1.getLocalTime() << OFendl;
+    COUT << "current time (HH:MM:SS&ZZ:ZZ): " << tmpString << OFendl;
+    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFTrue /*fraction*/, OFFalse /*timeZone*/, OFTrue /*delimiter*/);
+    COUT << "current time (HH:MM:SS.FFFFFF): " << tmpString << OFendl;
+    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFTrue /*fraction*/, OFTrue /*timeZone*/, OFFalse /*delimiter*/);
+    COUT << "current time (HHMMSS.FFFFFF&ZZZZ): " << tmpString << OFendl;
+    COUT << "current time in UTC: " << time1.getCoordinatedUniversalTime() << OFendl;
+    COUT << "current hour: " << time1.getHour() << OFendl;
+    COUT << "current minute: " << time1.getMinute() << OFendl;
+    COUT << "current second: " << time1.getSecond() << OFendl;
+    COUT << "current int second: " << time1.getIntSecond() << OFendl;
+    COUT << "current milli second: " << time1.getMilliSecond() << OFendl;
+    COUT << "current micro second: " << time1.getMicroSecond() << OFendl;
+    COUT << "current time zone: " << time1.getTimeZone() << OFendl;
+    COUT << "current time in hours: " << time1.getTimeInHours() << OFendl;
+    COUT << "current time in seconds: " << time1.getTimeInSeconds() << OFendl;
+    COUT << OFendl;
 
-    COUT << "current date/time with time zone: " << OFDate::getCurrentDate() << " " << OFTime::getCurrentTime().getCoordinatedUniversalTime() << OFendl;
-
-    time1.setTimeInSeconds(12 * 3600 + 15 * 60 + 45.99);
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFTrue /*fraction*/);
-    COUT << "time in seconds: " << tmpString << OFendl;
-    time1.getISOFormattedTime(tmpString, OFTrue /*seconds*/, OFTrue /*fraction*/);
-    time1.setTimeInHours(12 + 15 / 60 + 45.99 / 3600);
-    COUT << "time in hours: " << tmpString << OFendl;
-
-    dateTime1.setCurrentDateTime();
-    COUT << "current date/time (OFDateTime): " << dateTime1 << OFendl;
-    COUT << "current hour: " << dateTime1.getTime().getHour() << OFendl;
-    COUT << "current minute: " << dateTime1.getTime().getMinute() << OFendl;
-    COUT << "current second: " << dateTime1.getTime().getSecond() << OFendl;
-    COUT << "current int second: " << dateTime1.getTime().getIntSecond() << OFendl;
-    COUT << "current milli second: " << dateTime1.getTime().getMilliSecond() << OFendl;
-    COUT << "current micro second: " << dateTime1.getTime().getMicroSecond() << OFendl;
-    dateTime1.setDateTime(1999, 12, 31, 23, 59, 59, -1);
-    dateTime2.setDateTime(2000,  1,  1,  1, 59, 59,  1);
+    /* output current date/time */
+    dateTime1.setDateTime(date1, time1);
+    COUT << "current date/time (default): " << dateTime1 << OFendl;
     dateTime1.getISOFormattedDateTime(tmpString, OFTrue /*showSeconds*/, OFFalse /*showFraction*/, OFTrue /*showTimeZone*/);
-    dateTime2.getISOFormattedDateTime(tmpString2, OFTrue /*showSeconds*/, OFFalse /*showFraction*/, OFTrue /*showTimeZone*/);
-    COUT << "compare date/time: " << tmpString << " == " << tmpString2 << " : " << ((dateTime1 == dateTime2) ? "true" : "false") << OFendl;
-    COUT << "compare date/time: " << tmpString << " != " << tmpString2 << " : " << ((dateTime1 != dateTime2) ? "true" : "false") << OFendl;
+    COUT << "current date/time (YYYY-MM-DD HH:MM:SS&ZZ:ZZ): " << tmpString << OFendl;
+    dateTime1.getISOFormattedDateTime(tmpString, OFTrue /*showSeconds*/, OFTrue /*showFraction*/, OFTrue /*showTimeZone*/, OFFalse /*showDelimiter*/, "" /*dateTimeSeparator*/);
+    COUT << "current date/time (YYYYMMDDHHMMSS.FFFFFF&ZZZZ): " << tmpString << OFendl;
 
     return 0;
 }
@@ -149,7 +171,10 @@ int main()
  *
  * CVS/RCS Log:
  * $Log: tofdatim.cc,v $
- * Revision 1.9  2006-08-14 16:42:48  meichel
+ * Revision 1.10  2008-05-21 16:32:42  joergr
+ * Reimplemented tests for OFDate, OFTime and OFDateTime classes.
+ *
+ * Revision 1.9  2006/08/14 16:42:48  meichel
  * Updated all code in module ofstd to correctly compile if the standard
  *   namespace has not included into the global one with a "using" directive.
  *
