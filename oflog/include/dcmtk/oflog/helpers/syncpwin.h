380f78a19d59f5adf359c612dbe072972afa8b2c
Uli Schlachter <dicom@offis.de>
2010-11-24 13:56:52 +0000

    Add a previously missing include directive.

--- oflog/include/dcmtk/oflog/helpers/syncpwin.h
+++ oflog/include/dcmtk/oflog/helpers/syncpwin.h
@@ -21,6 +21,8 @@
 //   (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
 //   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include "dcmtk/ofstd/ofcast.h"
+
 //! @file
 //! This file contains implementations of synchronization
 //! primitives using the Win32 API. It does not contain any include
