38d87192891aa898a815036a8749c53e7aa931eb
Uli Schlachter <dicom@offis.de>
2011-06-07 07:06:57 +0000

    Added test cases for DcmElement::calcElementLength().

---
+++ dcmdata/tests/telemlen.cc
@@ -0,0 +1,167 @@
+/*
+ *
+ *  Copyright (C) 2011, OFFIS e.V.
+ *  All rights reserved.  See COPYRIGHT file for details.
+ *
+ *  This software and supporting documentation were developed by
+ *
+ *    OFFIS e.V.
+ *    R&D Division Health
+ *    Escherweg 2
+ *    D-26121 Oldenburg, Germany
+ *
+ *
+ *  Module:  dcmdata
+ *
+ *  Author:  Uli Schlachter
+ *
+ *  Purpose: test program for reading DICOM datasets
+ *
+ *  Last Update:      $Author: uli $
+ *  Update Date:      $Date: 2011-06-07 07:06:57 $
+ *  CVS/RCS Revision: $Revision: 1.1 $
+ *  Status:           $State: Exp $
+ *
+ *  CVS/RCS Log at end of file
+ *
+ */
+
+
+#include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first */
+
+#include "dcmtk/ofstd/oftest.h"
+#include "dcmtk/dcmdata/dctk.h"
+#include "dcmtk/dcmdata/dcpxitem.h"
+#include "dcmtk/dcmdata/dcostrmb.h"
+
+
+static void testElementLengthForElem(DcmElement& elem, const char *type,
+                                     offile_off_t len)
+{
+    const int maxSize = 100;
+    Uint8 buf[maxSize];
+    DcmOutputBufferStream stream(buf, maxSize);
+    void *unused;
+    offile_off_t length;
+    OFCondition cond;
+
+    elem.transferInit();
+    cond = elem.write(stream, EXS_LittleEndianExplicit, EET_ExplicitLength, NULL);
+    elem.transferEnd();
+
+    stream.flushBuffer(unused, length);
+    if (cond.bad())
+        OFCHECK_FAIL("Failed to write element " << type << " to stream: " << cond.text());
+    if (length != len)
+    {
+        OFCHECK_FAIL("Expected length (" << len << "), but got (" << length
+                << ") when writing " << type);
+    }
+
+    length = elem.calcElementLength(EXS_LittleEndianExplicit, EET_ExplicitLength);
+    if (length != len)
+    {
+        OFCHECK_FAIL("Expected length (" << len << "), but got (" << length
+                << ") while handling " << type);
+    }
+}
+
+static void testElementLengthForVR(DcmEVR vr, offile_off_t len)
+{
+    const DcmTag tag(DCM_PatientName, DcmVR(vr));
+
+    // Test all the calcElementLength and write implementations
+    DcmUnlimitedText ut(tag);
+    DcmByteString bs(tag);
+    DcmPixelData pd(tag);
+    DcmPolymorphOBOW po(tag);
+    DcmSequenceOfItems sq(tag);
+    DcmOtherByteOtherWord oo(tag);
+
+    // Make sure the PixelData assumes it is un-encapsulated, else
+    // calcElementLength() will fail (Yes, this is a hack)
+    pd.putUint8Array(NULL, 0);
+    pd.setVR(vr);
+
+    testElementLengthForElem(ut, "UnlimitedText", len);
+    testElementLengthForElem(bs, "ByteString", len);
+    testElementLengthForElem(pd, "PixelData", len);
+    testElementLengthForElem(po, "PolymorphOBOW", len);
+    testElementLengthForElem(sq, "SequenceOfItems", len);
+    testElementLengthForElem(oo, "OtherByteOtherWord", len);
+}
+
+// These don't make sense to be tested: EVR_item, EVR_metainfo,
+// EVR_dataset, EVR_fileFormat, EVR_dicomDir, EVR_dirRecord, EVR_pixelSQ
+
+OFTEST(dcmdata_elementLength_pixelItem)
+{
+    // EVR_pixelItem only makes sense for class DcmPixelItem.
+    // In fact, it enforces that VR in the constructor.
+    DcmPixelItem pi(DcmTag(DCM_PatientName, DcmVR(EVR_pixelItem)));
+    testElementLengthForElem(pi, "PixelItem", 8);
+}
+
+OFTEST(dcmdata_elementLength_pixelSequence)
+{
+    // DcmPixelSequence forces EVR_OB, no point in trying other VRs.
+    DcmPixelSequence ps(DcmTag(DCM_PatientName, DcmVR(EVR_OB)));
+    testElementLengthForElem(ps, "PixelSeq", 20);
+}
+
+/* Test extended length VRs */
+#define TEST_VR(vr) OFTEST(dcmdata_elementLength_ ## vr) { testElementLengthForVR(vr, 12); }
+TEST_VR(EVR_OB)
+TEST_VR(EVR_OF)
+TEST_VR(EVR_OW)
+TEST_VR(EVR_SQ)
+TEST_VR(EVR_UT)
+TEST_VR(EVR_ox)
+TEST_VR(EVR_lt)
+TEST_VR(EVR_UNKNOWN)
+TEST_VR(EVR_UN)
+/* Converted to UN during write */
+TEST_VR(EVR_na)
+TEST_VR(EVR_UNKNOWN2B)
+#undef TEST_VR
+
+/* Test "short" length VRs */
+#define TEST_VR(vr) OFTEST(dcmdata_elementLength_ ## vr) { testElementLengthForVR(vr, 8); }
+TEST_VR(EVR_AE)
+TEST_VR(EVR_AS)
+TEST_VR(EVR_AT)
+TEST_VR(EVR_CS)
+TEST_VR(EVR_DA)
+TEST_VR(EVR_DS)
+TEST_VR(EVR_DT)
+TEST_VR(EVR_FL)
+TEST_VR(EVR_FD)
+TEST_VR(EVR_IS)
+TEST_VR(EVR_LO)
+TEST_VR(EVR_PN)
+TEST_VR(EVR_SH)
+TEST_VR(EVR_SL)
+TEST_VR(EVR_SS)
+TEST_VR(EVR_ST)
+TEST_VR(EVR_TM)
+TEST_VR(EVR_UI)
+TEST_VR(EVR_UL)
+TEST_VR(EVR_US)
+TEST_VR(EVR_xs)
+TEST_VR(EVR_LT)
+TEST_VR(EVR_up)
+TEST_VR(EVR_PixelData)
+TEST_VR(EVR_OverlayData)
+#undef TEST_VR
+
+
+/*
+ *
+ * CVS/RCS Log:
+ * $Log: telemlen.cc,v $
+ * Revision 1.1  2011-06-07 07:06:57  uli
+ * Added test cases for DcmElement::calcElementLength().
+ *
+ *
+ *
+ */
--- dcmdata/tests/tests.cc
+++ dcmdata/tests/tests.cc
@@ -18,8 +18,8 @@
  *  Purpose: main test program
  *
  *  Last Update:      $Author: uli $
- *  Update Date:      $Date: 2011-05-25 10:05:55 $
- *  CVS/RCS Revision: $Revision: 1.1 $
+ *  Update Date:      $Date: 2011-06-07 07:06:57 $
+ *  CVS/RCS Revision: $Revision: 1.2 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -35,12 +35,53 @@ OFTEST_REGISTER(dcmdata_i2d_bmp);
 OFTEST_REGISTER(dcmdata_checkStringValue);
 OFTEST_REGISTER(dcmdata_pathAcces);
 OFTEST_REGISTER(dcmdata_dateTime);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_AE);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_AS);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_AT);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_CS);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_DA);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_DS);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_DT);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_FD);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_FL);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_IS);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_LO);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_LT);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_OB);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_OF);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_OW);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_OverlayData);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_PN);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_PixelData);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_SH);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_SL);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_SQ);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_SS);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_ST);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_TM);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_UI);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_UL);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_UN);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_UNKNOWN);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_UNKNOWN2B);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_US);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_UT);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_lt);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_na);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_ox);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_up);
+OFTEST_REGISTER(dcmdata_elementLength_EVR_xs);
+OFTEST_REGISTER(dcmdata_elementLength_pixelItem);
+OFTEST_REGISTER(dcmdata_elementLength_pixelSequence);
 OFTEST_MAIN("dcmdata")
 
 /*
 **
 ** CVS/RCS Log:
 ** $Log: tests.cc,v $
+** Revision 1.2  2011-06-07 07:06:57  uli
+** Added test cases for DcmElement::calcElementLength().
+**
 ** Revision 1.1  2011-05-25 10:05:55  uli
 ** Imported oftest and converted existing tests to oftest.
 **
