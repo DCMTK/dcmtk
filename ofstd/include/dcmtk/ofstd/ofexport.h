06c6f4636f0406c78c6d2dd81e72fd5dd9a90a16
Uli Schlachter <dicom@offis.de>
2012-02-15 11:37:42 +0000

    Don't use C++ comments in file included from C.

--- ofstd/include/dcmtk/ofstd/ofexport.h
+++ ofstd/include/dcmtk/ofstd/ofexport.h
@@ -18,8 +18,8 @@
  *  Purpose: common defines for import and exporting symbols
  *
  *  Last Update:      $Author: uli $
- *  Update Date:      $Date: 2012-02-01 09:49:18 $
- *  CVS/RCS Revision: $Revision: 1.1 $
+ *  Update Date:      $Date: 2012-02-15 11:37:42 $
+ *  CVS/RCS Revision: $Revision: 1.2 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -34,12 +34,13 @@
 
 #ifdef DCMTK_SHARED
 #ifdef _WIN32
-// Defines needed for building DLLs on windows
+/* Defines needed for building DLLs on windows */
 #define DCMTK_DECL_EXPORT __declspec(dllexport)
 #define DCMTK_DECL_IMPORT __declspec(dllimport)
 #elif defined(HAVE_HIDDEN_VISIBILITY)
-// GCC hides everything when given -fvisibility=hidden. The symbols which
-// should be visible have to get a default visibility again.
+/* GCC hides everything when given -fvisibility=hidden. The symbols which
+ * should be visible have to get a default visibility again.
+ */
 #define DCMTK_DECL_EXPORT __attribute__ ((visibility("default")))
 #endif
 #endif
@@ -58,6 +59,9 @@
 /*
  * CVS/RCS Log:
  * $Log: ofexport.h,v $
+ * Revision 1.2  2012-02-15 11:37:42  uli
+ * Don't use C++ comments in file included from C.
+ *
  * Revision 1.1  2012-02-01 09:49:18  uli
  * Moved some definitions to a file that can be included from C.
  *
