a1f55d8cf0f1c49239d88942f7775c3e9f654fb4
Joerg Riesmeier <dicom@offis.de>
2012-06-29 13:05:37 +0000

    Added some more simple tests for the integrated XML parser.

--- ofstd/tests/txml.cc
+++ ofstd/tests/txml.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2011, OFFIS e.V.
+ *  Copyright (C) 2011-2012, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -18,8 +18,8 @@
  *  Purpose: test program for xmlParser
  *
  *  Last Update:      $Author: joergr $
- *  Update Date:      $Date: 2011-11-24 09:53:43 $
- *  CVS/RCS Revision: $Revision: 1.1 $
+ *  Update Date:      $Date: 2012-06-29 13:05:37 $
+ *  CVS/RCS Revision: $Revision: 1.2 $
  *  Status:           $State: Exp $
  *
  *  CVS/RCS Log at end of file
@@ -34,14 +34,58 @@
 #include "dcmtk/ofstd/ofxml.h"
 
 
-#define XML_TEST_DATA "<?xml version=\"1.0\"?><root><element attribute=\"value\">some text</element></root>"
+#define XML_TEST_DATA "<?xml version=\"1.0\"?>"                         \
+    "<root>"                                                            \
+      "<element attribute=\"value\">some text</element>"                \
+      "<element attribute=\"other value\">some other text</element>"    \
+      "<error>error message</error>"                                    \
+      "<element>some totally different text</element>"                  \
+      "<parent>"                                                        \
+        "<child>no text</child>"                                        \
+        "<child />"                                                     \
+      "</parent>"                                                       \
+    "</root>"
+
 
 OFTEST(ofstd_xmlParser)
 {
+    int i = 0;
+
     /* perform some very simple tests on the XML data */
     XMLNode rootNode = XMLNode::parseString(XML_TEST_DATA, "root");
+
     OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNode("element").getAttribute("attribute"))), "value");
     OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNode("element").getText())), "some text");
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNode("element", 1).getText())), "some other text");
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNode("element", 2).getText())), "some totally different text");
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNode("element", -1).getText())), "some totally different text");
+
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNodeWithAttribute("element", "attribute").getText())), "some text");
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNodeWithAttribute("element", "attribute", "other value").getText())), "some other text");
+    rootNode.getChildNodeWithAttribute("element", "attribute", NULL, &i);
+    OFCHECK_EQUAL(i, 1);
+    rootNode.getChildNodeWithAttribute("element", "attribute", "other value", &i);
+    OFCHECK_EQUAL(i, 2);
+
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNodeByPath("element").getText())), "some text");
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNodeByPath("parent/child").getText())), "no text");
+
+    OFCHECK_EQUAL(rootNode.nChildNode(), 5);
+    OFCHECK_EQUAL(rootNode.nChildNode("element"), 3);
+    OFCHECK_EQUAL(rootNode.nChildNode("parent"), 1);
+    OFCHECK_EQUAL(rootNode.nChildNode("child"), 0);
+    OFCHECK_EQUAL(rootNode.getChildNode("parent").nChildNode(), 2);
+
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNode("element").getAttributeName(0))), "attribute");
+    OFCHECK_EQUAL(OFString(OFSTRING_GUARD(rootNode.getChildNode("element").getAttributeValue(0))), "value");
+    OFCHECK_EQUAL(rootNode.getChildNode("element").nAttribute(), 1);
+    OFCHECK_EQUAL(rootNode.getChildNode("element", 1).nAttribute(), 1);
+    OFCHECK_EQUAL(rootNode.getChildNode("element", 2).nAttribute(), 0);
+    OFCHECK_EQUAL(rootNode.getChildNode("error").nAttribute(), 0);
+    OFCHECK_EQUAL(rootNode.getChildNode("parent").nAttribute(), 0);
+
+    OFCHECK(!rootNode.getChildNode("parent").getChildNode("child", 1).isEmpty());
+    OFCHECK(rootNode.getChildNode("parent").getChildNode("child" ,2).isEmpty());
 }
 
 
@@ -49,6 +93,9 @@ OFTEST(ofstd_xmlParser)
  *
  * CVS/RCS Log:
  * $Log: txml.cc,v $
+ * Revision 1.2  2012-06-29 13:05:37  joergr
+ * Added some more simple tests for the integrated XML parser.
+ *
  * Revision 1.1  2011-11-24 09:53:43  joergr
  * Added first version of a simple non-validating XML parser written by Frank
  * Vanden Berghen. This parser is intended to be used for configuration files
