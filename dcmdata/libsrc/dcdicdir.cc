DCMTK/dcmtk

commit d084ebf4c0891d1d0d1b648a30e2ee703f1c28bb

Added support for wide chars to DICOMDIR code.
Added support for the Windows-specific wide character encoding (UTF-16) to
the classes and the command line tool that generate a DICOMDIR file.

dcmdata/libsrc/dcdicdir.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2012, OFFIS e.V.
+ *  Copyright (C) 1994-2014, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -66,15 +66,14 @@
 
 DcmDicomDir::DcmDicomDir()
   : errorFlag(EC_Normal),
-    dicomDirFileName(NULL),
+    dicomDirFileName(),
     modified(OFFalse),
     mustCreateNewDir(OFFalse),
     DirFile(new DcmFileFormat()),
     RootRec(NULL),
     MRDRSeq(NULL)
 {
-    dicomDirFileName = new char[ strlen( DEFAULT_DICOMDIR_NAME ) + 1 ];
-    strcpy( dicomDirFileName, DEFAULT_DICOMDIR_NAME );
+    dicomDirFileName.set(DEFAULT_DICOMDIR_NAME);
 
     OFCondition cond = DirFile->loadFile(dicomDirFileName);
     if (cond.bad())
@@ -96,19 +95,19 @@ DcmDicomDir::DcmDicomDir()
 // ********************************
 
 
-DcmDicomDir::DcmDicomDir(const char *fileName, const char *fileSetID)
+DcmDicomDir::DcmDicomDir(const OFFilename &fileName, const char *fileSetID)
   : errorFlag(EC_Normal),
-    dicomDirFileName(NULL),
+    dicomDirFileName(),
     modified(OFFalse),
     mustCreateNewDir(OFFalse),
     DirFile(new DcmFileFormat()),
     RootRec(NULL),
     MRDRSeq(NULL)
 {
-    if ( fileName == NULL || *fileName == '\0' )
-        fileName = DEFAULT_DICOMDIR_NAME;
-    dicomDirFileName = new char[ strlen( fileName ) + 1 ];
-    strcpy( dicomDirFileName, fileName );
+    if ( fileName.isEmpty() )
+        dicomDirFileName.set(DEFAULT_DICOMDIR_NAME);
+    else
+        dicomDirFileName = fileName;
 
     OFCondition cond = DirFile->loadFile(dicomDirFileName);
     if (cond.bad())
@@ -134,15 +133,13 @@ DcmDicomDir::DcmDicomDir(const char *fileName, const char *fileSetID)
  */
 DcmDicomDir::DcmDicomDir( const DcmDicomDir & old )
   : errorFlag(old.errorFlag),
-    dicomDirFileName(NULL),
+    dicomDirFileName(old.dicomDirFileName),
     modified(old.modified),
     mustCreateNewDir(old.mustCreateNewDir),
     DirFile(new DcmFileFormat(*old.DirFile)),
     RootRec(new DcmDirectoryRecord(*old.RootRec)),
     MRDRSeq(new DcmSequenceOfItems(*old.MRDRSeq))
 {
-    dicomDirFileName = new char[ strlen( old.dicomDirFileName ) + 1 ];
-    strcpy( dicomDirFileName, old.dicomDirFileName );
 }
 
 
@@ -155,7 +152,6 @@ DcmDicomDir::~DcmDicomDir()
         write();
 
     delete DirFile;
-    delete[] dicomDirFileName;
     delete RootRec;
     delete MRDRSeq;
 }
@@ -834,7 +830,7 @@ DcmFileFormat& DcmDicomDir::getDirFileFormat()
 // ********************************
 
 
-const char* DcmDicomDir::getDirFileName()
+const OFFilename &DcmDicomDir::getDirFileName()
 {
     return dicomDirFileName;
 }
@@ -993,7 +989,7 @@ OFCondition DcmDicomDir::write(const E_TransferSyntax oxfer,
                                const E_EncodingType enctype,
                                const E_GrpLenEncoding glenc)
 {
-    if ( oxfer != DICOMDIR_DEFAULT_TRANSFERSYNTAX )
+    if (oxfer != DICOMDIR_DEFAULT_TRANSFERSYNTAX)
     {
         DCMDATA_ERROR("DcmDicomDir::write() Wrong TransferSyntax used, only LittleEndianExplicit allowed");
     }
@@ -1001,18 +997,19 @@ OFCondition DcmDicomDir::write(const E_TransferSyntax oxfer,
     E_TransferSyntax outxfer = DICOMDIR_DEFAULT_TRANSFERSYNTAX;
 
     /* find the path of the dicomdir to be created */
-    OFString tempfiledir;
+    OFFilename tempfiledir;
     OFStandard::getDirNameFromPath(tempfiledir, dicomDirFileName, OFFalse);
-    if (tempfiledir.empty())
+    if (tempfiledir.isEmpty())
     {
         /* use current directory to avoid file creation in "temp" */
-        tempfiledir = ".";
+        tempfiledir.set(".", OFTrue /*convert*/);
     }
 
     OFString tempfile;
     int tempfilefd;
+    /* tbd: unfortunately, we cannot pass an OFFilename parameter :( */
     OFCondition status = OFTempFile::createFile(tempfile, &tempfilefd, O_RDWR | O_BINARY,
-            tempfiledir, TEMPNAME_TEMPLATE_PREFIX, "");
+        tempfiledir.getCharPointer(), TEMPNAME_TEMPLATE_PREFIX, "");
     if (status.bad())
         return status;
 
@@ -1038,14 +1035,14 @@ OFCondition DcmDicomDir::write(const E_TransferSyntax oxfer,
 
     DcmDataset &dset = getDataset(); // guaranteed to exist
     DcmMetaInfo &metainfo = *(getDirFileFormat().getMetaInfo());
-    DcmSequenceOfItems &localDirRecSeq = getDirRecSeq( dset );
-    DcmTag unresSeqTag( DCM_DirectoryRecordSequence );
-    DcmSequenceOfItems localUnresRecs( unresSeqTag );
+    DcmSequenceOfItems &localDirRecSeq = getDirRecSeq(dset);
+    DcmTag unresSeqTag(DCM_DirectoryRecordSequence);
+    DcmSequenceOfItems localUnresRecs(unresSeqTag);
 
     // insert Media Stored SOP Class UID
-    insertMediaSOPUID( metainfo );
+    insertMediaSOPUID(metainfo);
 
-    getDirFileFormat().validateMetaInfo( outxfer );
+    getDirFileFormat().validateMetaInfo(outxfer);
 
     {
         // it is important that the cache object is destroyed before the file is renamed!
@@ -1070,68 +1067,58 @@ OFCondition DcmDicomDir::write(const E_TransferSyntax oxfer,
     // outStream is closed here
     delete outStream;
 
-    char* backupname = NULL;
-    if ( !mustCreateNewDir )
+    OFFilename backupname;
+    if (!mustCreateNewDir)
     {
 #ifndef DICOMDIR_WITHOUT_BACKUP
-        backupname = new char[ 1 + strlen( dicomDirFileName ) + strlen( DICOMDIR_BACKUP_SUFFIX ) ];
-        strcpy( backupname, dicomDirFileName );
-
-#ifndef HAVE_LONG_FILE_NAMES
-        char *suffix = strrchr( backupname, '.' );
-        if ( suffix )
-            *suffix = '\0';
-#endif
-
-        strcat( backupname, DICOMDIR_BACKUP_SUFFIX );
-        unlink( backupname );
+        OFStandard::appendFilenameExtension(backupname, dicomDirFileName, DICOMDIR_BACKUP_SUFFIX);
+        OFStandard::deleteFile(backupname);
         if (errorFlag == EC_Normal)
         {
-            if (rename(dicomDirFileName, backupname) != 0)
+            if (!OFStandard::renameFile(dicomDirFileName, backupname))
             {
-              char buf[256];
-              const char *text = OFStandard::strerror(errno, buf, sizeof(buf));
-              if (text == NULL) text = "(unknown error code)";
-              errorFlag = makeOFCondition(OFM_dcmdata, 19, OF_error, text);
+                char buf[256];
+                const char *text = OFStandard::strerror(errno, buf, sizeof(buf));
+                if (text == NULL) text = "(unknown error code)";
+                errorFlag = makeOFCondition(OFM_dcmdata, 19, OF_error, text);
             }
         }
 #else
-        if ( unlink( dicomDirFileName ) != 0 )
+        if (!OFStandard::deleteFile(dicomDirFileName))
         {
-          char buf[256];
-          const char *text = OFStandard::strerror(errno, buf, sizeof(buf));
-          if (text == NULL) text = "(unknown error code)";
-          errorFlag = makeOFCondition(OFM_dcmdata, 19, OF_error, text);
+            char buf[256];
+            const char *text = OFStandard::strerror(errno, buf, sizeof(buf));
+            if (text == NULL) text = "(unknown error code)";
+            errorFlag = makeOFCondition(OFM_dcmdata, 19, OF_error, text);
         }
 #endif
     }
 
-    if (errorFlag == EC_Normal && rename( tempfile.c_str(), dicomDirFileName ) != 0)
+    if (errorFlag == EC_Normal && !OFStandard::renameFile(tempfile, dicomDirFileName))
     {
-      char buf[256];
-      const char *text = OFStandard::strerror(errno, buf, sizeof(buf));
-      if (text == NULL) text = "(unknown error code)";
-      errorFlag = makeOFCondition(OFM_dcmdata, 19, OF_error, text);
+        char buf[256];
+        const char *text = OFStandard::strerror(errno, buf, sizeof(buf));
+        if (text == NULL) text = "(unknown error code)";
+        errorFlag = makeOFCondition(OFM_dcmdata, 19, OF_error, text);
     }
 
     modified = OFFalse;
 
-    if (errorFlag == EC_Normal && backupname != NULL) {
+    if (errorFlag == EC_Normal) {
         /* remove backup */
-        unlink(backupname);
-        delete[] backupname;
+        OFStandard::deleteFile(backupname);
     }
 
     // remove all records from sequence localDirRecSeq
-    while ( localDirRecSeq.card() > 0 )
+    while (localDirRecSeq.card() > 0)
         localDirRecSeq.remove(OFstatic_cast(unsigned long, 0));
 
     // move records to which no pointer exists back
-    while ( localUnresRecs.card() > 0 )
+    while (localUnresRecs.card() > 0)
     {
         DcmItem *unresRecord =
             localUnresRecs.remove(OFstatic_cast(unsigned long, 0));
-        localDirRecSeq.insert( unresRecord );
+        localDirRecSeq.insert(unresRecord);
     }
     return errorFlag;
 }

dcmdata/libsrc/dcdirrec.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2013, OFFIS e.V.
+ *  Copyright (C) 1994-2014, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -143,10 +143,10 @@ DcmDirectoryRecord::DcmDirectoryRecord(const DcmTag &tag,
 
 DcmDirectoryRecord::DcmDirectoryRecord(const E_DirRecType recordType,
                                        const char *referencedFileID,
-                                       const char *sourceFilename,
+                                       const OFFilename &sourceFilename,
                                        DcmFileFormat *fileFormat)
   : DcmItem(ItemTag),
-    recordsOriginFile(NULL),
+    recordsOriginFile(),
     lowerLevelList(new DcmSequenceOfItems(DCM_DirectoryRecordSequence)),
     DirRecordType(recordType),
     referencedMRDR(NULL),
@@ -165,10 +165,10 @@ DcmDirectoryRecord::DcmDirectoryRecord(const E_DirRecType recordType,
 
 DcmDirectoryRecord::DcmDirectoryRecord(const char *recordTypeName,
                                        const char *referencedFileID,
-                                       const char *sourceFilename,
+                                       const OFFilename &sourceFilename,
                                        DcmFileFormat *fileFormat)
   : DcmItem(ItemTag),
-    recordsOriginFile(NULL),
+    recordsOriginFile(),
     lowerLevelList(new DcmSequenceOfItems(DCM_DirectoryRecordSequence)),
     DirRecordType(ERT_Private),
     referencedMRDR(NULL),
@@ -191,7 +191,7 @@ DcmDirectoryRecord::DcmDirectoryRecord(const DcmDirectoryRecord &old)
     recordsOriginFile(old.recordsOriginFile),
     lowerLevelList(new DcmSequenceOfItems(*old.lowerLevelList)),
     DirRecordType(old.DirRecordType),
-    referencedMRDR(old.referencedMRDR),
+    referencedMRDR(old.referencedMRDR),  // copies a pointer!
     numberOfReferences(old.numberOfReferences),
     offsetInFile(old.offsetInFile)
 {
@@ -203,19 +203,19 @@ DcmDirectoryRecord::DcmDirectoryRecord(const DcmDirectoryRecord &old)
 
 DcmDirectoryRecord& DcmDirectoryRecord::operator=(const DcmDirectoryRecord& obj)
 {
-  if (this != &obj)
-  {
-    // copy parent's member variables
-    DcmItem::operator=(obj);
-    // copy DcmDirectoryRecords' member variables
-    recordsOriginFile = obj.recordsOriginFile;
-    lowerLevelList = new DcmSequenceOfItems(*obj.lowerLevelList);
-    DirRecordType = obj.DirRecordType;
-    referencedMRDR = obj.referencedMRDR;
-    numberOfReferences = obj.numberOfReferences;
-    offsetInFile = obj.offsetInFile;
-  }
-  return *this;
+    if (this != &obj)
+    {
+        // copy parent's member variables
+        DcmItem::operator=(obj);
+        // copy DcmDirectoryRecords' member variables
+        recordsOriginFile = obj.recordsOriginFile;
+        lowerLevelList = new DcmSequenceOfItems(*obj.lowerLevelList);
+        DirRecordType = obj.DirRecordType;
+        referencedMRDR = obj.referencedMRDR;  // copies a pointer!
+        numberOfReferences = obj.numberOfReferences;
+        offsetInFile = obj.offsetInFile;
+    }
+    return *this;
 }
 
 
@@ -224,12 +224,12 @@ DcmDirectoryRecord& DcmDirectoryRecord::operator=(const DcmDirectoryRecord& obj)
 
 OFCondition DcmDirectoryRecord::copyFrom(const DcmObject& rhs)
 {
-  if (this != &rhs)
-  {
-    if (rhs.ident() != ident()) return EC_IllegalCall;
-    *this = OFstatic_cast(const DcmDirectoryRecord &, rhs);
-  }
-  return EC_Normal;
+    if (this != &rhs)
+    {
+        if (rhs.ident() != ident()) return EC_IllegalCall;
+        *this = OFstatic_cast(const DcmDirectoryRecord &, rhs);
+    }
+    return EC_Normal;
 }
 
 
@@ -239,7 +239,6 @@ OFCondition DcmDirectoryRecord::copyFrom(const DcmObject& rhs)
 DcmDirectoryRecord::~DcmDirectoryRecord()
 {
     delete lowerLevelList;
-    delete[] recordsOriginFile;
 }
 
 
@@ -258,7 +257,7 @@ E_DirRecType DcmDirectoryRecord::recordNameToType(const char *recordTypeName)
         if (i < DIM_OF_DRTypeNames && strcmp(DRTypeNames[i], recordTypeName) == 0)
             recType = OFstatic_cast(E_DirRecType, i);
         else if (strcmp(recordTypeName,"STRUCT REPORT") == 0)
-            recType = ERT_SRDocument; // we recognise the old name as well
+            recType = ERT_SRDocument; // we recognize the old name as well
         DCMDATA_TRACE("DcmDirectoryRecord::recordNameToType() input char*=\"" << recordTypeName
             << "\" output enum=" << recType);
     }
@@ -306,7 +305,7 @@ char *DcmDirectoryRecord::buildFileName(const char *origName,
             fclose(f);
             strcpy(destName, newname);
         } else {
-            /* we cannot find the file.  let the caller deal with this */
+            /* we cannot find the file. let the caller deal with this */
         }
         delete[] newname;
     }
@@ -595,7 +594,7 @@ static void hostToDicomFilename(char *fname)
 {
     /*
     ** Massage filename into dicom format.
-    ** Elmiminate any invalid characters.
+    ** Eliminate any invalid characters.
     ** Most commonly there is a '.' at the end of a filename.
     */
     int len = strlen(fname);
@@ -735,7 +734,7 @@ OFCondition DcmDirectoryRecord::setRecordInUseFlag(const Uint16 newFlag)
 
 Uint16 DcmDirectoryRecord::lookForRecordInUseFlag()
 {
-    Uint16 localFlag = Uint16(0xffff);     // default value: Record is in use
+    Uint16 localFlag = Uint16(0xffff);     // default value: record is in use
     if (!elementList->empty())
     {
         DcmStack stack;
@@ -877,16 +876,16 @@ Uint32 DcmDirectoryRecord::decreaseRefNum()
 //
 
 OFCondition DcmDirectoryRecord::fillElementsAndReadSOP(const char *referencedFileID,
-                                                       const char *sourceFileName,
+                                                       const OFFilename &sourceFileName,
                                                        DcmFileFormat *fileFormat)
 {
     OFCondition l_error = EC_Normal;
-    char *fileName = NULL;
+    OFFilename fileName;
     DcmFileFormat *refFile = NULL;
     /* This variable is only set if we created our own DcmFileFormat instance */
     DcmFileFormat *ownFile = NULL;
 
-    if (fileFormat != NULL && sourceFileName == NULL)
+    if (fileFormat != NULL && sourceFileName.isEmpty())
         return EC_IllegalCall;
 
     OFBool directFromFile = OFFalse;
@@ -901,13 +900,16 @@ OFCondition DcmDirectoryRecord::fillElementsAndReadSOP(const char *referencedFil
 
     if (referencedFileID != NULL && *referencedFileID != '\0')
     {
-        if (!sourceFileName)
+        if (sourceFileName.isEmpty())
         {
-            fileName = new char[strlen(referencedFileID) + 2];
-            buildFileName(referencedFileID, fileName);
+            /* create a new source filename */
+            char *newname = new char[strlen(referencedFileID) + 2];
+            buildFileName(referencedFileID, newname);
+            fileName.set(newname);
+            delete[] newname;
         } else {
-            fileName = new char[strlen(sourceFileName)+1];
-            strcpy(fileName, sourceFileName);
+            /* just copy the source filename */
+            fileName = sourceFileName;
         }
 
         if (DirRecordType != ERT_Mrdr)
@@ -1041,7 +1043,6 @@ OFCondition DcmDirectoryRecord::fillElementsAndReadSOP(const char *referencedFil
     }
 
     delete ownFile;
-    delete[] fileName;
 
     return l_error;
 }
@@ -1336,7 +1337,7 @@ OFCondition DcmDirectoryRecord::verify(const OFBool autocorrect)
     OFCondition err2 = EC_Normal;
     errorFlag = EC_Normal;
     if (autocorrect == OFTrue && DirRecordType != ERT_root)
-        errorFlag = fillElementsAndReadSOP(getReferencedFileName(), NULL);
+        errorFlag = fillElementsAndReadSOP(getReferencedFileName(), "");
 
     err1 = DcmItem::verify(autocorrect);
     err2 = lowerLevelList->verify(autocorrect);
@@ -1389,7 +1390,7 @@ DcmDirectoryRecord* DcmDirectoryRecord::getReferencedMRDR()
 
 
 OFCondition DcmDirectoryRecord::assignToSOPFile(const char *referencedFileID,
-                                                const char *sourceFileName)
+                                                const OFFilename &sourceFileName)
 {
     errorFlag = EC_Normal;
 
@@ -1435,7 +1436,7 @@ OFCondition DcmDirectoryRecord::assignToMRDR(DcmDirectoryRecord *mrdr)
         referencedMRDR->increaseRefNum();
 
         // set length of Referenced File ID to zero and fill data elements
-        errorFlag = fillElementsAndReadSOP(NULL, NULL);
+        errorFlag = fillElementsAndReadSOP(NULL, "");
     } else
         errorFlag = EC_IllegalCall;
 
@@ -1601,19 +1602,12 @@ OFCondition DcmDirectoryRecord::clearSub()
 }
 
 
-void DcmDirectoryRecord::setRecordsOriginFile(const char *fname)
+void DcmDirectoryRecord::setRecordsOriginFile(const OFFilename &fname)
 {
-    if (recordsOriginFile != NULL)
-        delete[] recordsOriginFile;
-    if (fname != NULL)
-    {
-        recordsOriginFile = new char[strlen(fname) + 1];
-        strcpy(recordsOriginFile, fname);
-    } else
-        recordsOriginFile = NULL;
+    recordsOriginFile = fname;
 }
 
-const char* DcmDirectoryRecord::getRecordsOriginFile()
+const OFFilename &DcmDirectoryRecord::getRecordsOriginFile()
 {
     return recordsOriginFile;
 }

dcmdata/apps/dcmgpdir.cc
@@ -96,7 +96,8 @@ static OFLogger dcmgpdirLogger = OFLog::getLogger("dcmtk.dcmdata." OFFIS_CONSOLE
 
 // ********************************************
 
-int main(int argc, char *argv[])
+// this macro either expands to main() or wmain()
+DCMTK_MAIN_FUNCTION
 {
     OFBool opt_write = OFTrue;
     OFBool opt_append = OFFalse;
@@ -104,12 +105,12 @@ int main(int argc, char *argv[])
     OFBool opt_recurse = OFFalse;
     E_EncodingType opt_enctype = EET_ExplicitLength;
     E_GrpLenEncoding opt_glenc = EGL_withoutGL;
-    const char *opt_output = DEFAULT_DICOMDIR_NAME;
+    OFFilename opt_output(DEFAULT_DICOMDIR_NAME);
     const char *opt_fileset = DEFAULT_FILESETID;
     const char *opt_descriptor = NULL;
     const char *opt_charset = DEFAULT_DESCRIPTOR_CHARSET;
-    const char *opt_directory = "";
-    const char *opt_pattern = "";
+    OFFilename opt_directory;
+    OFFilename opt_pattern;
     DicomDirInterface::E_ApplicationProfile opt_profile = DicomDirInterface::AP_GeneralPurpose;
 
 #ifdef BUILD_DCMGPDIR_AS_DCMMKDIR
@@ -290,8 +291,8 @@ int main(int argc, char *argv[])
         if (cmd.findOption("--recurse"))
         {
             opt_recurse = OFTrue;
-            if ((opt_directory == NULL) || (strlen(opt_directory) == 0))
-                opt_directory = ".";
+            if (opt_directory.isEmpty())
+                opt_directory.set(".", OFTrue /*convert*/);
         }
         cmd.endOptionBlock();
 
@@ -343,13 +344,13 @@ int main(int argc, char *argv[])
         }
         if (cmd.findOption("--icon-file-prefix"))
         {
-            const char *iconPrefix = NULL;
+            OFFilename iconPrefix;
             app.checkValue(cmd.getValue(iconPrefix));
             ddir.setIconPrefix(iconPrefix);
         }
         if (cmd.findOption("--default-icon"))
         {
-            const char *defaultIcon = NULL;
+            OFFilename defaultIcon;
             app.checkValue(cmd.getValue(defaultIcon));
             ddir.setDefaultIcon(defaultIcon);
         }
@@ -502,7 +503,7 @@ int main(int argc, char *argv[])
     }
 
     /* make sure input directory exists (if specified) */
-    if ((opt_directory != NULL) && (strlen(opt_directory) > 0))
+    if (!opt_directory.isEmpty())
     {
         if (!OFStandard::dirExists(opt_directory))
         {
@@ -512,9 +513,9 @@ int main(int argc, char *argv[])
     }
 
     /* create list of input files */
-    OFList<OFString> fileNames;
-    OFString pathname;
-    const char *param = NULL;
+    OFFilename paramValue;
+    OFFilename pathName;
+    OFList<OFFilename> fileNames;
     const int count = cmd.getParamCount();
     if (opt_recurse)
         OFLOG_INFO(dcmgpdirLogger, "determining input files ...");
@@ -531,18 +532,18 @@ int main(int argc, char *argv[])
         /* iterate over all input filenames */
         for (int i = 1; i <= count; i++)
         {
-            cmd.getParam(i, param);
+            cmd.getParam(i, paramValue);
             /* add input directory */
-            OFStandard::combineDirAndFilename(pathname, opt_directory, param, OFTrue /*allowEmptyDirName*/);
+            OFStandard::combineDirAndFilename(pathName, opt_directory, paramValue, OFTrue /*allowEmptyDirName*/);
             /* search directory recursively (if required) */
-            if (OFStandard::dirExists(pathname))
+            if (OFStandard::dirExists(pathName))
             {
                 if (opt_recurse)
-                    OFStandard::searchDirectoryRecursively(param, fileNames, opt_pattern, opt_directory);
+                    OFStandard::searchDirectoryRecursively(paramValue, fileNames, opt_pattern, opt_directory);
                 else
-                    OFLOG_WARN(dcmgpdirLogger, "ignoring directory because option --recurse is not set: " << param);
+                    OFLOG_WARN(dcmgpdirLogger, "ignoring directory because option --recurse is not set: " << paramValue);
             } else
-                fileNames.push_back(param);
+                fileNames.push_back(paramValue);
         }
     }
     /* check whether there are any input files */
@@ -573,15 +574,15 @@ int main(int argc, char *argv[])
         if (result.good())
         {
             /* collect 'bad' files */
-            OFList<OFString> badFiles;
+            OFList<OFFilename> badFiles;
             unsigned int goodFiles = 0;
-            OFListIterator(OFString) iter = fileNames.begin();
-            OFListIterator(OFString) last = fileNames.end();
+            OFListIterator(OFFilename) iter = fileNames.begin();
+            OFListIterator(OFFilename) last = fileNames.end();
             /* iterate over all input filenames */
             while ((iter != last) && result.good())
             {
                 /* add files to the DICOMDIR */
-                result = ddir.addDicomFile((*iter).c_str(), opt_directory);
+                result = ddir.addDicomFile((*iter), opt_directory);
                 if (result.bad())
                 {
                     badFiles.push_back(*iter);

dcmdata/include/dcmtk/dcmdata/dcdicdir.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2011, OFFIS e.V.
+ *  Copyright (C) 1994-2014, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -56,11 +56,11 @@ public:
     DcmDicomDir();
 
     /** constructor
-     *  @param fileName filename to read a DICOMDIR from. If NULL, an attempt is
-     *  made to read file DEFAULT_DICOMDIR_NAME ("DICOMDIR").
+     *  @param fileName filename to read a DICOMDIR from. If empty, an attempt is
+     *    made to read file DEFAULT_DICOMDIR_NAME ("DICOMDIR").
      *  @param fileSetID file set ID of this file set, used only for new DICOMDIRs
      */
-    DcmDicomDir( const char *fileName,
+    DcmDicomDir( const OFFilename &fileName,
                  const char *fileSetID = NULL );  // only used for new DICOMDIR
 
     /// destructor. If DICOMDIR was modified, writes new file.
@@ -91,7 +91,7 @@ public:
     /** returns file name from which DICOMDIR was read.
      *  @return filename of DICOMDIR
      */
-    virtual const char* getDirFileName();
+    virtual const OFFilename &getDirFileName();
 
     /// returns root directory record
     virtual DcmDirectoryRecord& getRootRecord();
@@ -196,7 +196,7 @@ public:
     OFCondition errorFlag;
 
     /// file name the DICOMDIR was read from, or DEFAULT_DICOMDIR_NAME
-    char * dicomDirFileName;
+    OFFilename dicomDirFileName;
 
     /** flag indicating whether or not this DICOMDIR has been modified after being read from file.
      *  If true, the destructor of this class will write the modified DICOMDIR back to file

commit 57635405955618ca8edf7f99e447bc76627b8098

Fixed issue when compiled with wide char support.
The initialization of an OFFilename instance with NULL is ambiguous when
compiled with WIDE_CHAR_FILE_IO_FUNCTIONS or WIDE_CHAR_MAIN_FUNCTION defined.
This issue was introduced with commit d084ebf, i.e. the previous one.

dcmdata/libsrc/dcdicdir.cc
@@ -84,7 +84,7 @@ DcmDicomDir::DcmDicomDir()
     }
 
     createNewElements( "" );      // create missing data elements
-    RootRec = new DcmDirectoryRecord( ERT_root, NULL, NULL);
+    RootRec = new DcmDirectoryRecord( ERT_root, NULL, OFFilename());
     DcmTag mrdrSeqTag( DCM_DirectoryRecordSequence );
     MRDRSeq = new DcmSequenceOfItems( mrdrSeqTag );
 
@@ -118,7 +118,7 @@ DcmDicomDir::DcmDicomDir(const OFFilename &fileName, const char *fileSetID)
     }
 
     createNewElements( fileSetID );   // create missing data elements
-    RootRec = new DcmDirectoryRecord( ERT_root, NULL, NULL);
+    RootRec = new DcmDirectoryRecord( ERT_root, NULL, OFFilename());
     DcmTag mrdrSeqTag( DCM_DirectoryRecordSequence );
     MRDRSeq = new DcmSequenceOfItems( mrdrSeqTag );
 
@@ -956,7 +956,7 @@ DcmDirectoryRecord* DcmDicomDir::matchOrCreateMRDR( const char *filename )
             newMRDR = matchRec;
         else if ( matchRec->getRecordType() != ERT_root )
         {
-            newMRDR = new DcmDirectoryRecord( ERT_Mrdr, filename, NULL);
+            newMRDR = new DcmDirectoryRecord( ERT_Mrdr, filename, OFFilename());
             if ( matchRec->assignToMRDR( newMRDR ) != EC_IllegalCall )
                 getMRDRSequence().insert( newMRDR );
             else

dcmdata/libsrc/dcdirrec.cc
@@ -112,7 +112,7 @@ static const short DIM_OF_DRTypeNames = (sizeof(DRTypeNames) / sizeof(DRTypeName
 
 DcmDirectoryRecord::DcmDirectoryRecord()
   : DcmItem(ItemTag),
-    recordsOriginFile(NULL),
+    recordsOriginFile(),
     lowerLevelList(new DcmSequenceOfItems(DCM_DirectoryRecordSequence)),
     DirRecordType(ERT_Private),
     referencedMRDR(NULL),
@@ -128,7 +128,7 @@ DcmDirectoryRecord::DcmDirectoryRecord()
 DcmDirectoryRecord::DcmDirectoryRecord(const DcmTag &tag,
                                        const Uint32 len)
   : DcmItem(tag, len),
-    recordsOriginFile(NULL),
+    recordsOriginFile(),
     lowerLevelList(new DcmSequenceOfItems(DCM_DirectoryRecordSequence)),
     DirRecordType(ERT_Private),
     referencedMRDR(NULL),