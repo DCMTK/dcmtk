d51f3a456bd104b2e19b72abb78258a97d43acec
Jan Schlamelcher <dicom@offis.de>
2014-02-27 15:57:26 +0100

    Fixed dll build on windows.
    Created a workaround for Visual Studio that disables dll storage specifiers
    when linking directly into the executable. This is needed since the
    built-in dictionary code is created by the tools mkdeftag and mkdictbi and
    thus those tools statically link the few required dcmdata source files
    instead of linking to dcmdata as a whole.

--- dcmdata/include/dcmtk/dcmdata/dcdefine.h
+++ dcmdata/include/dcmtk/dcmdata/dcdefine.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2011, OFFIS e.V.
+ *  Copyright (C) 2014, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -27,11 +27,17 @@
 
 #include "dcmtk/ofstd/ofdefine.h"
 
-
+// Workaround for Visual Studio to disable dll storage specifiers when linking directly into the executable.
+// This is needed since the built-in dictionary code is created by the tools mkdeftag and mkdictbi and thus
+// those tools statically link the few required dcmdata source files instead of linking to dcmdata as a whole.
+#ifndef DCMDATA_STATIC
 #ifdef dcmdata_EXPORTS
 #define DCMTK_DCMDATA_EXPORT DCMTK_DECL_EXPORT
 #else
 #define DCMTK_DCMDATA_EXPORT DCMTK_DECL_IMPORT
+#endif
+#else
+#define DCMTK_DCMDATA_EXPORT
 #endif
 
 
