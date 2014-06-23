95b63476a9795cf48ebf9ab3a1ababe767e3402d
Joerg Riesmeier <dicom@jriesmeier.com>
2014-06-23 12:17:32 +0200

    Fixed incomplete #include comment.

--- dcmnet/libsrc/dimse.cc
+++ dcmnet/libsrc/dimse.cc
@@ -114,7 +114,7 @@
 #include "dcmtk/dcmdata/dcvrul.h"      /* for class DcmUnsignedLong */
 #include "dcmtk/dcmdata/dcvrobow.h"    /* for class DcmOtherByteOtherWord */
 #include "dcmtk/dcmdata/dcvrsh.h"      /* for class DcmShortString */
-#include "dcmtk/dcmdata/dcvrae.h"      /* for class Dcm */
+#include "dcmtk/dcmdata/dcvrae.h"      /* for class DcmApplicationEntity */
 #include "dcmtk/dcmdata/dcdicent.h"    /* for DcmDictEntry, needed for MSVC5 */
 #include "dcmtk/dcmdata/dcwcache.h"    /* for class DcmWriteCache */
 #include "dcmtk/dcmdata/dcvrui.h"      /* for class DcmUniqueIdentifier */
