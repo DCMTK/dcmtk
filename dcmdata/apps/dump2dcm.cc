DCMTK/dcmtk

commit 093768ab7bcceaafc788fc3436eeb97fdb910435

Fixed most of the warnings in dcmdata.
Compiled with extensive warning options except -Wtraditional and corrected
most of the problems. Most of the remaining warnings in dcmdata are caused by
files included from ofstd and will be resolved in a different commit.

dcmdata/apps/dump2dcm.cc
@@ -157,12 +157,8 @@ stripWhitespace(char *s)
 static char *
 stripTrailingWhitespace(char *s)
 {
-    int i, n;
     if (s == NULL) return s;
-
-    n = strlen(s);
-    for (i = n - 1; i >= 0 && isspace(TO_UCHAR(s[i])); i--)
-        s[i] = '\0';
+    for (size_t i = strlen(s); i > 0 && isspace(TO_UCHAR(s[--i])); s[i] = '\0');
     return s;
 }
 
@@ -182,13 +178,9 @@ stripPrecedingWhitespace(char *s)
 static OFBool
 onlyWhitespace(const char *s)
 {
-    int len = strlen(s);
-    int charsFound = OFFalse;
-
-    for (int i = 0; (!charsFound) && (i < len); i++) {
-        charsFound = !isspace(TO_UCHAR(s[i]));
-    }
-    return (!charsFound) ? OFTrue : OFFalse;
+    while(*s) if (!isspace(TO_UCHAR(*s++)))
+        return OFFalse;
+    return OFTrue;
 }
 
 static char *
@@ -216,14 +208,9 @@ getLine(char *line, int maxLineLen, FILE *f, const unsigned long lineNumber)
 static OFBool
 isaCommentLine(const char *s)
 {
-    OFBool isComment = OFFalse; /* assumption */
-    int len = strlen(s);
-    int i = 0;
     // skip leading spaces
-    for (i = 0; i < len && isspace(TO_UCHAR(s[i])); i++) /* loop with empty body */;
-    // check for comment character
-    isComment = (s[i] == DCM_DumpCommentChar);
-    return isComment;
+    while(isspace(TO_UCHAR(*s))) ++s;
+    return *s == DCM_DumpCommentChar;
 }
 
 static OFBool
@@ -238,14 +225,14 @@ parseTag(char *&s, DcmTagKey &key)
     if (p)
     {
         // string all white spaces and read tag
-        int len = p - s + 1;
+        size_t len = p - s + 1;
         p = new char[len + 1];
         OFStandard::strlcpy(p, s, len + 1);
         stripWhitespace(p);
         s += len;
 
         if (sscanf(p, "(%x,%x)", &g, &e) == 2)
-            key.set(g, e);
+            key.set(OFstatic_cast(Uint16, g), OFstatic_cast(Uint16, e));
         else
             ok = OFFalse;
         delete[] p;
@@ -294,7 +281,7 @@ parseVR(char *&s, DcmEVR &vr)
 #undef TO_UCHAR
 
 
-static int
+static size_t
 searchLastClose(char *s, const char closeChar)
 {
     // search last close bracket in a line
@@ -320,7 +307,7 @@ searchLastClose(char *s, const char closeChar)
 }
 
 
-static int
+static size_t
 searchCommentOrEol(char *s)
 {
     char *comment = strchr(s, DCM_DumpCommentChar);
@@ -337,19 +324,11 @@ static char *
 convertNewlineCharacters(char *s)
 {
     // convert the string "\n" into the \r\n combination required by DICOM
-    if (s == NULL || s[0] == '\0') return s;
-    int len = strlen(s);
-    int i = 0;
-    for (i = 0; i < (len - 1); i++)
+    if (s) for (; *s; ++s) if (*s == '\\' && *(s+1) == 'n')
     {
-        if (s[i] == '\\' && s[i + 1] == 'n')
-        {
-            s[i] = '\r';
-            s[i + 1] = '\n';
-            i++;
-        }
+        *s = '\r';
+        *++s = '\n';
     }
-
     return s;
 }
 
@@ -357,7 +336,7 @@ static OFBool
 parseValue(char *&s, char *&value, DcmEVR &vr, const DcmTagKey &tagkey)
 {
     OFBool ok = OFTrue;
-    int len;
+    size_t len;
     value = NULL;
 
     s = stripPrecedingWhitespace(s);
@@ -456,7 +435,7 @@ putFileContentsIntoElement(DcmElement *elem, const char *filename)
     }
 
     const size_t len = OFStandard::getFileSize(filename);
-    unsigned long buflen = len;
+    size_t buflen = len;
     if (buflen & 1)
         buflen++; /* if odd then make even (DICOM requires even length values) */
 
@@ -464,18 +443,18 @@ putFileContentsIntoElement(DcmElement *elem, const char *filename)
     const DcmEVR evr = elem->getVR();
     /* create buffer of OB or OW data */
     if (evr == EVR_OB || evr == EVR_pixelItem)
-        ec = elem->createUint8Array(buflen, buf);
+        ec = elem->createUint8Array(OFstatic_cast(Uint32, buflen), buf);
     else if (evr == EVR_OW)
     {
         Uint16 *buf16 = NULL;
-        ec = elem->createUint16Array(buflen / 2, buf16);
+        ec = elem->createUint16Array(OFstatic_cast(Uint32, buflen / 2), buf16);
         buf = OFreinterpret_cast(Uint8 *, buf16);
     } else
         ec = EC_IllegalCall;
     if (ec.good())
     {
         /* read binary file into the buffer */
-        if (fread(buf, 1, OFstatic_cast(size_t, len), f) != len)
+        if (fread(buf, 1, len, f) != len)
         {
             char errBuf[256];
             OFLOG_ERROR(dump2dcmLogger, "error reading binary data file: " << filename
@@ -485,7 +464,7 @@ putFileContentsIntoElement(DcmElement *elem, const char *filename)
         else if (evr == EVR_OW)
         {
             /* swap 16 bit OW data (if necessary) */
-            swapIfNecessary(gLocalByteOrder, opt_fileContentsByteOrdering, buf, buflen, sizeof(Uint16));
+            swapIfNecessary(gLocalByteOrder, opt_fileContentsByteOrdering, buf, OFstatic_cast(Uint32, buflen), sizeof(Uint16));
         }
     }
     else if (ec == EC_MemoryExhausted)

dcmdata/apps/dcm2xml.cc
@@ -474,7 +474,7 @@ int main(int argc, char *argv[])
     {
         /* read DICOM file or data set */
         DcmFileFormat dfile;
-        OFCondition status = dfile.loadFile(ifname, opt_ixfer, EGL_noChange, opt_maxReadLength, opt_readMode);
+        OFCondition status = dfile.loadFile(ifname, opt_ixfer, EGL_noChange, OFstatic_cast(Uint32, opt_maxReadLength), opt_readMode);
         if (status.good())
         {
             // map "old" charset names to DICOM defined terms

dcmdata/apps/dcmconv.cc
@@ -78,7 +78,7 @@ static DcmTagKey parseTagKey(const char *tagName)
     return tagKey;
   } else     /* tag name has format "gggg,eeee" */
   {
-    return DcmTagKey(group,elem);
+    return DcmTagKey(OFstatic_cast(Uint16, group),OFstatic_cast(Uint16, elem));
   }
 }
 

dcmdata/apps/dcmcrle.cc
@@ -263,7 +263,7 @@ int main(int argc, char *argv[])
 
     // register RLE compression codec
     DcmRLEEncoderRegistration::registerCodecs(opt_uidcreation,
-      opt_fragmentSize, opt_createOffsetTable, opt_secondarycapture);
+      OFstatic_cast(Uint32, opt_fragmentSize), opt_createOffsetTable, opt_secondarycapture);
 
     /* make sure data dictionary is loaded */
     if (!dcmDataDict.isDictionaryLoaded())

dcmdata/apps/dcmdump.cc
@@ -105,7 +105,7 @@ static DcmTagKey parseTagKey(const char *tagName)
         return tagKey;
     } else     /* tag name has format "gggg,eeee" */
     {
-        return DcmTagKey(group,elem);
+        return DcmTagKey(OFstatic_cast(Uint16, group),OFstatic_cast(Uint16, elem));
     }
 }
 
@@ -736,7 +736,7 @@ static int dumpFile(STD_NAMESPACE ostream &out,
     DcmFileFormat dfile;
     DcmObject *dset = &dfile;
     if (readMode == ERM_dataset) dset = dfile.getDataset();
-    OFCondition cond = dfile.loadFile(ifname, xfer, EGL_noChange, maxReadLength, readMode);
+    OFCondition cond = dfile.loadFile(ifname, xfer, EGL_noChange, OFstatic_cast(Uint32, maxReadLength), readMode);
     if (cond.bad())
     {
         OFLOG_ERROR(dcmdumpLogger, OFFIS_CONSOLE_APPLICATION << ": " << cond.text()
@@ -789,7 +789,7 @@ static int dumpFile(STD_NAMESPACE ostream &out,
             if (printTagKeys[i])
                 searchKey = *printTagKeys[i];
             else if (sscanf(tagName, "%x,%x", &group, &elem) == 2)
-                searchKey.set(group, elem);
+                searchKey.set(OFstatic_cast(Uint16, group), OFstatic_cast(Uint16, elem));
             else {
                 OFLOG_FATAL(dcmdumpLogger, "Internal ERROR in File " << __FILE__ << ", Line "
                     << __LINE__ << OFendl << " -- Named tag inconsistency");

dcmdata/apps/img2dcm.cc
@@ -386,7 +386,7 @@ static OFCondition startConversion(OFCommandLine& cmd,
   {
     OFLOG_INFO(img2dcmLogger, OFFIS_CONSOLE_APPLICATION ": Saving output DICOM to file " << outputFile);
     DcmFileFormat dcmff(resultObject);
-    cond = dcmff.saveFile(outputFile.c_str(), writeXfer, lengthEnc, grpLengthEnc, padEnc, filepad, itempad, writeMode);
+    cond = dcmff.saveFile(outputFile.c_str(), writeXfer, lengthEnc,  grpLengthEnc, padEnc, OFstatic_cast(Uint32, filepad), OFstatic_cast(Uint32, itempad), writeMode);
   }
 
   // Cleanup and return

dcmdata/apps/mdfdsman.cc
@@ -142,7 +142,7 @@ static int readNextToken(const char *c, int& pos, DcmTagKey& key, Uint32& idx)
     unsigned int group=0;
     unsigned int elem=0;
     if (2 != sscanf(c+spos,"%x,%x", &group, &elem)) return 0; // parse error
-    key = DcmTagKey(group,elem);
+    key = DcmTagKey(OFstatic_cast(Uint16, group),OFstatic_cast(Uint16, elem));
     pos = ++lpos;
     return 1; // tag key
   }
@@ -253,7 +253,7 @@ static OFCondition splitTagPath(OFString &tag_path,
 {
     OFString target_tag;
     unsigned int group,elem;
-    int lpos,rpos;
+    size_t lpos,rpos;
     rpos=tag_path.size()-1;
     lpos=rpos;
     if (tag_path[rpos]==')')
@@ -273,7 +273,7 @@ static OFCondition splitTagPath(OFString &tag_path,
         // parse target_tag into DcmTagKey
         if (2 != sscanf(target_tag.c_str(),"(%x,%x)", &group, &elem))
             return makeOFCondition(OFM_dcmdata,22,OF_error,"Invalid target tag!");
-        key = DcmTagKey(group,elem);
+        key = DcmTagKey(OFstatic_cast(Uint16, group),OFstatic_cast(Uint16, elem));
     }
     else
     // otherwise we could have a dictionary name
@@ -442,7 +442,7 @@ OFCondition MdfDatasetManager::modifyOrInsertFromFile(OFString tag_path,
           if (fileLen & 1)
               return makeOFCondition(OFM_dcmdata, 22, OF_error, "Cannot insert/modify value with odd length from file!");
           // read element value from binary file (requires even length)
-          result = elem->createValueFromTempFile(fileStream.newFactory(), fileLen, EBO_LittleEndian);
+          result = elem->createValueFromTempFile(fileStream.newFactory(), OFstatic_cast(Uint32, fileLen), EBO_LittleEndian);
       }
       if (result.bad()) return result;
       if (update_metaheader)

dcmdata/apps/pdf2dcm.cc
@@ -215,10 +215,10 @@ static OFCondition insertPDFFile(
     DcmPolymorphOBOW *elem = new DcmPolymorphOBOW(DCM_EncapsulatedDocument);
     if (elem)
     {
-      Uint32 numBytes = fileSize;
+      size_t numBytes = fileSize;
       if (numBytes & 1) ++numBytes;
       Uint8 *bytes = NULL;
-      result = elem->createUint8Array(numBytes, bytes);
+      result = elem->createUint8Array(OFstatic_cast(Uint32, numBytes), bytes);
       if (result.good())
       {
         // blank pad byte

commit 0a0f2ac110ba4adce4a5524bda462af14faa6c5d

Fixed a bug in convertNewlineCharacters().
Fixed an ugly bug in convertNewlineCharacters() that caused dump2dcm to crash.
This issue was introduced with commit 093768a, i.e. the previous one.

dcmdata/apps/dump2dcm.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2013, OFFIS e.V.
+ *  Copyright (C) 1994-2014, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -320,7 +320,7 @@ searchCommentOrEol(char *s)
 }
 
 
-static char *
+static void
 convertNewlineCharacters(char *s)
 {
     // convert the string "\n" into the \r\n combination required by DICOM
@@ -329,7 +329,6 @@ convertNewlineCharacters(char *s)
         *s = '\r';
         *++s = '\n';
     }
-    return s;
 }
 
 static OFBool
@@ -351,7 +350,7 @@ parseValue(char *&s, char *&value, DcmEVR &vr, const DcmTagKey &tagkey)
             {
                 value = new char[len - 1];
                 OFStandard::strlcpy(value, s + 1, len - 1);
-                value = convertNewlineCharacters(value);
+                convertNewlineCharacters(value);
             }
             else
                 value = NULL;