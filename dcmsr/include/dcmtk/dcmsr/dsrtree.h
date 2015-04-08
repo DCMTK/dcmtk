ed8b577012be1fe14b4e711fd2060342b4a2cea7
Joerg Riesmeier <dicom@jriesmeier.com>
2015-04-08 12:22:18 +0200

    Fixed wrong position counter in tree node.
    Fixed issue with position counter not being incremented correctly when adding
    a new tree node below the current one. This mainly showed up when checking
    the position string directly after adding the new node to the tree.

--- dcmsr/include/dcmtk/dcmsr/dsrtree.h
+++ dcmsr/include/dcmtk/dcmsr/dsrtree.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2000-2014, OFFIS e.V.
+ *  Copyright (C) 2000-2015, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -598,6 +598,7 @@ size_t DSRTree<T>::addNode(T *node,
                         }
                         tempNode->Next = node;
                         node->Prev = tempNode;
+                        ++this->Position;
                     } else
                         this->NodeCursor->Down = node;
                     break;
