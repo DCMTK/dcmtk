39402bb22234a56489e5f602ebf806b842139321
Joerg Riesmeier <dicom@jriesmeier.com>
2015-08-28 13:43:20 +0200

    Fixed various issues in test case.
    Fixed various issues in test case "dcmnet_dimseDump_nullByte", e.g. use the
    attribute Affected SOP Instance UID instead of Patient's Name for the UI
    element in the C-ECHO Request message. Also changed order of parameters in
    OFCHECK_EQUAL() call.

--- dcmnet/tests/tdump.cc
+++ dcmnet/tests/tdump.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2009-2012, OFFIS e.V.
+ *  Copyright (C) 2009-2015, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -30,12 +30,12 @@
 
 OFTEST(dcmnet_dimseDump_nullByte)
 {
-    const char expected[] = "===================== INCOMING DIMSE MESSAGE ====================\nMessage Type                  : C-ECHO RQ\nMessage ID                    : 42\nData Set                      : present\n-----------------------------------------------------------------(fffe,e000) na (Item with explicit length #=1)          #   0, 1 Item\n(0010,0010) PN [1.2\0]                                   #   4, 1 PatientName\n(fffe,e00d) na (ItemDelimitationItem for re-encoding)   #   0, 0 ItemDelimitationItem\n======================= END DIMSE MESSAGE =======================";
+    const char expected[] = "===================== INCOMING DIMSE MESSAGE ====================\nMessage Type                  : C-ECHO RQ\nMessage ID                    : 42\nData Set                      : present\n-----------------------------------------------------------------(fffe,e000) na (Item with explicit length #=1)          #   0, 1 Item\n(0000,1000) UI [1.2\0]                                   #   4, 1 AffectedSOPInstanceUID\n(fffe,e00d) na (ItemDelimitationItem for re-encoding)   #   0, 0 ItemDelimitationItem\n======================= END DIMSE MESSAGE =======================";
     OFString result;
     T_DIMSE_C_EchoRQ rq = { 42, "1.2.3\0", DIMSE_DATASET_PRESENT };
 
     dcmEnableAutomaticInputDataCorrection.set(OFFalse);
-    DcmUniqueIdentifier* id = new DcmUniqueIdentifier(DCM_PatientName);
+    DcmUniqueIdentifier* id = new DcmUniqueIdentifier(DCM_AffectedSOPInstanceUID);
     id->putString("1.2\0", 4);
     dcmEnableAutomaticInputDataCorrection.set(OFTrue);
 
@@ -46,5 +46,5 @@ OFTEST(dcmnet_dimseDump_nullByte)
     // There was a bug in DIMSE_dumpMessage() where the \0-byte caused the
     // message to be cut off. This is what we are testing for.
     DIMSE_dumpMessage(result, rq, DIMSE_INCOMING, &item, 0);
-    OFCHECK_EQUAL(OFString(expected, sizeof(expected)-1), result);
+    OFCHECK_EQUAL(result, OFString(expected, sizeof(expected) - 1));
 }
