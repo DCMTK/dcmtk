e0ebbbea5d9bf678852c7bae4696313b431d7acb
Joerg Riesmeier <dicom@jriesmeier.com>
2016-02-08 15:57:19 +0100

    Added comment on missing conditional attribute.
    Added comment that the conditional attribute Pixel Origin Interpretation
    (0048,0301) is not yet supported by the DSRSpatialCoordinatesValue class.
    This attribute is required if the referenced SOP instance is a VL Whole
    Slide Microscopy Image.

--- dcmsr/include/dcmtk/dcmsr/dsrscovl.h
+++ dcmsr/include/dcmtk/dcmsr/dsrscovl.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2000-2015, OFFIS e.V.
+ *  Copyright (C) 2000-2016, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -261,6 +261,8 @@ class DCMTK_DCMSR_EXPORT DSRSpatialCoordinatesValue
     DSRGraphicDataList      GraphicDataList;
     /// Fiducial UID (VR=UI, VM=1, type 3)
     OFString                FiducialUID;
+    /// Pixel Origin Interpretation (VR=CS, VM=1, type 1C)
+     // - tbd: conditional attribute not yet supported
 };
 
 
