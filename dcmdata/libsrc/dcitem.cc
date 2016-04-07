DCMTK/dcmtk

commit 4636d9c81ebbccda271eb8705ddbcd6b5d530aeb

Added a warning on invalid attribute tags.
Added a warning message on invalid DICOM elements in a dataset or item,
i.e. elements with an attribute tag of the group 0x0000 to 0x0003, 0x0005,
0x0007 or 0xFFFF. Now, for both file meta information header and dataset
a warning is reported.

Thanks to Mathieu Malaterre <mathieu.malaterre@gmail.com> for the original
reports and a sample DICOM file.

dcmdata/libsrc/dcitem.cc
@@ -1324,6 +1324,9 @@ OFCondition DcmItem::read(DcmInputStream & inStream,
                     /* for this element; hence lastElementComplete is true again */
                     if (errorFlag.good())
                         lastElementComplete = OFTrue;
+                    /* in data sets, group 0x0000 to 0x0003, 0x0005, 0x0007 and 0xFFFF are not allowed */
+                    if ((newTag.getGroup() == 0x0000) || (newTag.getGroup() == 0x0002) || !newTag.hasValidGroup())
+                        DCMDATA_WARN("DcmItem: Invalid Element " << newTag << " found in data set");
                 }
             } else
             {

commit 7544c11c24dd65bdb12e1ff587380ad838c3a837

Fixed an incorrect warning for command sets.
Fixed an incorrect warning on invalid data elements in a command set
(i.e. group 0x0000) caused by a previous commit (4636d9c).

Thanks to Peter Klotz <Peter.Klotz@ith-icoserve.com> for the report.

dcmdata/libsrc/dcitem.cc
@@ -1324,8 +1324,9 @@ OFCondition DcmItem::read(DcmInputStream & inStream,
                     /* for this element; hence lastElementComplete is true again */
                     if (errorFlag.good())
                         lastElementComplete = OFTrue;
-                    /* in data sets, group 0x0000 to 0x0003, 0x0005, 0x0007 and 0xFFFF are not allowed */
-                    if ((newTag.getGroup() == 0x0000) || (newTag.getGroup() == 0x0002) || !newTag.hasValidGroup())
+                    /* in data or command sets, group 0x0001 to 0x0003, 0x0005, 0x0007 and 0xFFFF are not allowed. */
+                    /* (we cannot check for group 0x0000 since we do not now whether we read a data or command set.)*/
+                    if (!newTag.hasValidGroup() || (newTag.getGroup() == 0x0002))
                         DCMDATA_WARN("DcmItem: Invalid Element " << newTag << " found in data set");
                 }
             } else {