f6cc20243fb3215b7e75307c94b05484203fb973
Michael Onken <onken@open-connections.de>
2016-08-06 07:53:02 +0200

    Updated copyright.

--- dcmiod/libsrc/iodcommn.cc
+++ dcmiod/libsrc/iodcommn.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2016, Open Connections GmbH
+ *  Copyright (C) 2015-2016, Open Connections GmbH
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation are maintained by
