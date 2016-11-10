6922a0a5fba7613fc5852b887709785295562104
Joerg Riesmeier <dicom@jriesmeier.com>
2016-11-10 11:04:36 +0100

    Fixed code value for "Pulse Sequence Name" (CP-1578).
    Fixed wrong definition of code value for "Pulse Sequence Name" (see CP-1578).

--- dcmsr/libcmr/tid1600.cc
+++ dcmsr/libcmr/tid1600.cc
@@ -647,7 +647,7 @@ OFCondition TID1600_ImageLibrary::addMagneticResonanceDescriptors(DSRDocumentSub
     if ((getStringValueFromDataset(dataset, DCM_PulseSequenceName, sequenceName).good() && !sequenceName.empty()) ||
         (getStringValueFromDataset(dataset, DCM_SequenceName, sequenceName).good() && !sequenceName.empty()))
     {
-        CHECK_RESULT(tree.addContentItem(RT_hasAcqContext, VT_Text, DSRCodedEntryValue("110909", "DCM", "Pulse Sequence Name") /* wrong definition: will be fixed with CP-1578 */, check));
+        CHECK_RESULT(tree.addContentItem(RT_hasAcqContext, VT_Text, DSRCodedEntryValue("128230", "DCM", "Pulse Sequence Name") /* fixed with CP-1578 */, check));
         CHECK_RESULT(tree.getCurrentContentItem().setStringValue(sequenceName, check));
         CHECK_RESULT(tree.getCurrentContentItem().setAnnotationText("TID 1606 - Row 1"));
     }
