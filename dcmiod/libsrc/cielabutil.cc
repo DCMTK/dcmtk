ea2c8a91c205f58bf0d3d1bbf4c78973ca70a6fb
Joerg Riesmeier <dicom@jriesmeier.com>
2016-11-16 11:23:57 +0100

    Replaced non-ASCII character in comment header.

--- dcmiod/libsrc/cielabutil.cc
+++ dcmiod/libsrc/cielabutil.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2005–2010, Pascal Getreuer
+ *  Copyright (C) 2005-2010, Pascal Getreuer
  *  Copyright (C) 2016, Open Connections GmbH
  *  All rights reserved.  See COPYRIGHT file for details.
  *
