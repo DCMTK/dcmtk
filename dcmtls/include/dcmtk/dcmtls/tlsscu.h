7ed5b81469c235bc168c3e8c1542c89d159e7a2f
Michael Onken <onken@open-connections.de>
2017-02-01 11:57:39 +0100

    Added missing documentation.
    Closes DCMTK Bug #541.

--- dcmtls/include/dcmtk/dcmtls/tlsscu.h
+++ dcmtls/include/dcmtk/dcmtls/tlsscu.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2010-2013, OFFIS e.V.
+ *  Copyright (C) 2010-2017, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -35,6 +35,7 @@
  *  class is derived from the general DcmSCU class and just adds the corresponding TLS
  *  capabilities.
  *  @warning This class is EXPERIMENTAL. Be careful to use it in production environment.
+ *  API changes in the future are possible and likely to happen.
  */
 class DCMTK_DCMTLS_EXPORT DcmTLSSCU : public DcmSCU
 {
@@ -46,9 +47,9 @@ public:
   DcmTLSSCU();
 
   /** Initialize SCU with connection peer information
-   *  @param peerHost [in] The hostname or IP of the host to talk to
-   *  @param peerAETtitle [in] AETitle of peer host to be used
-   *  @param portNum [in] TCP/IP port number to talk to
+   *  @param peerHost    The hostname or IP of the host to talk to
+   *  @param peerAETitle AETitle of peer host to be used
+   *  @param portNum     TCP/IP port number to talk to
    */
   DcmTLSSCU(const OFString& peerHost,
             const OFString& peerAETitle,
@@ -58,44 +59,45 @@ public:
     */
   virtual ~DcmTLSSCU();
 
-  /** Initialize network, i.e. prepare for association negotiation.
+  /** Initialize network, i.e. prepare for association negotiation
    *  @return EC_Normal if initialization was successful, otherwise error code
    */
   virtual OFCondition initNetwork();
 
-  /** Negotiate association by using presentation contexts and parameters
-   *  as defined by earlier function calls.
-   *  @return EC_Normal if negotiation was successful, otherwise error code
+  /** Negotiates association by using presentation contexts and parameters
+   *  as defined by earlier function calls
+   *  @return EC_Normal if negotiation was successful, otherwise error code.
    */
   virtual OFCondition negotiateAssociation();
 
   /** Closes the association of this SCU
    *  @deprecated The use of this method is deprecated. Please use
    *    DcmSCU::releaseAssociation() or DcmSCU::abortAssociation() instead.
-   *  @param closeType [in] Define whether to release or abort the association
+   *  @param closeType Define whether to release or abort the association
    */
   virtual void closeAssociation(const DcmCloseAssociationType closeType);
 
-  /** TODO: Not documented yet
-   *  @param str [in] Not documented yet
+  /** Add file with trusted certificate (used if authentication is enabled)
+   *  @param str Filename
    */
   virtual void addTrustedCertFile(const OFString& str);
 
-  /** TODO: Not documented yet
-   *  @param certDir [in] Not documented yet
+  /** Add directory to list of directories containing trusted certificates (used
+   *  if authentication is enabled)
+   *  @param certDir Not documented yet
    */
   virtual void addTrustedCertDir(const OFString& certDir);
 
-  /** TODO: Not documented yet
-   *  @param privateKey    [in] File with private key to authenticate with
-   *  @param certFile      [in] File with certificate to authenticate with
-   *  @param passphrase    [in] Password to access key. NULL asks user on console.
-   *                            "" will send empty password. Default is asking the
-   *                            user to enter password.
-   *  @param privKeyFormat [in] Format of private key parameter. Default is
-   *                            SSL_FILETYPE_PEM.
-   *  @param certFormat    [in] Format of certificate file parameter. Default is
-   *                            SSL_FILETYPE_PEM.
+  /** Turn on authentication for TLS
+   *  @param privateKey    File with private key to authenticate with
+   *  @param certFile      File with certificate to authenticate with
+   *  @param passphrase    Password to access key. NULL asks user on console.
+   *         "" will send empty password. Default is asking the
+   *         user to enter password.
+   *  @param privKeyFormat Format of private key parameter. Default is
+   *                       SSL_FILETYPE_PEM.
+   *  @param certFormat    Format of certificate file parameter. Default is
+   *                       SSL_FILETYPE_PEM.
    */
   virtual void enableAuthentication(const OFString& privateKey,
                                     const OFString& certFile,
@@ -103,40 +105,50 @@ public:
                                     const int privKeyFormat = SSL_FILETYPE_PEM,
                                     const int certFormat = SSL_FILETYPE_PEM);
 
-  /** Disables authentication. However, DcmTLSSCU will try to establish secured connection
-   *  in terms of encrypting data. Default is that authentication is disabled.
+  /** Disables authentication. However, DcmTLSSCU will try to establish secured
+   *  connection in terms of encrypting data. Default is that authentication is disabled.
    */
   virtual void disableAuthentication();
 
-  /** TODO: Not documented yet
-   *  @param cs [in] TODO: Not documented yet
+  /** Add ciphersuite to list of supported ciphersuites
+   *  @param cs The ciphersuite to add
    */
   virtual void addCiphersuite(const OFString& cs);
 
-  /** TODO: Not documented yet
-   *  @param seedFile [in] TODO: Not documented yet
+  /** Set file to be used as random seed for initializing the Pseudo Random
+   *  Number Generator (PRNG)
+   *  @param seedFile The seed file to be used.
    */
   virtual void setReadSeedFile(const OFString& seedFile);
 
-  /** TODO: Not documented yet
-   *  @param seedFile [in] TODO: Not documented yet
+  /** Set file to be use to store the updated random seed to store the updated
+   *  random seed from the Pseudo Random Number Generator (PRNG).
+   *  @param seedFile The seed file to be used for writing back seed info
    */
   virtual void setWriteSeedFile(const OFString& seedFile);
 
   /** Set whether peer's certificate must be there, only is checked if there or is ignored
-   *  @param cert [in] peer certificate verification mode. The following values are permitted:
-   *                   DCV_requireCertificate, DCV_checkCertificate, DCV_ignoreCertificate
+   *  @param cert Peer certificate verification mode. The following values are permitted:
+   *         DCV_requireCertificate, DCV_checkCertificate, DCV_ignoreCertificate
    */
   virtual void setPeerCertVerification(const DcmCertificateVerification cert);
 
-  /** Not documented yet
-   *  @param dhParam [in] TODO: Not documented yet
+  /** Set Diffie-Hellman parameters
+   *  @param dhParam Diffie-Hellman parameters to be used.
    */
   virtual void setDHParam(const OFString& dhParam);
 
   /** Returns OFTrue if authentication is enabled
-   *  @param ... TODO: Not documented yet
-   *  @return Return value OFTrue
+   *  @param privKeyFile   The file containing the private key used
+   *  @param certFile      The file containing the certificate used
+   *  @param passphrase    The passphrase used for unlocking the private key
+   *         file. If NULL, password is asked from STDIN. If empty string (""),
+   *         password is sent empty. All others values are sent as given.
+   *  @param privKeyFormat Format of the private key in privKeyFile, see
+   *         documentation of m_privateKeyFileFormat.
+   *  @param certFormat    The certificate format of certificate in certFile.
+   *         See documentation of m_certKeyFileFormat.
+   *  @return Returns OFTrue if authentication is enabled, OFFalse otherwise
    */
   virtual OFBool getAuthenticationParams(OFString& privKeyFile,
                                          OFString& certFile,
@@ -144,33 +156,36 @@ public:
                                          int& privKeyFormat,
                                          int& certFormat) const;
 
-  /** TODO: Not documented yet
-   *  @param files [out] TODO: Not documented yet
+  /** Get files considered for trusted certificates.
+   *  @param files The file names that are considered as trusted
+   *         certificates
    */
   virtual void getTrustedCertFiles(OFList<OFString>& files /*out*/) const;
 
-  /** TODO: Not documented yet
-   *  @param trustedDirs [out] TODO: Not documented yet
+  /** Get directories containing considered to contain trusted certificates.
+   *  @param trustedDirs directories consiered to contain trusted certificates.
    */
   virtual void getTrustedCertDirs(OFList<OFString>& trustedDirs /*out*/) const;
 
-  /** TODO: Not documented yet
-   *  @return TODO: Not documented yet
+  /** Get ciphersuites set to be supported
+   *  @return Ciphersuites set to be supported
    */
   virtual OFString getCiphersuites() const;
 
-  /** TODO: Not documented yet
-   *  @return TODO: Not documented yet
+  /** Get random seed file used for initializing Pseudo Random Number
+   *  Generator (PRNG)
+   *  @return Random seed file used for reading
    */
   virtual OFString getReadSeedFile() const;
 
-  /** TODO: Not documented yet
-   *  @return TODO: Not documented yet
+  /** Get random seed file the PRNG should use to store back updated random
+   *  seed information
+   *  @return Get random seed file used for writing back updated seed
    */
   virtual OFString getWriteSeedFile() const;
 
-  /** TODO: Not documented yet
-   *  @return TODO: Not documented yet
+  /** Get Diffie-Hellman parameters set to be used
+   *  @return Diffie-Hellman parameters set to be used
    */
   virtual OFString getDHParam() const;
 
@@ -183,7 +198,7 @@ private:
 
   /** Private undefined operator=. Shall never be called.
    *  @param src Source object
-   *  @return Reference to this
+   *  @return Reference to this object
    */
   DcmTLSSCU &operator=(const DcmTLSSCU &src);
 
@@ -211,18 +226,18 @@ private:
   /// Might be either "SSL_FILETYPE_PEM" (default) or "SSL_FILETYPE_ASN1"
   int m_certKeyFileFormat;
 
-  /// Password used for unlocking the private key file. If NULL, password is asked from
-  /// STDIN. If empty string (""), password is sent empty. All others values are sent as
-  /// given.
+  ///
   char* m_passwd;
 
-  /// String containing all cipher suites being (additionally? TODO) configured
+  /// String containing all cipher suites being configured to be used
   OFString m_ciphersuites;
 
-  /// TODO: Not documented yet
+  /// Random seed file used for initializing Pseudo Random Number
+  /// Generator (PRNG)
   OFString m_readSeedFile;
 
-  /// TODO: Not documented yet
+  /// Random seed file used for writing updated seed from Pseudo Random Number
+  /// Generator back to a file
   OFString m_writeSeedFile;
 
   /// Denotes how certificates are handled, i.e. whether they are required, validated or
