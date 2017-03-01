1c551d44004ee4682cf6a5c48c766b648965c635
Jan Schlamelcher <dicom@offis.de>
2017-03-01 14:40:23 +0100

    Fixed dcmqrdb falsely retrying response conversion.
    The dcmqrdb library does no longer retry response conversion if the fall-back
    character set and the originally requested one are the same. This is more
    efficient and prevents duplicated warning output.

--- dcmqrdb/libsrc/dcmqrdbi.cc
+++ dcmqrdb/libsrc/dcmqrdbi.cc
@@ -1715,7 +1715,11 @@ OFCondition DcmQueryRetrieveIndexDatabaseHandle::nextFindResponse (
                     << "\" to character set \""
                     << characterSetName(*destinationCharacterSet)
                     << "\" failed, (error message: " << status.text() << ')');
-                if (fallbackCharacterSet && isConversionNecessary(specificCharacterSet, *fallbackCharacterSet)) {
+                if (
+                    fallbackCharacterSet &&
+                    *fallbackCharacterSet != *destinationCharacterSet &&
+                    isConversionNecessary(specificCharacterSet, *fallbackCharacterSet)
+                ) {
                     DCMQRDB_INFO("Trying to convert response from character set \""
                         << characterSetName(specificCharacterSet)
                         << "\" to fall-back character set \""
