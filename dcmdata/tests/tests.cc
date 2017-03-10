4f60854d81b0bda71bf8cf6722bc4ce13132dead
Joerg Riesmeier <dicom@jriesmeier.com>
2017-03-10 17:11:15 +0100

    Fixed copyright date.

--- dcmdata/tests/tests.cc
+++ dcmdata/tests/tests.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2011-2016 OFFIS e.V.
+ *  Copyright (C) 2011-2017 OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
--- dcmdata/tests/tvrcomp.cc
+++ dcmdata/tests/tvrcomp.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2017, OFFIS e.V.
+ *  Copyright (C) 2015-2017, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
