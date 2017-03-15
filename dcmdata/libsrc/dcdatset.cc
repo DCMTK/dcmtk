DCMTK/dcmtk

commit f58cfe99ffa582dc90b80cf087eb94fef93ea897

Parser can now be stopped before a certain tag.
Added methods to the dcmdata parser that allow the read process
to be stopped before a certain tag is fully processed. This also
works if the given tag (which must be on the main dataset level)
is not present in file, i.e. no higher tag is parsed/processed.
Added --stop-before-elem command line option to dcmdump to
enable use of these new methods.

This closes DCMTK patch #347. Thanks to Jeroen Eggermont for
the suggestion and the (partial) patch.

dcmdata/libsrc/dcdatset.cc
@@ -369,6 +369,15 @@ OFCondition DcmDataset::read(DcmInputStream &inStream,
                              const E_GrpLenEncoding glenc,
                              const Uint32 maxReadLength)
 {
+  return readUntilTag(inStream, xfer, glenc, maxReadLength, DCM_UndefinedTagKey);
+}
+
+OFCondition DcmDataset::readUntilTag(DcmInputStream &inStream,
+                                     const E_TransferSyntax xfer,
+                                     const E_GrpLenEncoding glenc,
+                                     const Uint32 maxReadLength,
+                                     const DcmTagKey &stopParsingAtElement)
+{
     /* check if the stream variable reported an error */
     errorFlag = inStream.status();
     /* if the stream did not report an error but the stream */
@@ -442,7 +451,7 @@ OFCondition DcmDataset::read(DcmInputStream &inStream,
         }
         /* pass processing the task to class DcmItem */
         if (errorFlag.good())
-            errorFlag = DcmItem::read(inStream, OriginalXfer, glenc, maxReadLength);
+            errorFlag = DcmItem::readUntilTag(inStream, xfer, glenc, maxReadLength, stopParsingAtElement);
 
     }
 
@@ -639,6 +648,15 @@ OFCondition DcmDataset::loadFile(const OFFilename &fileName,
                                  const E_GrpLenEncoding groupLength,
                                  const Uint32 maxReadLength)
 {
+  return loadFileUntilTag(fileName, readXfer, groupLength, maxReadLength, DCM_UndefinedTagKey);
+}
+
+OFCondition DcmDataset::loadFileUntilTag(const OFFilename &fileName,
+                                 const E_TransferSyntax readXfer,
+                                 const E_GrpLenEncoding groupLength,
+                                 const Uint32 maxReadLength,
+                                 const DcmTagKey &stopParsingAtElement)
+{
     OFCondition l_error = EC_InvalidFilename;
     /* check parameters first */
     if (!fileName.isEmpty())
@@ -657,7 +675,7 @@ OFCondition DcmDataset::loadFile(const OFFilename &fileName,
             {
                 /* read data from file */
                 transferInit();
-                l_error = read(fileStream, readXfer, groupLength, maxReadLength);
+                l_error = readUntilTag(fileStream, readXfer, groupLength, maxReadLength, stopParsingAtElement);
                 transferEnd();
             }
         }

dcmdata/libsrc/dcfilefo.cc
@@ -693,7 +693,15 @@ OFCondition DcmFileFormat::read(DcmInputStream &inStream,
                                 const E_TransferSyntax xfer,
                                 const E_GrpLenEncoding glenc,
                                 const Uint32 maxReadLength)
+{
+  return readUntilTag(inStream,xfer,glenc,maxReadLength,DCM_UndefinedTagKey);
+}
 
+OFCondition DcmFileFormat::readUntilTag(DcmInputStream &inStream,
+                                        const E_TransferSyntax xfer,
+                                        const E_GrpLenEncoding glenc,
+                                        const Uint32 maxReadLength,
+                                        const DcmTagKey &stopParsingAtElement)
 {
     if (getTransferState() == ERW_notInitialized)
         errorFlag = EC_IllegalCall;
@@ -749,7 +757,7 @@ OFCondition DcmFileFormat::read(DcmInputStream &inStream,
                 {
                     if (dataset && dataset->transferState() != ERW_ready)
                     {
-                        errorFlag = dataset->read(inStream, newxfer, glenc, maxReadLength);
+                        errorFlag = dataset->readUntilTag(inStream, newxfer, glenc, maxReadLength,stopParsingAtElement);
                     }
                 }
             }
@@ -761,7 +769,7 @@ OFCondition DcmFileFormat::read(DcmInputStream &inStream,
             setTransferState(ERW_ready);
     }
     return errorFlag;
-}  // DcmFileFormat::read()
+}  // DcmFileFormat::readUntilTag()
 
 
 // ********************************
@@ -873,15 +881,26 @@ OFCondition DcmFileFormat::write(DcmOutputStream &outStream,
 
 // ********************************
 
-
 OFCondition DcmFileFormat::loadFile(const OFFilename &fileName,
                                     const E_TransferSyntax readXfer,
                                     const E_GrpLenEncoding groupLength,
                                     const Uint32 maxReadLength,
                                     const E_FileReadMode readMode)
 {
+  return loadFileUntilTag(fileName, readXfer, groupLength, maxReadLength, readMode, DCM_UndefinedTagKey);
+}
+
+
+OFCondition DcmFileFormat::loadFileUntilTag(
+                                    const OFFilename &fileName,
+                                    const E_TransferSyntax readXfer,
+                                    const E_GrpLenEncoding groupLength,
+                                    const Uint32 maxReadLength,
+                                    const E_FileReadMode readMode,
+                                    const DcmTagKey &stopParsingAtElement)
+{
     if (readMode == ERM_dataset)
-        return getDataset()->loadFile(fileName, readXfer, groupLength, maxReadLength);
+        return getDataset()->loadFileUntilTag(fileName, readXfer, groupLength, maxReadLength, stopParsingAtElement);
 
     OFCondition l_error = EC_InvalidFilename;
     /* check parameters first */
@@ -902,7 +921,7 @@ OFCondition DcmFileFormat::loadFile(const OFFilename &fileName,
                 FileReadMode = readMode;
                 /* read data from file */
                 transferInit();
-                l_error = read(fileStream, readXfer, groupLength, maxReadLength);
+                l_error = readUntilTag(fileStream, readXfer, groupLength, maxReadLength, stopParsingAtElement);
                 transferEnd();
                 /* restore old value */
                 FileReadMode = oldMode;

dcmdata/apps/dcmdump.cc
@@ -69,6 +69,7 @@ static int dumpFile(STD_NAMESPACE ostream &out,
                     const OFBool loadIntoMemory,
                     const OFBool stopOnErrors,
                     const OFBool convertToUTF8,
+                    const DcmTagKey &stopParsingAtElement,
                     const char *pixelDirectory);
 
 // ********************************************
@@ -162,6 +163,7 @@ DCMTK_MAIN_FUNCTION
     const char *scanPattern = "";
     const char *pixelDirectory = NULL;
     OFBool convertToUTF8 = OFFalse;
+    DcmTagKey stopParsingBeforeElement = DCM_UndefinedTagKey;
 
 #ifdef HAVE_GUSI_H
     /* needed for Macintosh */
@@ -250,6 +252,8 @@ DCMTK_MAIN_FUNCTION
       cmd.addSubGroup("other parsing options:");
         cmd.addOption("--stop-after-elem",     "+st", 1, "[t]ag: \"gggg,eeee\" or dictionary name",
                                                          "stop parsing after element specified by t");
+        cmd.addOption("--stop-before-elem",    "+sb", 1, "[t]ag: \"gggg,eeee\" or dictionary name",
+                                                         "stop parsing before element specified by t");
       cmd.addSubGroup("automatic data correction:");
         cmd.addOption("--enable-correction",   "+dc",    "enable automatic data correction (default)");
         cmd.addOption("--disable-correction",  "-dc",    "disable automatic data correction");
@@ -597,6 +601,7 @@ DCMTK_MAIN_FUNCTION
       if (cmd.findOption("--ignore-errors")) stopOnErrors = OFFalse;
       cmd.endOptionBlock();
 
+      cmd.beginOptionBlock();
       if (cmd.findOption("--stop-after-elem"))
       {
         const char *tagName = NULL;
@@ -607,6 +612,17 @@ DCMTK_MAIN_FUNCTION
         else
           app.printError("no valid key given for option --stop-after-elem");
       }
+      if (cmd.findOption("--stop-before-elem"))
+      {
+        const char *tagName = NULL;
+        app.checkValue(cmd.getValue(tagName));
+        DcmTagKey key = parseTagKey(tagName);
+        if (key != DCM_UndefinedTagKey)
+          stopParsingBeforeElement = key;
+        else
+          app.printError("no valid key given for option --stop-before-elem");
+      }
+      cmd.endOptionBlock();
 
       if (cmd.findOption("--search", 0, OFCommandLine::FOM_FirstFromLeft))
       {
@@ -720,7 +736,7 @@ DCMTK_MAIN_FUNCTION
         /* print header with filename */
         COUT << "# " << OFFIS_CONSOLE_APPLICATION << " (" << fileCounter << "/" << count << "): " << current << OFendl;
       }
-      errorCount += dumpFile(COUT, current, readMode, xfer, printFlags, loadIntoMemory, stopOnErrors, convertToUTF8, pixelDirectory);
+      errorCount += dumpFile(COUT, current, readMode, xfer, printFlags, loadIntoMemory, stopOnErrors, convertToUTF8, stopParsingBeforeElement, pixelDirectory);
     }
 
     return errorCount;
@@ -765,6 +781,7 @@ static int dumpFile(STD_NAMESPACE ostream &out,
                     const OFBool loadIntoMemory,
                     const OFBool stopOnErrors,
                     const OFBool convertToUTF8,
+                    const DcmTagKey &stopParsingAtElement,
                     const char *pixelDirectory)
 {
     int result = 0;
@@ -778,7 +795,17 @@ static int dumpFile(STD_NAMESPACE ostream &out,
     DcmFileFormat dfile;
     DcmObject *dset = &dfile;
     if (readMode == ERM_dataset) dset = dfile.getDataset();
-    OFCondition cond = dfile.loadFile(ifname, xfer, EGL_noChange, OFstatic_cast(Uint32, maxReadLength), readMode);
+    OFCondition cond;
+
+    if (stopParsingAtElement == DCM_UndefinedTagKey)
+    {
+        cond = dfile.loadFile(ifname, xfer, EGL_noChange, OFstatic_cast(Uint32, maxReadLength), readMode);
+    }
+    else
+    {
+        // instead of using loadFile(), we call loadFileUntilTag().
+        cond = dfile.loadFileUntilTag(ifname, xfer, EGL_noChange, OFstatic_cast(Uint32, maxReadLength), readMode, stopParsingAtElement);
+    }
     if (cond.bad())
     {
         OFLOG_ERROR(dcmdumpLogger, OFFIS_CONSOLE_APPLICATION << ": " << cond.text()

dcmdata/docs/dcmdump.man
@@ -224,6 +224,12 @@ other parsing options:
   +st  --stop-after-elem  [t]ag: "gggg,eeee" or dictionary name
          stop parsing after element specified by t
 
+  +sb  --stop-before-elem [t]ag: "gggg,eeee" or dictionary name
+         stop parsing before element specified by t
+
+         # only considers elements on main dataset level and also
+         # works if the given tag is not present in the file
+
 automatic data correction:
 
   +dc  --enable-correction

dcmdata/include/dcmtk/dcmdata/dcdatset.h
@@ -185,6 +185,30 @@ class DCMTK_DCMDATA_EXPORT DcmDataset
                              const E_GrpLenEncoding glenc = EGL_noChange,
                              const Uint32 maxReadLength = DCM_MaxReadLength);
 
+    /** This function reads the information of all attributes which
+     *  are captured in the input stream and captures this information
+     *  in this->elementList, up to the attribute tag stopParsingAtElement.
+     *  Each attribute is represented as an
+     *  element in this list. Having read all information for this
+     *  particular data set or command, this function will also take
+     *  care of group length (according to what is specified in glenc)
+     *  and padding elements (don't change anything).
+     *  @param inStream      The stream which contains the information.
+     *  @param xfer          The transfer syntax which was used to encode
+     *                       the information in inStream.
+     *  @param glenc         Encoding type for group length; specifies what
+     *                       will be done with group length tags.
+     *  @param maxReadLength Maximum read length for reading an attribute value.
+     *  @param stopParsingAtElement parsing of the input stream is stopped when
+     *                       this tag key or any higher tag is encountered.
+     *  @return status, EC_Normal if successful, an error code otherwise
+     */
+    virtual OFCondition readUntilTag(DcmInputStream &inStream,
+                                     const E_TransferSyntax xfer = EXS_Unknown,
+                                     const E_GrpLenEncoding glenc = EGL_noChange,
+                                     const Uint32 maxReadLength = DCM_MaxReadLength,
+                                     const DcmTagKey &stopParsingAtElement = DCM_UndefinedTagKey);
+
     /** write dataset to a stream
      *  @param outStream DICOM output stream
      *  @param oxfer output transfer syntax (EXS_Unknown means use original)
@@ -296,6 +320,27 @@ class DCMTK_DCMDATA_EXPORT DcmDataset
                                  const E_TransferSyntax readXfer = EXS_Unknown,
                                  const E_GrpLenEncoding groupLength = EGL_noChange,
                                  const Uint32 maxReadLength = DCM_MaxReadLength);
+
+    /** load object from a DICOM file, up to the attribute tag stopParsingAtElement.
+     *  This method only supports DICOM objects stored as a dataset, i.e. without meta header.
+     *  Use DcmFileFormat::loadFile() to load files with meta header.
+     *  @param fileName name of the file to load (may contain wide chars if support enabled).
+     *    Since there are various constructors for the OFFilename class, a "char *", "OFString"
+     *    or "wchar_t *" can also be passed directly to this parameter.
+     *  @param readXfer transfer syntax used to read the data (auto detection if EXS_Unknown)
+     *  @param groupLength flag, specifying how to handle the group length tags
+     *  @param maxReadLength maximum number of bytes to be read for an element value.
+     *    Element values with a larger size are not loaded until their value is retrieved
+     *    (with getXXX()) or loadAllDataIntoMemory() is called.
+     *  @param stopParsingAtElement parsing of the input stream is stopped when
+     *                       this tag key or any higher tag is encountered.
+     *  @return status, EC_Normal if successful, an error code otherwise
+     */
+    virtual OFCondition loadFileUntilTag(const OFFilename &fileName,
+                                 const E_TransferSyntax readXfer = EXS_Unknown,
+                                 const E_GrpLenEncoding groupLength = EGL_noChange,
+                                 const Uint32 maxReadLength = DCM_MaxReadLength,
+                                 const DcmTagKey &stopParsingAtElement = DCM_UndefinedTagKey);
 
     /** save object to a DICOM file.
      *  This method only supports DICOM objects stored as a dataset, i.e. without meta header.

dcmdata/include/dcmtk/dcmdata/dcfilefo.h
@@ -185,6 +185,23 @@ class DCMTK_DCMDATA_EXPORT DcmFileFormat
                              const E_GrpLenEncoding glenc = EGL_noChange,
                              const Uint32 maxReadLength = DCM_MaxReadLength);
 
+    /** read object from a stream, up to the attribute tag stopParsingAtElement.
+     *  @param inStream DICOM input stream
+     *  @param xfer transfer syntax to use when parsing
+     *  @param glenc handling of group length parameters
+     *  @param maxReadLength attribute values larger than this value are skipped
+     *    while parsing and read later upon first access if the stream type supports
+     *    this.
+     *  @param stopParsingAtElement parsing of the input stream is stopped when
+     *                       this tag key or any higher tag is encountered.
+     *  @return EC_Normal if successful, an error code otherwise
+     */
+    virtual OFCondition readUntilTag(DcmInputStream &inStream,
+                                     const E_TransferSyntax xfer = EXS_Unknown,
+                                     const E_GrpLenEncoding glenc = EGL_noChange,
+                                     const Uint32 maxReadLength = DCM_MaxReadLength,
+                                     const DcmTagKey &stopParsingAtElement = DCM_UndefinedTagKey);
+
     /** write fileformat to a stream
      *  @param outStream DICOM output stream
      *  @param oxfer output transfer syntax
@@ -277,6 +294,31 @@ class DCMTK_DCMDATA_EXPORT DcmFileFormat
                                  const E_GrpLenEncoding groupLength = EGL_noChange,
                                  const Uint32 maxReadLength = DCM_MaxReadLength,
                                  const E_FileReadMode readMode = ERM_autoDetect);
+
+    /** load object from a DICOM file, up to the attribute tag stopParsingAtElement.
+     *  This method supports DICOM objects stored as a file (with meta header) or as a
+     *  dataset (without meta header).  By default, the presence of a meta header is
+     *  detected automatically.
+     *  @param fileName name of the file to load (may contain wide chars if support enabled).
+     *    Since there are various constructors for the OFFilename class, a "char *", "OFString"
+     *    or "wchar_t *" can also be passed directly to this parameter.
+     *  @param readXfer transfer syntax used to read the data (auto detection if EXS_Unknown)
+     *  @param groupLength flag, specifying how to handle the group length tags
+     *  @param maxReadLength maximum number of bytes to be read for an element value.
+     *    Element values with a larger size are not loaded until their value is retrieved
+     *    (with getXXX()) or loadAllDataIntoMemory() is called.
+     *  @param readMode read file with or without meta header, i.e. as a fileformat or a
+     *    dataset.  Use ERM_fileOnly in order to force the presence of a meta header.
+     *  @param stopParsingAtElement parsing of the input stream is stopped when
+     *                       this tag key or any higher tag is encountered.
+     *  @return status, EC_Normal if successful, an error code otherwise
+     */
+    virtual OFCondition loadFileUntilTag(const OFFilename &fileName,
+                                 const E_TransferSyntax readXfer = EXS_Unknown,
+                                 const E_GrpLenEncoding groupLength = EGL_noChange,
+                                 const Uint32 maxReadLength = DCM_MaxReadLength,
+                                 const E_FileReadMode readMode = ERM_autoDetect,
+                                 const DcmTagKey &stopParsingAtElement = DCM_UndefinedTagKey);
 
     /** save object to a DICOM file.
      *  @param fileName name of the file to save (may contain wide chars if support enabled).

dcmdata/include/dcmtk/dcmdata/dcitem.h
@@ -243,6 +243,28 @@ class DCMTK_DCMDATA_EXPORT DcmItem
                              const E_GrpLenEncoding glenc = EGL_noChange,
                              const Uint32 maxReadLength = DCM_MaxReadLength);
 
+    /** This function reads the information of all attributes which
+     *  are captured in the input stream and captures this information
+     *  in elementList, up to the attribute tag stopParsingAtElement.
+     *  Each attribute is represented as an element
+     *  in this list. If not all information for an attribute could be
+     *  read from the stream, the function returns EC_StreamNotifyClient.
+     *  @param inStream      The stream which contains the information.
+     *  @param ixfer         The transfer syntax which was used to encode
+     *                       the information in inStream.
+     *  @param glenc         Encoding type for group length; specifies
+     *                       what will be done with group length tags.
+     *  @param maxReadLength Maximum read length for reading an attribute value.
+     *  @param stopParsingAtElement parsing of the input stream is stopped when
+     *                       this tag key or any higher tag is encountered.
+     *  @return status, EC_Normal if successful, an error code otherwise
+     */
+    virtual OFCondition readUntilTag(DcmInputStream &inStream,
+                                     const E_TransferSyntax ixfer,
+                                     const E_GrpLenEncoding glenc = EGL_noChange,
+                                     const Uint32 maxReadLength = DCM_MaxReadLength,
+                                     const DcmTagKey &stopParsingAtElement = DCM_UndefinedTagKey);
+
     /** write object to a stream
      *  @param outStream DICOM output stream
      *  @param oxfer output transfer syntax

dcmdata/libsrc/dcitem.cc
@@ -1278,6 +1278,15 @@ OFCondition DcmItem::read(DcmInputStream & inStream,
                           const E_GrpLenEncoding glenc,
                           const Uint32 maxReadLength)
 {
+	return DcmItem::readUntilTag(inStream, xfer, glenc, maxReadLength, DCM_UndefinedTagKey);
+}
+
+OFCondition DcmItem::readUntilTag(DcmInputStream & inStream,
+                                  const E_TransferSyntax xfer,
+                                  const E_GrpLenEncoding glenc,
+                                  const Uint32 maxReadLength,
+                                  const DcmTagKey &stopParsingAtElement)
+{
     /* check if this is an illegal call; if so set the error flag and do nothing, else go ahead */
     if (getTransferState() == ERW_notInitialized)
     {
@@ -1354,17 +1363,30 @@ OFCondition DcmItem::read(DcmInputStream & inStream,
                     /* in case of implicit VR, check whether the "default VR" is really appropriate */
                     if (DcmXfer(xfer).isImplicitVR())
                         checkAndUpdateVR(*this, newTag);
-                    /* read the actual data value which belongs to this element */
-                    /* (attribute) and insert this information into the elementList */
-                    errorFlag = readSubElement(inStream, newTag, newValueLength, xfer, glenc, maxReadLength);
-                    /* if reading was successful, we read the entire data value information */
-                    /* for this element; hence lastElementComplete is true again */
-                    if (errorFlag.good())
+
+                    /* check if we want to stop parsing at this point, in the main dataset only */
+                    if( (stopParsingAtElement != DCM_UndefinedTagKey) && (newTag.getXTag()>=stopParsingAtElement) && ident() == EVR_dataset)
+                    {
+                      lastElementComplete = OFTrue;
+                      readStopElem = OFTrue;
+                      DCMDATA_WARN("DcmItem: Element " << newTag.getTagName() << " " << newTag
+                        << " encountered, skipping rest of dataset");
+                    }
+                    else
+                    {
+                      /* read the actual data value which belongs to this element */
+                      /* (attribute) and insert this information into the elementList */
+                      errorFlag = readSubElement(inStream, newTag, newValueLength, xfer, glenc, maxReadLength);
+                      /* if reading was successful, we read the entire data value information */
+                      /* for this element; hence lastElementComplete is true again */
+                      if (errorFlag.good())
                         lastElementComplete = OFTrue;
-                    /* in data or command sets, group 0x0001 to 0x0003, 0x0005, 0x0007 and 0xFFFF are not allowed. */
-                    /* (we cannot check for group 0x0000 since we do not know whether we read a data or command set.)*/
-                    if (!newTag.hasValidGroup() || (newTag.getGroup() == 0x0002))
-                        DCMDATA_WARN("DcmItem: Invalid Element " << newTag << " found in data set");
+
+                      /* in data or command sets, group 0x0001 to 0x0003, 0x0005, 0x0007 and 0xFFFF are not allowed. */
+                      /* (we cannot check for group 0x0000 since we do not know whether we read a data or command set.)*/
+                      if (!newTag.hasValidGroup() || (newTag.getGroup() == 0x0002))
+                          DCMDATA_WARN("DcmItem: Invalid Element " << newTag << " found in data set");
+                    }
                 }
             } else {
                 /* if lastElementComplete is false, we have only read the current element's */

commit ef9173b958218c7c66b8baf5541709267f17de13

Fixed bug introduced with commit f58cfe9.
Fixed bug introduced with commit f58cfe9 that prevented successful
reading of DICOM files without metaheader.

dcmdata/libsrc/dcdatset.cc
@@ -369,7 +369,7 @@ OFCondition DcmDataset::read(DcmInputStream &inStream,
                              const E_GrpLenEncoding glenc,
                              const Uint32 maxReadLength)
 {
-  return readUntilTag(inStream, xfer, glenc, maxReadLength, DCM_UndefinedTagKey);
+  return DcmDataset::readUntilTag(inStream, xfer, glenc, maxReadLength, DCM_UndefinedTagKey);
 }
 
 OFCondition DcmDataset::readUntilTag(DcmInputStream &inStream,
@@ -451,7 +451,7 @@ OFCondition DcmDataset::readUntilTag(DcmInputStream &inStream,
         }
         /* pass processing the task to class DcmItem */
         if (errorFlag.good())
-            errorFlag = DcmItem::readUntilTag(inStream, xfer, glenc, maxReadLength, stopParsingAtElement);
+            errorFlag = DcmItem::readUntilTag(inStream, OriginalXfer, glenc, maxReadLength, stopParsingAtElement);
 
     }
 
@@ -648,7 +648,7 @@ OFCondition DcmDataset::loadFile(const OFFilename &fileName,
                                  const E_GrpLenEncoding groupLength,
                                  const Uint32 maxReadLength)
 {
-  return loadFileUntilTag(fileName, readXfer, groupLength, maxReadLength, DCM_UndefinedTagKey);
+  return DcmDataset::loadFileUntilTag(fileName, readXfer, groupLength, maxReadLength, DCM_UndefinedTagKey);
 }
 
 OFCondition DcmDataset::loadFileUntilTag(const OFFilename &fileName,

dcmdata/libsrc/dcfilefo.cc
@@ -694,7 +694,7 @@ OFCondition DcmFileFormat::read(DcmInputStream &inStream,
                                 const E_GrpLenEncoding glenc,
                                 const Uint32 maxReadLength)
 {
-  return readUntilTag(inStream,xfer,glenc,maxReadLength,DCM_UndefinedTagKey);
+  return DcmFileFormat::readUntilTag(inStream,xfer,glenc,maxReadLength,DCM_UndefinedTagKey);
 }
 
 OFCondition DcmFileFormat::readUntilTag(DcmInputStream &inStream,
@@ -887,7 +887,7 @@ OFCondition DcmFileFormat::loadFile(const OFFilename &fileName,
                                     const Uint32 maxReadLength,
                                     const E_FileReadMode readMode)
 {
-  return loadFileUntilTag(fileName, readXfer, groupLength, maxReadLength, readMode, DCM_UndefinedTagKey);
+  return DcmFileFormat::loadFileUntilTag(fileName, readXfer, groupLength, maxReadLength, readMode, DCM_UndefinedTagKey);
 }
 
 