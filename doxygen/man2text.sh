0406716d9f52c6a194a9a5a9dc325d32a71e625d
Marco Eichelberg <dicom@offis.de>
2017-08-04 09:27:31 +0200

    Enforcing max line width in preformatted man pages.
    Now enforcing 80 characters per line maximum when generating man pages in
    pre-formatted text format.

--- doxygen/man2text.sh
+++ doxygen/man2text.sh
@@ -3,5 +3,5 @@
 # Generate a text representation of the man pages
 cd manpages/man1/
 for file in *.1 ; do \
-	man -l $file | col -bxp > ../../man2text/`echo $file | sed 's/\(.*\.\)1/\1txt/'` ;\
+	MANWIDTH=80 man -e utf8 -l $file | col -bxp > ../../man2text/`echo $file | sed 's/\(.*\.\)1/\1txt/'` ;\
 done
