60fa4314b18209020be95e0de2dbf9cb79811910
Marco Eichelberg <dicom@offis.de>
2017-08-06 10:45:00 +0200

    Now generating preformatted man pages as pure ASCII.

--- doxygen/man2text.sh
+++ doxygen/man2text.sh
@@ -3,5 +3,5 @@
 # Generate a text representation of the man pages
 cd manpages/man1/
 for file in *.1 ; do \
-	MANWIDTH=80 man -e utf8 -l $file | col -bxp > ../../man2text/`echo $file | sed 's/\(.*\.\)1/\1txt/'` ;\
+	MANWIDTH=80 man -E ascii -l $file | col -bxp > ../../man2text/`echo $file | sed 's/\(.*\.\)1/\1txt/'` ;\
 done
