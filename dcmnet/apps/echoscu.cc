6ace5a71f7955c3cb0c6233136551261b3337f3f
Joerg Riesmeier <dicom@jriesmeier.com>
2017-12-12 14:14:26 +0100

    Added option --socket-timeout to echoscu.
    Added option --socket-timeout to echoscu (as already done for e.g. storescu).
    
    Thanks to Michael Craggs <Michael.Craggs@ith-icoserve.com> for the suggested
    patch.
    
    This partly closed DCMTK Feature #711.

--- dcmnet/apps/echoscu.cc
+++ dcmnet/apps/echoscu.cc
@@ -29,6 +29,7 @@
 
 #include "dcmtk/dcmnet/dimse.h"
 #include "dcmtk/dcmnet/diutil.h"
+#include "dcmtk/dcmnet/dcmtrans.h"    /* for dcmSocketSend/ReceiveTimeout */
 #include "dcmtk/dcmdata/dcfilefo.h"
 #include "dcmtk/dcmdata/dcdict.h"
 #include "dcmtk/dcmdata/dcuid.h"
@@ -136,6 +137,7 @@ main(int argc, char *argv[])
   OFCmdUnsignedInt maxXferSyntaxes         = OFstatic_cast(OFCmdUnsignedInt, (DIM_OF(transferSyntaxes)));
   OFBool           opt_secureConnection    = OFFalse; /* default: no secure connection */
   int              opt_acse_timeout        = 30;
+  OFCmdSignedInt   opt_socket_timeout      = 60;
 
 #ifdef WITH_OPENSSL
   int         opt_keyFileFormat = SSL_FILETYPE_PEM;
@@ -190,6 +192,8 @@ main(int argc, char *argv[])
 
     cmd.addSubGroup("other network options:");
       cmd.addOption("--timeout",           "-to",  1, "[s]econds: integer (default: unlimited)", "timeout for connection requests");
+      CONVERT_TO_STRING("[s]econds: integer (default: " << opt_socket_timeout << ")", optString1);
+      cmd.addOption("--socket-timeout",    "-ts",  1, optString1.c_str(), "timeout for network socket (0 for none)");
       CONVERT_TO_STRING("[s]econds: integer (default: " << opt_acse_timeout << ")", optString2);
       cmd.addOption("--acse-timeout",      "-ta",  1, optString2.c_str(), "timeout for ACSE messages");
       cmd.addOption("--dimse-timeout",     "-td",  1, "[s]econds: integer (default: unlimited)", "timeout for DIMSE messages");
@@ -279,6 +283,12 @@ main(int argc, char *argv[])
         app.checkValue(cmd.getValueAndCheckMin(opt_timeout, 1));
         dcmConnectionTimeout.set(OFstatic_cast(Sint32, opt_timeout));
       }
+
+      if (cmd.findOption("--socket-timeout"))
+        app.checkValue(cmd.getValueAndCheckMin(opt_socket_timeout, -1));
+      // always set the timeout values since the global default might be different
+      dcmSocketSendTimeout.set(OFstatic_cast(Sint32, opt_socket_timeout));
+      dcmSocketReceiveTimeout.set(OFstatic_cast(Sint32, opt_socket_timeout));
 
       if (cmd.findOption("--acse-timeout"))
       {
