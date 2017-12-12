8e4366b0c021a29adf21f46cff0727a70b69e12a
Jan Schlamelcher <dicom@offis.de>
2017-12-12 17:26:35 +0100

    Modified recently introduced OFrvalue_ref_upcast.
    Modified C++11 implementation of OFrvalue_ref_upcast to also work around
    issues with older versions of GCC in addition to being a work around for move
    emulation for pre C++11 compilers.

--- ofstd/include/dcmtk/ofstd/ofutil.h
+++ ofstd/include/dcmtk/ofstd/ofutil.h
@@ -50,7 +50,7 @@ using OFrvalue = T;
 
 #define OFrvalue_ref(T) T&&
 #define OFrvalue_access(RV) RV
-#define OFrvalue_ref_upcast(T, RV) RV
+#define OFrvalue_ref_upcast(T, RV) static_cast<T&&>(RV)
 
 #else // fallback implementations
 
@@ -208,8 +208,9 @@ struct OFrvalue : OFrvalue_base<T>::type
 
 /** Upcast an rvalue reference to an rvalue reference of one of its bases.
  *  This is a helper macro for being used with DCMTK's fallback implementation
- *  of move semantics. C++11 rvalue references allow implicit upcasts,
- *  therefore, this macro has no effect if C++11 is enabled.
+ *  of move semantics. C++11 rvalue references should normally allow implicit
+ *  upcasts, therefore, this macro typically has no effect if C++11 is enabled
+ *  (it may be used to work around the behavior of older GCC versions).
  *  @param T the base class to upcast to
  *  @param RV the rvalue reference to upcast
  */
