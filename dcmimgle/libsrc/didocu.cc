DCMTK/dcmtk

commit 76ee9d7ec1e0c499d23aceb87dc6a52a3bc95b86

Print error details if conversion fails.
Print details of the returned OFCondition object in case pixel data
conversion fails.

dcmimgle/libsrc/didocu.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1996-2011, OFFIS e.V.
+ *  Copyright (C) 1996-2018, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -114,6 +114,7 @@ DiDocument::DiDocument(DcmObject *object,
 void DiDocument::convertPixelData()
 {
     DcmStack pstack;
+    OFCondition status;
     DcmXfer xfer(Xfer);
     DCMIMGLE_DEBUG("transfer syntax of DICOM dataset: " << xfer.getXferName() << " (" << xfer.getXferID() << ")");
     // only search on main dataset level
@@ -147,7 +148,8 @@ void DiDocument::convertPixelData()
                     pstack.push(Object);
                     // dummy stack entry
                     pstack.push(PixelData);
-                    if (PixelData->chooseRepresentation(EXS_LittleEndianExplicit, NULL, pstack).good())
+                    status = PixelData->chooseRepresentation(EXS_LittleEndianExplicit, NULL, pstack);
+                    if (status.good())
                     {
                         // set transfer syntax to unencapsulated/uncompressed
                         if (xfer.isEncapsulated())
@@ -156,7 +158,7 @@ void DiDocument::convertPixelData()
                             DCMIMGLE_DEBUG("decompressed complete pixel data in memory: " << PixelData->getLength(Xfer) << " bytes");
                         }
                     } else
-                        DCMIMGLE_ERROR("can't change to unencapsulated representation for pixel data");
+                        DCMIMGLE_ERROR("can't change to unencapsulated representation for pixel data: " << status.text());
                 }
                 // determine color model of the decompressed image
                 OFCondition status = PixelData->getDecompressedColorModel(OFstatic_cast(DcmItem *, Object), PhotometricInterpretation);

commit d3679d67a915055422eefcbbf3558dba88e35f93

Made log output more consistent.
Removed inconsistency of log output introduced with commit 76ee9d7.

dcmimgle/libsrc/didocu.cc
@@ -157,8 +157,10 @@ void DiDocument::convertPixelData()
                             Xfer = EXS_LittleEndianExplicit;
                             DCMIMGLE_DEBUG("decompressed complete pixel data in memory: " << PixelData->getLength(Xfer) << " bytes");
                         }
-                    } else
-                        DCMIMGLE_ERROR("can't change to unencapsulated representation for pixel data: " << status.text());
+                    } else {
+                        DCMIMGLE_ERROR("can't change to unencapsulated representation for pixel data");
+                        DCMIMGLE_DEBUG("DcmPixelData::chooseRepresentation() returned: " << status.text());
+                    }
                 }
                 // determine color model of the decompressed image
                 status = PixelData->getDecompressedColorModel(OFstatic_cast(DcmItem *, Object), PhotometricInterpretation);