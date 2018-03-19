4233d7daa56548b83d0cb3207046040a67982e89
Michael Onken <onken@open-connections.de>
2018-03-19 08:07:26 +0100

    Initialize members.

--- dcmiod/include/dcmtk/dcmiod/modequipment.h
+++ dcmiod/include/dcmtk/dcmiod/modequipment.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2015-2016, Open Connections GmbH
+ *  Copyright (C) 2015-2018, Open Connections GmbH
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation are maintained by
@@ -51,7 +51,11 @@ public:
 
       /** Default Constructor
        */
-      EquipmentInfo() {}
+      EquipmentInfo() :
+        m_Manufacturer(),
+        m_ManufacturerModelName(),
+        m_DeviceSerialNumber(),
+        m_SoftwareVersions() {}
 
       /** Convenience Constructor setting commonly used values
        *  @param manufacturer Manufacturer
