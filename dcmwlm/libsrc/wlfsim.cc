DCMTK/dcmtk

commit d92d5d1f47a7c00d4f3d53da3033d10a37da8697

Replaced strcpy by OFStandard::strlcpy.

dcmwlm/libsrc/wlfsim.cc
@@ -390,7 +390,7 @@ void WlmFileSystemInteractionManager::GetAttributeValueForMatchingRecord( DcmTag
       else
       {
         value = new char[ 2 ];
-        strcpy( value, "4" );           // a value of "4" in attribute PregnancyStatus means "unknown" in DICOM
+        OFStandard::strlcpy( value, "4", sizeof(value) );           // a value of "4" in attribute PregnancyStatus means "unknown" in DICOM
       }
     }
     else
@@ -399,12 +399,12 @@ void WlmFileSystemInteractionManager::GetAttributeValueForMatchingRecord( DcmTag
       if( cond.good() && val != NULL )
       {
         value = new char[ strlen( val ) + 1 ];
-        strcpy( value, val );
+        OFStandard::strlcpy( value, val, sizeof(value) );
       }
       else
       {
         value = new char[ 1 ];
-        strcpy( value, "" );
+        OFStandard::strlcpy( value, "", sizeof(value) );
       }
     }
   }
@@ -428,18 +428,18 @@ void WlmFileSystemInteractionManager::GetAttributeValueForMatchingRecord( DcmTag
       if( cond.good() && val != NULL )
       {
         value = new char[ strlen( val ) + 1 ];
-        strcpy( value, val );
+        OFStandard::strlcpy( value, val, sizeof(value) );
       }
       else
       {
         value = new char[ 1 ];
-        strcpy( value, "" );
+        OFStandard::strlcpy( value, "", sizeof(value) );
       }
     }
     else
     {
       value = new char[ 1 ];
-      strcpy( value, "" );
+      OFStandard::strlcpy( value, "", sizeof(value) );
     }
   }
 }

dcmdata/apps/dcmdump.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2017, OFFIS e.V.
+ *  Copyright (C) 1994-2018, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -133,7 +133,10 @@ static OFBool addPrintTagName(const char *tagName)
         printTagKeys[printTagCount] = NULL;
     }
 
-    printTagNames[printTagCount] = strcpy(OFstatic_cast(char*, malloc(strlen(tagName)+1)), tagName);
+    size_t buflen = strlen(tagName)+1;
+    char *buf = OFstatic_cast(char*, malloc(buflen));
+    OFStandard::strlcpy(buf, tagName, buflen);
+    printTagNames[printTagCount] = buf;
     printTagCount++;
     return OFTrue;
 }

dcmnet/apps/movescu.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2017, OFFIS e.V.
+ *  Copyright (C) 1994-2018, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -1421,7 +1421,7 @@ static OFCondition storeSCP(
 #ifdef _WIN32
         tmpnam(imageFileName);
 #else
-        strcpy(imageFileName, NULL_DEVICE_NAME);
+        OFStandard::strlcpy(imageFileName, NULL_DEVICE_NAME, 2048);
 #endif
     } else {
         sprintf(imageFileName, "%s.%s",
@@ -1646,14 +1646,14 @@ moveSCU(T_ASC_Association *assoc, const char *fname)
     callbackData.presId = presId;
 
     req.MessageID = msgId;
-    strcpy(req.AffectedSOPClassUID, sopClass);
+    OFStandard::strlcpy(req.AffectedSOPClassUID, sopClass, sizeof(req.AffectedSOPClassUID));
     req.Priority = DIMSE_PRIORITY_MEDIUM;
     req.DataSetType = DIMSE_DATASET_PRESENT;
     if (opt_moveDestination == NULL) {
         /* set the destination to be me */
         ASC_getAPTitles(assoc->params, req.MoveDestination, NULL, NULL);
     } else {
-        strcpy(req.MoveDestination, opt_moveDestination);
+        OFStandard::strlcpy(req.MoveDestination, opt_moveDestination, sizeof(req.MoveDestination));
     }
 
     if (movescuLogger.isEnabledFor(OFLogger::DEBUG_LOG_LEVEL))

dcmnet/apps/storescp.cc
@@ -2000,7 +2000,7 @@ static OFCondition storeSCP(
 #ifdef _WIN32
     tmpnam(imageFileName);
 #else
-    strcpy(imageFileName, NULL_DEVICE_NAME);
+    OFStandard::strlcpy(imageFileName, NULL_DEVICE_NAME, 2048);
 #endif
   }
   else

dcmnet/apps/storescu.cc
@@ -1341,8 +1341,8 @@ storeSCU(T_ASC_Association *assoc, const char *fname)
   /* prepare the transmission of data */
   bzero(OFreinterpret_cast(char *, &req), sizeof(req));
   req.MessageID = msgId;
-  strcpy(req.AffectedSOPClassUID, sopClass);
-  strcpy(req.AffectedSOPInstanceUID, sopInstance);
+  OFStandard::strlcpy(req.AffectedSOPClassUID, sopClass, sizeof(req.AffectedSOPClassUID));
+  OFStandard::strlcpy(req.AffectedSOPInstanceUID, sopInstance, sizeof(req.AffectedSOPInstanceUID));
   req.DataSetType = DIMSE_DATASET_PRESENT;
   req.Priority = DIMSE_PRIORITY_MEDIUM;
 

dcmnet/libsrc/assoc.cc
@@ -687,7 +687,7 @@ ASC_addPresentationContext(
         return EC_MemoryExhausted;
     }
     pc->presentationContextID = presentationContextID;
-    strcpy(pc->abstractSyntax, abstractSyntax);
+    OFStandard::strlcpy(pc->abstractSyntax, abstractSyntax, sizeof(pc->abstractSyntax));
     pc->result = ASC_P_NOTYETNEGOTIATED;
     pc->proposedSCRole = ascRole2dulRole(proposedRole);
     pc->acceptedSCRole = ascRole2dulRole(ASC_SC_ROLE_DEFAULT);
@@ -704,7 +704,7 @@ ASC_addPresentationContext(
     {
         transfer = (DUL_TRANSFERSYNTAX*)malloc(sizeof(DUL_TRANSFERSYNTAX));
         if (transfer == NULL) return EC_MemoryExhausted;
-        strcpy(transfer->transferSyntax, transferSyntaxList[i]);
+        OFStandard::strlcpy(transfer->transferSyntax, transferSyntaxList[i], sizeof(transfer->transferSyntax));
         LST_Enqueue(&lst, (LST_NODE*)transfer);
     }
     pc->proposedTransferSyntax = lst;
@@ -820,11 +820,11 @@ ASC_getPresentationContext(T_ASC_Parameters * params,
     presentationContext->resultReason = (T_ASC_P_ResultReason) pc->result;
     presentationContext->proposedRole = dulRole2ascRole(pc->proposedSCRole);
     presentationContext->acceptedRole = dulRole2ascRole(pc->acceptedSCRole);
-    strcpy(presentationContext->abstractSyntax,
-           pc->abstractSyntax);
+    OFStandard::strlcpy(presentationContext->abstractSyntax,
+           pc->abstractSyntax, sizeof(presentationContext->abstractSyntax));
     if (presentationContext->resultReason == ASC_P_ACCEPTANCE) {
-        strcpy(presentationContext->acceptedTransferSyntax,
-            pc->acceptedTransferSyntax);
+        OFStandard::strlcpy(presentationContext->acceptedTransferSyntax,
+            pc->acceptedTransferSyntax, sizeof(presentationContext->acceptedTransferSyntax));
     } else {
         presentationContext->acceptedTransferSyntax[0] = '\0';
     }
@@ -841,8 +841,8 @@ ASC_getPresentationContext(T_ASC_Parameters * params,
         {
           return makeDcmnetCondition(ASCC_CODINGERROR, OF_error, "ASC Coding error in ASC_getPresentationContext: too many transfer syntaxes");
         }
-        strcpy(presentationContext->proposedTransferSyntaxes[count],
-               transfer->transferSyntax);
+        OFStandard::strlcpy(presentationContext->proposedTransferSyntaxes[count],
+               transfer->transferSyntax, sizeof(presentationContext->proposedTransferSyntaxes[count]));
         count++;
         transfer = (DUL_TRANSFERSYNTAX*) LST_Next(l);
     }
@@ -872,7 +872,7 @@ ASC_acceptPresentationContext(
                               params->DULparams.requestedPresentationContext,
                                                 presentationContextID);
     if (proposedContext == NULL) return ASC_BADPRESENTATIONCONTEXTID;
-    strcpy(proposedContext->acceptedTransferSyntax, transferSyntax);
+    OFStandard::strlcpy(proposedContext->acceptedTransferSyntax, transferSyntax, sizeof(proposedContext->acceptedTransferSyntax));
 
     /* we want to mark this proposed context as being ok */
     proposedContext->result = ASC_P_ACCEPTANCE;
@@ -910,9 +910,9 @@ ASC_acceptPresentationContext(
     if (acceptedContext != NULL) {
         /* it is already in the list, mark it as accepted */
         acceptedContext->result = ASC_P_ACCEPTANCE;
-        strcpy(acceptedContext->abstractSyntax,
-               proposedContext->abstractSyntax);
-        strcpy(acceptedContext->acceptedTransferSyntax, transferSyntax);
+        OFStandard::strlcpy(acceptedContext->abstractSyntax,
+               proposedContext->abstractSyntax, sizeof(acceptedContext->abstractSyntax));
+        OFStandard::strlcpy(acceptedContext->acceptedTransferSyntax, transferSyntax, sizeof(acceptedContext->acceptedTransferSyntax));
         acceptedContext->proposedSCRole = proposedContext->proposedSCRole;
         acceptedContext->acceptedSCRole = ascRole2dulRole(acceptedRole);
     } else {
@@ -970,14 +970,14 @@ ASC_refusePresentationContext(
     if (acceptedContext != NULL) {
         /* it is already in the list, mark it as refused */
         acceptedContext->result = resultReason;
-        strcpy(acceptedContext->abstractSyntax,
-               proposedContext->abstractSyntax);
+        OFStandard::strlcpy(acceptedContext->abstractSyntax,
+               proposedContext->abstractSyntax, sizeof(acceptedContext->abstractSyntax));
         /* we must send back a transfer syntax even though this
          * presentation context is refused.  Some software implementations
          * seem to get confused if we don't.
          */
-        strcpy(acceptedContext->acceptedTransferSyntax,
-                UID_LittleEndianImplicitTransferSyntax);
+        OFStandard::strlcpy(acceptedContext->acceptedTransferSyntax,
+                UID_LittleEndianImplicitTransferSyntax, sizeof(acceptedContext->acceptedTransferSyntax));
     } else {
 
         /*
@@ -1912,7 +1912,7 @@ updateRequestedPCFromAcceptedPC(
 
     rpc->result = apc->result;
     if (apc->result == ASC_P_ACCEPTANCE) {
-        strcpy(rpc->acceptedTransferSyntax, apc->acceptedTransferSyntax);
+        OFStandard::strlcpy(rpc->acceptedTransferSyntax, apc->acceptedTransferSyntax, sizeof(rpc->acceptedTransferSyntax));
     } else {
         rpc->acceptedTransferSyntax[0] = '\0';
     }
@@ -2050,10 +2050,10 @@ ASC_requestAssociation(T_ASC_Network * network,
         (*assoc)->sendPDVLength = sendLen;
         (*assoc)->sendPDVBuffer = (unsigned char*)malloc(size_t(sendLen));
         if ((*assoc)->sendPDVBuffer == NULL) return EC_MemoryExhausted;
-        strcpy(params->theirImplementationClassUID,
-           params->DULparams.calledImplementationClassUID);
-        strcpy(params->theirImplementationVersionName,
-           params->DULparams.calledImplementationVersionName);
+        OFStandard::strlcpy(params->theirImplementationClassUID,
+           params->DULparams.calledImplementationClassUID, sizeof(params->theirImplementationClassUID));
+        OFStandard::strlcpy(params->theirImplementationVersionName,
+           params->DULparams.calledImplementationVersionName, sizeof(params->theirImplementationVersionName));
 
         /* make sure accepted PCs are marked as such in the requested PC list */
         cond = updateRequestedPCListFromAcceptedPCList(&params->DULparams);
@@ -2081,10 +2081,10 @@ ASC_acknowledgeAssociation(
       assoc->params->DULparams.maxPDU = dcmEnableBackwardCompatibility.get() | DUL_DULCOMPAT | DUL_DIMSECOMPAT;
     }
 
-    strcpy(assoc->params->DULparams.calledImplementationClassUID,
-        assoc->params->ourImplementationClassUID);
-    strcpy(assoc->params->DULparams.calledImplementationVersionName,
-        assoc->params->ourImplementationVersionName);
+    OFStandard::strlcpy(assoc->params->DULparams.calledImplementationClassUID,
+        assoc->params->ourImplementationClassUID, sizeof(assoc->params->DULparams.calledImplementationClassUID));
+    OFStandard::strlcpy(assoc->params->DULparams.calledImplementationVersionName,
+        assoc->params->ourImplementationVersionName, sizeof(assoc->params->DULparams.calledImplementationVersionName));
 
     OFCondition cond = DUL_AcknowledgeAssociationRQ(&assoc->DULassociation,
                                         &assoc->params->DULparams,

dcmnet/libsrc/dul.cc
@@ -141,6 +141,7 @@ END_EXTERN_C
 #include "dcmtk/dcmnet/cond.h"
 #include "dcmtk/dcmnet/lst.h"
 #include "dcmtk/ofstd/ofconsol.h"
+#include "dcmtk/ofstd/ofstd.h"
 
 #include "dcmtk/dcmnet/dul.h"
 #include "dulstruc.h"
@@ -2093,7 +2094,7 @@ createNetworkKey(const char *mode,
     }
     *key = (PRIVATE_NETWORKKEY *) malloc(sizeof(PRIVATE_NETWORKKEY));
     if (*key == NULL) return EC_MemoryExhausted;
-    (void) strcpy((*key)->keyType, KEY_NETWORK);
+    OFStandard::strlcpy((*key)->keyType, KEY_NETWORK, sizeof((*key)->keyType));
 
     (*key)->applicationFunction = 0;
 
@@ -2288,10 +2289,10 @@ createAssociationKey(PRIVATE_NETWORKKEY ** networkKey,
     if (key == NULL) return EC_MemoryExhausted;
     key->receivePDUQueue = NULL;
 
-    (void) strcpy(key->keyType, KEY_ASSOCIATION);
+    OFStandard::strlcpy(key->keyType, KEY_ASSOCIATION, sizeof(key->keyType));
     key->applicationFunction = (*networkKey)->applicationFunction;
 
-    (void) strcpy(key->remoteNode, remoteNode);
+    OFStandard::strlcpy(key->remoteNode, remoteNode, sizeof(key->remoteNode));
     key->presentationContextID = 0;
     key->timeout = (*networkKey)->timeout;
     key->timerStart = 0;
@@ -2385,11 +2386,11 @@ get_association_parameter(void *paramAddress,
     if ((paramType == DUL_K_STRING) && (outputLength < strlen((char*)paramAddress))) return DUL_INSUFFICIENTBUFFERLENGTH;
 
     switch (paramType) {
-    case DUL_K_INTEGER:
+      case DUL_K_INTEGER:
         (void) memcpy(outputAddress, paramAddress, paramLength);
         break;
-    case DUL_K_STRING:
-        strcpy((char*)outputAddress, (char*)paramAddress);
+      case DUL_K_STRING:
+        OFStandard::strlcpy((char*)outputAddress, (char*)paramAddress, outputLength);
         break;
     }
     return EC_Normal;

dcmpstat/apps/dcmpsrcv.cc
@@ -13,7 +13,7 @@
  *
  *  Module:  dcmpstat
  *
- *  Authors: Marco Eichelberg
+ *  Authors: Marco EichelbergimageFileName
  *
  *  Purpose: Presentation State Viewer - Network Receive Component (Store SCP)
  *
@@ -40,6 +40,7 @@ END_EXTERN_C
 #include "dcmtk/dcmnet/diutil.h"
 #include "dcmtk/dcmdata/cmdlnarg.h"
 #include "dcmtk/ofstd/ofconapp.h"
+#include "dcmtk/ofstd/ofstd.h"
 #include "dcmtk/dcmqrdb/dcmqrdbi.h"     /* for LOCK_IMAGE_FILES */
 #include "dcmtk/dcmqrdb/dcmqrdbs.h"     /* for DcmQueryRetrieveDatabaseStatus */
 #include "dcmtk/dcmpstat/dvpsmsg.h"     /* for class DVPSIPCClient */
@@ -503,7 +504,7 @@ static OFCondition storeSCP(
         /* callback will send back sop class not supported status */
         status = STATUS_STORE_Refused_SOPClassNotSupported;
         /* must still receive data */
-        strcpy(imageFileName, NULL_DEVICE_NAME);
+        OFStandard::strlcpy(imageFileName, NULL_DEVICE_NAME, sizeof(imageFileName));
     }
     else
     {
@@ -512,7 +513,7 @@ static OFCondition storeSCP(
       {
         OFLOG_ERROR(dcmpsrcvLogger, "Unable to access database '" << dbfolder << "'");
         /* must still receive data */
-        strcpy(imageFileName, NULL_DEVICE_NAME);
+        OFStandard::strlcpy(imageFileName, NULL_DEVICE_NAME, sizeof(imageFileName));
         /* callback will send back out of resources status */
         status = STATUS_STORE_Refused_OutOfResources;
         dbhandle = NULL;
@@ -526,7 +527,7 @@ static OFCondition storeSCP(
         {
             OFLOG_ERROR(dcmpsrcvLogger, "storeSCP: Database: DB_makeNewStoreFileName Failed");
             /* must still receive data */
-            strcpy(imageFileName, NULL_DEVICE_NAME);
+            OFStandard::strlcpy(imageFileName, NULL_DEVICE_NAME, sizeof(imageFileName));
             /* callback will send back out of resources status */
             status = STATUS_STORE_Refused_OutOfResources;
         }
@@ -564,11 +565,8 @@ static OFCondition storeSCP(
     if (cond.bad() || (context.status != STATUS_Success))
     {
         /* remove file */
-        if (strcpy(imageFileName, NULL_DEVICE_NAME) != 0)
-        {
-          OFLOG_INFO(dcmpsrcvLogger, "Store SCP: Deleting Image File: " << imageFileName);
-          OFStandard::deleteFile(imageFileName);
-        }
+        OFLOG_INFO(dcmpsrcvLogger, "Store SCP: Deleting Image File: " << imageFileName);
+        OFStandard::deleteFile(imageFileName);
         if (dbhandle) dbhandle->pruneInvalidRecords();
     }
 

commit 82a740ed5c9f0ffda3cfcf4c0494c8df4f09a408

Fixed incorrect strlcpy buffer size.
Fixed incorrect strlcpy buffer size introduced with commit d92d5d1f.

dcmwlm/libsrc/wlfsim.cc
@@ -370,6 +370,7 @@ void WlmFileSystemInteractionManager::GetAttributeValueForMatchingRecord( DcmTag
   unsigned long i;
   const char *val = NULL;
   Uint16 v;
+  size_t len = 0;
 
   // if the element in question is not contained in another sequence
   if( numOfSuperiorSequences == 0 )
@@ -384,13 +385,15 @@ void WlmFileSystemInteractionManager::GetAttributeValueForMatchingRecord( DcmTag
       cond = matchingRecords[idx]->findAndGetUint16( tag, v, 0, OFFalse );
       if( cond.good() )
       {
-        value = new char[20];
+        len = 20;
+        value = new char[len];
         sprintf( value, "%u", v );
       }
       else
       {
-        value = new char[ 2 ];
-        OFStandard::strlcpy( value, "4", sizeof(value) );           // a value of "4" in attribute PregnancyStatus means "unknown" in DICOM
+        len = 2;
+        value = new char[len];
+        OFStandard::strlcpy( value, "4", len );           // a value of "4" in attribute PregnancyStatus means "unknown" in DICOM
       }
     }
     else
@@ -398,13 +401,15 @@ void WlmFileSystemInteractionManager::GetAttributeValueForMatchingRecord( DcmTag
       cond = matchingRecords[idx]->findAndGetString( tag, val, OFFalse );
       if( cond.good() && val != NULL )
       {
-        value = new char[ strlen( val ) + 1 ];
-        OFStandard::strlcpy( value, val, sizeof(value) );
+        len = strlen( val ) + 1;
+        value = new char[len];
+        OFStandard::strlcpy( value, val, len );
       }
       else
       {
-        value = new char[ 1 ];
-        OFStandard::strlcpy( value, "", sizeof(value) );
+        len = 1;
+        value = new char[len];
+        OFStandard::strlcpy( value, "", len );
       }
     }
   }
@@ -427,19 +432,22 @@ void WlmFileSystemInteractionManager::GetAttributeValueForMatchingRecord( DcmTag
       cond = sequenceElement->getItem( superiorSequenceArray[ numOfSuperiorSequences - 1 ].currentItem )->findAndGetString( tag, val, OFFalse );
       if( cond.good() && val != NULL )
       {
-        value = new char[ strlen( val ) + 1 ];
-        OFStandard::strlcpy( value, val, sizeof(value) );
+        len = strlen( val ) + 1;
+        value = new char[len];
+        OFStandard::strlcpy( value, val, len );
       }
       else
       {
-        value = new char[ 1 ];
-        OFStandard::strlcpy( value, "", sizeof(value) );
+        len = 1;
+        value = new char[len];
+        OFStandard::strlcpy( value, "", len );
       }
     }
     else
     {
-      value = new char[ 1 ];
-      OFStandard::strlcpy( value, "", sizeof(value) );
+      len = 1;
+      value = new char[len];
+      OFStandard::strlcpy( value, "", len );
     }
   }
 }