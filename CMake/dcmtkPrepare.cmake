c9d5b7d15fe9d8532680e72b1e4838b224b4bfc6
Michael Onken <onken@open-connections.de>
2018-09-04 18:18:14 +0200

    Load external dictionaries on Windows per default.
    An earlier commit (49b2a6) refactored evaluation of dictionary
    configuration leading to the current situation where an external
    dictionary specified by DCMTK's DCMDICTPATH environment variable
    is not read on DCMTK startup on Windows systems.
    
    This commit reverts that change, i.e. it ensures that external
    dictionaries specified via DCMDICTPATH are loaded per default on
    Windows.

--- CMake/dcmtkPrepare.cmake
+++ CMake/dcmtkPrepare.cmake
@@ -123,7 +123,7 @@ DCMTK_INFERABLE_OPTION(DCMTK_ENABLE_STL_SYSTEM_ERROR "Enable use of STL system_e
 # the external default dictionary if it is already compiled in.
 if(WIN32 OR MINGW)
   option(DCMTK_ENABLE_BUILTIN_DICTIONARY "Configure DCMTK with compiled-in data dictionary." ON)
-  option(DCMTK_ENABLE_EXTERNAL_DICTIONARY "Configure DCMTK to load external dictionary from default path on startup." OFF)
+  option(DCMTK_ENABLE_EXTERNAL_DICTIONARY "Configure DCMTK to load external dictionary from default path on startup." ON)
 else() # built-in dictionary turned off on Unix per default
   option(DCMTK_ENABLE_BUILTIN_DICTIONARY "Configure DCMTK with compiled-in data dictionary." OFF)
   option(DCMTK_ENABLE_EXTERNAL_DICTIONARY "Configure DCMTK to load external dictionary from default path on startup." ON)
