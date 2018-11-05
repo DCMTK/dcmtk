16e121fb91b4488aebef53d02349a7fca8ce1a0d
Jan Schlamelcher <dicom@offis.de>
2018-11-05 11:50:08 +0100

    Added forgotten configure test source file.

---
+++ config/tests/iconv.cc
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (C) 2018, OFFIS e.V.
+ *  All rights reserved.  See COPYRIGHT file for details.
+ *
+ *  This software and supporting documentation were developed by
+ *
+ *    OFFIS e.V.
+ *    R&D Division Health
+ *    Escherweg 2
+ *    D-26121 Oldenburg, Germany
+ *
+ *
+ *  Module:  config
+ *
+ *  Author:  Jan Schlamelcher
+ *
+ *  Purpose: Analyze behavior of an STDLIBC iconv implementation.
+ */
+
+#include <iconv.h>
+#include <stdio.h>
+
+int main()
+{
+    char input[2] = { '\366', '\0' };
+    char output[8];
+    iconv_t i = iconv_open( "ISO-8859-1", "ASCII" );
+    if( (iconv_t)-1 != i )
+    {
+        iconv( i, 0, 0, 0, 0 );
+        char* in = input;
+        char* out = output;
+        size_t ins = 1;
+        size_t outs = 8;
+        const size_t result = iconv( i, &in, &ins, &out, &outs );
+        iconv_close( i );
+        if( ~(size_t)0 == result )
+        {
+            printf( "AbortTranscodingOnIllegalSequence" );
+            return 0;
+        }
+        if( !output[0] )
+        {
+            printf( "DiscardIllegalSequences" );
+            return 0;
+        }
+    }
+    return 1;
+}
