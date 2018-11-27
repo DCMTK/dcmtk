97e6f9f6091bcbdf7df77571df4088fc3a2a2bd1
Jan Schlamelcher <dicom@offis.de>
2018-11-27 11:33:26 +0100

    Updated DIMSE compatibility flag.
    Updated DIMSE compatibility flag to reflect recent changes in the networking
    code. Windows now uses socklen_t as it originally should, due to the
    respective configuration test having being fixed recently. This normally
    should not affect the behavior of the network code (as socklen_t should
    typically be defined to the type that was used before anyway), but better safe
    than sorry.

--- dcmnet/include/dcmtk/dcmnet/dul.h
+++ dcmnet/include/dcmtk/dcmnet/dul.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2017, OFFIS e.V.
+ *  Copyright (C) 1994-2018, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were partly developed by
@@ -402,7 +402,7 @@ typedef enum {
 */
 
 #define DUL_DULCOMPAT     2768240730UL
-#define DUL_DIMSECOMPAT   851968UL
+#define DUL_DIMSECOMPAT   917504UL
 #define DUL_MAXPDUCOMPAT  4278190335UL
 
 /* Define the function prototypes for this facility.
