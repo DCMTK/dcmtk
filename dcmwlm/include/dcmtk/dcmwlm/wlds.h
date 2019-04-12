DCMTK/dcmtk

commit 459a638e4dab967b065811a9cf63af0604ec46f1

New wlmscpfs options (dumping requests, sleeping).
Added new options --request-file-path and --request-file-format to
wlmscpfs in order to allow dumping incoming C-FIND requests to text
files using a configurable directory and file name.

Added option --sleep-before that tells wlmscpfs to wait a given number
of seconds before evaluating an incoming C-FIND response.

All options together allows users to "interactively" fill the worklist
database for an incoming request.

Acknowledgement: Thanks to Viximed for sponsoring this feature.

dcmwlm/include/dcmtk/dcmwlm/wlds.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1996-2017, OFFIS e.V.
+ *  Copyright (C) 1996-2019, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -21,6 +21,12 @@
 
 #ifndef WlmDataSource_h
 #define WlmDataSource_h
+
+#define WLM_CALLING_AETITLE_PLACEHOLDER "#a"
+#define WLM_CALLED_AETITLE_PLACEHOLDER "#c"
+#define WLM_TIMESTAMP_PLACEHOLDER "#t"
+#define WLM_PATIENT_ID_PLACEHOLDER "#p"
+#define WLM_PROCESS_ID_PLACEHOLDER "#i"
 
 #include "dcmtk/config/osconfig.h"
 #include "dcmtk/dcmwlm/wltypdef.h"
@@ -48,6 +54,8 @@ class DCMTK_DCMWLM_EXPORT WlmDataSource
 
     /// indicates if the application shall fail on an invalid C-Find RQ message
     OFBool failOnInvalidQuery;
+    /// calling AE Title
+    OFString callingApplicationEntityTitle;
     /// called AE title
     OFString calledApplicationEntityTitle;
     /// the search mask which is contained in the C-Find RQ message
@@ -345,11 +353,6 @@ class DCMTK_DCMWLM_EXPORT WlmDataSource
        * @return Indicates if the disconnection was completed successfully.
        */
     virtual OFCondition DisconnectFromDataSource() = 0;
-
-      /** Set value in member variable.
-       *  @param value The value to set.
-       */
-    void SetCalledApplicationEntityTitle( const OFString& value );
 
       /** Set value in member variable.
        *  @param value The value to set.

dcmwlm/include/dcmtk/dcmwlm/wldsfs.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1996-2011, OFFIS e.V.
+ *  Copyright (C) 1996-2019, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by

dcmwlm/libsrc/wlds.cc
@@ -53,7 +53,7 @@ WlmDataSource::WlmDataSource()
 // Task         : Constructor.
 // Parameters   : none.
 // Return Value : none.
-  : failOnInvalidQuery( OFTrue ), calledApplicationEntityTitle(""),
+  : failOnInvalidQuery( OFTrue ), callingApplicationEntityTitle(""), calledApplicationEntityTitle(""),
     identifiers( NULL ), errorElements( NULL ), offendingElements( NULL ), errorComment( NULL ),
     foundUnsupportedOptionalKey( OFFalse ), readLockSetOnDataSource( OFFalse ),
     noSequenceExpansion( OFFalse ), returnedCharacterSet( RETURN_NO_CHARACTER_SET ), matchingDatasets(),
@@ -88,18 +88,6 @@ WlmDataSource::~WlmDataSource()
   delete offendingElements;
   delete errorElements;
   delete errorComment;
-}
-
-// ----------------------------------------------------------------------------
-
-void WlmDataSource::SetCalledApplicationEntityTitle( const OFString& value )
-// Date         : December 10, 2001
-// Author       : Thomas Wilkens
-// Task         : Sets the member variable that specifies called application entity title.
-// Parameters   : value - Value for calledApplicationEntityTitle.
-// Return Value : none.
-{
-  calledApplicationEntityTitle = value;
 }
 
 // ----------------------------------------------------------------------------

dcmwlm/libsrc/wldsfs.cc
@@ -27,9 +27,13 @@ BEGIN_EXTERN_C
 #include <fcntl.h>     // for O_RDWR
 #endif
 END_EXTERN_C
-#include "dcmtk/dcmnet/dicom.h"
-#include "dcmtk/dcmwlm/wltypdef.h"
 #include "dcmtk/ofstd/oftypes.h"
+#include "dcmtk/ofstd/ofstd.h"
+#include "dcmtk/ofstd/ofdatime.h"
+#include "dcmtk/oflog/internal/env.h"
+#include "dcmtk/dcmnet/dicom.h"
+#include "dcmtk/dcmnet/dimse.h"
+#include "dcmtk/dcmwlm/wltypdef.h"
 #include "dcmtk/dcmdata/dcdatset.h"
 #include "dcmtk/dcmdata/dcsequen.h"
 #include "dcmtk/dcmdata/dcvrat.h"
@@ -38,7 +42,6 @@ END_EXTERN_C
 #include "dcmtk/dcmdata/dcvrcs.h"
 #include "dcmtk/dcmwlm/wlds.h"
 #include "dcmtk/dcmwlm/wlfsim.h"
-#include "dcmtk/ofstd/ofstd.h"
 
 #include "dcmtk/dcmwlm/wldsfs.h"
 
@@ -50,8 +53,7 @@ WlmDataSourceFileSystem::WlmDataSourceFileSystem()
 // Task         : Constructor.
 // Parameters   : none.
 // Return Value : none.
-  : fileSystemInteractionManager( ), dfPath( "" ), enableRejectionOfIncompleteWlFiles( OFTrue ),
-    handleToReadLockFile( 0 )
+  : fileSystemInteractionManager( ), dfPath( "" ), enableRejectionOfIncompleteWlFiles( OFTrue ), handleToReadLockFile( 0 )
 {
 }
 
@@ -303,8 +305,8 @@ WlmDataSourceStatusType WlmDataSourceFileSystem::StartFindRequest( const DcmData
     << "=============================");
 
   // Set a read lock on the worklist files which shall be read from.
-  if( !SetReadlock() )
-    return( WLM_REFUSED_OUT_OF_RESOURCES );
+  if (!SetReadlock())
+    return(WLM_REFUSED_OUT_OF_RESOURCES);
 
   // dump some information if required
   DCMWLM_INFO("Determining matching records from worklist files");

dcmwlm/libsrc/wlmactmg.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1996-2018, OFFIS e.V.
+ *  Copyright (C) 1996-2019, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -39,16 +39,19 @@
 #include "dcmtk/dcmnet/dimse.h"
 #include "dcmtk/dcmnet/diutil.h"
 #include "dcmtk/ofstd/ofstd.h"
+#include "dcmtk/ofstd/ofdatime.h"
 #include "dcmtk/dcmdata/dcdicent.h"  // needed by MSVC5 with STL
+#include "dcmtk/oflog/internal/env.h"
 #include "dcmtk/dcmwlm/wlmactmg.h"
+
 
 // ----------------------------------------------------------------------------
 
-// We need two global functions, because we need to pass a function pointer for a callback function
+// We need three global functions, because we need to pass a function pointer for a callback function
 // to a certain function in dcmnet. This function pointer cannot point to an element function of the
 // above defined class, because dcmnet expects it to be a pointer to a global function. Hence, function
-// FindCallback() needs to be global. Function AddStatusDetail() is used in FindCallback() that's why
-// it is also defined as global.
+// FindCallback() needs to be global. Function AddStatusDetail() and storeRequestFile() are used in
+// FindCallback() that's why they are also defined as global.
 
 static void FindCallback( void *callbackData, OFBool cancelled, T_DIMSE_C_FindRQ * /*request*/, DcmDataset *requestIdentifiers, int responseCount, T_DIMSE_C_FindRSP *response, DcmDataset **responseIdentifiers, DcmDataset **statusDetail );
 // Task         : This function will try to select another record from a database which matches the
@@ -79,6 +82,16 @@ static OFString AddStatusDetail( DcmDataset **statusDetail, const DcmElement *el
 //                               container.
 // Return Value : none.
 
+static void storeRequestToFile(DcmDataset& request, const OFString& callingAE, const OFString& calledAE, const OFString& reqFilePath, const OFString& reqFileFormat);
+// Task        : Store request to file using the directory and file name format
+// Parameters  : request       - [in] The incoming C-FIND request
+//               callingAE     - [in] The peer's Calling AE Title
+//               calledAE      - [in] Our AE Title
+//               reqFilePath   - [in] The request file directory to write to
+//               reqFileFormat - [in] The request file name format to use
+// Return Value: none
+
+
 // ----------------------------------------------------------------------------
 
 WlmActivityManager::WlmActivityManager(
@@ -86,6 +99,7 @@ WlmActivityManager::WlmActivityManager(
     OFCmdUnsignedInt opt_portv,
     OFBool opt_refuseAssociationv,
     OFBool opt_rejectWithoutImplementationUIDv,
+    OFCmdUnsignedInt opt_sleepBeforeFindReqv,
     OFCmdUnsignedInt opt_sleepAfterFindv,
     OFCmdUnsignedInt opt_sleepDuringFindv,
     OFCmdUnsignedInt opt_maxPDUv,
@@ -106,6 +120,7 @@ WlmActivityManager::WlmActivityManager(
 //                opt_portv                           - [in] The port on which the application is supposed to listen.
 //                opt_refuseAssociationv              - [in] Specifies if an association shall always be refused by the SCP.
 //                opt_rejectWithoutImplementationUIDv - [in] Specifies if the application shall reject an association if no implementation class UID is provided by the calling SCU.
+//                opt_sleepBeforeFindReqv             - [in] Specifies how many seconds the application is supposed to sleep before handling a C-FIND-Req.
 //                opt_sleepAfterFindv                 - [in] Specifies how many seconds the application is supposed to sleep after having handled a C-FIND-Rsp.
 //                opt_sleepDuringFindv                - [in] Specifies how many seconds the application is supposed to sleep during the handling of a C-FIND-Rsp.
 //                opt_maxPDUv                         - [in] Maximum length of a PDU that can be received in bytes.
@@ -118,7 +133,7 @@ WlmActivityManager::WlmActivityManager(
 //                argvv                               - [in/out] Holds complete commandline
 // Return Value : none.
   : dataSource( dataSourcev ), opt_port( opt_portv ), opt_refuseAssociation( opt_refuseAssociationv ),
-    opt_rejectWithoutImplementationUID( opt_rejectWithoutImplementationUIDv ),
+    opt_rejectWithoutImplementationUID( opt_rejectWithoutImplementationUIDv ), opt_sleepBeforeFindReq( opt_sleepBeforeFindReqv ),
     opt_sleepAfterFind( opt_sleepAfterFindv ), opt_sleepDuringFind( opt_sleepDuringFindv ),
     opt_maxPDU( opt_maxPDUv ), opt_networkTransferSyntax( opt_networkTransferSyntaxv ),
     opt_failInvalidQuery( opt_failInvalidQueryv ),
@@ -162,6 +177,25 @@ WlmActivityManager::~WlmActivityManager()
   delete[] supportedAbstractSyntaxes;
 
   OFStandard::shutdownNetwork();
+}
+
+// ----------------------------------------------------------------------------
+
+OFBool WlmActivityManager::setRequestFilePath(const OFString& path, const OFString& format)
+// Date         : March 08, 2019
+// Author       : Michael Onken
+// Task         : Set the directory where to store request files to.
+// Parameters   : path   - [in] The directory to store request files to.
+//                format - [in] the format for the request file names.
+// Return Value : OFTrue if directory and format is accepted, OFFalse otherwise.
+{
+  if (OFStandard::dirExists(path) && OFStandard::isWriteable(path))
+  {
+    opt_requestFilePath = path;
+    opt_requestFileFormat = format;
+    return OFTrue;
+  }
+  return OFFalse;
 }
 
 // ----------------------------------------------------------------------------
@@ -427,7 +461,6 @@ OFCondition WlmActivityManager::WaitForAssociation( T_ASC_Network * net )
 
   // Condition 5: if the called application entity title is not supported
   // within the data source we want to refuse the association request
-  dataSource->SetCalledApplicationEntityTitle( assoc->params->DULparams.calledAPTitle );
   if( !dataSource->IsCalledApplicationEntityTitleSupported() )
   {
     RefuseAssociation( &assoc, WLM_BAD_AE_SERVICE );
@@ -738,10 +771,33 @@ struct WlmFindContextType
 //          in wltypdef.h because it makes use of class WlmDataSource which is
 //          unknown in wltypdef.h.)
 {
+  WlmFindContextType() :
+    dataSource(NULL),
+    priorStatus(WLM_SUCCESS),
+    ourAETitle(""),
+    theirAETitle(""),
+    opt_sleepBeforeFindReq(0),
+    opt_sleepDuringFind(0),
+    opt_reqFilePath(),
+    opt_reqFileFormat("#t.dump") {};
+
   WlmDataSource *dataSource;
   WlmDataSourceStatusType priorStatus;
   DIC_AE ourAETitle;
+  DIC_AE theirAETitle;
+  OFCmdUnsignedInt opt_sleepBeforeFindReq;
   OFCmdUnsignedInt opt_sleepDuringFind;
+  /// directory to store request files to (if enabled, otherwise empty)
+  OFString opt_reqFilePath;
+  /// request file name format:
+  /// Several placeholder can be used by(denoted by #) :
+  /// #a: calling application entity title of the peer Storage SCU
+  /// #c: called application entity title used by the peer Storage SCU to address storescp
+  /// #p: patient ID if present, otherwise empty string
+  /// #t: timestamp in the format YYYYMMDDhhmmssffffff
+  /// Default is #t.dump
+  OFString opt_reqFileFormat;
+
 };
 
 // ----------------------------------------------------------------------------
@@ -762,8 +818,11 @@ OFCondition WlmActivityManager::HandleFindSCP( T_ASC_Association *assoc, T_DIMSE
   WlmFindContextType context;
   context.dataSource = dataSource;
   context.priorStatus = WLM_PENDING;
-  ASC_getAPTitles( assoc->params, NULL, 0, context.ourAETitle, sizeof(context.ourAETitle), NULL, 0);
+  ASC_getAPTitles( assoc->params, context.theirAETitle, sizeof(context.theirAETitle), context.ourAETitle, sizeof(context.ourAETitle), NULL, 0);
   context.opt_sleepDuringFind = opt_sleepDuringFind;
+  context.opt_sleepBeforeFindReq = opt_sleepBeforeFindReq;
+  context.opt_reqFilePath = opt_requestFilePath;
+  context.opt_reqFileFormat = opt_requestFileFormat;
 
   // Dump some information if required.
   DCMWLM_INFO(DIMSE_dumpMessage(temp_str, *request, DIMSE_INCOMING, NULL, presID));
@@ -1027,11 +1086,12 @@ static void FindCallback( void *callbackData, OFBool cancelled, T_DIMSE_C_FindRQ
   WlmFindContextType *context = NULL;
   WlmDataSource *dataSource = NULL;
   OFCmdUnsignedInt opt_sleepDuringFind = 0;
-
+  OFCmdUnsignedInt opt_sleepBeforeFindReq = 0;
   // Recover contents of context.
   context = (WlmFindContextType*)callbackData;
   dataSource = context->dataSource;
   opt_sleepDuringFind = context->opt_sleepDuringFind;
+  opt_sleepBeforeFindReq = context->opt_sleepBeforeFindReq;
 
   // Determine the data source's current status.
   dbstatus = context->priorStatus;
@@ -1043,6 +1103,20 @@ static void FindCallback( void *callbackData, OFBool cancelled, T_DIMSE_C_FindRQ
     DCMWLM_INFO("Find SCP Request Identifiers:" << OFendl
       << DcmObject::PrintHelper(*requestIdentifiers) << OFendl
       << "=============================");
+
+    // If desired, dump request to file
+    if (!context->opt_reqFilePath.empty())
+    {
+      DCMWLM_INFO("Storing request dataset to file");
+      storeRequestToFile(*requestIdentifiers, context->theirAETitle, context->ourAETitle, context->opt_reqFilePath, context->opt_reqFileFormat);
+    }
+
+    // If desired, sleep before actually trying to get answer for FIND request
+    if (opt_sleepBeforeFindReq > 0)
+    {
+      DCMWLM_INFO("SLEEPING (before evaluating find request): " << opt_sleepBeforeFindReq << " secs");
+      OFStandard::sleep((unsigned int)opt_sleepBeforeFindReq);
+    }
 
     // Determine the records that match the search mask. After this call, the
     // matching records will be available through dataSource->nextFindResponse(...).)
@@ -1109,5 +1183,55 @@ static void FindCallback( void *callbackData, OFBool cancelled, T_DIMSE_C_FindRQ
     default:
       // other status codes may not have any status detail
       break;
+  }
+}
+
+// ----------------------------------------------------------------------------
+
+static void storeRequestToFile(DcmDataset& request, const OFString& callingAE, const OFString& calledAE, const OFString& reqFilePath, const OFString& reqFileFormat)
+{
+  OFString fileName = reqFileFormat;
+  // Called Application Entity Title
+  OFString::replace_all(fileName, WLM_CALLED_AETITLE_PLACEHOLDER, calledAE);
+  // Calling Application Entity Title
+  OFString::replace_all(fileName, WLM_CALLING_AETITLE_PLACEHOLDER, callingAE);
+
+  // Process ID
+  int processID = dcmtk::log4cplus::internal::get_process_id();
+  OFOStringStream convInt;
+  convInt << processID;
+  OFString::replace_all(fileName, WLM_PROCESS_ID_PLACEHOLDER, convInt.str().c_str());
+
+  // Timestamp
+  if (reqFileFormat.find("#t") != OFString_npos)
+  {
+    OFString ts;
+    OFDateTime dt;
+    dt.setCurrentDateTime();
+    dt.getISOFormattedDateTime(ts, OFTrue /* seconds */, OFTrue /* fraction */, OFFalse /* no tz */, OFFalse /* no delimiters */, "" /* no date / time separator */);
+    OFString::replace_all(ts, ".", "");
+    OFString::replace_all(fileName, WLM_TIMESTAMP_PLACEHOLDER, ts);
+  }
+
+  // Patient ID goes last since it might contain placeholders again (".#x...)"
+  OFString patientID;
+  request.findAndGetOFStringArray(DCM_PatientID, patientID);
+  OFString::replace_all(fileName, WLM_PATIENT_ID_PLACEHOLDER, patientID);
+
+  // Finally store file
+  STD_NAMESPACE ofstream outputStream;
+  OFString fullPath;
+  OFStandard::combineDirAndFilename(fullPath, reqFilePath, fileName, OFFalse /* no empty dir name, shouldnt happen anyway...*/);
+  outputStream.open(fullPath.c_str());
+  if (outputStream.good())
+  {
+    DcmObject::PrintHelper printer(request);
+    outputStream << printer;
+    outputStream.close();
+  }
+  if (!outputStream)
+  {
+    /* report details on file i/o error */
+    DCMWLM_ERROR("Could not write request to file: " << fileName << ": " << OFStandard::getLastSystemErrorCode().message());
   }
 }

dcmwlm/apps/wlcefs.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1996-2017, OFFIS e.V.
+ *  Copyright (C) 1996-2019, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -68,8 +68,9 @@ WlmConsoleEngineFileSystem::WlmConsoleEngineFileSystem( int argc, char *argv[], 
 //                dataSourcev     - [in] Pointer to the dataSource object.
 // Return Value : none.
   : opt_returnedCharacterSet( RETURN_NO_CHARACTER_SET ),
-    opt_dfPath( "" ), opt_port( 0 ), opt_refuseAssociation( OFFalse ),
-    opt_rejectWithoutImplementationUID( OFFalse ), opt_sleepAfterFind( 0 ), opt_sleepDuringFind( 0 ),
+    opt_dfPath( "" ), opt_rfPath( "" ), opt_rfFormat( "#t.dump" ), opt_refuseAssociation( OFFalse ),
+    opt_rejectWithoutImplementationUID( OFFalse ), opt_sleepBeforeFindReq ( 0 ),
+    opt_sleepAfterFind( 0 ), opt_sleepDuringFind( 0 ),
     opt_maxPDU( ASC_DEFAULTMAXPDU ), opt_networkTransferSyntax( EXS_Unknown ),
     opt_failInvalidQuery( OFTrue ), opt_singleProcess( OFTrue ),
     opt_forkedChild( OFFalse ), opt_maxAssociations( 50 ), opt_noSequenceExpansion( OFFalse ),
@@ -119,6 +120,7 @@ WlmConsoleEngineFileSystem::WlmConsoleEngineFileSystem( int argc, char *argv[], 
       OFString opt5 = "[p]ath: string (default: ";
       opt5 += opt_dfPath;
       opt5 += ")";
+
       cmd->addOption("--data-files-path",     "-dfp", 1, opt5.c_str(), "path to worklist data files" );
     cmd->addSubGroup("handling of worklist files:");
       cmd->addOption("--enable-file-reject",  "-efr",    "enable rejection of incomplete worklist files\n(default)");
@@ -131,6 +133,9 @@ WlmConsoleEngineFileSystem::WlmConsoleEngineFileSystem( int argc, char *argv[], 
       cmd->addOption("--keep-char-set",       "-csk",    "return character set provided in file");
     cmd->addSubGroup("other processing options:");
       cmd->addOption("--no-sq-expansion",     "-nse",    "disable expansion of empty sequences in C-FIND\nrequest messages");
+      cmd->addOption("--request-file-path",  "-rfp", 1,   "[p]ath: string", "path to store request files to");
+      cmd->addOption("--request-file-format","-rff", 1,  "[f]ormat: string (default: #t.dump)", "request file name format");
+
 
   cmd->addGroup("network options:");
     cmd->addSubGroup("preferred network transfer syntaxes:");
@@ -170,6 +175,7 @@ WlmConsoleEngineFileSystem::WlmConsoleEngineFileSystem( int argc, char *argv[], 
       cmd->addOption("--refuse",                         "refuse association");
       cmd->addOption("--reject",                         "reject association if no implement. class UID");
       cmd->addOption("--no-fail",                        "don't fail on an invalid query");
+      cmd->addOption("--sleep-before",                1, "[s]econds: integer", "sleep s seconds before find (default: 0)");
       cmd->addOption("--sleep-after",                 1, "[s]econds: integer", "sleep s seconds after find (default: 0)");
       cmd->addOption("--sleep-during",                1, "[s]econds: integer", "sleep s seconds during find (default: 0)");
       OFString opt3 = "set max receive pdu to n bytes (default: ";
@@ -228,6 +234,12 @@ WlmConsoleEngineFileSystem::WlmConsoleEngineFileSystem( int argc, char *argv[], 
 #endif
 
     if( cmd->findOption("--data-files-path") ) app->checkValue(cmd->getValue(opt_dfPath));
+    if( cmd->findOption("--request-file-path") ) app->checkValue(cmd->getValue(opt_rfPath));
+    if( cmd->findOption("--request-file-format") )
+    {
+        app->checkDependence("--request-file-format", "--request-file-path", !opt_rfPath.empty());
+        app->checkValue(cmd->getValue(opt_rfFormat));
+    }
 
     cmd->beginOptionBlock();
     if( cmd->findOption("--enable-file-reject") ) opt_enableRejectionOfIncompleteWlFiles = OFTrue;
@@ -299,6 +311,7 @@ WlmConsoleEngineFileSystem::WlmConsoleEngineFileSystem( int argc, char *argv[], 
     if( cmd->findOption("--refuse") ) opt_refuseAssociation = OFTrue;
     if( cmd->findOption("--reject") ) opt_rejectWithoutImplementationUID = OFTrue;
     if( cmd->findOption("--no-fail") ) opt_failInvalidQuery = OFFalse;
+    if( cmd->findOption("--sleep-before") ) app->checkValue(cmd->getValueAndCheckMin(opt_sleepBeforeFindReq, 0));
     if( cmd->findOption("--sleep-after") ) app->checkValue(cmd->getValueAndCheckMin(opt_sleepAfterFind, 0));
     if( cmd->findOption("--sleep-during") ) app->checkValue(cmd->getValueAndCheckMin(opt_sleepDuringFind, 0));
     if( cmd->findOption("--max-pdu") ) app->checkValue(cmd->getValueAndCheckMinMax(opt_maxPDU, ASC_MINIMUMPDUSIZE, ASC_MAXIMUMPDUSIZE));
@@ -360,15 +373,35 @@ int WlmConsoleEngineFileSystem::StartProvidingService()
 
   // start providing the basic worklist management service
   WlmActivityManager *activityManager = new WlmActivityManager(
-      dataSource, opt_port,
+      dataSource,
+      opt_port,
       opt_refuseAssociation,
       opt_rejectWithoutImplementationUID,
-      opt_sleepAfterFind, opt_sleepDuringFind,
-      opt_maxPDU, opt_networkTransferSyntax,
+      opt_sleepBeforeFindReq,
+      opt_sleepAfterFind,
+      opt_sleepDuringFind,
+      opt_maxPDU,
+      opt_networkTransferSyntax,
       opt_failInvalidQuery,
-      opt_singleProcess, opt_maxAssociations,
-      opt_blockMode, opt_dimse_timeout, opt_acse_timeout,
-      opt_forkedChild, command_argc, command_argv );
+      opt_singleProcess,
+      opt_maxAssociations,
+      opt_blockMode,
+      opt_dimse_timeout,
+      opt_acse_timeout,
+      opt_forkedChild,
+      command_argc,
+      command_argv );
+
+  if (!activityManager->setRequestFilePath(opt_rfPath, opt_rfFormat))
+  {
+      // dump error if given directory is not sufficient
+      OFLOG_ERROR(wlmscpfsLogger, "Request file directory (" << opt_rfPath << ") cannot be created or is not writable");
+      // free memory
+      delete activityManager;
+      // return errro
+      return( 1 );
+  }
+
   cond = activityManager->StartProvidingService();
   if( cond.bad() )
   {

dcmwlm/apps/wlcefs.h
@@ -42,12 +42,24 @@ class WlmConsoleEngineFileSystem
     WlmReturnedCharacterSetType opt_returnedCharacterSet;
     /// path to database files
     OFString opt_dfPath;
+    /// path to store request files
+    OFString opt_rfPath;
+    /// format for request files if stored.
+    /// Several placeholders can be used by(denoted by #):<br>
+    ///   \#a: calling application entity title of the peer SCU<br>
+    ///   \#c: called application entity title (AE title of worklist SCP application)<br>
+    ///   \#i: process id of the worklist SCP application process handling the request<br>
+    ///   \#p: patient ID if present, otherwise empty string<br>
+    ///   \#t: timestamp in the format YYYYMMDDhhmmssffffff<br>
+    OFString opt_rfFormat;
     /// port on which this application is listening
     OFCmdUnsignedInt opt_port;
     /// indicates if incoming associations shall be refused or not
     OFBool opt_refuseAssociation;
     /// indicates if incoming associations shall be refused if no implementation class uid is specified
     OFBool opt_rejectWithoutImplementationUID;
+    /// indicates how many seconds the application is supposed to sleep before handling a find request
+    OFCmdUnsignedInt opt_sleepBeforeFindReq;
     /// indicates how long the application shall sleep after a find
     OFCmdUnsignedInt opt_sleepAfterFind;
     /// indicates how long the application shall sleep during a find

commit 8ed4c6b564c3030592ddd86deae4ca38a4a3f663

Fixed wlmscpfs bug rejecting all Called AE Titles.
Fixed a bug that made wlmscpfs reject all Called AE Titles, introduced
in commit 459a638e4dab967b065811a9cf63af0604ec46f1.

Thanks to Stefano Magni for the report.

dcmwlm/include/dcmtk/dcmwlm/wlds.h
@@ -349,6 +349,11 @@ class DCMTK_DCMWLM_EXPORT WlmDataSource
        */
     virtual OFCondition ConnectToDataSource() = 0;
 
+      /** Set value in member variable.
+       *  @param value The value to set.
+       */
+    void SetCalledApplicationEntityTitle( const OFString& value );
+
       /** Disconnects from the data source.
        * @return Indicates if the disconnection was completed successfully.
        */

dcmwlm/include/dcmtk/dcmwlm/wldsfs.h
@@ -112,6 +112,11 @@ class DCMTK_DCMWLM_EXPORT WlmDataSourceFileSystem : public WlmDataSource
        */
     OFCondition DisconnectFromDataSource();
 
+     /** Set value in member variable.
+      *  @param value The value to set.
+      */
+     void SetCalledApplicationEntityTitle( const OFString& value );
+
       /** Set value in member variable.
        *  @param value The value to set.
        */

dcmwlm/libsrc/wlds.cc
@@ -92,6 +92,18 @@ WlmDataSource::~WlmDataSource()
 
 // ----------------------------------------------------------------------------
 
+void WlmDataSource::SetCalledApplicationEntityTitle( const OFString& value )
+// Date         : December 10, 2001
+// Author       : Thomas Wilkens
+// Task         : Sets the member variable that specifies called application entity title.
+// Parameters   : value - Value for calledApplicationEntityTitle.
+// Return Value : none.
+{
+  calledApplicationEntityTitle = value;
+}
+
+// ----------------------------------------------------------------------------
+
 void WlmDataSource::SetFailOnInvalidQuery( OFBool value )
 // Date         : December 10, 2001
 // Author       : Thomas Wilkens

dcmwlm/libsrc/wldsfs.cc
@@ -106,6 +106,7 @@ OFCondition WlmDataSourceFileSystem::DisconnectFromDataSource()
   return( cond );
 }
 
+
 // ----------------------------------------------------------------------------
 
 void WlmDataSourceFileSystem::SetDfPath( const OFString& value )

dcmwlm/libsrc/wlmactmg.cc
@@ -461,6 +461,7 @@ OFCondition WlmActivityManager::WaitForAssociation( T_ASC_Network * net )
 
   // Condition 5: if the called application entity title is not supported
   // within the data source we want to refuse the association request
+  dataSource->SetCalledApplicationEntityTitle( assoc->params->DULparams.calledAPTitle );
   if( !dataSource->IsCalledApplicationEntityTitleSupported() )
   {
     RefuseAssociation( &assoc, WLM_BAD_AE_SERVICE );