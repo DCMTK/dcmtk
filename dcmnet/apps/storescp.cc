DCMTK/dcmtk

commit ac56966a4e0d6af4c1cf9378fb890e2f914b6524

Use OFnullptr as sentinel parameter for execl calls.
Use OFnullptr instead of NULL as final sentinel parameter in all excel
calls to avoid gcc 8.3 warnings on platforms where NULL is not defined
as a pointer.

dcmnet/apps/storescp.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1994-2018, OFFIS e.V.
+ *  Copyright (C) 1994-2019, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -2392,7 +2392,7 @@ static void executeCommand( const OFString &cmd )
     // we 'emulate' a call to system() by passing the command to /bin/sh
     // which hopefully exists on all Posix systems.
 
-    if (execl( "/bin/sh", "/bin/sh", "-c", cmd.c_str(), NULL ) < 0)
+    if (execl( "/bin/sh", "/bin/sh", "-c", cmd.c_str(), OFnullptr ) < 0)
       OFLOG_ERROR(storescpLogger, "cannot execute /bin/sh");
 
     // if execl succeeds, this part will not get executed.

dcmpstat/libsrc/dviface.cc
@@ -2223,7 +2223,7 @@ OFCondition DVInterface::sendIOD(const char * targetID,
   } else {
     // we are the child process
     if (execl(sender_application, sender_application, configPath.c_str(),
-            targetID, studyUID, seriesUID, instanceUID, NULL) < 0)
+            targetID, studyUID, seriesUID, instanceUID, OFnullptr) < 0)
     {
       DCMPSTAT_ERROR("Unable to execute '" << sender_application << "'");
     }
@@ -2286,7 +2286,7 @@ OFCondition DVInterface::startReceiver()
       // we are the parent process, continue loop
     } else {
       // we are the child process
-      if (execl(receiver_application, receiver_application, configPath.c_str(), getTargetID(i, DVPSE_receiver), NULL) < 0)
+      if (execl(receiver_application, receiver_application, configPath.c_str(), getTargetID(i, DVPSE_receiver), OFnullptr) < 0)
       {
           DCMPSTAT_ERROR("Unable to execute '" << receiver_application << "'");
       }
@@ -2341,7 +2341,7 @@ OFCondition DVInterface::terminateReceiver()
     // we are the parent process, continue loop
   } else {
     // we are the child process
-    if (execl(receiver_application, receiver_application, configPath.c_str(), "--terminate", NULL) < 0)
+    if (execl(receiver_application, receiver_application, configPath.c_str(), "--terminate", OFnullptr) < 0)
     {
         DCMPSTAT_ERROR("Unable to execute '" << receiver_application << "'");
     }
@@ -2409,11 +2409,11 @@ OFCondition DVInterface::startQueryRetrieveServer()
       char str_timeout[20];
       sprintf(str_timeout, "%lu", OFstatic_cast(unsigned long, timeout));
       execl(server_application, server_application, "-c", config_filename.c_str(), "--allow-shutdown",
-        "--timeout", str_timeout, NULL);
+        "--timeout", str_timeout, OFnullptr);
     }
     else
     {
-      execl(server_application, server_application, "-c", config_filename.c_str(), "--allow-shutdown", NULL);
+      execl(server_application, server_application, "-c", config_filename.c_str(), "--allow-shutdown", OFnullptr);
     }
 
     DCMPSTAT_ERROR("Unable to execute '" << server_application << "'");
@@ -3412,13 +3412,13 @@ OFCondition DVInterface::startPrintSpooler()
       if (detailedLog)
       {
         if (execl(spooler_application, spooler_application, "--verbose", "--dump", "--spool", printJobIdentifier.c_str(),
-          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, NULL) < 0)
+          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, OFnullptr) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << spooler_application << "'");
         }
       } else {
         if (execl(spooler_application, spooler_application, "--spool", printJobIdentifier.c_str(),
-          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, NULL) < 0)
+          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, OFnullptr) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << spooler_application << "'");
         }
@@ -3548,12 +3548,12 @@ OFCondition DVInterface::startPrintServer()
       if (detailedLog)
       {
         if (execl(application, application, "--logfile", "--verbose", "--dump", "--printer", printer, "--config",
-            configPath.c_str(), NULL) < 0)
+            configPath.c_str(), OFnullptr) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << application << "'");
         }
       } else {
-        if (execl(application, application, "--logfile", "--printer", printer, "--config", configPath.c_str(), NULL) < 0)
+        if (execl(application, application, "--logfile", "--printer", printer, "--config", configPath.c_str(), OFnullptr) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << application << "'");
         }
@@ -3909,7 +3909,7 @@ OFCondition DVInterface::startExternalApplication(const char *application, const
   else
   {
     // we are the child process
-    if (execl(application, application, filename, NULL) < 0)
+    if (execl(application, application, filename, OFnullptr) < 0)
     {
       DCMPSTAT_ERROR("Unable to execute '" << application << "'");
     }

commit 05a6ee50fcf67d71c0e89687399fbb0b31741bf3

Use null pointer as sentinel for execl calls.
Since commit ac56966a4 caused warnings on a number of platforms, the
execl calls now use 0 casted to pointer to avoid "missing sentinel"
warnings on gcc 8.3.

dcmnet/apps/storescp.cc
@@ -2392,7 +2392,7 @@ static void executeCommand( const OFString &cmd )
     // we 'emulate' a call to system() by passing the command to /bin/sh
     // which hopefully exists on all Posix systems.
 
-    if (execl( "/bin/sh", "/bin/sh", "-c", cmd.c_str(), OFnullptr ) < 0)
+    if (execl( "/bin/sh", "/bin/sh", "-c", cmd.c_str(), OFreinterpret_cast(char *, 0) ) < 0)
       OFLOG_ERROR(storescpLogger, "cannot execute /bin/sh");
 
     // if execl succeeds, this part will not get executed.

dcmpstat/libsrc/dviface.cc
@@ -2223,7 +2223,7 @@ OFCondition DVInterface::sendIOD(const char * targetID,
   } else {
     // we are the child process
     if (execl(sender_application, sender_application, configPath.c_str(),
-            targetID, studyUID, seriesUID, instanceUID, OFnullptr) < 0)
+            targetID, studyUID, seriesUID, instanceUID, OFreinterpret_cast(char *, 0)) < 0)
     {
       DCMPSTAT_ERROR("Unable to execute '" << sender_application << "'");
     }
@@ -2286,7 +2286,7 @@ OFCondition DVInterface::startReceiver()
       // we are the parent process, continue loop
     } else {
       // we are the child process
-      if (execl(receiver_application, receiver_application, configPath.c_str(), getTargetID(i, DVPSE_receiver), OFnullptr) < 0)
+      if (execl(receiver_application, receiver_application, configPath.c_str(), getTargetID(i, DVPSE_receiver), OFreinterpret_cast(char *, 0)) < 0)
       {
           DCMPSTAT_ERROR("Unable to execute '" << receiver_application << "'");
       }
@@ -2341,7 +2341,7 @@ OFCondition DVInterface::terminateReceiver()
     // we are the parent process, continue loop
   } else {
     // we are the child process
-    if (execl(receiver_application, receiver_application, configPath.c_str(), "--terminate", OFnullptr) < 0)
+    if (execl(receiver_application, receiver_application, configPath.c_str(), "--terminate", OFreinterpret_cast(char *, 0)) < 0)
     {
         DCMPSTAT_ERROR("Unable to execute '" << receiver_application << "'");
     }
@@ -2409,11 +2409,11 @@ OFCondition DVInterface::startQueryRetrieveServer()
       char str_timeout[20];
       sprintf(str_timeout, "%lu", OFstatic_cast(unsigned long, timeout));
       execl(server_application, server_application, "-c", config_filename.c_str(), "--allow-shutdown",
-        "--timeout", str_timeout, OFnullptr);
+        "--timeout", str_timeout, OFreinterpret_cast(char *, 0));
     }
     else
     {
-      execl(server_application, server_application, "-c", config_filename.c_str(), "--allow-shutdown", OFnullptr);
+      execl(server_application, server_application, "-c", config_filename.c_str(), "--allow-shutdown", OFreinterpret_cast(char *, 0));
     }
 
     DCMPSTAT_ERROR("Unable to execute '" << server_application << "'");
@@ -3412,13 +3412,13 @@ OFCondition DVInterface::startPrintSpooler()
       if (detailedLog)
       {
         if (execl(spooler_application, spooler_application, "--verbose", "--dump", "--spool", printJobIdentifier.c_str(),
-          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, OFnullptr) < 0)
+          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, OFreinterpret_cast(char *, 0)) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << spooler_application << "'");
         }
       } else {
         if (execl(spooler_application, spooler_application, "--spool", printJobIdentifier.c_str(),
-          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, OFnullptr) < 0)
+          "--printer", printer, "--config", configPath.c_str(), "--sleep", sleepStr, OFreinterpret_cast(char *, 0)) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << spooler_application << "'");
         }
@@ -3548,12 +3548,12 @@ OFCondition DVInterface::startPrintServer()
       if (detailedLog)
       {
         if (execl(application, application, "--logfile", "--verbose", "--dump", "--printer", printer, "--config",
-            configPath.c_str(), OFnullptr) < 0)
+            configPath.c_str(), OFreinterpret_cast(char *, 0)) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << application << "'");
         }
       } else {
-        if (execl(application, application, "--logfile", "--printer", printer, "--config", configPath.c_str(), OFnullptr) < 0)
+        if (execl(application, application, "--logfile", "--printer", printer, "--config", configPath.c_str(), OFreinterpret_cast(char *, 0)) < 0)
         {
           DCMPSTAT_ERROR("Unable to execute '" << application << "'");
         }
@@ -3909,7 +3909,7 @@ OFCondition DVInterface::startExternalApplication(const char *application, const
   else
   {
     // we are the child process
-    if (execl(application, application, filename, OFnullptr) < 0)
+    if (execl(application, application, filename, OFreinterpret_cast(char *, 0)) < 0)
     {
       DCMPSTAT_ERROR("Unable to execute '" << application << "'");
     }