f86acc7c4a257fd96afda14521d8da81e6b341e2
Joerg Riesmeier <dicom@jriesmeier.com>
2020-09-02 15:31:53 +0200

    Improved documentation of option --grayscale.
    Thanks to GitHub user "malaterre" for the hint and the original patch.

--- dcmimage/apps/dcm2pnm.cc
+++ dcmimage/apps/dcm2pnm.cc
@@ -132,7 +132,7 @@ int main(int argc, char *argv[])
     OFCmdUnsignedInt    opt_frameCount = 1;               /* default: one frame */
     OFBool              opt_useFrameNumber = OFFalse;     /* default: use frame counter */
     OFBool              opt_multiFrame = OFFalse;         /* default: no multiframes */
-    int                 opt_convertToGrayscale = 0;       /* default: color or grayscale */
+    int                 opt_convertToGrayscale = 0;       /* default: no conversion */
     int                 opt_changePolarity = 0;           /* default: normal polarity */
     int                 opt_useAspectRatio = 1;           /* default: use aspect ratio for scaling */
     OFCmdUnsignedInt    opt_useInterpolation = 1;         /* default: use interpolation method '1' for scaling */
@@ -371,7 +371,7 @@ int main(int argc, char *argv[])
 #endif
 
      cmd.addSubGroup("other transformations:");
-      cmd.addOption("--grayscale",          "+G",      "convert to grayscale if necessary");
+      cmd.addOption("--grayscale",          "+G",      "convert color image to grayscale (monochrome)");
       cmd.addOption("--change-polarity",    "+P",      "change polarity (invert pixel output)");
       cmd.addOption("--clip-region",        "+C",   4, "[l]eft [t]op [w]idth [h]eight: integer",
                                                        "clip image region (l, t, w, h)");
@@ -1101,7 +1101,7 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        /* convert to grayscale if necessary */
+        /* convert to grayscale if image is not monochrome */
         if ((opt_convertToGrayscale) && (!di->isMonochrome()))
         {
              OFLOG_INFO(dcm2pnmLogger, "converting image to grayscale");
