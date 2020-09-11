cd938e07be9fe8085298f893cb30558349a13714
Joerg Riesmeier <dicom@jriesmeier.com>
2020-09-11 17:56:23 +0200

    Extended range of application-specific errors.
    Extended range of application-specific / user-defined errors. Now, the
    exit codes 120 to 127 also belong to this group.

--- ofstd/include/dcmtk/ofstd/ofexit.h
+++ ofstd/include/dcmtk/ofstd/ofexit.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2018-2019, OFFIS e.V.
+ *  Copyright (C) 2018-2020, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -56,7 +56,7 @@
 // processing errors (80-99)
 // - defined in the respective modules / tools
 
-// user-defined errors (100-119)
+// application-specific or user-defined errors (100-127)
 // - defined in the respective modules / tools
 
 // GNU recommends that the codes 128-255 be reserved for serious errors:
