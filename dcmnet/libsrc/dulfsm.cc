d444d77825f06bb2b542f1cc7ff8e0073867f226
Marco Eichelberg <dicom@offis.de>
2020-11-16 09:15:26 +0100

    Added typecast required due to commit #09e11a591.
    Added typecast to logger call required due to the changes introduced
    with commit #09e11a591.

--- dcmnet/libsrc/dulfsm.cc
+++ dcmnet/libsrc/dulfsm.cc
@@ -777,7 +777,7 @@ PRV_StateMachine(PRIVATE_NETWORKKEY ** network,
 
     /* dump information if required */
     DCMNET_TRACE("DUL  FSM Table: State: " << state << " Event: " << event << OFendl
-            << "DUL  Event:  " << entry->eventName << OFendl
+            << "DUL  Event:  " << OFconst_cast(const char *, entry->eventName) << OFendl
             << "DUL  Action: " << entry->actionName);
 
     /* if the state table's entry specifies an action function, execute this function and return */
