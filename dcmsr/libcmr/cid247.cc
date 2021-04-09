39d11cff209e78365b9264d7fccd55251f41f6e8
Joerg Riesmeier <dicom@jriesmeier.com>
2021-04-09 20:57:27 +0200

    Added support for CID 247.
    Added support for Context Group CID 247 (Laterality Left-Right Only).
    As always, the source and header files were generated automatically.

---
+++ dcmsr/libcmr/cid247.cc
@@ -0,0 +1,163 @@
+/*
+ *
+ *  Copyright (C) 2015-2021, J. Riesmeier, Oldenburg, Germany
+ *  All rights reserved.  See COPYRIGHT file for details.
+ *
+ *  Source file for class CID247_LateralityLeftRightOnly
+ *
+ *  Generated automatically from DICOM PS 3.16-2021b
+ *  File created on 2021-03-30 10:12:43 by J. Riesmeier
+ *
+ */
+
+
+#include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first */
+
+#include "dcmtk/dcmsr/cmr/cid247.h"
+
+
+// general information on CID 247 (Laterality Left-Right Only)
+#define CONTEXT_GROUP_NUMBER  "247"
+#define CONTEXT_GROUP_VERSION "20190524"
+#define CONTEXT_GROUP_UID     "1.2.840.10008.6.1.1284"
+#define CONTEXT_GROUP_TYPE    OFTrue  /* extensible */
+
+// initialize global/static variable
+CID247_LateralityLeftRightOnly::CodeList *CID247_LateralityLeftRightOnly::Codes = NULL;
+
+
+CID247_LateralityLeftRightOnly::CID247_LateralityLeftRightOnly(const DSRCodedEntryValue &selectedValue)
+  : DSRContextGroup(CONTEXT_GROUP_NUMBER, "DCMR", CONTEXT_GROUP_VERSION, CONTEXT_GROUP_UID, selectedValue)
+{
+    setExtensible(CONTEXT_GROUP_TYPE);
+}
+
+
+CID247_LateralityLeftRightOnly::CID247_LateralityLeftRightOnly(const EnumType selectedValue,
+                                                               const OFBool enhancedEncodingMode)
+  : DSRContextGroup(CONTEXT_GROUP_NUMBER, "DCMR", CONTEXT_GROUP_VERSION, CONTEXT_GROUP_UID, getCodedEntry(selectedValue, enhancedEncodingMode))
+{
+    setExtensible(CONTEXT_GROUP_TYPE);
+}
+
+
+OFCondition CID247_LateralityLeftRightOnly::selectValue(const EnumType selectedValue,
+                                                        const OFBool enhancedEncodingMode)
+{
+    /* never check the coded entry */
+    return DSRContextGroup::selectValue(getCodedEntry(selectedValue, enhancedEncodingMode), OFFalse /*check*/, OFFalse /*definedContextGroup*/);
+}
+
+
+OFCondition CID247_LateralityLeftRightOnly::findCodedEntry(const DSRCodedEntryValue &searchForCodedEntry,
+                                                           DSRCodedEntryValue *foundCodedEntry,
+                                                           const OFBool enhancedEncodingMode) const
+{
+    OFCondition result = SR_EC_CodedEntryNotInContextGroup;
+    /* first, search for standard codes */
+    CodeList::const_iterator iter = getCodes().begin();
+    CodeList::const_iterator last = getCodes().end();
+    /* iterate over coded entry list */
+    while (iter != last)
+    {
+        /* if found, exit loop */
+        if (searchForCodedEntry == iter->second)
+        {
+            /* return coded entry (if requested) */
+            if (foundCodedEntry != NULL)
+            {
+                *foundCodedEntry = iter->second;
+                /* also set enhanced encoding mode (if enabled) */
+                if (!foundCodedEntry->isEmpty() && enhancedEncodingMode)
+                    setEnhancedEncodingMode(*foundCodedEntry);
+            }
+            result = SR_EC_CodedEntryInStandardContextGroup;
+            break;
+        }
+        ++iter;
+    }
+    /* if not, continue with extended codes */
+    if (result.bad())
+    {
+        result = DSRContextGroup::findCodedEntry(searchForCodedEntry, foundCodedEntry);
+        /* tbd: set "enhanced encoding mode" to mark a local/extended version? */
+    }
+    return result;
+}
+
+
+void CID247_LateralityLeftRightOnly::printCodes(STD_NAMESPACE ostream &stream) const
+{
+    /* print standard codes */
+    stream << "Standard codes:" << OFendl;
+    CodeList::const_iterator iter = getCodes().begin();
+    CodeList::const_iterator last = getCodes().end();
+    /* iterate over coded entry list */
+    while (iter != last)
+    {
+        stream << "  ";
+        /* print coded entry */
+        DSRCodedEntryValue(iter->second).print(stream);
+        stream << OFendl;
+        ++iter;
+    }
+    /* print extended codes */
+    DSRContextGroup::printCodes(stream);
+}
+
+
+// static functions
+
+void CID247_LateralityLeftRightOnly::initialize()
+{
+    /* create and initialize code list */
+    getCodes();
+}
+
+
+void CID247_LateralityLeftRightOnly::cleanup()
+{
+    /* delete code list, it will be recreated automatically when needed */
+    delete Codes;
+    Codes = NULL;
+}
+
+
+DSRCodedEntryValue CID247_LateralityLeftRightOnly::getCodedEntry(const EnumType value,
+                                                                 const OFBool enhancedEncodingMode)
+{
+    DSRCodedEntryValue codedEntry;
+    /* search for given enumerated value */
+    CodeList::iterator iter = getCodes().find(value);
+    /* if found, set the coded entry */
+    if (iter != getCodes().end())
+    {
+        codedEntry = iter->second;
+        /* also set enhanced encoding mode (if enabled) */
+        if (!codedEntry.isEmpty() && enhancedEncodingMode)
+            setEnhancedEncodingMode(codedEntry);
+    }
+    return codedEntry;
+}
+
+
+CID247_LateralityLeftRightOnly::CodeList &CID247_LateralityLeftRightOnly::getCodes()
+{
+    /* check whether code list has already been created and initialized */
+    if (Codes == NULL)
+    {
+        /* create a new code list (should never fail) */
+        Codes = new CodeList();
+        /* and initialize it by adding the coded entries */
+        Codes->insert(OFMake_pair(Right, DSRBasicCodedEntry("24028007", "SCT", "Right")));
+        Codes->insert(OFMake_pair(Left, DSRBasicCodedEntry("7771000", "SCT", "Left")));
+    }
+    /* should never be NULL */
+    return *Codes;
+}
+
+
+OFCondition CID247_LateralityLeftRightOnly::setEnhancedEncodingMode(DSRCodedEntryValue &codedEntryValue)
+{
+    return codedEntryValue.setEnhancedEncodingMode(CONTEXT_GROUP_NUMBER, "DCMR", CONTEXT_GROUP_VERSION, CONTEXT_GROUP_UID);
+}
