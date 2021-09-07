32a1c570c3e677b5c2faa11e242a09d68643b9e2
Marco Eichelberg <dicom@offis.de>
2021-09-07 11:01:07 +0200

    Fixed Posix feature flags for Android build.

--- CMake/dcmtkPrepare.cmake
+++ CMake/dcmtkPrepare.cmake
@@ -423,6 +423,8 @@ else()   # ... for non-Windows systems
     # On DragonFly BSD, we don't set any of the feature macros because they are not needed and
     # actually cause problems such as strlcpy() not being declared in <cstring>.
   # Solaris, FreeBSD and newer versions of OpenBSD fail with these flags
+  elseif(CMAKE_SYSTEM_NAME MATCHES "Android")
+    # On Android, we don't set any of the feature macros
   elseif(NOT CMAKE_SYSTEM_NAME MATCHES "SunOS" AND NOT CMAKE_SYSTEM_NAME MATCHES "FreeBSD" AND (NOT CMAKE_SYSTEM_NAME MATCHES "OpenBSD" OR CMAKE_SYSTEM_VERSION VERSION_LESS 4))
     # Compiler flags for all other non-Windows systems
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_XOPEN_SOURCE_EXTENDED -D_XOPEN_SOURCE=500 -D_BSD_SOURCE -D_DEFAULT_SOURCE -D_BSD_COMPAT -D_OSF_SOURCE -D_POSIX_C_SOURCE=199506L")
