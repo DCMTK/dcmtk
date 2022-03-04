462936150584c9fb9a42856a36b7036efc53f6cf
100029962 <joger.hansegard@ge.com>
2022-03-04 21:33:16 +0100

    Assign value to response status code, and ask question about where to assign response status code.

--- dcmnet/libsrc/scu.cc
+++ dcmnet/libsrc/scu.cc
@@ -2119,6 +2119,9 @@ OFCondition DcmSCU::sendNCREATERequest(const T_ASC_PresentationContextID presID,
 
     T_ASC_PresentationContextID pcid = presID;
     result = receiveDIMSECommand(&pcid, &response, &statusDetail, OFnullptr /* commandSet */);
+
+    rspStatusCode = response.msg.NCreateRSP.DimseStatus; // TODO: Is this the right place to assign the status?
+                                                         //       It looks like DimseStatus can contain valid status even if cond is bad.
     if (result.bad() || pcid != presID) {
         delete statusDetail;
         return result;
