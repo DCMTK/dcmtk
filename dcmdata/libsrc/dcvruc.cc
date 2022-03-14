1e04eee6f0e44d9b0ed6431e2cdaa43cf6b06142
Joerg Riesmeier <dicom@jriesmeier.com>
2022-03-14 18:07:31 +0100

    Specified "non-signficant characters" for VR UC.
    The backslash and the space character are now specified as not significant
    for the Value Representation "Unlimited Characters" (UC), since this is how
    this VR is defined in DICOM PS3.5. With this change, a UC value consisting
    of spaces and/or backslashes only is considered an empty value.

--- dcmdata/libsrc/dcvruc.cc
+++ dcmdata/libsrc/dcvruc.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2015-2017, OFFIS e.V.
+ *  Copyright (C) 2015-2022, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -33,6 +33,7 @@ DcmUnlimitedCharacters::DcmUnlimitedCharacters(const DcmTag &tag,
   : DcmCharString(tag, len)
 {
     setMaxLength(DCM_UndefinedLength);
+    setNonSignificantChars(" \\");
 }
 
 
