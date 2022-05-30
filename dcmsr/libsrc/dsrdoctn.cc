DCMTK/dcmtk

commit b48f82c20e4aeb8d8cec0221e215dd71b75024a1

Fixed issue with unwanted debug message.
Fixed issue with unwanted debug message ("D: INTERNAL ERROR...") on empty
coded entry value that is e.g. used as the concept name of an IMAGE content
item, e.g. when printing it to the console.

dcmsr/libsrc/dsrdoctn.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2000-2019, OFFIS e.V.
+ *  Copyright (C) 2000-2022, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -149,8 +149,8 @@ OFCondition DSRDocumentTreeNode::print(STD_NAMESPACE ostream &stream,
     stream << valueTypeToDefinedTerm(ValueType);
     DCMSR_PRINT_ANSI_ESCAPE_CODE(DCMSR_ANSI_ESCAPE_CODE_DELIMITER)
     stream << ":";
-    /* only print valid concept name codes */
-    if (ConceptName.isValid() || (flags & PF_printInvalidCodes))
+    /* only print empty or valid concept name codes */
+    if (ConceptName.isEmpty() || ConceptName.isValid() || (flags & PF_printInvalidCodes))
     {
         DCMSR_PRINT_ANSI_ESCAPE_CODE(DCMSR_ANSI_ESCAPE_CODE_CONCEPT_NAME)
         ConceptName.print(stream, (flags & PF_printConceptNameCodes) > 0 /*printCodeValue*/, flags);

dcmsr/libsrc/dsrcodvl.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2000-2019, OFFIS e.V.
+ *  Copyright (C) 2000-2022, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -69,7 +69,7 @@ DSRBasicCodedEntry::DSRBasicCodedEntry(const OFString &codeValue,
 // implementation of class DSRCodedEntryValue
 
 DSRCodedEntryValue::DSRCodedEntryValue()
-  : CodeValueType(DSRTypes::CVT_auto),
+  : CodeValueType(DSRTypes::CVT_Short /* should not be "auto" */),
     CodeValue(),
     CodingSchemeDesignator(),
     CodingSchemeVersion(),
@@ -81,6 +81,7 @@ DSRCodedEntryValue::DSRCodedEntryValue()
     ContextGroupLocalVersion(),
     ContextGroupExtensionCreatorUID()
 {
+    /* empty coded entry */
 }
 
 

commit 8399564aebcbf65e896d02df1ebf8dc10488bb05

Fixed wrong print() output of empty concept names.
Fixed wrong print() output of empty concept names, which was introduced
with a recent commit (b48f82c2).

dcmsr/libsrc/dsrdoctn.cc
@@ -149,8 +149,8 @@ OFCondition DSRDocumentTreeNode::print(STD_NAMESPACE ostream &stream,
     stream << valueTypeToDefinedTerm(ValueType);
     DCMSR_PRINT_ANSI_ESCAPE_CODE(DCMSR_ANSI_ESCAPE_CODE_DELIMITER)
     stream << ":";
-    /* only print empty or valid concept name codes */
-    if (ConceptName.isEmpty() || ConceptName.isValid() || (flags & PF_printInvalidCodes))
+    /* only print valid concept name codes (unless explicitly requested otherwise) */
+    if (ConceptName.isValid() || (flags & PF_printInvalidCodes))
     {
         DCMSR_PRINT_ANSI_ESCAPE_CODE(DCMSR_ANSI_ESCAPE_CODE_CONCEPT_NAME)
         ConceptName.print(stream, (flags & PF_printConceptNameCodes) > 0 /*printCodeValue*/, flags);