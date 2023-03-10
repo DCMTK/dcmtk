DCMTK/dcmtk

commit d269161f77b5bb63481e6964c0380f5770e731d1

Added support for the new RFC 8996 TLS profiles.
Added support for the new TLS secure transport connection profiles
defined in Supplement 230, The "BCP 195 RFC 8996" TLS profile
and the "Modified BCP 195 RFC 8996" TLS profile. Also implemented
recommendations of RFC 9325, which is now part of BCP 195.
DCMTK now requires OpenSSL 1.0.2 or newer, since the requirements
of RFC 9325 cannot be implemented with older versions.
Note that support for the Modified BCP 195 RFC 8996 TLS profile
will not be available yet, because of limitations in OpenSSL.
The profile requires support for the TLS 1.2 Camellia ciphersuites
in GCM mode, which OpenSSL does not implement. A configure test will
automatically enable support for this profile when DCMTK is compiled
with a future OpenSSL library that implements this.

This closes DCMTK issue #1030.

dcmnet/docs/dcmrecv.man
@@ -139,17 +139,28 @@ certification authority:
 
 security profile:
 
+  +pn   --profile-8996
+          BCP 195 RFC 8996 TLS Profile (default)
+
+  +pm   --profile-8996-mod
+          Modified BCP 195 RFC 8996 TLS Profile
+
+          # only available if underlying TLS library supports
+          # all TLS features required for this profile
+
   +py   --profile-bcp195-nd
-          Non-downgrading BCP 195 TLS Profile (default)
+          Non-downgrading BCP 195 TLS Profile (retired)
 
   +px   --profile-bcp195
-          BCP 195 TLS Profile
+          BCP 195 TLS Profile (retired)
 
   +pz   --profile-bcp195-ex
-          Extended BCP 195 TLS Profile
+          Extended BCP 195 TLS Profile (retired)
 
   +pb   --profile-basic
           Basic TLS Secure Transport Connection Profile (retired)
+
+          # only available if underlying TLS library supports 3DES
 
   +pa   --profile-aes
           AES TLS Secure Transport Connection Profile (retired)

dcmnet/docs/echoscu.man
@@ -151,17 +151,28 @@ certification authority:
 
 security profile:
 
+  +pn   --profile-8996
+          BCP 195 RFC 8996 TLS Profile (default)
+
+  +pm   --profile-8996-mod
+          Modified BCP 195 RFC 8996 TLS Profile
+
+          # only available if underlying TLS library supports
+          # all TLS features required for this profile
+
   +py   --profile-bcp195-nd
-          Non-downgrading BCP 195 TLS Profile (default)
+          Non-downgrading BCP 195 TLS Profile (retired)
 
   +px   --profile-bcp195
-          BCP 195 TLS Profile
+          BCP 195 TLS Profile (retired)
 
   +pz   --profile-bcp195-ex
-          Extended BCP 195 TLS Profile
+          Extended BCP 195 TLS Profile (retired)
 
   +pb   --profile-basic
           Basic TLS Secure Transport Connection Profile (retired)
+
+          # only available if underlying TLS library supports 3DES
 
   +pa   --profile-aes
           AES TLS Secure Transport Connection Profile (retired)
@@ -369,6 +380,6 @@ It is an error if no data dictionary can be loaded.
 
 \section echoscu_copyright COPYRIGHT
 
-Copyright (C) 1994-2022 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
+Copyright (C) 1994-2023 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
 
 */

dcmnet/docs/findscu.man
@@ -202,17 +202,28 @@ certification authority:
 
 security profile:
 
+  +pn   --profile-8996
+          BCP 195 RFC 8996 TLS Profile (default)
+
+  +pm   --profile-8996-mod
+          Modified BCP 195 RFC 8996 TLS Profile
+
+          # only available if underlying TLS library supports
+          # all TLS features required for this profile
+
   +py   --profile-bcp195-nd
-          Non-downgrading BCP 195 TLS Profile (default)
+          Non-downgrading BCP 195 TLS Profile (retired)
 
   +px   --profile-bcp195
-          BCP 195 TLS Profile
+          BCP 195 TLS Profile (retired)
 
   +pz   --profile-bcp195-ex
-          Extended BCP 195 TLS Profile
+          Extended BCP 195 TLS Profile (retired)
 
   +pb   --profile-basic
           Basic TLS Secure Transport Connection Profile (retired)
+
+          # only available if underlying TLS library supports 3DES
 
   +pa   --profile-aes
           AES TLS Secure Transport Connection Profile (retired)
@@ -465,6 +476,6 @@ It is an error if no data dictionary can be loaded.
 
 \section findscu_copyright COPYRIGHT
 
-Copyright (C) 1994-2022 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
+Copyright (C) 1994-2023 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
 
 */

dcmnet/docs/storescp.man
@@ -259,17 +259,28 @@ certification authority:
 
 security profile:
 
+  +pn   --profile-8996
+          BCP 195 RFC 8996 TLS Profile (default)
+
+  +pm   --profile-8996-mod
+          Modified BCP 195 RFC 8996 TLS Profile
+
+          # only available if underlying TLS library supports
+          # all TLS features required for this profile
+
   +py   --profile-bcp195-nd
-          Non-downgrading BCP 195 TLS Profile (default)
+          Non-downgrading BCP 195 TLS Profile (retired)
 
   +px   --profile-bcp195
-          BCP 195 TLS Profile
+          BCP 195 TLS Profile (retired)
 
   +pz   --profile-bcp195-ex
-          Extended BCP 195 TLS Profile
+          Extended BCP 195 TLS Profile (retired)
 
   +pb   --profile-basic
           Basic TLS Secure Transport Connection Profile (retired)
+
+          # only available if underlying TLS library supports 3DES
 
   +pa   --profile-aes
           AES TLS Secure Transport Connection Profile (retired)
@@ -963,6 +974,6 @@ It is an error if no data dictionary can be loaded.
 
 \section storescp_copyright COPYRIGHT
 
-Copyright (C) 1996-2022 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
+Copyright (C) 1996-2023 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
 
 */

dcmnet/docs/storescu.man
@@ -341,17 +341,28 @@ certification authority:
 
 security profile:
 
+  +pn   --profile-8996
+          BCP 195 RFC 8996 TLS Profile (default)
+
+  +pm   --profile-8996-mod
+          Modified BCP 195 RFC 8996 TLS Profile
+
+          # only available if underlying TLS library supports
+          # all TLS features required for this profile
+
   +py   --profile-bcp195-nd
-          Non-downgrading BCP 195 TLS Profile (default)
+          Non-downgrading BCP 195 TLS Profile (retired)
 
   +px   --profile-bcp195
-          BCP 195 TLS Profile
+          BCP 195 TLS Profile (retired)
 
   +pz   --profile-bcp195-ex
-          Extended BCP 195 TLS Profile
+          Extended BCP 195 TLS Profile (retired)
 
   +pb   --profile-basic
           Basic TLS Secure Transport Connection Profile (retired)
+
+          # only available if underlying TLS library supports 3DES
 
   +pa   --profile-aes
           AES TLS Secure Transport Connection Profile (retired)
@@ -590,6 +601,6 @@ It is an error if no data dictionary can be loaded.
 
 \section storescu_copyright COPYRIGHT
 
-Copyright (C) 1996-2022 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
+Copyright (C) 1996-2023 by OFFIS e.V., Escherweg 2, 26121 Oldenburg, Germany.
 
 */

dcmtls/libsrc/tlsopt.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2017-2021, OFFIS e.V.
+ *  Copyright (C) 2017-2023, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -25,8 +25,9 @@
 #include "dcmtk/ofstd/ofconapp.h"
 #include "dcmtk/dcmtls/tlscond.h"
 #include "dcmtk/dcmnet/assoc.h"       /* for ASC_setTransportLayer() */
+#include "tlsfmacr.h"                 /* for OpenSSL feature macros */
 
-void DcmTLSOptions::printLibraryVersion()
+void DcmTLSOptionsBase::printLibraryVersion()
 {
 #ifdef WITH_OPENSSL
     COUT << "- " << DcmTLSTransportLayer::getOpenSSLVersionName() << OFendl;
@@ -34,31 +35,130 @@ void DcmTLSOptions::printLibraryVersion()
 }
 
 #ifdef WITH_OPENSSL
-DcmTLSOptions::DcmTLSOptions(T_ASC_NetworkRole networkRole)
+DcmTLSOptionsBase::DcmTLSOptionsBase(T_ASC_NetworkRole networkRole)
 : opt_keyFileFormat( DCF_Filetype_PEM )
 , opt_doAuthenticate( OFFalse )
 , opt_privateKeyFile( OFnullptr )
 , opt_certificateFile( OFnullptr )
 , opt_passwd( OFnullptr )
-, opt_tlsProfile( TSP_Profile_BCP195_ND ) // default: BCP 195 ND profile
+, opt_tlsProfile( TSP_Profile_BCP_195_RFC_8996 ) // default: BCP 195 RFC 8996 TLS Profile
 , opt_readSeedFile( OFnullptr )
 , opt_writeSeedFile( OFnullptr )
 , opt_certVerification( DCV_requireCertificate )
 , opt_dhparam( OFnullptr )
 , opt_secureConnection( OFFalse ) // default: no secure connection
 , opt_networkRole( networkRole )
+, opt_clientSNI( OFnullptr )
+, opt_serverSNI( OFnullptr )
 , tLayer( OFnullptr )
 #else
-DcmTLSOptions::DcmTLSOptions(T_ASC_NetworkRole /* networkRole */)
+DcmTLSOptionsBase::DcmTLSOptionsBase(T_ASC_NetworkRole /* networkRole */)
 #endif
+{
+}
+
+DcmTLSOptionsBase::~DcmTLSOptionsBase()
+{
+#ifdef WITH_OPENSSL
+  delete tLayer;
+#endif
+}
+
+#ifdef WITH_OPENSSL
+OFBool DcmTLSOptionsBase::listOfCiphersRequested(OFCommandLine& cmd)
+{
+  if (cmd.findOption("--list-ciphers")) return OFTrue;
+  return OFFalse;
+}
+#else
+OFBool DcmTLSOptionsBase::listOfCiphersRequested(OFCommandLine& /* cmd */)
+{
+  return OFFalse;
+}
+#endif
+
+#ifdef WITH_OPENSSL
+void DcmTLSOptionsBase::printSupportedCiphersuites(OFConsoleApplication& app, STD_NAMESPACE ostream& os)
+{
+  DcmTLSCiphersuiteHandler csh;
+  app.printHeader(OFTrue /*print host identifier*/);
+  os << OFendl << "Supported TLS ciphersuites are:" << OFendl;
+  csh.printSupportedCiphersuites(os);
+}
+#else
+void DcmTLSOptionsBase::printSupportedCiphersuites(OFConsoleApplication& /* app */, STD_NAMESPACE ostream& /* os */)
+{
+}
+#endif
+
+OFBool DcmTLSOptionsBase::secureConnectionRequested() const
+{
+#ifdef WITH_OPENSSL
+  return opt_secureConnection;
+#else
+  return OFFalse;
+#endif
+}
+
+DcmTransportLayer *DcmTLSOptionsBase::getTransportLayer()
+{
+#ifdef WITH_OPENSSL
+  return tLayer;
+#else
+  return NULL;
+#endif
+}
+
+OFCondition DcmTLSOptionsBase::writeRandomSeed()
+{
+#ifdef WITH_OPENSSL
+    if( opt_writeSeedFile && tLayer)
+    {
+        if( tLayer->canWriteRandomSeed() )
+        {
+            if( ! tLayer->writeRandomSeed( opt_writeSeedFile ) )
+                return DCMTLS_EC_FailedToWriteRandomSeedFile( opt_writeSeedFile );
+        }
+        else return DCMTLS_EC_FailedToWriteRandomSeedFile( opt_writeSeedFile );
+    }
+#endif
+    return EC_Normal;
+}
+
+#ifdef WITH_OPENSSL
+OFCondition DcmTLSOptionsBase::verifyClientCertificate(const char *fileName)
+{
+  if (tLayer) return tLayer->verifyClientCertificate(fileName, opt_keyFileFormat);
+  return EC_IllegalCall;
+}
+#else
+OFCondition DcmTLSOptionsBase::verifyClientCertificate(const char * /* fileName */)
+{
+  return EC_IllegalCall;
+}
+#endif
+
+#ifdef WITH_OPENSSL
+OFCondition DcmTLSOptionsBase::isRootCertificate(const char *fileName)
+{
+  return DcmTLSTransportLayer::isRootCertificate(fileName, opt_keyFileFormat);
+}
+#else
+OFCondition DcmTLSOptionsBase::isRootCertificate(const char * /* fileName */)
+{
+  return EC_IllegalCall;
+}
+#endif
+
+// ----------------------------------------------------------------------------
+
+DcmTLSOptions::DcmTLSOptions(T_ASC_NetworkRole networkRole)
+: DcmTLSOptionsBase(networkRole)
 {
 }
 
 DcmTLSOptions::~DcmTLSOptions()
 {
-#ifdef WITH_OPENSSL
-  delete tLayer;
-#endif
 }
 
 #ifdef WITH_OPENSSL
@@ -94,9 +194,13 @@ void DcmTLSOptions::addTLSCommandlineOptions(OFCommandLine& cmd)
       cmd.addOption("--enable-crl-vfy",     "+crv",    "enable leaf CRL verification");
       cmd.addOption("--enable-crl-all",     "+cra",    "enable full chain CRL verification");
     cmd.addSubGroup("security profile:");
-      cmd.addOption("--profile-bcp195-nd",  "+py",     "Non-downgrading BCP 195 TLS Profile (default)");
-      cmd.addOption("--profile-bcp195",     "+px",     "BCP 195 TLS Profile");
-      cmd.addOption("--profile-bcp195-ex",  "+pz",     "Extended BCP 195 TLS Profile");
+      cmd.addOption("--profile-8996",       "+pn",     "BCP 195 RFC 8996 TLS Profile (default)");
+#ifdef DCMTK_Modified_BCP195_RFC8996_TLS_Profile_Supported
+      cmd.addOption("--profile-8996-mod",   "+pm",     "Modified BCP 195 RFC 8996 TLS Profile");
+#endif
+      cmd.addOption("--profile-bcp195-nd",  "+py",     "Non-downgrading BCP 195 TLS Profile (retired)");
+      cmd.addOption("--profile-bcp195",     "+px",     "BCP 195 TLS Profile (retired)");
+      cmd.addOption("--profile-bcp195-ex",  "+pz",     "Extended BCP 195 TLS Profile (retired)");
       if (csh.cipher3DESsupported())
       {
         cmd.addOption("--profile-basic",    "+pb",     "Basic TLS Secure Transport Connection Profile\n(retired)");
@@ -118,6 +222,18 @@ void DcmTLSOptions::addTLSCommandlineOptions(OFCommandLine& cmd)
         cmd.addOption("--dhparam",          "+dp",  1, "[f]ilename: string",
                                                        "read DH parameters for DH/DSS ciphersuites");
       }
+    cmd.addSubGroup("server name indication:");
+      if (opt_networkRole != NET_ACCEPTOR)
+      {
+        cmd.addOption("--request-sni",              1, "[s]erver name: string (default: no SNI)",
+                                                       "request server name s");
+      }
+      if (opt_networkRole != NET_REQUESTOR)
+      {
+        cmd.addOption("--expect-sni",              1, "[s]erver name: string (default: no SNI)",
+                                                       "expect requests for server name s");
+      }
+
     cmd.addSubGroup("pseudo random generator:");
       cmd.addOption("--seed",               "+rs",  1, "[f]ilename: string",
                                                        "seed random generator with contents of f");
@@ -200,6 +316,17 @@ void DcmTLSOptions::parseArguments(OFConsoleApplication& app, OFCommandLine& cmd
         app.checkDependence("--dhparam", tlsopts, opt_secureConnection);
         app.checkValue( cmd.getValue( opt_dhparam ) );
     }
+
+    if ( cmd.findOption( "--request-sni" ) )
+    {
+        app.checkValue( cmd.getValue( opt_clientSNI ) );
+    }
+
+    if ( (opt_networkRole != NET_REQUESTOR) && cmd.findOption( "--expect-sni" ) )
+    {
+        app.checkValue( cmd.getValue( opt_serverSNI ) );
+    }
+
     if( cmd.findOption( "--seed" ) )
     {
         app.checkDependence("--seed", tlsopts, opt_secureConnection);
@@ -240,6 +367,18 @@ void DcmTLSOptions::parseArguments(OFConsoleApplication& app, OFCommandLine& cmd
     cmd.endOptionBlock();
 
     cmd.beginOptionBlock();
+    if (cmd.findOption("--profile-8996"))
+    {
+        app.checkDependence("--profile-8996", tlsopts, opt_secureConnection);
+        opt_tlsProfile = TSP_Profile_BCP_195_RFC_8996;
+    }
+#ifdef DCMTK_Modified_BCP195_RFC8996_TLS_Profile_Supported
+    if (cmd.findOption("--profile-8996-mod"))
+    {
+        app.checkDependence("--profile-8996-mod", tlsopts, opt_secureConnection);
+        opt_tlsProfile = TSP_Profile_BCP_195_RFC_8996_Modified;
+    }
+#endif
     if (cmd.findOption("--profile-bcp195"))
     {
         app.checkDependence("--profile-bcp195", tlsopts, opt_secureConnection);
@@ -381,10 +520,10 @@ OFCondition DcmTLSOptions::createTransportLayer(
         // replace the low-level error message with an easier to understand one
         if (cond.bad()) return DCMTLS_EC_FailedToLoadPrivateKey( opt_privateKeyFile );
 
-        cond = tLayer->setCertificateFile(opt_certificateFile, opt_keyFileFormat);
+        cond = tLayer->setCertificateFile(opt_certificateFile, opt_keyFileFormat, opt_tlsProfile);
 
         // replace the low-level error message with an easier to understand one
-        if (cond.bad()) DCMTLS_EC_FailedToLoadCertificate( opt_certificateFile );
+        if (cond.bad()) return DCMTLS_EC_FailedToLoadCertificate( opt_certificateFile );
 
         if (! tLayer->checkPrivateKeyMatchesCertificate())
            return DCMTLS_EC_MismatchedPrivateKeyAndCertificate( opt_privateKeyFile, opt_certificateFile );
@@ -408,6 +547,10 @@ OFCondition DcmTLSOptions::createTransportLayer(
 
       cond = tLayer->activateCipherSuites();
       if (cond.bad()) return cond;
+
+      // set SNI names
+      tLayer->setClientSNI(opt_clientSNI);
+      tLayer->setServerSNI(opt_serverSNI);
 
       // Loading of DH parameters should happen after the call to setTLSProfile()
       // because otherwise we cannot check profile specific restrictions
@@ -441,88 +584,3 @@ OFCondition DcmTLSOptions::createTransportLayer(
 }
 #endif
 
-#ifdef WITH_OPENSSL
-OFBool DcmTLSOptions::listOfCiphersRequested(OFCommandLine& cmd)
-{
-  if (cmd.findOption("--list-ciphers")) return OFTrue;
-  return OFFalse;
-}
-#else
-OFBool DcmTLSOptions::listOfCiphersRequested(OFCommandLine& /* cmd */)
-{
-  return OFFalse;
-}
-#endif
-
-#ifdef WITH_OPENSSL
-void DcmTLSOptions::printSupportedCiphersuites(OFConsoleApplication& app, STD_NAMESPACE ostream& os)
-{
-  DcmTLSCiphersuiteHandler csh;
-  app.printHeader(OFTrue /*print host identifier*/);
-  os << OFendl << "Supported TLS ciphersuites are:" << OFendl;
-  csh.printSupportedCiphersuites(os);
-}
-#else
-void DcmTLSOptions::printSupportedCiphersuites(OFConsoleApplication& /* app */, STD_NAMESPACE ostream& /* os */)
-{
-}
-#endif
-
-OFBool DcmTLSOptions::secureConnectionRequested() const
-{
-#ifdef WITH_OPENSSL
-  return opt_secureConnection;
-#else
-  return OFFalse;
-#endif
-}
-
-DcmTransportLayer *DcmTLSOptions::getTransportLayer()
-{
-#ifdef WITH_OPENSSL
-  return tLayer;
-#else
-  return NULL;
-#endif
-}
-
-OFCondition DcmTLSOptions::writeRandomSeed()
-{
-#ifdef WITH_OPENSSL
-    if( opt_writeSeedFile && tLayer)
-    {
-        if( tLayer->canWriteRandomSeed() )
-        {
-            if( ! tLayer->writeRandomSeed( opt_writeSeedFile ) )
-                return DCMTLS_EC_FailedToWriteRandomSeedFile( opt_writeSeedFile );
-        }
-        else return DCMTLS_EC_FailedToWriteRandomSeedFile( opt_writeSeedFile );
-    }
-#endif
-    return EC_Normal;
-}
-
-#ifdef WITH_OPENSSL
-OFCondition DcmTLSOptions::verifyClientCertificate(const char *fileName)
-{
-  if (tLayer) return tLayer->verifyClientCertificate(fileName, opt_keyFileFormat);
-  return EC_IllegalCall;
-}
-#else
-OFCondition DcmTLSOptions::verifyClientCertificate(const char * /* fileName */)
-{
-  return EC_IllegalCall;
-}
-#endif
-
-#ifdef WITH_OPENSSL
-OFCondition DcmTLSOptions::isRootCertificate(const char *fileName)
-{
-  return DcmTLSTransportLayer::isRootCertificate(fileName, opt_keyFileFormat);
-}
-#else
-OFCondition DcmTLSOptions::isRootCertificate(const char * /* fileName */)
-{
-  return EC_IllegalCall;
-}
-#endif

CMake/3rdparty.cmake
@@ -69,7 +69,7 @@ if(DCMTK_USE_FIND_PACKAGE)
       include(CheckCXXSourceCompiles)
       set(TEMP_INCLUDES "${CMAKE_REQUIRED_INCLUDES}")
       list(APPEND CMAKE_REQUIRED_INCLUDES "${OPENSSL_INCLUDE_DIR}")
-      CHECK_CXX_SOURCE_COMPILES("extern \"C\" {\n#include <openssl/ssl.h>\n}\nint main(){\n#if OPENSSL_VERSION_NUMBER < 0x10001000L\n#error OpenSSL too old\n#endif\n}\n" OPENSSL_VERSION_CHECK)
+      CHECK_CXX_SOURCE_COMPILES("extern \"C\" {\n#include <openssl/ssl.h>\n}\nint main(){\n#if OPENSSL_VERSION_NUMBER < 0x10002000L\n#error OpenSSL too old\n#endif\n}\n" OPENSSL_VERSION_CHECK)
       set(CMAKE_REQUIRED_INCLUDES "${TEMP_INCLUDES}")
       if(OPENSSL_VERSION_CHECK)
         message(STATUS "Info: DCMTK OPENSSL support will be enabled")
@@ -81,7 +81,7 @@ if(DCMTK_USE_FIND_PACKAGE)
           set(OPENSSL_LIBS ${OPENSSL_LIBS} dl)
         endif()
       else()
-        message(STATUS "Info: DCMTK OPENSSL support will be disabled: DCMTK requires OpenSSL version 1.0.1 or newer")
+        message(STATUS "Info: DCMTK OPENSSL support will be disabled: DCMTK requires OpenSSL version 1.0.2 or newer")
         set(DCMTK_WITH_OPENSSL OFF CACHE BOOL "" FORCE)
         set(WITH_OPENSSL "")
       endif()

commit fcfa55a5df98282a0f23f4a5efa5a99795abec6b

Fixed various bugs in dcmtls and updated docs.
Fixed various bugs in the revised dcmtls module introduced with commit
d269161f7. Completed dcmtls documentation.

The dcmtls module now offers support for the TLS "server name indication"
(SNI) extension, as recommended by RFC 9325. SNI allows a client to
transmit the name of the intended server during TLS negotiation and
allows a virtual server to redirect the request accordingly.
When SNI is active in a DICOM association acceptor, it will reject
incoming network connections when the client has included the SNI
extension in the TLS ClientHello message with an incorrect server name.
Association negotiation will continue when either the correct SNI
name is present, or the SNI extension is absent. Older servers will
simply ignore the SNI extension.

Thanks to Joerg Riesmeier for the testing and bug reports.

dcmnet/docs/dcmrecv.man
@@ -139,7 +139,7 @@ certification authority:
 
 security profile:
 
-  +pn   --profile-8996
+  +pg   --profile-8996
           BCP 195 RFC 8996 TLS Profile (default)
 
   +pm   --profile-8996-mod
@@ -179,6 +179,14 @@ ciphersuite:
 
   +dp   --dhparam  [f]ilename: string
           read DH parameters for DH/DSS ciphersuites
+
+server name indication:
+
+        --no-sni
+          do not use SNI (default)
+
+        --expect-sni  [s]erver name: string
+          expect requests for server name s
 
 pseudo random generator:
 

dcmnet/docs/echoscu.man
@@ -151,7 +151,7 @@ certification authority:
 
 security profile:
 
-  +pn   --profile-8996
+  +pg   --profile-8996
           BCP 195 RFC 8996 TLS Profile (default)
 
   +pm   --profile-8996-mod
@@ -188,6 +188,14 @@ ciphersuite:
 
   +cs   --cipher  [c]iphersuite name: string
           add ciphersuite to list of negotiated suites
+
+server name indication:
+
+        --no-sni
+          do not use SNI (default)
+
+        --request-sni  [s]erver name: string
+          request server name s
 
 pseudo random generator:
 

dcmnet/docs/findscu.man
@@ -202,7 +202,7 @@ certification authority:
 
 security profile:
 
-  +pn   --profile-8996
+  +pg   --profile-8996
           BCP 195 RFC 8996 TLS Profile (default)
 
   +pm   --profile-8996-mod
@@ -239,6 +239,14 @@ ciphersuite:
 
   +cs   --cipher  [c]iphersuite name: string
           add ciphersuite to list of negotiated suites
+
+server name indication:
+
+        --no-sni
+          do not use SNI (default)
+
+        --request-sni  [s]erver name: string
+          request server name s
 
 pseudo random generator:
 

dcmnet/docs/storescp.man
@@ -259,7 +259,7 @@ certification authority:
 
 security profile:
 
-  +pn   --profile-8996
+  +pg   --profile-8996
           BCP 195 RFC 8996 TLS Profile (default)
 
   +pm   --profile-8996-mod
@@ -299,6 +299,14 @@ ciphersuite:
 
   +dp   --dhparam  [f]ilename: string
           read DH parameters for DH/DSS ciphersuites
+
+server name indication:
+
+        --no-sni
+          do not use SNI (default)
+
+        --expect-sni  [s]erver name: string
+          expect requests for server name s
 
 pseudo random generator:
 

dcmnet/docs/storescu.man
@@ -341,7 +341,7 @@ certification authority:
 
 security profile:
 
-  +pn   --profile-8996
+  +pg   --profile-8996
           BCP 195 RFC 8996 TLS Profile (default)
 
   +pm   --profile-8996-mod
@@ -378,6 +378,14 @@ ciphersuite:
 
   +cs   --cipher  [c]iphersuite name: string
           add ciphersuite to list of negotiated suites
+
+server name indication:
+
+        --no-sni
+          do not use SNI (default)
+
+        --request-sni  [s]erver name: string
+          request server name s
 
 pseudo random generator:
 

dcmtls/docs/certstor.dox
@@ -64,30 +64,54 @@ The DICOM standard defines a number of security profiles for the use of the
 DICOM network protocol over TLS. DCMTK supports the following profiles:
 
 \verbatim
+  +pg   --profile-8996
+          BCP 195 RFC 8996 TLS Profile (default)
+
+  +pm   --profile-8996-mod
+          Modified BCP 195 RFC 8996 TLS Profile
+
+          # only available if underlying TLS library supports
+          # all TLS features required for this profile
+
   +py   --profile-bcp195-nd
-          Non-downgrading BCP 195 TLS Profile (default)
+          Non-downgrading BCP 195 TLS Profile (retired)
 
   +px   --profile-bcp195
-          BCP 195 TLS Profile
+          BCP 195 TLS Profile (retired)
 
   +pz   --profile-bcp195-ex
-          Extended BCP 195 TLS Profile
-
-  +pa   --profile-aes
-          AES TLS Secure Transport Connection Profile (retired)
+          Extended BCP 195 TLS Profile (retired)
 
   +pb   --profile-basic
           Basic TLS Secure Transport Connection Profile (retired)
 
+          # only available if underlying TLS library supports 3DES
+
+  +pa   --profile-aes
+          AES TLS Secure Transport Connection Profile (retired)
+
   +pn   --profile-null
-          Authenticated unencrypted communication (retired, was used in IHE ATNA)
+          Authenticated unencrypted communication
+          (retired, was used in IHE ATNA)
 \endverbatim
 
-The Non-downgrading BCP 195 TLS Profile, which is selected by default, is
-currently the most secure choice, as older protocol versions and cipher suites
-are explicitly disabled. It does not support backward compatibility with the
-older profiles. It uses either TLS version 1.2 or 1.3 and will default to TLS
-1.3 when possible.
+The BCP 195 RFC 8996 TLS Profile, which is selected by default, is a secure
+choice while providing backward compatibility with the earlier BCP 195 profiles.
+It does not support backward compatibility with the older profiles.
+It uses either TLS version 1.2 or 1.3 and will default to TLS 1.3 when possible.
+
+The Modified BCP 195 RFC 8996 TLS Profile, which adds further constraints
+compared to the BCP 195 RFC 8996 TLS Profile, cannot be implemented with
+OpenSSL 1.0.2 to 3.0.x because it requires support for the TLS 1.2 Camellia
+ciphersuites in GCM mode, which is not available in OpenSSL. Should future
+OpenSSL releases add support for these ciphersuites, then this option will
+automatically be enabled when compiling DCMTK.
+
+The Non-downgrading BCP 195 TLS Profile, which was the default in DCMTK 3.6.7,
+also disables older protocol versions and cipher suites. It does, however,
+support DHE ciphersuites, whereas RFC 9325 recommends that these should not
+be used. It uses either TLS version 1.2 or 1.3 and will default to TLS 1.3
+when possible.
 
 The BCP 195 TLS Profile, which was the default up to DCMTK 3.6.6, will try to
 negotiate cryptographic algorithms that are currently considered secure, but
@@ -127,7 +151,8 @@ using the following option:
           add ciphersuite to list of negotiated suites
 \endverbatim
 
-The list of supported ciphersuites can be printed using the following option:
+The list of supported TLS 1.0-1.2 ciphersuites can be printed
+using the following option:
 
 \verbatim
   +cc   --list-ciphers

dcmtls/libsrc/tlsopt.cc
@@ -194,7 +194,7 @@ void DcmTLSOptions::addTLSCommandlineOptions(OFCommandLine& cmd)
       cmd.addOption("--enable-crl-vfy",     "+crv",    "enable leaf CRL verification");
       cmd.addOption("--enable-crl-all",     "+cra",    "enable full chain CRL verification");
     cmd.addSubGroup("security profile:");
-      cmd.addOption("--profile-8996",       "+pn",     "BCP 195 RFC 8996 TLS Profile (default)");
+      cmd.addOption("--profile-8996",       "+pg",     "BCP 195 RFC 8996 TLS Profile (default)");
 #ifdef DCMTK_Modified_BCP195_RFC8996_TLS_Profile_Supported
       cmd.addOption("--profile-8996-mod",   "+pm",     "Modified BCP 195 RFC 8996 TLS Profile");
 #endif
@@ -223,14 +223,15 @@ void DcmTLSOptions::addTLSCommandlineOptions(OFCommandLine& cmd)
                                                        "read DH parameters for DH/DSS ciphersuites");
       }
     cmd.addSubGroup("server name indication:");
+      cmd.addOption("--no-sni",                        "do not use SNI (default)");
       if (opt_networkRole != NET_ACCEPTOR)
       {
-        cmd.addOption("--request-sni",              1, "[s]erver name: string (default: no SNI)",
+        cmd.addOption("--request-sni",              1, "[s]erver name: string",
                                                        "request server name s");
       }
       if (opt_networkRole != NET_REQUESTOR)
       {
-        cmd.addOption("--expect-sni",              1, "[s]erver name: string (default: no SNI)",
+        cmd.addOption("--expect-sni",              1, "[s]erver name: string",
                                                        "expect requests for server name s");
       }
 
@@ -324,14 +325,20 @@ void DcmTLSOptions::parseArguments(OFConsoleApplication& app, OFCommandLine& cmd
         app.checkValue(cmd.getValue(opt_dhparam));
     }
 
-    if (cmd.findOption("--request-sni"))
+    if ((opt_networkRole != NET_ACCEPTOR) && cmd.findOption("--request-sni"))
     {
         app.checkValue(cmd.getValue(opt_clientSNI));
     }
-
     if ((opt_networkRole != NET_REQUESTOR) && cmd.findOption("--expect-sni"))
     {
         app.checkValue(cmd.getValue(opt_serverSNI));
+    }
+    if (cmd.findOption("--no-sni"))
+    {
+        app.checkConflict("--no-sni", "--request-sni", (opt_clientSNI != NULL));
+        app.checkConflict("--no-sni", "--expect-sni", (opt_serverSNI != NULL));
+        opt_clientSNI = OFnullptr;
+        opt_serverSNI = OFnullptr;
     }
 
     if (cmd.findOption("--seed"))