bdc18c85b7fca130464dd745ea3efef3c9b8b94e
Joerg Riesmeier <dicom@jriesmeier.com>
2024-01-14 14:09:03 +0100

    Create all instances on heap instead of stack.
    Changed code to create all instances of the RT classes on the heap
    instead of the stack (use "new" and "delete" instead of automatic
    variables) in order to reduce stack requirements, which seemed to
    cause problems on some platforms.
    
    Also enhanced error handling and log output.

--- dcmrt/tests/drttest.cc
+++ dcmrt/tests/drttest.cc
@@ -1,13 +1,14 @@
 /*
  *
  *  Copyright (c) 2008-2012, OFFIS e.V. and ICSMED AG, Oldenburg, Germany
+ *  Copyright (C) 2013-2024, J. Riesmeier, Oldenburg, Germany
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  Test read and write methods of DICOM RT classes
  *
  *  Generated manually
  *  File created on 2008-12-08
- *  Last modified on 2012-10-09 by Riesmeier
+ *  Last modified on 2024-01-14 by Riesmeier
  *
  */
 
@@ -40,92 +41,158 @@ int main()
     DCMRT_INFO("DCMRT Test Program" << OFendl << "------------------" << OFendl);
 
     /* test RT dose */
-    DRTDoseIOD rtdose;
-    status = fileformat.loadFile(RTDOSE_FILENAME);
-    if (status.good())
-        status = rtdose.read(*fileformat.getDataset());
-    DCMRT_INFO("Read RT Dose: " << status.text());
-
-    fileformat.clear();
-    status = rtdose.write(*fileformat.getDataset());
-    DCMRT_INFO("Save RT Dose: " << status.text());
-    if (status.good())
-        status = fileformat.saveFile("rtdose.dcm");
-    DCMRT_INFO("Write RT Dose: " << status.text() << OFendl);
+    DRTDoseIOD *rtdose = new DRTDoseIOD();
+    if (rtdose != NULL)
+    {
+        status = fileformat.loadFile(RTDOSE_FILENAME);
+        if (status.good())
+            status = rtdose->read(*fileformat.getDataset());
+        DCMRT_INFO("Read RT Dose: " << status.text());
+        if (status.good())
+        {
+            fileformat.clear();
+            status = rtdose->write(*fileformat.getDataset());
+            DCMRT_INFO("Save RT Dose: " << status.text());
+            if (status.good())
+                status = fileformat.saveFile("rtdose.dcm");
+            DCMRT_INFO("Write RT Dose: " << status.text());
 
 #ifdef TEST_COPY_CONSTRUCTOR
-    DRTDoseIOD rtdose2(rtdose);
-    fileformat.clear();
-    status = rtdose2.write(*fileformat.getDataset());
-    DCMRT_INFO("Write copy of RT Dose: " << status.text());
-    if (status.good())
-        status = fileformat.saveFile("rtdose2.dcm");
-    DCMRT_INFO("Save copy of RT Dose: " << status.text() << OFendl);
+            DRTDoseIOD *rtdose2 = new DRTDoseIOD(*rtdose);
+            if (rtdose2 != NULL)
+            {
+                fileformat.clear();
+                status = rtdose2->write(*fileformat.getDataset());
+                DCMRT_INFO("Write copy of RT Dose: " << status.text());
+                if (status.good())
+                    status = fileformat.saveFile("rtdose2.dcm");
+                DCMRT_INFO("Save copy of RT Dose: " << status.text());
+                delete rtdose2;
+                rtdose2 = NULL;
+            } else
+                DCMRT_ERROR("Cannot allocate memory for copy of RT Dose");
 #endif
+        } else
+            DCMRT_INFO("Skipping further tests for RT Dose");
+        delete rtdose;
+        rtdose = NULL;
+    } else
+        DCMRT_ERROR("Cannot allocate memory for RT Dose");
+    DCMRT_INFO("" /* empty line */);
 
     /* test RT image */
-    DRTImageIOD rtimage;
-    status = fileformat.loadFile(RTIMAGE_FILENAME);
-    if (status.good())
-        status = rtimage.read(*fileformat.getDataset());
-    DCMRT_INFO("Read RT Image: " << status.text());
-    fileformat.clear();
-    status = rtimage.write(*fileformat.getDataset());
-    DCMRT_INFO("Write RT Image: " << status.text());
-    if (status.good())
-        status = fileformat.saveFile("rtimage.dcm");
-    DCMRT_INFO("Save RT Image: " << status.text() << OFendl);
+    DRTImageIOD *rtimage = new DRTImageIOD();
+    if (rtimage != NULL)
+    {
+        status = fileformat.loadFile(RTIMAGE_FILENAME);
+        if (status.good())
+            status = rtimage->read(*fileformat.getDataset());
+        DCMRT_INFO("Read RT Image: " << status.text());
+        if (status.good())
+        {
+            fileformat.clear();
+            status = rtimage->write(*fileformat.getDataset());
+            DCMRT_INFO("Write RT Image: " << status.text());
+            if (status.good())
+                status = fileformat.saveFile("rtimage.dcm");
+            DCMRT_INFO("Save RT Image: " << status.text());
+        } else
+            DCMRT_INFO("Skipping further tests for RT Image");
+        delete rtimage;
+        rtimage = NULL;
+    } else
+        DCMRT_ERROR("Cannot allocate memory for RT Image");
+    DCMRT_INFO("" /* empty line */);
 
     /* test RT plan */
-    DRTPlanIOD rtplan;
-    status = fileformat.loadFile(RTPLAN_FILENAME);
-    if (status.good())
-        status = rtplan.read(*fileformat.getDataset());
-    DCMRT_INFO("Read RT Plan: " << status.text());
-    fileformat.clear();
-    status = rtplan.write(*fileformat.getDataset());
-    DCMRT_INFO("Write RT Plan: " << status.text());
-    if (status.good())
-        status = fileformat.saveFile("rtplan.dcm");
-    DCMRT_INFO("Save RT Plan: " << status.text() << OFendl);
-
+    DRTPlanIOD *rtplan = new DRTPlanIOD();
+    if (rtplan != NULL)
+    {
+        status = fileformat.loadFile(RTPLAN_FILENAME);
+        if (status.good())
+            status = rtplan->read(*fileformat.getDataset());
+        DCMRT_INFO("Read RT Plan: " << status.text());
+        if (status.good())
+        {
+            fileformat.clear();
+            status = rtplan->write(*fileformat.getDataset());
+            DCMRT_INFO("Write RT Plan: " << status.text());
+            if (status.good())
+                status = fileformat.saveFile("rtplan.dcm");
+            DCMRT_INFO("Save RT Plan: " << status.text());
 #ifdef TEST_ASSIGNMENT_OPERATOR
-    DRTPlanIOD rtplan2 = rtplan;
-    fileformat.clear();
-    status = rtplan2.write(*fileformat.getDataset());
-    DCMRT_INFO("Write copy of RT Plan: " << status.text());
-    if (status.good())
-        status = fileformat.saveFile("rtplan2.dcm");
-    DCMRT_INFO("Save copy of RT Plan: " << status.text() << OFendl);
+            DRTPlanIOD *rtplan2 = new DRTPlanIOD();
+            if (rtplan2 != NULL)
+            {
+                *rtplan2 = *rtplan;
+                fileformat.clear();
+                status = rtplan2->write(*fileformat.getDataset());
+                DCMRT_INFO("Write copy of RT Plan: " << status.text());
+                if (status.good())
+                    status = fileformat.saveFile("rtplan2.dcm");
+                DCMRT_INFO("Save copy of RT Plan: " << status.text());
+                delete rtplan2;
+                rtplan2= NULL;
+            } else
+                DCMRT_ERROR("Cannot allocate memory for copy of RT Plan");
 #endif
+        } else
+            DCMRT_INFO("Skipping further tests for RT Plan");
+        delete rtplan;
+        rtplan = NULL;
+    } else
+        DCMRT_ERROR("Cannot allocate memory for RT Plan");
+    DCMRT_INFO("" /* empty line */);
 
     /* test RT structure set */
-    DRTStructureSetIOD rtstruct;
-    status = fileformat.loadFile(RTSTRUCT_FILENAME);
-    if (status.good())
-        status = rtstruct.read(*fileformat.getDataset());
-    DCMRT_INFO("Read RT Structure Set: " << status.text());
-    fileformat.clear();
-    status = rtstruct.write(*fileformat.getDataset());
-    DCMRT_INFO("Write RT Structure Set: " << status.text());
-    if (status.good())
-        status = fileformat.saveFile("rtstruct.dcm");
-    DCMRT_INFO("Save RT Structure Set: " << status.text() << OFendl);
+    DRTStructureSetIOD *rtstruct = new DRTStructureSetIOD();
+    if (rtstruct != NULL)
+    {
+        status = fileformat.loadFile(RTSTRUCT_FILENAME);
+        if (status.good())
+            status = rtstruct->read(*fileformat.getDataset());
+        DCMRT_INFO("Read RT Structure Set: " << status.text());
+        if (status.good())
+        {
+            fileformat.clear();
+            status = rtstruct->write(*fileformat.getDataset());
+            DCMRT_INFO("Write RT Structure Set: " << status.text());
+            if (status.good())
+                status = fileformat.saveFile("rtstruct.dcm");
+            DCMRT_INFO("Save RT Structure Set: " << status.text());
+        } else
+            DCMRT_ERROR("Skipping further tests for RT Structure Set");
+        delete rtstruct;
+        rtstruct = NULL;
+    } else
+        DCMRT_ERROR("Cannot allocate memory for RT Structure Set");
+    DCMRT_INFO("" /* empty line */);
 
     /* test RT treatment summary record */
-    DRTTreatmentSummaryRecordIOD rttreat;
-    status = fileformat.loadFile(RTTREAT_FILENAME);
-    if (status.good())
-        status = rttreat.read(*fileformat.getDataset());
-    DCMRT_INFO("Read RT Treatment Summary Record: " << status.text());
-    fileformat.clear();
-    rttreat.getRequestAttributesSequence().clear();
-    DCMRT_INFO("Fix invalid sequence elements: " << status.text());
-    status = rttreat.write(*fileformat.getDataset());
-    DCMRT_INFO("Write RT Treatment Summary Record: " << status.text());
-    if (status.good())
-        status = fileformat.saveFile("rttreat.dcm");
-    DCMRT_INFO("Save RT Treatment Summary Record: " << status.text() << OFendl);
+    DRTTreatmentSummaryRecordIOD *rttreat = new DRTTreatmentSummaryRecordIOD();
+    if (rttreat != NULL)
+    {
+        status = fileformat.loadFile(RTTREAT_FILENAME);
+        if (status.good())
+            status = rttreat->read(*fileformat.getDataset());
+        DCMRT_INFO("Read RT Treatment Summary Record: " << status.text());
+        if (status.good())
+        {
+            fileformat.clear();
+            rttreat->getRequestAttributesSequence().clear();
+            DCMRT_INFO("Fix invalid sequence elements: " << status.text());
+            status = rttreat->write(*fileformat.getDataset());
+            DCMRT_INFO("Write RT Treatment Summary Record: " << status.text());
+            if (status.good())
+                status = fileformat.saveFile("rttreat.dcm");
+            DCMRT_INFO("Save RT Treatment Summary Record: " << status.text());
+        } else
+            DCMRT_ERROR("Skipping further tests for RT Treatment Summary Record");
+        delete rttreat;
+        rttreat = NULL;
+    } else
+        DCMRT_ERROR("Cannot allocate memory for RT Treatment Summary Record");
+    DCMRT_INFO("" /* empty line */);
 
     return 0;
 }
