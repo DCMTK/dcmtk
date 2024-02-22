d95c56c85ab3fceaf7fd5e46219800149a497535
Joerg Riesmeier <dicom@jriesmeier.com>
2024-02-22 14:59:10 +0100

    Added support for new Storage SOP Classes.
    Added definition of new Storage SOP Class UIDs introduced with Supplement
    226 (Cutaneous Confocal Microscopy). Also added support for these Storage
    SOP Classes to the various networking tools.

--- dcmdata/include/dcmtk/dcmdata/dcuid.h
+++ dcmdata/include/dcmtk/dcmdata/dcuid.h
@@ -660,6 +660,8 @@ DCMTK_DCMDATA_EXPORT unsigned long dcmGuessModalityBytes(const char *sopClassUID
 #define UID_OphthalmicOpticalCoherenceTomographyBscanVolumeAnalysisStorage "1.2.840.10008.5.1.4.1.1.77.1.5.8"
 #define UID_VLWholeSlideMicroscopyImageStorage                     "1.2.840.10008.5.1.4.1.1.77.1.6"
 #define UID_DermoscopicPhotographyImageStorage                     "1.2.840.10008.5.1.4.1.1.77.1.7"
+#define UID_ConfocalMicroscopyImageStorage                         "1.2.840.10008.5.1.4.1.1.77.1.8"
+#define UID_ConfocalMicroscopyTiledPyramidalImageStorage           "1.2.840.10008.5.1.4.1.1.77.1.9"
 #define UID_RETIRED_VLMultiframeImageStorage                       "1.2.840.10008.5.1.4.1.1.77.2"
 #define UID_LensometryMeasurementsStorage                          "1.2.840.10008.5.1.4.1.1.78.1"
 #define UID_AutorefractionMeasurementsStorage                      "1.2.840.10008.5.1.4.1.1.78.2"
