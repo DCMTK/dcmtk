191c587c9d74f5a72980483f2db654ab3b842446
Marco Eichelberg <dicom@offis.de>
2024-03-04 21:44:22 +0100

    Fixed commit #eeb7f7e4b.

--- ofstd/include/dcmtk/ofstd/oflimits.h
+++ ofstd/include/dcmtk/ofstd/oflimits.h
@@ -25,7 +25,30 @@
 #include "dcmtk/config/osconfig.h" // make sure OS specific configuration is included first
 #include <limits>
 
+#ifdef HAVE_CXX11
+
 template<typename T>
 using OFnumeric_limits = std::numeric_limits<T>;
+
+#else // fallback implementation of C++11 features
+
+template<typename T>
+struct OFnumeric_limits : std::numeric_limits<T>
+{
+    static const int max_digits10 = 0;
+    static inline T lowest() { return (std::numeric_limits<T>::min)(); }
+};
+template<>
+struct OFnumeric_limits<float> : std::numeric_limits<float>
+{
+    static inline float lowest() { return -(std::numeric_limits<float>::max)(); }
+};
+template<>
+struct OFnumeric_limits<double> : std::numeric_limits<double>
+{
+    static inline double lowest() { return -(std::numeric_limits<double>::max)(); }
+};
+
+#endif // fallback implementation of C++11 features based on std::numeric_limits
 
 #endif // OFLIMITS_H
