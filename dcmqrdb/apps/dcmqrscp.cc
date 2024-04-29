DCMTK/dcmtk

commit 96955b1573db41366d1b6cf7becda410b75b5596

Added TLS support to dcmqrscp.
Added TLS support to the dcmqrscp tool.

dcmqrdb/apps/dcmqrscp.cc
@@ -58,6 +58,7 @@ END_EXTERN_C
 #include "dcmtk/dcmdata/dcostrmz.h"    /* for dcmZlibCompressionLevel */
 #include "dcmtk/ofstd/ofgrp.h"
 #include "dcmtk/ofstd/ofpwd.h"
+#include "dcmtk/dcmtls/tlsopt.h"      /* for DcmTLSOptions */
 
 #ifdef WITH_SQL_DATABASE
 #include "dcmtk/dcmqrdbx/dcmqrdbq.h"
@@ -113,8 +114,12 @@ main(int argc, char *argv[])
   OFCmdUnsignedInt overrideMaxPDU = 0;
   DcmQueryRetrieveOptions options;
   DcmAssociationConfiguration asccfg;
+  DcmTLSOptions tlsOptions(NET_ACCEPTORREQUESTOR);
 
   OFStandard::initializeNetwork();
+#ifdef WITH_OPENSSL
+  DcmTLSTransportLayer::initializeOpenSSL();
+#endif
 
   char tempstr[20];
   OFString temp_str;
@@ -283,6 +288,9 @@ main(int argc, char *argv[])
       cmd.addOption("--discard-illegal",        "-Cd",     "discard characters that cannot be represented\nin destination character set");
 #endif
 
+  // add TLS specific command line options if (and only if) we are compiling with OpenSSL
+  tlsOptions.addTLSCommandlineOptions(cmd);
+
   cmd.addGroup("output options:");
     cmd.addSubGroup("bit preserving mode:");
       cmd.addOption("--normal",                 "-B",      "allow implicit format conversions (default)");
@@ -333,11 +341,13 @@ main(int argc, char *argv[])
         {
           app.printHeader(OFTrue /*print host identifier*/);
           COUT << OFendl << "External libraries used:";
-#if !defined(WITH_ZLIB) && !defined(WITH_TCPWRAPPER) && !defined(DCMTK_ENABLE_CHARSET_CONVERSION)
+#if !defined(WITH_ZLIB) && !defined(WITH_TCPWRAPPER) && !defined(DCMTK_ENABLE_CHARSET_CONVERSION) && !defined(WITH_OPENSSL)
           COUT << " none" << OFendl;
 #else
           COUT << OFendl;
 #endif
+        // print OpenSSL version if (and only if) we are compiling with OpenSSL
+        tlsOptions.printLibraryVersion();
 #ifdef WITH_ZLIB
           COUT << "- ZLIB, Version " << zlibVersion() << OFendl;
 #endif
@@ -347,6 +357,13 @@ main(int argc, char *argv[])
 #ifdef DCMTK_ENABLE_CHARSET_CONVERSION
           COUT << "- " << OFCharacterEncoding::getLibraryVersionString() << OFendl;
 #endif
+          return 0;
+        }
+
+        // check if the command line contains the --list-ciphers option
+        if (tlsOptions.listOfCiphersRequested(cmd))
+        {
+          tlsOptions.printSupportedCiphersuites(app, COUT);
           return 0;
         }
       }
@@ -769,6 +786,9 @@ main(int argc, char *argv[])
         dcmZlibCompressionLevel.set(OFstatic_cast(int, compressionLevel));
       }
 #endif
+
+      // evaluate (most of) the TLS command line options (if we are compiling with OpenSSL)
+      tlsOptions.parseArguments(app, cmd);
     }
 
     /* print resource identifier */
@@ -862,6 +882,13 @@ main(int argc, char *argv[])
       return 10;
     }
 
+    /* create a secure transport layer if requested and OpenSSL is available */
+    cond = tlsOptions.createTransportLayer(options.net_, NULL, app, cmd);
+    if (cond.bad()) {
+      OFLOG_FATAL(dcmqrscpLogger, DimseCondition::dump(temp_str, cond));
+      return 10;
+    }
+
 #if defined(HAVE_SETUID) && defined(HAVE_GRP_H) && defined(HAVE_PWD_H)
     OFStandard::OFGroup grp;
     OFStandard::OFPasswd pwd;
@@ -904,7 +931,7 @@ main(int argc, char *argv[])
     DcmQueryRetrieveIndexDatabaseHandleFactory factory(&config);
 #endif
 
-    DcmQueryRetrieveSCP scp(config, options, factory, asccfg);
+    DcmQueryRetrieveSCP scp(config, options, factory, asccfg, tlsOptions);
     scp.setDatabaseFlags(opt_checkFindIdentifier, opt_checkMoveIdentifier);
 
     /* loop waiting for associations */
@@ -912,6 +939,15 @@ main(int argc, char *argv[])
     {
       cond = scp.waitForAssociation(options.net_);
       if (!options.singleProcess_) scp.cleanChildren();  /* clean up any child processes */
+
+      /* since dcmqrscp is usually terminated with SIGTERM or the like,
+       * we write back an updated random seed after every association handled.
+       */
+      cond = tlsOptions.writeRandomSeed();
+      if (cond.bad()) {
+          // failure to write back the random seed is a warning, not an error
+          OFLOG_WARN(dcmqrscpLogger, DimseCondition::dump(temp_str, cond));
+      }
     }
 
     cond = ASC_dropNetwork(&options.net_);

dcmqrdb/apps/CMakeLists.txt
@@ -5,5 +5,5 @@ endforeach()
 
 # make sure executables are linked to the corresponding libraries
 foreach(PROGRAM dcmqrscp dcmqridx dcmqrti)
-  DCMTK_TARGET_LINK_MODULES(${PROGRAM} dcmqrdb dcmnet dcmdata oflog ofstd)
+  DCMTK_TARGET_LINK_MODULES(${PROGRAM} dcmqrdb dcmtls dcmnet dcmdata oflog ofstd)
 endforeach()

dcmqrdb/apps/Makefile.in
@@ -37,20 +37,25 @@ dcmnetinc = -I$(dcmnetdir)/include
 dcmnetlibdir = -L$(dcmnetdir)/libsrc
 dcmnetlib = -ldcmnet
 
-LOCALINCLUDES = $(dcmnetinc) $(dcmdatainc) $(ofstdinc) $(ofloginc)
-LIBDIRS = -L$(top_srcdir)/libsrc $(dcmnetlibdir) $(dcmdatalibdir) $(ofloglibdir) \
-	$(ofstdlibdir) $(oficonvlibdir)
-LOCALLIBS = -ldcmqrdb $(dcmnetlib) $(dcmdatalib) $(ofloglib) $(ofstdlib) $(oficonvlib) \
+dcmtlsdir = $(top_srcdir)/../dcmtls
+dcmtlsinc = -I$(dcmtlsdir)/include
+dcmtlslibdir = -L$(dcmtlsdir)/libsrc
+dcmtlslib = -ldcmtls
+
+LOCALINCLUDES = $(dcmnetinc) $(dcmdatainc) $(ofstdinc) $(ofloginc) $(dcmtlsinc)
+LIBDIRS = -L$(top_srcdir)/libsrc $(dcmnetlibdir) $(dcmdatalibdir) \
+	$(ofstdlibdir) $(ofloglibdir) $(dcmtlslibdir) $(oficonvlibdir)
+LOCALLIBS = -ldcmqrdb $(dcmnetlib) $(dcmtlslib) $(dcmdatalib) \
+    $(ofstdlib) $(ofloglib) $(oficonvlib)\
 	$(ZLIBLIBS) $(TCPWRAPPERLIBS) $(CHARCONVLIBS) $(MATHLIBS)
 
 objs = dcmqrscp.o dcmqrti.o dcmqridx.o
 progs = dcmqrscp dcmqrti dcmqridx
 
-
 all: $(progs)
 
 dcmqrscp: dcmqrscp.o
-	$(CXX) $(CXXFLAGS) $(LIBDIRS) $(LDFLAGS) -o $@ dcmqrscp.o $(LOCALLIBS) $(LIBS)
+	$(CXX) $(CXXFLAGS) $(LIBDIRS) $(LDFLAGS) -o $@ dcmqrscp.o $(dcmtlslib) $(LOCALLIBS) $(OPENSSLLIBS) $(LIBS)
 
 dcmqrti: dcmqrti.o
 	$(CXX) $(CXXFLAGS) $(LIBDIRS) $(LDFLAGS) -o $@ dcmqrti.o $(LOCALLIBS) $(LIBS)

dcmqrdb/docs/dcmqrscp.man
@@ -422,6 +422,57 @@ specific character set:
           in destination character set
 \endverbatim
 
+\subsection dcmqrscp_tls_options transport layer security (TLS) options
+\verbatim
+  transport protocol stack:
+    -tls  --disable-tls           use normal TCP/IP connection (default)
+    +tls  --enable-tls            [p]rivate key file, [c]ertificate file: string
+                                  use authenticated secure TLS connection
+  private key password (only with --enable-tls):
+    +ps   --std-passwd            prompt user to type password on stdin (default)
+    +pw   --use-passwd            [p]assword: string
+                                  use specified password
+    -pw   --null-passwd           use empty string as password
+  key and certificate file format:
+    -pem  --pem-keys              read keys and certificates as PEM file (default)
+    -der  --der-keys              read keys and certificates as DER file
+  certification authority:
+    +cf   --add-cert-file         [f]ilename: string
+                                  add certificate file to list of certificates
+    +cd   --add-cert-dir          [d]irectory: string
+                                  add certificates in d to list of certificates
+    +crl  --add-crl-file          [f]ilename: string
+                                  add certificate revocation list file
+                                  (implies --enable-crl-vfy)
+    +crv  --enable-crl-vfy        enable leaf CRL verification
+    +cra  --enable-crl-all        enable full chain CRL verification
+  security profile:
+    +py   --profile-bcp195-nd     Non-downgrading BCP 195 TLS Profile (default)
+    +px   --profile-bcp195        BCP 195 TLS Profile
+    +pz   --profile-bcp195-ex     Extended BCP 195 TLS Profile
+    +pa   --profile-aes           AES TLS Secure Transport Connection Profile
+                                  (retired)
+    +pn   --profile-null          Authenticated unencrypted communication
+                                  (retired, was used in IHE ATNA)
+  ciphersuite:
+    +cc   --list-ciphers          show list of supported TLS ciphersuites and exit
+    +cs   --cipher                [c]iphersuite name: string
+                                  add ciphersuite to list of negotiated suites
+                                  (not with --profile-bcp195-ex)
+    +dp   --dhparam               [f]ilename: string
+                                  read DH parameters for DH/DSS ciphersuites
+  pseudo random generator:
+    +rs   --seed                  [f]ilename: string
+                                  seed random generator with contents of f
+    +ws   --write-seed            write back modified seed (only with --seed)
+    +wf   --write-seed-file       [f]ilename: string (only with --seed)
+                                  write modified seed to file f
+  peer authentication:
+    -rc   --require-peer-cert     verify peer certificate, fail if absent (default)
+    -vc   --verify-peer-cert      verify peer certificate if present
+    -ic   --ignore-peer-cert      don't verify peer certificate
+\endverbatim
+
 \subsection dcmqrscp_output_options output options
 \verbatim
 bit preserving mode:

dcmqrdb/include/dcmtk/dcmqrdb/dcmqrsrv.h
@@ -1,174 +1,0 @@
-/*
- *
- *  Copyright (C) 1993-2023, OFFIS e.V.
- *  All rights reserved.  See COPYRIGHT file for details.
- *
- *  This software and supporting documentation were developed by
- *
- *    OFFIS e.V.
- *    R&D Division Health
- *    Escherweg 2
- *    D-26121 Oldenburg, Germany
- *
- *
- *  Module:  dcmqrdb
- *
- *  Author:  Marco Eichelberg
- *
- *  Purpose: class DcmQueryRetrieveSCP
- *
- */
-
-#ifndef DCMQRSRV_H
-#define DCMQRSRV_H
-
-#include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first */
-#include "dcmtk/ofstd/oftypes.h"
-#include "dcmtk/dcmnet/assoc.h"
-#include "dcmtk/dcmnet/dimse.h"
-#include "dcmtk/dcmnet/dcasccfg.h"
-#include "dcmtk/dcmqrdb/dcmqrptb.h"
-
-class DcmQueryRetrieveConfig;
-class DcmQueryRetrieveOptions;
-class DcmQueryRetrieveDatabaseHandle;
-class DcmQueryRetrieveDatabaseHandleFactory;
-
-/// enumeration describing reasons for refusing an association request
-enum CTN_RefuseReason
-{
-    /// too many concurrent associations
-    CTN_TooManyAssociations,
-    /// fork system function failed
-    CTN_CannotFork,
-    /// bad application context (not DICOM)
-    CTN_BadAppContext,
-    /// unknown peer application entity title (access not authorised)
-    CTN_BadAEPeer,
-    /// unknown peer application entity title (access not authorised)
-    CTN_BadAEService,
-    /// other non-specific reason
-    CTN_NoReason
-};
-
-/** main class for Query/Retrieve Service Class Provider
- */
-class DCMTK_DCMQRDB_EXPORT DcmQueryRetrieveSCP
-{
-public:
-
-  /** constructor
-   *  @param config SCP configuration facility
-   *  @param options SCP configuration options
-   *  @param factory factory object used to create database handles
-   */
-  DcmQueryRetrieveSCP(
-    const DcmQueryRetrieveConfig& config,
-    const DcmQueryRetrieveOptions& options,
-    const DcmQueryRetrieveDatabaseHandleFactory& factory,
-    const DcmAssociationConfiguration& associationConfiguration);
-
-  /// destructor
-  virtual ~DcmQueryRetrieveSCP() { }
-
-  /** wait for incoming A-ASSOCIATE requests, perform association negotiation
-   *  and serve the requests. May fork child processes depending on availability
-   *  of the fork() system function and configuration options.
-   *  @param theNet network structure for listen socket
-   *  @return EC_Normal if successful, an error code otherwise
-   */
-  OFCondition waitForAssociation(T_ASC_Network *theNet);
-
-  /** set database flags
-   *  @param dbCheckFindIdentifier flag indicating that a check should be performed for C-FIND requests
-   *  @param dbCheckMoveIdentifier flag indicating that a check should be performed for C-MOVE requests
-   */
-  void setDatabaseFlags(
-    OFBool dbCheckFindIdentifier,
-    OFBool dbCheckMoveIdentifier);
-
-  /** clean up terminated child processes.
-   */
-  void cleanChildren();
-
-private:
-
-  /// private undefined copy constructor
-  DcmQueryRetrieveSCP(const DcmQueryRetrieveSCP& other);
-
-  /// private undefined assignment operator
-  DcmQueryRetrieveSCP& operator=(const DcmQueryRetrieveSCP& other);
-
-  /** perform association negotiation for an incoming A-ASSOCIATE request based
-   *  on the SCP configuration and option flags. No A-ASSOCIATE response is generated,
-   *  this is left to the caller.
-   *  @param assoc incoming association
-   *  @return EC_Normal if successful, an error code otherwise
-   */
-  OFCondition negotiateAssociation(T_ASC_Association * assoc);
-
-  OFCondition refuseAssociation(T_ASC_Association ** pAssoc, CTN_RefuseReason reason);
-
-  OFCondition handleAssociation(
-    T_ASC_Association ** assoc,
-    OFBool correctUIDPadding);
-
-  OFCondition echoSCP(
-    T_ASC_Association * assoc,
-    T_DIMSE_C_EchoRQ * req,
-    T_ASC_PresentationContextID presId);
-
-  OFCondition findSCP(
-    T_ASC_Association * assoc,
-    T_DIMSE_C_FindRQ * request,
-    T_ASC_PresentationContextID presID,
-    DcmQueryRetrieveDatabaseHandle& dbHandle);
-
-  OFCondition getSCP(
-    T_ASC_Association * assoc,
-    T_DIMSE_C_GetRQ * request,
-    T_ASC_PresentationContextID presID,
-    DcmQueryRetrieveDatabaseHandle& dbHandle);
-
-  OFCondition moveSCP(
-    T_ASC_Association * assoc,
-    T_DIMSE_C_MoveRQ * request,
-    T_ASC_PresentationContextID presID,
-    DcmQueryRetrieveDatabaseHandle& dbHandle);
-
-  OFCondition storeSCP(
-    T_ASC_Association * assoc,
-    T_DIMSE_C_StoreRQ * req,
-    T_ASC_PresentationContextID presId,
-    DcmQueryRetrieveDatabaseHandle& dbHandle,
-    OFBool correctUIDPadding);
-
-  OFCondition dispatch(
-    T_ASC_Association *assoc,
-    OFBool correctUIDPadding);
-
-  static void refuseAnyStorageContexts(T_ASC_Association *assoc);
-
-  /// configuration facility
-  const DcmQueryRetrieveConfig *config_;
-
-  /// child process table, only used in multi-processing mode
-  DcmQueryRetrieveProcessTable processtable_;
-
-  /// flag for database interface: check C-FIND identifier
-  OFBool dbCheckFindIdentifier_;
-
-  /// flag for database interface: check C-MOVE identifier
-  OFBool dbCheckMoveIdentifier_;
-
-  /// factory object used to create database handles
-  const DcmQueryRetrieveDatabaseHandleFactory& factory_;
-
-  /// SCP configuration options
-  const DcmQueryRetrieveOptions& options_;
-
-  /// Association configuration profiles read from configuration file
-  const DcmAssociationConfiguration& associationConfiguration_;
-};
-
-#endif

dcmqrdb/libsrc/CMakeLists.txt
@@ -13,4 +13,4 @@ DCMTK_ADD_LIBRARY(dcmqrdb
   dcmqrtis.cc
 )
 
-DCMTK_TARGET_LINK_MODULES(dcmqrdb ofstd dcmdata dcmnet)
+DCMTK_TARGET_LINK_MODULES(dcmqrdb ofstd dcmdata dcmnet dcmtls)

dcmqrdb/libsrc/Makefile.in
@@ -16,9 +16,10 @@ ofstddir = $(top_srcdir)/../ofstd
 oflogdir = $(top_srcdir)/../oflog
 dcmdatadir = $(top_srcdir)/../dcmdata
 dcmnetdir = $(top_srcdir)/../dcmnet
+dcmtlsdir = $(top_srcdir)/../dcmtls
 
 LOCALINCLUDES = -I$(dcmnetdir)/include -I$(dcmdatadir)/include \
-	-I$(ofstddir)/include -I$(oflogdir)/include
+	-I$(ofstddir)/include -I$(oflogdir)/include -I$(dcmtlsdir)/include
 LOCALDEFS =
 
 objs = dcmqrcbf.o dcmqrcbg.o dcmqrcbm.o dcmqrcbs.o dcmqrcnf.o dcmqrdbi.o  \

dcmqrdb/libsrc/dcmqrsrv.cc
@@ -30,7 +30,9 @@
 #include "dcmtk/dcmqrdb/dcmqrcbm.h"    /* for class DcmQueryRetrieveMoveContext */
 #include "dcmtk/dcmqrdb/dcmqrcbg.h"    /* for class DcmQueryRetrieveGetContext */
 #include "dcmtk/dcmqrdb/dcmqrcbs.h"    /* for class DcmQueryRetrieveStoreContext */
+#include "dcmtk/dcmtls/tlsopt.h"      /* for DcmTLSOptions */
 
+#include <openssl/crypto.h>
 
 static void findCallback(
   /* in */
@@ -100,7 +102,8 @@ DcmQueryRetrieveSCP::DcmQueryRetrieveSCP(
   const DcmQueryRetrieveConfig& config,
   const DcmQueryRetrieveOptions& options,
   const DcmQueryRetrieveDatabaseHandleFactory& factory,
-  const DcmAssociationConfiguration& associationConfiguration)
+  const DcmAssociationConfiguration& associationConfiguration,
+  DcmTLSOptions& tlsOptions)
 : config_(&config)
 , processtable_()
 , dbCheckFindIdentifier_(OFFalse)
@@ -108,6 +111,7 @@ DcmQueryRetrieveSCP::DcmQueryRetrieveSCP(
 , factory_(factory)
 , options_(options)
 , associationConfiguration_(associationConfiguration)
+, tlsOptions_(tlsOptions)
 {
 }
 
@@ -1000,7 +1004,7 @@ OFCondition DcmQueryRetrieveSCP::waitForAssociation(T_ASC_Network * theNet)
 
     if (ASC_associationWaiting(theNet, timeout))
     {
-        cond = ASC_receiveAssociation(theNet, &assoc, (int)options_.maxPDU_);
+        cond = ASC_receiveAssociation(theNet, &assoc, (int)options_.maxPDU_, NULL, NULL, tlsOptions_.secureConnectionRequested());
         if (cond.bad())
         {
           DCMQRDB_INFO("Failed to receive association: " << DimseCondition::dump(temp_str, cond));
@@ -1118,7 +1122,13 @@ OFCondition DcmQueryRetrieveSCP::waitForAssociation(T_ASC_Network * theNet)
             }
             else if (pid > 0)
             {
-                /* parent process, note process in table */
+                // parent process, note process in table and close network connection.
+
+                // set the parent process mode in the association.
+                // This will prevent the TLS connection to be shutdown by ASC_dropAssociation().
+                ASC_setParentProcessMode(assoc);
+
+                // note process in table
                 processtable_.addProcessToTable(pid, assoc);
             }
             else

commit ebaf6941b754e9117474764a1560bf6c45d7711c

Fixed issue with error handling (TLS support).
Fixed issue with error handling that was introduced with commit
96955b1573, i.e. when adding support for TLS.

dcmqrdb/apps/dcmqrscp.cc
@@ -943,10 +943,10 @@ main(int argc, char *argv[])
       /* since dcmqrscp is usually terminated with SIGTERM or the like,
        * we write back an updated random seed after every association handled.
        */
-      cond = tlsOptions.writeRandomSeed();
-      if (cond.bad()) {
+      const OFCondition cond2 = tlsOptions.writeRandomSeed();
+      if (cond2.bad()) {
           // failure to write back the random seed is a warning, not an error
-          OFLOG_WARN(dcmqrscpLogger, DimseCondition::dump(temp_str, cond));
+          OFLOG_WARN(dcmqrscpLogger, DimseCondition::dump(temp_str, cond2));
       }
     }
 