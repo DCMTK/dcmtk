DCMTK/dcmtk

commit c34f4e46e672ad21accf04da0dc085e43be6f5e1

Fixed memory leak in single process mode.
Fixed a memory leak in dcmqrscp's single process mode.

Thanks to <songxiangpu@mail.sdu.edu.cn> for the bug report and test data.

dcmqrdb/libsrc/dcmqrsrv.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 1993-2021, OFFIS e.V.
+ *  Copyright (C) 1993-2022, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -1126,21 +1126,17 @@ OFCondition DcmQueryRetrieveSCP::waitForAssociation(T_ASC_Network * theNet)
 #endif
     }
 
-    // cleanup code
+    // clean-up association
     OFCondition oldcond = cond;    /* store condition flag for later use */
-    if (!options_.singleProcess_ && (cond != ASC_SHUTDOWNAPPLICATION))
+    cond = ASC_dropAssociation(assoc);
+    if (cond.bad())
     {
-        /* the child will handle the association, we can drop it */
-        cond = ASC_dropAssociation(assoc);
-        if (cond.bad())
-        {
-            DCMQRDB_ERROR("Cannot Drop Association: " << DimseCondition::dump(temp_str, cond));
-        }
-        cond = ASC_destroyAssociation(&assoc);
-        if (cond.bad())
-        {
-            DCMQRDB_ERROR("Cannot Destroy Association: " << DimseCondition::dump(temp_str, cond));
-        }
+        DCMQRDB_ERROR("Cannot Drop Association: " << DimseCondition::dump(temp_str, cond));
+    }
+    cond = ASC_destroyAssociation(&assoc);
+    if (cond.bad())
+    {
+        DCMQRDB_ERROR("Cannot Destroy Association: " << DimseCondition::dump(temp_str, cond));
     }
 
     if (oldcond == ASC_SHUTDOWNAPPLICATION) cond = oldcond; /* abort flag is reported to top-level wait loop */

commit ee6690797516de8e892196fdec9b03367d5b9976

Fixed crash shutting down via private SOP class.
Fixed crash (segmentation fault) when shutting down the application via
the private SOP Class. This issue was introduced with commit c34f4e46e6,
which fixed a memory leak.

Thanks to DCMTK forum user "nbeck" for the original report.

dcmqrdb/libsrc/dcmqrsrv.cc
@@ -1140,20 +1140,22 @@ OFCondition DcmQueryRetrieveSCP::waitForAssociation(T_ASC_Network * theNet)
 #endif
     }
 
-    // clean-up association
-    OFCondition oldcond = cond;    /* store condition flag for later use */
-    cond = ASC_dropAssociation(assoc);
-    if (cond.bad())
+    // avoid double deletion of memory
+    if (cond != ASC_SHUTDOWNAPPLICATION)
     {
-        DCMQRDB_ERROR("Cannot Drop Association: " << DimseCondition::dump(temp_str, cond));
-    }
-    cond = ASC_destroyAssociation(&assoc);
-    if (cond.bad())
-    {
-        DCMQRDB_ERROR("Cannot Destroy Association: " << DimseCondition::dump(temp_str, cond));
+      // clean-up association
+      cond = ASC_dropAssociation(assoc);
+      if (cond.bad())
+      {
+          DCMQRDB_ERROR("Cannot Drop Association: " << DimseCondition::dump(temp_str, cond));
+      }
+      cond = ASC_destroyAssociation(&assoc);
+      if (cond.bad())
+      {
+          DCMQRDB_ERROR("Cannot Destroy Association: " << DimseCondition::dump(temp_str, cond));
+      }
     }
 
-    if (oldcond == ASC_SHUTDOWNAPPLICATION) cond = oldcond; /* abort flag is reported to top-level wait loop */
     return cond;
 }
 