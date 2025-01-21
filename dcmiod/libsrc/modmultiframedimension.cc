b7fba5ab939474db292af5e2c38df74684da54c1
Joerg Riesmeier <dicom@jriesmeier.com>
2025-01-21 11:45:06 +0100

    Made sure all member variables are initialized.
    Initialized member variable that was missing in the member initializer
    list of the constructor. This issue was reported at runtime when compiling
    with gcc and option -fsanitize=undefined.

--- dcmiod/libsrc/modmultiframedimension.cc
+++ dcmiod/libsrc/modmultiframedimension.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2015-2024, Open Connections GmbH
+ *  Copyright (C) 2015-2025, Open Connections GmbH
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation are maintained by
@@ -46,6 +46,7 @@ IODMultiframeDimensionModule::IODMultiframeDimensionModule()
     : IODModule()
     , m_DimensionIndexSequence()
     , m_DimensionOrganizationSequence()
+    , m_CheckOnWrite(OFTrue)
 {
     resetRules();
 }
