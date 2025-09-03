c56b483097dec403ce4c9668497a6531bc0e36e8
Marco Eichelberg <eichelberg@offis.de>
2025-09-03 14:20:22 +0200

    Added new tool dcmencap that encapsulates documents.
    Added new tool dcmencap that encapsulates documents of all types currently
    supported in DICOM (PDF, CDA, STL, MTL and OBJ) into their corresponding
    Encapulated Document Storage SOP Class in DICOM. The formerly separate tools
    pdf2dcm, cda2dcm and stl2dcm are now deprecated and act as stubs that call
    dcmencap. The document format (and, thus, SOP class) is now automatically
    determined by default.

--- dcmdata/apps/pdf2dcm.cc
+++ dcmdata/apps/pdf2dcm.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2005-2019, OFFIS e.V.
+ *  Copyright (C) 2025, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -13,122 +13,33 @@
  *
  *  Module:  dcmdata
  *
- *  Author:  Marco Eichelberg, Pedro Arizpe
+ *  Authors: Marco Eichelberg
  *
- *  Purpose: Encapsulate PDF file into a DICOM file
+ *  Purpose: Proxy stub that calls dcmencap
  *
  */
 
 #include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first */
-#include "dcmtk/dcmdata/dctk.h"
-#include "dcmtk/dcmdata/dcencdoc.h"
-#include "dcmtk/ofstd/ofconapp.h"
+#include "dcmtk/ofstd/ofstub.h"
+#include "dcmtk/ofstd/ofstd.h"
+#include <cstring>
 
-#ifdef WITH_ZLIB
-#include <zlib.h>        /* for zlibVersion() */
-#endif
-
-#define OFFIS_CONSOLE_APPLICATION "pdf2dcm"
-
-static OFLogger pdf2dcmLogger = OFLog::getLogger("dcmtk.apps." OFFIS_CONSOLE_APPLICATION);
-
-static char rcsid[] = "$dcmtk: " OFFIS_CONSOLE_APPLICATION " v"
-OFFIS_DCMTK_VERSION " " OFFIS_DCMTK_RELEASEDATE " $";
-
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-  OFConsoleApplication app(OFFIS_CONSOLE_APPLICATION, "Encapsulate PDF file into DICOM format", rcsid);
-  OFCommandLine cmd;
-  int errorCode = EXITCODE_NO_ERROR;
-  OFCondition result = EC_Normal;
-  DcmFileFormat fileformat;
-  DcmEncapsulatedDocument encapsulator;
+    // create an argv array that is one entry larger than the one specified by the user
+    char **my_argv = new char *[argc+2];
+    char filetype[100];
+    OFStandard::strlcpy(filetype, "--filetype-pdf", sizeof(filetype));
 
-  OFLOG_TRACE(pdf2dcmLogger, "Including necessary options");
-  encapsulator.addPDFCommandlineOptions(cmd);
-  OFLOG_TRACE(pdf2dcmLogger, "Evaluating command line");
-  prepareCmdLineArgs(argc, argv, OFFIS_CONSOLE_APPLICATION);
+    // copy arguments, then add file type argument and NULL pointer
+    memcpy(my_argv, argv, argc * sizeof(char *));
+    my_argv[argc] = filetype;
+    my_argv[argc+1] = NULL;
 
-  if (app.parseCommandLine(cmd, argc, argv)) {
-    OFLOG_TRACE(pdf2dcmLogger, "Checking exclusive options first");
-    if (cmd.hasExclusiveOption())
-    {
-      if (cmd.findOption("--version"))
-      {
-        app.printHeader(OFTrue /*print host identifier*/);
-        COUT << OFendl << "External libraries used: ";
-#ifdef WITH_ZLIB
-        COUT << OFendl << "- ZLIB, Version " << zlibVersion() << OFendl;
-#else
-        COUT << " none" << OFendl;
-#endif
-        return EXITCODE_NO_ERROR;
-      }
-    }
-    encapsulator.parseArguments(app, cmd);
-  }
-  //print resource identifier
-  OFLOG_DEBUG(pdf2dcmLogger, rcsid << OFendl);
+    // call stub
+    int result = OFstub_main(argc+1, my_argv, "pdf2dcm", "dcmencap");
 
-  OFLOG_DEBUG(pdf2dcmLogger, "making sure data dictionary is loaded");
-  if (!dcmDataDict.isDictionaryLoaded())
-  {
-    OFLOG_WARN(pdf2dcmLogger, "no data dictionary loaded, check environment variable: "
-      << DCM_DICT_ENVIRONMENT_VARIABLE);
-  }
-  OFLOG_DEBUG(pdf2dcmLogger, "Creating identifiers (and reading series data)");
-  result = encapsulator.createIdentifiers(pdf2dcmLogger);
-  if (result.bad())
-  {
-    OFLOG_FATAL(pdf2dcmLogger, "There was an error while reading the series data");
-    return EXITCODE_INVALID_INPUT_FILE;
-  }
-  OFLOG_INFO(pdf2dcmLogger, "creating encapsulated PDF object");
-  errorCode = encapsulator.insertEncapsulatedDocument(fileformat.getDataset(), pdf2dcmLogger);
-  if (errorCode != EXITCODE_NO_ERROR)
-  {
-    OFLOG_ERROR(pdf2dcmLogger, "unable to create PDF encapsulation to DICOM format");
-    return errorCode;
-  }
-  OFLOG_INFO(pdf2dcmLogger, "Generating an instance number that is guaranteed to be unique within a series.");
-  result = encapsulator.createHeader(fileformat.getDataset(), pdf2dcmLogger);
-  if (result.bad())
-  {
-    OFLOG_ERROR(pdf2dcmLogger, "unable to create DICOM header: " << result.text());
-    return EXITCODE_CANNOT_WRITE_OUTPUT_FILE;
-  }
-  OFLOG_INFO(pdf2dcmLogger, "writing encapsulated PDF object as file " << encapsulator.getOutputFileName());
-
-  OFLOG_INFO(pdf2dcmLogger, "Check if new output transfer syntax is possible");
-
-  DcmXfer opt_oxferSyn(encapsulator.getTransferSyntax());
-
-  fileformat.getDataset()->chooseRepresentation(encapsulator.getTransferSyntax(), NULL);
-  if (fileformat.getDataset()->canWriteXfer(encapsulator.getTransferSyntax()))
-  {
-    OFLOG_INFO(pdf2dcmLogger, "Output transfer syntax " << opt_oxferSyn.getXferName() << " can be written");
-  }
-  else {
-    OFLOG_ERROR(pdf2dcmLogger, "No conversion to transfer syntax " << opt_oxferSyn.getXferName() << " possible!");
-    return EXITCODE_COMMANDLINE_SYNTAX_ERROR;
-  }
-  OFLOG_INFO(pdf2dcmLogger, "Checking for DICOM key overriding");
-  result = encapsulator.applyOverrideKeys(fileformat.getDataset());
-  if (result.bad())
-  {
-    OFLOG_ERROR(pdf2dcmLogger, "There was a problem while overriding a key:" << OFendl
-      << result.text());
-    return EXITCODE_CANNOT_WRITE_OUTPUT_FILE;
-  }
-  OFLOG_INFO(pdf2dcmLogger, "write converted DICOM file with metaheader");
-  result = encapsulator.saveFile(fileformat);
-  if (result.bad())
-  {
-    OFLOG_ERROR(pdf2dcmLogger, result.text() << ": writing file: " << encapsulator.getOutputFileName());
-    return EXITCODE_CANNOT_WRITE_OUTPUT_FILE;
-  }
-
-  OFLOG_INFO(pdf2dcmLogger, "PDF encapsulation successful");
-
-  return EXITCODE_NO_ERROR;
+    // clean up (Windows only, on Posix systems the stub will not return)
+    delete[] my_argv;
+    return result;
 }
--- dcmdata/apps/stl2dcm.cc
+++ dcmdata/apps/stl2dcm.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2018-2019, OFFIS e.V.
+ *  Copyright (C) 2025, OFFIS e.V.
  *  All rights reserved.  See COPYRIGHT file for details.
  *
  *  This software and supporting documentation were developed by
@@ -13,122 +13,33 @@
  *
  *  Module:  dcmdata
  *
- *  Author:  Pedro Arizpe
+ *  Authors: Marco Eichelberg
  *
- *  Purpose: Encapsulate STL file into a DICOM file
+ *  Purpose: Proxy stub that calls dcmencap
  *
  */
 
-#include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first*/
-#include "dcmtk/dcmdata/dctk.h"
-#include "dcmtk/dcmdata/dcencdoc.h"
-#include "dcmtk/ofstd/ofconapp.h"
+#include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first */
+#include "dcmtk/ofstd/ofstub.h"
+#include "dcmtk/ofstd/ofstd.h"
+#include <cstring>
 
-#ifdef WITH_ZLIB
-#include <zlib.h>        /* for zlibVersion() */
-#endif
-
-#define OFFIS_CONSOLE_APPLICATION "stl2dcm"
-
-static OFLogger stl2dcmLogger = OFLog::getLogger("dcmtk.apps." OFFIS_CONSOLE_APPLICATION);
-
-static char rcsid[] = "$dcmtk: " OFFIS_CONSOLE_APPLICATION " v"
-OFFIS_DCMTK_VERSION " " OFFIS_DCMTK_RELEASEDATE " $";
-
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-  OFConsoleApplication app(OFFIS_CONSOLE_APPLICATION, "Encapsulate STL file into DICOM format", rcsid);
-  OFCommandLine cmd;
-  int errorCode = EXITCODE_NO_ERROR;
-  OFCondition result = EC_Normal;
-  DcmFileFormat fileformat;
-  DcmEncapsulatedDocument encapsulator;
+    // create an argv array that is one entry larger than the one specified by the user
+    char **my_argv = new char *[argc+2];
+    char filetype[100];
+    OFStandard::strlcpy(filetype, "--filetype-stl", sizeof(filetype));
 
-  OFLOG_TRACE(stl2dcmLogger, "Including necessary options");
-  encapsulator.addSTLCommandlineOptions(cmd);
-  OFLOG_TRACE(stl2dcmLogger, "Evaluating command line");
-  prepareCmdLineArgs(argc, argv, OFFIS_CONSOLE_APPLICATION);
+    // copy arguments, then add file type argument and NULL pointer
+    memcpy(my_argv, argv, argc * sizeof(char *));
+    my_argv[argc] = filetype;
+    my_argv[argc+1] = NULL;
 
-  if (app.parseCommandLine(cmd, argc, argv)) {
-    OFLOG_TRACE(stl2dcmLogger, "Checking exclusive options first");
-    if (cmd.hasExclusiveOption())
-    {
-      if (cmd.findOption("--version"))
-      {
-        app.printHeader(OFTrue /*print host identifier*/);
-        COUT << OFendl << "External libraries used: ";
-#ifdef WITH_ZLIB
-        COUT << OFendl << "- ZLIB, Version " << zlibVersion() << OFendl;
-#else
-        COUT << " none" << OFendl;
-#endif
-        return EXITCODE_NO_ERROR;
-      }
-    }
-    encapsulator.parseArguments(app, cmd);
-  }
-  //print resource identifier
-  OFLOG_DEBUG(stl2dcmLogger, rcsid << OFendl);
+    // call stub
+    int result = OFstub_main(argc+1, my_argv, "stl2dcm", "dcmencap");
 
-  OFLOG_DEBUG(stl2dcmLogger, "making sure data dictionary is loaded");
-  if (!dcmDataDict.isDictionaryLoaded())
-  {
-    OFLOG_WARN(stl2dcmLogger, "no data dictionary loaded, check environment variable: "
-      << DCM_DICT_ENVIRONMENT_VARIABLE);
-  }
-  OFLOG_DEBUG(stl2dcmLogger, "Creating identifiers (and reading series data)");
-  result = encapsulator.createIdentifiers(stl2dcmLogger);
-  if (result.bad())
-  {
-    OFLOG_FATAL(stl2dcmLogger, "There was an error while reading the series data");
-    return EXITCODE_INVALID_INPUT_FILE;
-  }
-  OFLOG_INFO(stl2dcmLogger, "creating encapsulated STL object");
-  errorCode = encapsulator.insertEncapsulatedDocument(fileformat.getDataset(), stl2dcmLogger);
-  if (errorCode != EXITCODE_NO_ERROR)
-  {
-    OFLOG_ERROR(stl2dcmLogger, "unable to create STL encapsulation to DICOM format");
-    return errorCode;
-  }
-  OFLOG_INFO(stl2dcmLogger, "Generating an instance number that is guaranteed to be unique within a series.");
-  result = encapsulator.createHeader(fileformat.getDataset(), stl2dcmLogger);
-  if (result.bad())
-  {
-    OFLOG_ERROR(stl2dcmLogger, "unable to create DICOM header: " << result.text());
-    return EXITCODE_CANNOT_WRITE_OUTPUT_FILE;
-  }
-  OFLOG_INFO(stl2dcmLogger, "writing encapsulated STL object as file " << encapsulator.getOutputFileName());
-
-  OFLOG_INFO(stl2dcmLogger, "Check if new output transfer syntax is possible");
-
-  DcmXfer opt_oxferSyn(encapsulator.getTransferSyntax());
-
-  fileformat.getDataset()->chooseRepresentation(encapsulator.getTransferSyntax(), NULL);
-  if (fileformat.getDataset()->canWriteXfer(encapsulator.getTransferSyntax()))
-  {
-    OFLOG_INFO(stl2dcmLogger, "Output transfer syntax " << opt_oxferSyn.getXferName() << " can be written");
-  }
-  else {
-    OFLOG_ERROR(stl2dcmLogger, "No conversion to transfer syntax " << opt_oxferSyn.getXferName() << " possible!");
-    return EXITCODE_COMMANDLINE_SYNTAX_ERROR;
-  }
-  OFLOG_INFO(stl2dcmLogger, "Checking for DICOM key overriding");
-  result = encapsulator.applyOverrideKeys(fileformat.getDataset());
-  if (result.bad())
-  {
-    OFLOG_ERROR(stl2dcmLogger, "There was a problem while overriding a key:" << OFendl
-      << result.text());
-    return EXITCODE_CANNOT_WRITE_OUTPUT_FILE;
-  }
-  OFLOG_INFO(stl2dcmLogger, "write converted DICOM file with metaheader");
-  result = encapsulator.saveFile(fileformat);
-  if (result.bad())
-  {
-    OFLOG_ERROR(stl2dcmLogger, result.text() << ": writing file: " << encapsulator.getOutputFileName());
-    return EXITCODE_CANNOT_WRITE_OUTPUT_FILE;
-  }
-
-  OFLOG_INFO(stl2dcmLogger, "STL encapsulation successful");
-
-  return EXITCODE_NO_ERROR;
+    // clean up (Windows only, on Posix systems the stub will not return)
+    delete[] my_argv;
+    return result;
 }
