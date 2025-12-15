936288d9ebca1ed2a41345887f21c57e4a86fdc2
Marco Eichelberg <eichelberg@offis.de>
2025-12-15 08:28:12 +0100

    Updated version information for DCMTK release 3.7.0.

--- CMake/dcmtkPrepare.cmake
+++ CMake/dcmtkPrepare.cmake
@@ -47,18 +47,18 @@ endif()
 
 # Basic version information
 set(DCMTK_MAJOR_VERSION 3)
-set(DCMTK_MINOR_VERSION 6)
-set(DCMTK_BUILD_VERSION 9)
+set(DCMTK_MINOR_VERSION 7)
+set(DCMTK_BUILD_VERSION 0)
 # The ABI is not guaranteed to be stable between different snapshots/releases,
 # so this particular version number is increased for each snapshot or release.
-set(DCMTK_ABI_VERSION 19)
+set(DCMTK_ABI_VERSION 20)
 
 # Package "release" settings (some are currently unused and, therefore, disabled)
 set(DCMTK_PACKAGE_NAME "dcmtk")
-set(DCMTK_PACKAGE_DATE "DEV")
+set(DCMTK_PACKAGE_DATE "2025-12-15")
 set(DCMTK_PACKAGE_VERSION "${DCMTK_MAJOR_VERSION}.${DCMTK_MINOR_VERSION}.${DCMTK_BUILD_VERSION}")
 set(DCMTK_PACKAGE_VERSION_NUMBER ${DCMTK_MAJOR_VERSION}${DCMTK_MINOR_VERSION}${DCMTK_BUILD_VERSION})
-set(DCMTK_PACKAGE_VERSION_SUFFIX "+")
+set(DCMTK_PACKAGE_VERSION_SUFFIX "")
 #set(DCMTK_PACKAGE_TARNAME "dcmtk-${DCMTK_PACKAGE_VERSION}")
 #set(DCMTK_PACKAGE_STRING "dcmtk ${DCMTK_PACKAGE_VERSION}")
 #set(DCMTK_PACKAGE_BUGREPORT "bugs@dcmtk.org")
