4182744456de4db0253937531fe9b7d7018e662f
Marco Eichelberg <eichelberg@offis.de>
2026-01-31 17:03:05 +0100

    Added aliases for common DCMTK tools.
    Added aliases for common tools in the 'dcmtk' app.
    Furthermore, tools that are know but not present in the same directory
    as dcmtk, are now denoted with an asterisk in the help output.

--- dcmapps/apps/dcmtk.cc
+++ dcmapps/apps/dcmtk.cc
@@ -13,7 +13,7 @@
  *
  *  Module:  dcmtkapp
  *
- *  Author:  Tingyan Xu
+ *  Author:  Tingyan Xu, Marco Eichelberg
  *
  *  Purpose: dcmtk command line tool
  *
@@ -25,6 +25,7 @@
 #include "dcmtk/ofstd/ofconsol.h"     /* for COUT/CERR */
 #include "dcmtk/ofstd/ofconapp.h"     /* for OFConsoleApplication */
 #include "dcmtk/dcmdata/dcuid.h"      /* for dcmtk version name */
+#include "dcmtk/ofstd/ofwhere.h"      /* for OFgetExecutablePath() */
 
 #define OFFIS_CONSOLE_APPLICATION "dcmtk"
 #define OFFIS_CONSOLE_DESCRIPTION "Execute dcmtk command line tool"
@@ -35,94 +36,144 @@ OFFIS_DCMTK_VERSION " " OFFIS_DCMTK_RELEASEDATE " $";
 // tool category for command line tools
 enum DcmAppType
 {
-  AT_conversion,
-  AT_compression,
-  AT_network,
-  AT_misc
+    // internal type, only used for printTools()
+    AT_anyType,
+    // the following values denote specific categories
+    AT_conversion,
+    AT_compression,
+    AT_network,
+    AT_misc,
+    AT_alias
 };
 
 // struct for the list of tools
 struct DcmToolEntry
 {
-  OFString name;
-  OFString description;
-  DcmAppType apptype;
+    OFString name;
+    OFString command;
+    OFString description;
+    DcmAppType apptype;
 };
 
 // list of DCMTK command line tools in alphabetical order
 static DcmToolEntry tools[] =
 {
-    {"dcm2img",  "Convert DICOM image to standard image format", AT_conversion },
-    {"dcm2json", "Convert DICOM file and data set to JSON", AT_conversion },
-    {"dcm2xml",  "Convert DICOM file and data set to XML", AT_conversion },
-    {"dcmcjpeg", "Encode DICOM file to JPEG transfer syntax", AT_compression },
-    {"dcmcjpls", "Encode DICOM file to JPEG-LS transfer syntax", AT_compression },
-    {"dcmconv",  "Convert DICOM file encoding", AT_conversion },
-    {"dcmcrle",  "Encode DICOM file to RLE transfer syntax", AT_compression },
-    {"dcmdecap", "Extract file from DICOM encapsulated storage", AT_conversion },
-    {"dcmdjpeg", "Decode JPEG-compressed DICOM file", AT_compression },
-    {"dcmdjpls", "Decode JPEG-LS compressed DICOM file", AT_compression },
-    {"dcmdrle",  "Decode RLE-compressed DICOM file", AT_compression },
-    {"dcmdspfn", "Export standard display curves to a text file", AT_misc },
-    {"dcmdump",  "Dump DICOM file and data set", AT_conversion },
-    {"dcmencap", "Encapsulate document into DICOM format", AT_conversion },
-    {"dcmftest", "Test if file uses DICOM part 10 format", AT_misc },
-    {"dcmicmp",  "Compare DICOM images and compute difference metrics", AT_misc },
-    {"dcmmkcrv", "Add 2D curve data to image", AT_misc },
-    {"dcmmkdir", "Create a DICOMDIR file", AT_misc },
-    {"dcmmklut", "Create DICOM look-up tables", AT_misc },
-    {"dcmodify", "Modify DICOM files", AT_conversion },
-    {"dcmp2pgm", "Read DICOM image and presentation state and render bitmap", AT_conversion },
-    {"dcmprscp", "DICOM basic grayscale print management SCP", AT_network },
-    {"dcmprscu", "Print spooler for presentation state viewer", AT_network },
-    {"dcmpschk", "Checking tool for presentation states", AT_misc },
-    {"dcmpsmk",  "Create DICOM grayscale softcopy presentation state", AT_misc },
-    {"dcmpsprt", "Read DICOM images and presentation states and render print job", AT_misc },
-    {"dcmpsrcv", "Network receive for presentation state viewer", AT_network },
-    {"dcmpssnd", "Network send for presentation state viewer", AT_network },
-    {"dcmqridx", "Register a DICOM image file in an image database index file", AT_misc },
-    {"dcmqrscp", "DICOM image archive (central test node)", AT_network },
-    {"dcmqrti",  "Telnet Initiator", AT_misc },
-    {"dcmquant", "Convert DICOM color images to palette color", AT_misc },
-    {"dcmrecv",  "Simple DICOM storage SCP (receiver)", AT_network },
-    {"dcmscale", "Scale DICOM images", AT_misc },
-    {"dcmsend",  "Simple DICOM storage SCU (sender)", AT_network },
-    {"dcmsign",  "Sign and Verify DICOM Files", AT_misc },
-    {"dcod2lum", "Convert hardcopy characteristic curve file to softcopy format", AT_misc },
-    {"dconvlum", "Convert VeriLUM \"CCx_xx.dat\" files to DCMTK display files", AT_misc },
-    {"drtdump",  "Dump DICOM RT file and data set", AT_conversion },
-    {"dsr2html", "Render DICOM SR file and data set to HTML/XHTML", AT_conversion },
-    {"dsr2xml",  "Convert DICOM SR file and data set to XML", AT_conversion },
-    {"dsrdump",  "Dump DICOM SR file and data set", AT_conversion },
-    {"dump2dcm", "Convert ASCII dump to DICOM file", AT_conversion },
-    {"echoscu",  "DICOM verification (C-ECHO) SCU", AT_network },
-    {"findscu",  "DICOM query (C-FIND) SCU", AT_network },
-    {"getscu",   "DICOM retrieve (C-GET) SCU", AT_network },
-    {"img2dcm",  "Convert standard image formats into DICOM format", AT_conversion },
-    {"json2dcm", "Convert JSON document to DICOM file or data set", AT_conversion },
-    {"movescu",  "DICOM retrieve (C-MOVE) SCU", AT_network },
-    {"storescp", "DICOM storage (C-STORE) SCP", AT_network },
-    {"storescu", "DICOM storage (C-STORE) SCU", AT_network },
-    {"termscu",  "DICOM termination SCU", AT_network },
-    {"wlmscpfs", "DICOM Basic Worklist Management SCP (based on data files)", AT_network },
-    {"xml2dcm",  "Convert XML document to DICOM file or data set", AT_conversion },
-    {"xml2dsr",  "Convert XML document to DICOM SR file", AT_conversion }
+    // the length of the first value ("name") should not exceed 8 characters
+    {"dcm2img",  "dcm2img",  "Convert DICOM image to standard image format", AT_conversion },
+    {"dcm2json", "dcm2json", "Convert DICOM file and data set to JSON", AT_conversion },
+    {"dcm2xml",  "dcm2xml",  "Convert DICOM file and data set to XML", AT_conversion },
+    {"dcmcjpeg", "dcmcjpeg", "Encode DICOM file to JPEG transfer syntax", AT_compression },
+    {"dcmcjpls", "dcmcjpls", "Encode DICOM file to JPEG-LS transfer syntax", AT_compression },
+    {"dcmconv",  "dcmconv",  "Convert DICOM file encoding", AT_conversion },
+    {"dcmcrle",  "dcmcrle",  "Encode DICOM file to RLE transfer syntax", AT_compression },
+    {"dcmdecap", "dcmdecap", "Extract file from DICOM encapsulated storage", AT_conversion },
+    {"dcmdjpeg", "dcmdjpeg", "Decode JPEG-compressed DICOM file", AT_compression },
+    {"dcmdjpls", "dcmdjpls", "Decode JPEG-LS compressed DICOM file", AT_compression },
+    {"dcmdrle",  "dcmdrle",  "Decode RLE-compressed DICOM file", AT_compression },
+    {"dcmdspfn", "dcmdspfn", "Export standard display curves to a text file", AT_misc },
+    {"dcmdump",  "dcmdump",  "Dump DICOM file and data set", AT_conversion },
+    {"dcmencap", "dcmencap", "Encapsulate document into DICOM format", AT_conversion },
+    {"dcmftest", "dcmftest", "Test if file uses DICOM part 10 format", AT_misc },
+    {"dcmicmp",  "dcmicmp",  "Compare DICOM images and compute difference metrics", AT_misc },
+    {"dcmmkcrv", "dcmmkcrv", "Add 2D curve data to image", AT_misc },
+    {"dcmmkdir", "dcmmkdir", "Create a DICOMDIR file", AT_misc },
+    {"dcmmklut", "dcmmklut", "Create DICOM look-up tables", AT_misc },
+    {"dcmodify", "dcmodify", "Modify DICOM files", AT_conversion },
+    {"dcmp2pgm", "dcmp2pgm", "Read DICOM image and presentation state and render bitmap", AT_conversion },
+    {"dcmprscp", "dcmprscp", "DICOM basic grayscale print management SCP", AT_network },
+    {"dcmprscu", "dcmprscu", "Print spooler for presentation state viewer", AT_network },
+    {"dcmpschk", "dcmpschk", "Checking tool for presentation states", AT_misc },
+    {"dcmpsmk",  "dcmpsmk",  "Create DICOM grayscale softcopy presentation state", AT_misc },
+    {"dcmpsprt", "dcmpsprt", "Read DICOM images and presentation states and render print job", AT_misc },
+    {"dcmpsrcv", "dcmpsrcv", "Network receive for presentation state viewer", AT_network },
+    {"dcmpssnd", "dcmpssnd", "Network send for presentation state viewer", AT_network },
+    {"dcmqridx", "dcmqridx", "Register a DICOM image file in an image database index file", AT_misc },
+    {"dcmqrscp", "dcmqrscp", "DICOM image archive (central test node)", AT_network },
+    {"dcmqrti",  "dcmqrti",  "Telnet Initiator", AT_misc },
+    {"dcmquant", "dcmquant", "Convert DICOM color images to palette color", AT_misc },
+    {"dcmrecv",  "dcmrecv",  "Simple DICOM storage SCP (receiver)", AT_network },
+    {"dcmscale", "dcmscale", "Scale DICOM images", AT_misc },
+    {"dcmsend",  "dcmsend",  "Simple DICOM storage SCU (sender)", AT_network },
+    {"dcmsign",  "dcmsign",  "Sign and Verify DICOM Files", AT_misc },
+    {"dcod2lum", "dcod2lum", "Convert hardcopy characteristic curve file to softcopy format", AT_misc },
+    {"dconvlum", "dconvlum", "Convert VeriLUM \"CCx_xx.dat\" files to DCMTK display files", AT_misc },
+    {"drtdump",  "drtdump",  "Dump DICOM RT file and data set", AT_conversion },
+    {"dsr2html", "dsr2html", "Render DICOM SR file and data set to HTML/XHTML", AT_conversion },
+    {"dsr2xml",  "dsr2xml",  "Convert DICOM SR file and data set to XML", AT_conversion },
+    {"dsrdump",  "dsrdump",  "Dump DICOM SR file and data set", AT_conversion },
+    {"dump2dcm", "dump2dcm", "Convert ASCII dump to DICOM file", AT_conversion },
+    {"echoscu",  "echoscu",  "DICOM verification (C-ECHO) SCU", AT_network },
+    {"findscu",  "findscu",  "DICOM query (C-FIND) SCU", AT_network },
+    {"getscu",   "getscu",   "DICOM retrieve (C-GET) SCU", AT_network },
+    {"img2dcm",  "img2dcm",  "Convert standard image formats into DICOM format", AT_conversion },
+    {"json2dcm", "json2dcm", "Convert JSON document to DICOM file or data set", AT_conversion },
+    {"movescu",  "movescu",  "DICOM retrieve (C-MOVE) SCU", AT_network },
+    {"storescp", "storescp", "DICOM storage (C-STORE) SCP", AT_network },
+    {"storescu", "storescu", "DICOM storage (C-STORE) SCU", AT_network },
+    {"termscu",  "termscu",  "DICOM termination SCU", AT_network },
+    {"wlmscpfs", "wlmscpfs", "DICOM Basic Worklist Management SCP (based on data files)", AT_network },
+    {"xml2dcm",  "xml2dcm",  "Convert XML document to DICOM file or data set", AT_conversion },
+    {"xml2dsr",  "xml2dsr",  "Convert XML document to DICOM SR file", AT_conversion },
+
+    // aliases for common tools
+    {"convert",  "dcmconv",  "Changes the encoding of a DICOM file (calls dcmconv)", AT_alias },
+    {"dump",     "dcmdump",  "Dumps the contents of a DICOM file (calls dcmdump)", AT_alias },
+    {"echo",     "echoscu",  "Checks whether a DICOM server is accessible (calls echoscu)", AT_alias },
+    {"find",     "findscu",  "Queries a Q/R or Modality Worklist server (calls findscu)", AT_alias },
+    {"get",      "getscu",   "Downloads from a Q/R server using C-GET (calls getscu)", AT_alias },
+    {"mkdir",    "dcmmkdir", "Creates a DICOMDIR file for storage media (calls dcmmkdir)", AT_alias },
+    {"modify",   "dcmodify", "Modifies the contents of a DICOM file (calls dcmodify)", AT_alias },
+    {"move",     "movescu",  "Downloads from a Q/R server using C-MOVE (calls movescu)", AT_alias },
+    {"send",     "dcmsend",  "Transfers DICOM instances to a Storage server (calls dcmsend)", AT_alias },
+    {"sign",     "dcmsign",  "Creates and verifies digital signatures (calls dcmsign)", AT_alias },
+    {"toimg",    "dcm2img",  "Renders a DICOM image to another image format (calls dcm2img)", AT_alias },
+    {"tojson",   "dcm2json", "Converts a DICOM file to JSON format (calls dcm2json)", AT_alias },
+    {"toxml",    "dcm2xml",  "Converts a DICOM file to XML format (calls dcm2xml)", AT_alias }
+
 };
 
 static const size_t numTools = sizeof(tools) / sizeof(*tools);
 
-static void printTools(DcmAppType toolType)
+static OFBool getExecutablePath(OFString& dirname)
 {
-  size_t len = 0;
-  for (size_t i = 0; i < numTools; ++i)
+    // get real path in which the stub executable is located
+    int dirname_length = 0;
+    int length = OFgetExecutablePath(NULL, 0, &dirname_length);
+    if (length == 0)
     {
-        if (tools[i].apptype == toolType)
+        fprintf(stderr, "F: Cannot determine location of dcmtk executable\n");
+        return OFFalse;
+    }
+    char *path = new char[length + 1];
+    OFgetExecutablePath(path, length, &dirname_length);
+    path[length] = '\0';
+    path[dirname_length] = '\0';
+    dirname = path;
+    delete[] path;
+    return OFTrue;
+}
+
+static void printTools(DcmAppType toolType, OFString& dirname, OFBool& toolMissing)
+{
+    OFString executable_name;
+    for (size_t i = 0; i < numTools; ++i)
+    {
+        if ((toolType == AT_anyType && tools[i].apptype != AT_alias) || (tools[i].apptype == toolType))
         {
-            len = tools[i].name.size();
-            COUT << "      " << tools[i].name << ": ";
-            for (size_t j = 1; j <= 8 - len; j++)
-                COUT << " ";
-            COUT << tools[i].description << OFendl;
+            executable_name = dirname;
+            executable_name += PATH_SEPARATOR;
+            executable_name += tools[i].command;
+#ifdef _WIN32
+            executable_name += ".exe";
+#endif
+            const OFBool fileExists = OFStandard::fileExists(executable_name);
+            COUT << "  " << (toolType == AT_anyType ? "" : "  ")
+                 << tools[i].name << (fileExists ? " " : "*");
+            // we assume that the maximum name length is 8 characters
+            COUT << OFString(8 - tools[i].name.size(), ' ');
+            COUT << " - " << tools[i].description << OFendl;
+            if (!fileExists)
+                toolMissing = OFTrue;
         }
     }
 }
@@ -142,30 +193,39 @@ static void printHelp(OFBool sorted)
          << "  -d  --debug    debug mode, print debug information\n" << OFendl;
 
     COUT << "tools:" << OFendl;
+
+    OFBool toolMissing = OFFalse;
+    OFString dirname;
+    OFBool result = getExecutablePath(dirname);
+    if (!result) return;
+
     if (sorted)
     {
+        // print tools sorted by category
         COUT << "  conversion tools:" << OFendl;
-        printTools(AT_conversion);
+        printTools(AT_conversion, dirname, toolMissing);
 
         COUT << OFendl << "  compression tools:" << OFendl;
-        printTools(AT_compression);
+        printTools(AT_compression, dirname, toolMissing);
 
         COUT << OFendl << "  network tools:" << OFendl;
-        printTools(AT_network);
+        printTools(AT_network, dirname, toolMissing);
 
         COUT << OFendl << "  miscellaneous tools:" << OFendl;
-        printTools(AT_misc);
+        printTools(AT_misc, dirname, toolMissing);
+
+        COUT << OFendl << "  aliases for common tools:" << OFendl;
+        printTools(AT_alias, dirname, toolMissing);
     }
     else
     {
-        size_t len;
-        for (size_t i = 0; i < numTools; ++i) {
-            len = tools[i].name.size();
-            COUT << "   " << tools[i].name << ": ";
-            for (size_t j = 1; j <= 8 - len; j++)
-                COUT << " ";
-            COUT << tools[i].description << OFendl;
-        }
+        // print all tools as a flat list
+        printTools(AT_anyType, dirname, toolMissing);
+    }
+
+    if (toolMissing)
+    {
+        COUT << "\n  * denotes tools that cannot be called because they are not present\n    in the directory where the '" << OFFIS_CONSOLE_APPLICATION << "' tool is located." << OFendl;
     }
 }
 
@@ -218,16 +278,17 @@ int main(int argc, char* argv[])
     }
 
     // find corresponding applet and call.
-    for (size_t i = 0; i < numTools; ++i) {
+    for (size_t i = 0; i < numTools; ++i)
+    {
         if (tool == tools[i].name)
         {
-            return OFstub_main(argc - offset, argv + offset, tool.c_str(), tool.c_str(), OFFalse /* printWarning */, debug);
+            return OFstub_main(argc - offset, argv + offset, tools[i].command.c_str(), tools[i].command.c_str(), OFFalse /* printWarning */, debug);
         }
     }
 
-   //  If not in list, print error
+    //  if not in list, print error
     CERR << OFFIS_CONSOLE_APPLICATION << ": there is no DCMTK tool named \"" << tool
-        << "\".\nTo show the list of available tools, call \"" << OFFIS_CONSOLE_APPLICATION
-        << " --help\"." << OFendl;
+         << "\".\nTo show the list of available tools, call \"" << OFFIS_CONSOLE_APPLICATION
+         << " --help\"." << OFendl;
     return EXITCODE_COMMANDLINE_SYNTAX_ERROR;
 }
