db90adcc256bec93cafca122d88091cf533f3f8d
Marco Eichelberg <eichelberg@offis.de>
2026-05-22 18:33:43 +0200

    Fixed linker error on NetBSD.

--- dcmwlm/tests/tests.cc
+++ dcmwlm/tests/tests.cc
@@ -21,7 +21,19 @@
 
 #include "dcmtk/config/osconfig.h"
 #include "dcmtk/ofstd/oftest.h"
+#include "dcmtk/dcmnet/dcmlayer.h"
 
 OFTEST_REGISTER(dcmwlm_aetitle_validation);
+
+#ifdef WITH_TCPWRAPPER
+// This dummy function creates a dependency on libdcmnet that is required when compiling
+// on NetBSD with libwrap support enabled. Otherwise there
+// would be a linker error complaining about unresolved symbols allow_severity and deny_severity.
+
+DcmTransportLayer *dcmwlm_dummyFunction()
+{
+  return new DcmTransportLayer();
+}
+#endif
 
 OFTEST_MAIN("dcmwlm")
